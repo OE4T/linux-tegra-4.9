3e18f8c77dad3d763206a265ccc55484a8f4a7af
Robert Morell <rmorell@nvidia.com>
2011-03-03 15:04:19 -0800

    video: tegra: Wire up output connectedness
    This makes the core dc driver keep track of whether a particular input
    in enabled.  It is up to the output ops to maintain the connected status
    if a detect op is plugged in, otherwise it is assumed that the output is
    always connected.
    
    bug 818525
    
    Original-Change-Id: I794d7e2db347f63bbb1a7d80bca1a53d9d10c210
    Signed-off-by: Robert Morell <rmorell@nvidia.com>
    Reviewed-on: http://git-master/r/40522
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>
    
    Rebase-Id: Rfeeae486b6a39b95d9f1d95b697132b476735f50

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -746,6 +746,7 @@ static bool tegra_dc_hdmi_detect(struct tegra_dc *dc)
 	tegra_fb_update_monspecs(dc->fb, &specs, tegra_dc_hdmi_mode_filter);
 	dev_info(&dc->ndev->dev, "display detected\n");
 
+	dc->connected = true;
 	tegra_dc_ext_process_hotplug(dc->ndev->id);
 
 	return true;
@@ -769,6 +770,7 @@ static void tegra_dc_hdmi_detect_worker(struct work_struct *work)
 		tegra_dc_disable(dc);
 		tegra_fb_update_monspecs(dc->fb, NULL, NULL);
 
+		dc->connected = false;
 		tegra_dc_ext_process_hotplug(dc->ndev->id);
 	}
 }

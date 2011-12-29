688598577316cc6117763cc10e2f5512ebd126b7
Terje Bergstrom <tbergstrom@nvidia.com>
2011-12-29 16:03:51 +0200

    video: tegra: host: Move device data to nvhost_device
    Move all device data from nvhost_channeldesc, nvhost_moduledesc and
    nvhost_module to nvhost_device. nvhost_devices are also assigned into
    a hierarchy to prepare for implementation of runtime power management.
    
    Change-Id: I1e18daae8fe538086cd1f453d316e0f73e9d7d92
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/72844
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Reviewed-on: http://git-master/r/74560
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    Tested-by: Varun Wadekar <vwadekar@nvidia.com>
    
    Rebase-Id: Rf91ddb3562aa236e894bb165e5ae4401335144ef

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1968,7 +1968,7 @@ static irqreturn_t tegra_dc_irq(int irq, void *ptr)
 	unsigned long underflow_mask;
 	u32 val;
 
-	if (!nvhost_module_powered(&dc->ndev->host->mod)) {
+	if (!nvhost_module_powered(dc->ndev->host->dev)) {
 		WARN(1, "IRQ when DC not powered!\n");
 		tegra_dc_io_start(dc);
 		status = tegra_dc_readl(dc, DC_CMD_INT_STATUS);

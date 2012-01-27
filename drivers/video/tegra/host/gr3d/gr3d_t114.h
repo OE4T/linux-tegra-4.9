90afd51d3d1afaed0c8528f739fba71e53ee5743
Scott Williams <scwilliams@nvidia.com>
2012-01-27 13:53:25 -0800

    video: tegra: host: Tegra11x 3D support
    Add support for context switch for Tegra11x 3D unit.
    
    Bug 839973
    
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/57220
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Tested-by: Scott Williams <scwilliams@nvidia.com>
    (cherry picked from commit aafea7c82885d16b350336036813b3863509de58)
    
    Change-Id: I2f65840077a250f0348e9d3bfa8d37c237e9d0a5
    Signed-off-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-on: http://git-master/r/77873
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-by: Mark Stadler <mastadler@nvidia.com>
    
    Rebase-Id: R56a4c7bf1bf4bbe66e2d513acaf8acb8c7d91596

---
+++ drivers/video/tegra/host/gr3d/gr3d_t114.h
@@ -0,0 +1,30 @@
+/*
+ * drivers/video/tegra/host/t30/3dctx_t114.h
+ *
+ * Tegra Graphics Host Context Switching for Tegra11x SOCs
+ *
+ * Copyright (c) 2011-2012, NVIDIA Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __NVHOST_3DCTX_T114_H
+#define __NVHOST_3DCTX_T114_H
+
+struct nvhost_hwctx_handler;
+
+int t114_nvhost_3dctx_handler_init(struct nvhost_hwctx_handler *h);
+
+#endif

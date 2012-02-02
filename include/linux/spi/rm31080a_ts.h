56cd4e4c69480a0a58a5002193141878ad81b404
Ali Ekici <aekici@nvidia.com>
2012-02-01 17:30:17 -0800

    input: touchscreen: Raydium DirectTouch driver
    Modified driver files to support unified DirectTouch which
    can support Kai and Cardhu together.  Vendor-supplied source
    code update.
    
    Bug 832605
    
    Signed-off-by: Ali Ekici <aekici@nvidia.com>
    Change-Id: If0799147b50e9c2cf102d2216103e30b525026e4
    Reviewed-on: http://git-master/r/80128
    Reviewed-by: Robert Collins <rcollins@nvidia.com>
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>
    Tested-by: Robert Collins <rcollins@nvidia.com>
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    (cherry picked from commit 1dfd9d747b8ceb344a435daac60d30f1a6441bcb)
    Reviewed-on: http://git-master/r/83568
    Tested-by: Gerrit_Virtual_Submit
    
    Rebase-Id: Rb00482627510c2fb79b03e5f7b4f056451235f21

--- include/linux/spi/rm31080a_ts.h
+++ include/linux/spi/rm31080a_ts.h
@@ -11,13 +11,12 @@
 #define RM_IOCTL_SCRIBER_CTRL    0x1006
 #define RM_IOCTL_READ_RAW_DATA   0x1007
 #define RM_IOCTL_AUTOSCAN_CTRL   0x1008
+#define RM_IOCTL_NOISE_CHECK     0x1009
+#define RM_IOCTL_GET_PARAMETER   0x100A
+#define RM_IOCTL_SET_PARAMETER   0x100B
 
-
-//#define RM_INPUT_RESOLUTION_X 1536
-//#define RM_INPUT_RESOLUTION_Y 960
-#define RM_INPUT_RESOLUTION_X 4096
-#define RM_INPUT_RESOLUTION_Y 4096
-
+#define RM_INPUT_RESOLUTION_X    4096
+#define RM_INPUT_RESOLUTION_Y    4096
 
 #define RM_TS_SIGNAL            44
 #define RM_TS_MAX_POINTS        16
@@ -26,17 +25,21 @@
 #define RM_SIGNAL_SUSPEND       0x00000002
 #define RM_SIGNAL_RESUME        0x00000003
 
-typedef struct
-{
-    unsigned char  ucTouchCount;
-    unsigned char  ucID[RM_TS_MAX_POINTS];
-    unsigned short usX[RM_TS_MAX_POINTS];
-    unsigned short usY[RM_TS_MAX_POINTS];
-    unsigned short usZ[RM_TS_MAX_POINTS];
-}rm_touch_event;
+typedef struct {
+	unsigned char ucTouchCount;
+	unsigned char ucID[RM_TS_MAX_POINTS];
+	unsigned short usX[RM_TS_MAX_POINTS];
+	unsigned short usY[RM_TS_MAX_POINTS];
+	unsigned short usZ[RM_TS_MAX_POINTS];
+} rm_touch_event;
+
 
 struct rm_spi_ts_platform_data{
 	int gpio_reset;
+	int x_size;
+	int y_size;
+	unsigned char* config;
 };
 
-#endif //_RM31080A_TS_H_
+
+#endif				//_RM31080A_TS_H_

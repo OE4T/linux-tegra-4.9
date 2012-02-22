78b001d3318c1ce90af86793410dfc9a8bfe4a4c
Krishna Reddy <vdumpa@nvidia.com>
2012-02-22 15:57:46 -0800

    video: tegra: nvmap: don't override contiguous flag
    Don't override contiguous flag in nvmap_page_alloc().
    This is causing incorrect iovm_commit accounting.
    
    Bug 938864
    
    Change-Id: If30ea43702465980914b12816fa28eac9e14581d
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/85319
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Kamal Balagopalan <kbalagopalan@nvidia.com>
    Tested-by: Kamal Balagopalan <kbalagopalan@nvidia.com>
    
    Rebase-Id: Rf2a1ca866d3ed2ec626c73f52143c041988b4dc3

--- drivers/video/tegra/nvmap/nvmap_handle.c
+++ drivers/video/tegra/nvmap/nvmap_handle.c
@@ -159,11 +159,6 @@ static int handle_page_alloc(struct nvmap_client *client,
 
 	prot = nvmap_pgprot(h, pgprot_kernel);
 
-#ifdef CONFIG_NVMAP_ALLOW_SYSMEM
-	if (nr_page == 1)
-		contiguous = true;
-#endif
-
 	h->pgalloc.area = NULL;
 	if (contiguous) {
 		struct page *page;

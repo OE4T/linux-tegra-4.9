de880caac522f0c8c92d6ea22934c695650087aa
Terje Bergstrom <tbergstrom@nvidia.com>
2012-03-13 13:49:02 +0200

    video: tegra: host: Replace license information
    Replace license information in nvhost with GPLv2. Also adds
    copyright year 2012 in files which have been changed in 2012.
    
    Change-Id: I86e8ed27095df13d99e0250e57e244d531fdacec
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/89735
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    
    Rebase-Id: Re09e2377400f588c019a8965292c6acf4295bbe8

--- drivers/video/tegra/host/bus.c
+++ drivers/video/tegra/host/bus.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2010 Google, Inc.
  * Author: Erik Gilling <konkers@google.com>
  *
- * Copyright (C) 2010-2011 NVIDIA Corporation
+ * Copyright (C) 2010-2012 NVIDIA Corporation
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and

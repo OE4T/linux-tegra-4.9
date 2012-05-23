d2e7686c2f678b80ce75bbbe42bcec122eb9eab1
Krishna Reddy <vdumpa@nvidia.com>
2012-05-23 16:10:32 -0700

    video: tegra: nvmap: Use config option NVMAP_CACHE_MAINT_BY_SET_WAYS
    Change-Id: I762929a0d72a0b26aa1451a946993f4a57806fd4
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/104292
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Kaz Fukuoka <kfukuoka@nvidia.com>
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Hiro Sugawara <hsugawara@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    
    Rebase-Id: R60bea1e3a77785559b320c12252ef2cd0fe8eb72

--- drivers/video/tegra/nvmap/nvmap_ioctl.c
+++ drivers/video/tegra/nvmap/nvmap_ioctl.c
@@ -540,6 +540,7 @@ static bool fast_cache_maint(struct nvmap_client *client, struct nvmap_handle *h
 {
 	int ret = false;
 
+#if defined(CONFIG_NVMAP_CACHE_MAINT_BY_SET_WAYS)
 	if ((op == NVMAP_CACHE_OP_INV) ||
 		((end - start) < FLUSH_CLEAN_BY_SET_WAY_THRESHOLD))
 		goto out;
@@ -559,6 +560,7 @@ static bool fast_cache_maint(struct nvmap_client *client, struct nvmap_handle *h
 	}
 	ret = true;
 out:
+#endif
 	return ret;
 }
 

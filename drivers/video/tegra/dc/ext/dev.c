abebc6c86c87b7922e69fbe93626db0da8d36900
Deepak Nibade <dnibade@nvidia.com>
2012-09-21 17:25:47 +0530

    video: tegra: dc: fix memory leak during video playback
    -flip_worker flips the windows and in next iteration old handles are
    queued up and freed
    -during 1080p video playback with hdmi connected, a call from
    tegra_fb_blank disables window flag
    -because of disabled window flag, old handles of one of the window are
    not queued for deletion in next iteration
    -so using old handle as condition of deletion marking instead of window
    flag
    
    bug 1028179
    
    Reviewed-on: http://git-master/r/134388
    (cherry picked from commit be51b49e6ec894ec4a9977fed5690b55144b3d71)
    
    Change-Id: I1cabaad4fc0d7f7812e0a12a32774484fe7c86c6
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/143466
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: R69a19cbf615587fe254c768353e0374e55c4a44e

--- drivers/video/tegra/dc/ext/dev.c
+++ drivers/video/tegra/dc/ext/dev.c
@@ -383,7 +383,12 @@ static void tegra_dc_ext_flip_worker(struct work_struct *work)
 			list_del(&data->timestamp_node);
 		mutex_unlock(&ext_win->queue_lock);
 
-		if (win->flags & TEGRA_WIN_FLAG_ENABLED) {
+		if (skip_flip)
+			old_handle = flip_win->handle[TEGRA_DC_Y];
+		else
+			old_handle = ext_win->cur_handle[TEGRA_DC_Y];
+
+		if (old_handle) {
 			int j;
 			for (j = 0; j < TEGRA_DC_NUM_PLANES; j++) {
 				if (skip_flip)

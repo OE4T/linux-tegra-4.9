47517cf3993bfc49c4df3c8009e7bd3e4f359a5f
Prashant Malani <pmalani@nvidia.com>
2012-10-10 18:50:57 -0700

    video: tegra: dc: enable cp with global alpha
    When global alpha is enabled, color palette also
    has to be enabled, for the change to take effect.
    
    This also enabled global alpha for 11x
    
    Bug 672060
    
    Change-Id: Ic2759dd7f15fa58a134ac5dfbaa968096e8a750a
    Signed-off-by: Prashant Malani <pmalani@nvidia.com>
    Reviewed-on: http://git-master/r/143417
    Reviewed-by: Kevin Huang (Eng-SW) <kevinh@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>
    
    Rebase-Id: R9542631b4d129c3464fe56a9e8d8ba69bd6339a6

--- drivers/video/tegra/dc/window.c
+++ drivers/video/tegra/dc/window.c
@@ -463,6 +463,16 @@ int tegra_dc_update_windows(struct tegra_dc_win *windows[], int n)
 		else if (tegra_dc_fmt_bpp(win->fmt) < 24)
 			win_options |= COLOR_EXPAND;
 
+#if  defined(CONFIG_ARCH_TEGRA_3x_SOC) || defined(CONFIG_ARCH_TEGRA_11x_SOC)
+		if (win->global_alpha == 255) {
+			tegra_dc_writel(dc, 0, DC_WIN_GLOBAL_ALPHA);
+		} else {
+			tegra_dc_writel(dc, GLOBAL_ALPHA_ENABLE |
+				win->global_alpha, DC_WIN_GLOBAL_ALPHA);
+			win_options |= CP_ENABLE;
+		}
+#endif
+
 		if (win->ppflags & TEGRA_WIN_PPFLAG_CP_ENABLE)
 			win_options |= CP_ENABLE;
 
@@ -470,14 +480,6 @@ int tegra_dc_update_windows(struct tegra_dc_win *windows[], int n)
 		win_options |= V_DIRECTION_DECREMENT(invert_v);
 
 		tegra_dc_writel(dc, win_options, DC_WIN_WIN_OPTIONS);
-
-#ifdef CONFIG_ARCH_TEGRA_3x_SOC
-		if (win->global_alpha == 255)
-			tegra_dc_writel(dc, 0, DC_WIN_GLOBAL_ALPHA);
-		else
-			tegra_dc_writel(dc, GLOBAL_ALPHA_ENABLE |
-				win->global_alpha, DC_WIN_GLOBAL_ALPHA);
-#endif
 
 		win->dirty = no_vsync ? 0 : 1;
 

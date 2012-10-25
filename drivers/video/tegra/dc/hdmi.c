708f2b57818bcb9cf7712f233c49d3527647fdeb
Bhanu Chetlapalli <bchetlapalli@nvidia.com>
2012-10-25 12:46:40 -0700

    video: tegra: dc: hdmi: Fill tdms_config for T148
    Change-Id: Ib400f32da2c667330e1600a186da3c127171da9d
    Signed-off-by: Bhanu Chetlapalli <bchetlapalli@nvidia.com>
    Reviewed-on: http://git-master/r/147686
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jeff Smith <jsmith@nvidia.com>
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -298,6 +298,26 @@ const struct tdms_config tdms_config[] = {
 		PEAK_CURRENT_LANE2(0x15) | PEAK_CURRENT_LANE3(0x15),
 	},
 };
+#elif defined(CONFIG_ARCH_TEGRA_14x_SOC)
+const struct tdms_config tdms_config[] = {
+	/* TODO: 480p modes */
+	/* TODO: 720p modes */
+	{ /* 1080p modes */
+	.pclk = INT_MAX,
+	.pll0 = SOR_PLL_BG_V17_S(3) | SOR_PLL_ICHPMP(1) | SOR_PLL_RESISTORSEL |
+		SOR_PLL_VCOCAP(3) | SOR_PLL_TX_REG_LOAD(0),
+	.pll1 = SOR_PLL_TMDS_TERM_ENABLE | SOR_PLL_PE_EN |
+		SOR_PLL_TMDS_TERMADJ(0x6),
+	.pe_current = PE_CURRENT0(PE_CURRENT_4_0_mA) |
+		PE_CURRENT1(PE_CURRENT_4_0_mA) |
+		PE_CURRENT2(PE_CURRENT_4_0_mA) |
+		PE_CURRENT3(PE_CURRENT_4_0_mA),
+	.drive_current = DRIVE_CURRENT_LANE0(DRIVE_CURRENT_10_500_mA) |
+		DRIVE_CURRENT_LANE1(DRIVE_CURRENT_10_500_mA) |
+		DRIVE_CURRENT_LANE2(DRIVE_CURRENT_10_500_mA) |
+		DRIVE_CURRENT_LANE3(DRIVE_CURRENT_10_500_mA),
+	},
+};
 #else
 #warning tdms_config needs to be defined for your arch
 #endif

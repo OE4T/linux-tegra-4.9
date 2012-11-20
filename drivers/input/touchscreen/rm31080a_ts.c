c8dc678a98ccce194aee3de0cbf39ca981d10ee6
Xiaohui Tao <xtao@nvidia.com>
2012-11-20 13:47:46 -0800

    input: touch: raydium: regulator modification
    The spi checking function was moved above the
    regulator init function.
    Clean up code update accordingly.
    
    Change-Id: Ic1f6db0c1f5e79d3a9379a5a93bf0ca3ce40a9b9
    Signed-off-by: Xiaohui Tao <xtao@nvidia.com>
    Signed-off-by: David Jung <djung@nvidia.com>
    Reviewed-on: http://git-master/r/165144
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- drivers/input/touchscreen/rm31080a_ts.c
+++ drivers/input/touchscreen/rm31080a_ts.c
@@ -2296,13 +2296,13 @@ static int rm31080_spi_probe(struct spi_device *spi)
 
 	if (spi->max_speed_hz > MAX_SPI_FREQ_HZ) {
 		dev_err(&spi->dev, "SPI CLK %d Hz?\n", spi->max_speed_hz);
-		return -EINVAL;
+		goto err_unregister_notifier;
 	}
 
 	rm31080_init_ts_structure_part();
 
 	if (!rm31080_spi_checking(0))
-		return -EINVAL;
+		goto err_unregister_notifier;
 
 	if (misc_register(&raydium_ts_miscdev) != 0) {
 		dev_err(&spi->dev, "Raydium TS: cannot register miscdev\n");

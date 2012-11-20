a3c6929ebc107442a72f837668658d3aa83b1f0c
Joshua Primero <jprimero@nvidia.com>
2012-11-20 15:47:24 -0800

    drivers: misc: nct: Add support for multiple cdevs
    Added support for nct multiple cooling devices. Accomplished this
    by refactoring the nct platform data structure to be based on trip
    temperatures rather than cdevs.
    
    Change-Id: I3a8abcf92ead71f4575e27698d7bed647078a8cc
    Signed-off-by: Joshua Primero <jprimero@nvidia.com>
    Reviewed-on: http://git-master/r/165188

--- include/linux/nct1008.h
+++ include/linux/nct1008.h
@@ -32,23 +32,15 @@ struct nct1008_data;
 enum nct1008_chip { NCT1008, NCT72 };
 
 
-struct active_temp_state {
+struct nct_trip_temp {
+	char *cdev_type;
 	long trip_temp;
+	enum thermal_trip_type trip_type;
 	unsigned long state;
-};
-
-#define MAX_ACTIVE_TEMP_STATE 16
-
-struct nct1008_cdev {
-	bool enable;
-	char *type;
-	long trip_temp;
 	long hysteresis;
-	int tc1;
-	int tc2;
-	int passive_delay;
-	struct active_temp_state states[MAX_ACTIVE_TEMP_STATE];
 };
+
+#define NCT_MAX_TRIPS (32)
 
 struct nct1008_platform_data {
 	bool supported_hwrev;
@@ -58,7 +50,9 @@ struct nct1008_platform_data {
 	s8 shutdown_ext_limit;
 	s8 shutdown_local_limit;
 
-	struct nct1008_cdev passive;
-	struct nct1008_cdev active;
+	int passive_delay;
+
+	int num_trips;
+	struct nct_trip_temp trips[NCT_MAX_TRIPS];
 };
 #endif /* _LINUX_NCT1008_H */

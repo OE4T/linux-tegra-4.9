b1e35b092c078ca9c63214932a3b67d46da21f52
Terje Bergstrom <tbergstrom@nvidia.com>
2012-11-22 15:24:22 +0200

    video: tegra: host: Reset T114 3D on powerup
    3D is in invalid state when we power it on for the first time. Reset
    it at power-up.
    
    Bug 1172645
    
    Change-Id: I2786389c7bf75eed5a411174da1d01c50a041494
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/165736
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>
    Reviewed-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/host/t114/t114.c
+++ drivers/video/tegra/host/t114/t114.c
@@ -126,6 +126,7 @@ static struct nvhost_device_data tegra_gr3d03_info = {
 	NVHOST_DEFAULT_CLOCKGATE_DELAY,
 	.can_powergate	= true,
 	.powergate_delay = 250,
+	.powerup_reset	= true,
 	.moduleid	= NVHOST_MODULE_NONE,
 };
 

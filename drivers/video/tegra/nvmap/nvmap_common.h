645fcf57dc01ff0bfe97695477900bedfb7cd821
Krishna Reddy <vdumpa@nvidia.com>
2012-12-17 17:41:16 -0800

    video: tegra: nvmap: Fix redundant L2 flushes on t11x
    Bug 1198897
    
    Change-Id: I45ad1ce97bfe28a9c7e56b213072de8f3adaeeae
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/172089
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/video/tegra/nvmap/nvmap_common.h
+++ drivers/video/tegra/nvmap/nvmap_common.h
@@ -28,12 +28,20 @@ extern size_t cache_maint_outer_threshold;
 
 static inline void inner_flush_cache_all(void)
 {
+#if defined(CONFIG_ARCH_TEGRA_11x_SOC)
+	v7_flush_kern_cache_all();
+#else
 	on_each_cpu((smp_call_func_t)v7_flush_kern_cache_all, NULL, 1);
+#endif
 }
 
 static inline void inner_clean_cache_all(void)
 {
+#if defined(CONFIG_ARCH_TEGRA_11x_SOC)
+	v7_clean_kern_cache_all(NULL);
+#else
 	on_each_cpu(v7_clean_kern_cache_all, NULL, 1);
+#endif
 }
 
 extern void __flush_dcache_page(struct address_space *, struct page *);

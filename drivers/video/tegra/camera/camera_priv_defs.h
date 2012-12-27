320ad72b1018ce7b9fa050c56e2878b7dfb79502
Jihoon Bang <jbang@nvidia.com>
2012-12-27 10:31:01 -0800

    media: video: tegra: camera: move tegra_camera
    Move tegra_camera from platform_driver to
    part of nvhost vi driver. Split tegra_camera.c file into
    multiple files incluidng camera.c, camera_power.c,
    camera_emc.c and camera_clk.c according to functionality.
    
    tegra_camera is registered/unregistered in nvhost vi driver.
    
    Bug 1189789
    
    Reviewed-on: http://git-master/r/174508
    (cherry picked from commit e503a08b809844b53b7737e504e9f376f4a8a1eb)
    
    Change-Id: Ia8e189e809e18e16b780d3ff064bc96db84ade85
    Signed-off-by: Jihoon Bang <jbang@nvidia.com>
    Reviewed-on: http://git-master/r/194805
    Reviewed-by: Mandar Padmawar <mpadmawar@nvidia.com>
    Tested-by: Mandar Padmawar <mpadmawar@nvidia.com>

---
+++ drivers/video/tegra/camera/camera_priv_defs.h
@@ -0,0 +1,90 @@
+/*
+ * drivers/video/tegra/camera/camera_priv_defs.h
+ *
+ * Copyright (C) 2013 Nvidia Corp
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __DRIVERS_VIDEO_TEGRA_CAMERA_CAMERA_PRIV_DEFS_H
+#define __DRIVERS_VIDEO_TEGRA_CAMERA_CAMERA_PRIV_DEFS_H
+
+#include <linux/miscdevice.h>
+#include <linux/platform_device.h>
+#include <linux/ioctl.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/regulator/consumer.h>
+#include <linux/clk.h>
+#include <linux/io.h>
+#include <linux/uaccess.h>
+#include <linux/delay.h>
+#include <linux/export.h>
+#include <linux/slab.h>
+
+#include <mach/powergate.h>
+#include <mach/clk.h>
+#include <mach/mc.h>
+#ifdef CONFIG_ARCH_TEGRA_11x_SOC
+#include <mach/isomgr.h>
+#endif
+
+#include <video/tegra_camera.h>
+
+
+/*
+ * CAMERA_*_CLK is only for internal driver use.
+ * TEGRA_CAMERA_*_CLK is enum used between driver and user space.
+ * TEGRA_CAMERA_*_CLK is defined in tegra_camera.h
+ */
+enum {
+	CAMERA_VI_CLK,
+	CAMERA_VI_SENSOR_CLK,
+	CAMERA_EMC_CLK,
+	CAMERA_ISP_CLK,
+	CAMERA_CSUS_CLK,
+	CAMERA_CSI_CLK,
+#ifdef CONFIG_ARCH_TEGRA_11x_SOC
+	CAMERA_CILAB_CLK,
+	CAMERA_CILCD_CLK,
+	CAMERA_CILE_CLK,
+	CAMERA_PLL_D2_CLK,
+#endif
+	CAMERA_CLK_MAX,
+};
+
+struct tegra_camera {
+	struct device *dev;
+	struct miscdevice misc_dev;
+	struct clk *clk[CAMERA_CLK_MAX];
+	struct regulator *reg;
+	struct tegra_camera_clk_info info;
+	struct mutex tegra_camera_lock;
+	atomic_t in_use;
+	int power_on;
+#ifdef CONFIG_ARCH_TEGRA_11x_SOC
+	tegra_isomgr_handle isomgr_handle;
+#endif
+};
+
+struct camera_clk {
+	int index;
+	char *name;
+};
+
+struct tegra_camera *tegra_camera_register(struct platform_device *ndev);
+int tegra_camera_unregister(struct tegra_camera *camera);
+#ifdef CONFIG_PM
+int tegra_camera_suspend(struct tegra_camera *camera);
+int tegra_camera_resume(struct tegra_camera *camera);
+#endif
+
+#endif

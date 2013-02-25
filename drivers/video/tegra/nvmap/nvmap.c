eeb7b55fb1b45e355007ce8e9f2ad98afa58d27a
Bryan Wu <pengw@nvidia.com>
2013-02-25 11:46:28 -0800

    video: nvmap: export some API symbols for V4L2 videobuf2 nvmap driver
    Bug 1240806
    
    Change-Id: Ib77f4057b645cec4191a1e38b8063afe3410861b
    Signed-off-by: Bryan Wu <pengw@nvidia.com>
    Reviewed-on: http://git-master/r/226471
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap.c
+++ drivers/video/tegra/nvmap/nvmap.c
@@ -27,6 +27,7 @@
 #include <linux/vmalloc.h>
 #include <linux/wait.h>
 #include <linux/slab.h>
+#include <linux/export.h>
 
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
@@ -651,6 +652,7 @@ phys_addr_t nvmap_pin(struct nvmap_client *client,
 
 	return _nvmap_pin(client, ref);
 }
+EXPORT_SYMBOL(nvmap_pin);
 
 phys_addr_t nvmap_handle_address(struct nvmap_client *c, unsigned long id)
 {
@@ -683,6 +685,7 @@ void nvmap_unpin(struct nvmap_client *client, struct nvmap_handle_ref *ref)
 	if (handle_unpin(client, ref->handle, false))
 		wake_up(&client->share->pin_wait);
 }
+EXPORT_SYMBOL(nvmap_unpin);
 
 void nvmap_unpin_handles(struct nvmap_client *client,
 			 struct nvmap_handle **h, int nr)
@@ -835,6 +838,7 @@ void *nvmap_mmap(struct nvmap_handle_ref *ref)
 	 * nvmap_handle_put will be called by unmapping this address */
 	return p;
 }
+EXPORT_SYMBOL(nvmap_mmap);
 
 void nvmap_munmap(struct nvmap_handle_ref *ref, void *addr)
 {
@@ -867,6 +871,7 @@ void nvmap_munmap(struct nvmap_handle_ref *ref, void *addr)
 	}
 	nvmap_handle_put(h);
 }
+EXPORT_SYMBOL(nvmap_munmap);
 
 struct nvmap_handle_ref *nvmap_alloc(struct nvmap_client *client, size_t size,
 				     size_t align, unsigned int flags,
@@ -893,6 +898,7 @@ struct nvmap_handle_ref *nvmap_alloc(struct nvmap_client *client, size_t size,
 
 	return r;
 }
+EXPORT_SYMBOL(nvmap_alloc);
 
 /* allocates memory with specifed iovm_start address. */
 struct nvmap_handle_ref *nvmap_alloc_iovm(struct nvmap_client *client,
@@ -956,6 +962,7 @@ void nvmap_free(struct nvmap_client *client, struct nvmap_handle_ref *r)
 
 	nvmap_free_handle_id(client, nvmap_ref_to_id(r));
 }
+EXPORT_SYMBOL(nvmap_free);
 
 void nvmap_handle_put(struct nvmap_handle *h)
 {

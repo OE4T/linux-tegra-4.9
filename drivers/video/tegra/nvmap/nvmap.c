f5bbce505fed9aa34970246039414d6455050438
Hiroshi Doyu <hdoyu@nvidia.com>
2013-03-22 13:03:28 +0200

    video: tegra: nvmap: Fix rewinding crash
    Change unsigned int to int for rewinding at error.
    
    bug 1246095
    
    Change-Id: I5a2e1a6f02f61681a70acf28ded315ffde9c1b86
    Signed-off-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Reviewed-on: http://git-master/r/212081
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap.c
+++ drivers/video/tegra/nvmap/nvmap.c
@@ -280,7 +280,7 @@ int nvmap_pin_ids(struct nvmap_client *client,
 		  unsigned int nr, const unsigned long *ids)
 {
 	int ret = 0;
-	unsigned int i;
+	int i;
 	struct nvmap_handle **h = (struct nvmap_handle **)ids;
 	struct nvmap_handle_ref *ref;
 

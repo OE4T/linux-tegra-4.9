965a5cc63f44284184d2948843ad4fad9821b44a
Bibek Basu <bbasu@nvidia.com>
2013-04-01 16:28:22 +0530

    t12x: video: tegra: dc: fix broken build
    Blender gen2 features are not available
    for T148.
    
    Change-Id: I84e28cd19d4f8647d715c020f5775997901ffbc5
    Signed-off-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-on: http://git-master/r/215029
    Reviewed-by: Kiran Adduri <kadduri@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Chao Xu <cxu@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -633,7 +633,7 @@ static void _dump_regs(struct tegra_dc *dc, void *data,
 		DUMP_REG(DC_WIN_BUF_STRIDE);
 		DUMP_REG(DC_WIN_UV_BUF_STRIDE);
 #endif
-#if !defined(CONFIG_TEGRA_DC_BLENDER_GEN2) || defined(CONFIG_ARCH_TEGRA_14x_SOC)
+#if !defined(CONFIG_TEGRA_DC_BLENDER_GEN2)
 		DUMP_REG(DC_WIN_BLEND_NOKEY);
 		DUMP_REG(DC_WIN_BLEND_1WIN);
 		DUMP_REG(DC_WIN_BLEND_2WIN_X);

c6be24726523001dd67177b202ccf02fd86beda0
Terje Bergstrom <tbergstrom@nvidia.com>
2013-04-18 10:22:20 +0300

    video: tegra: host: Do not change devid -1 to 0
    Device id -1 implies no suffix to device name. Device id 0 implies that
    suffix .0 is added. Do not change the id mid-flight.
    
    Change-Id: I97cacc9fd4b0c5f6895847e202f141a8e17f38ca
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/220494

--- drivers/video/tegra/host/bus_client.c
+++ drivers/video/tegra/host/bus_client.c
@@ -1282,12 +1282,9 @@ static struct device *nvhost_client_device_create(
 	}
 	use_dev_name = get_device_name_for_dev(pdev);
 
-	if (pdev->id == -1)
-		pdev->id = 0;
-
 	dev = device_create(host->nvhost_class,
 			NULL, devno, NULL,
-			(pdev->id == 0) ?
+			(pdev->id <= 0) ?
 			IFACE_NAME "-%s%s" :
 			IFACE_NAME "-%s%s.%d",
 			cdev_name, use_dev_name, pdev->id);

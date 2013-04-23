5e0c23748f9cce48ec58caa81d2e2d76046160cc
Krishna Reddy <vdumpa@nvidia.com>
2013-04-22 23:10:56 -0700

    arm: tegra: dalmore: remove __initdata keyword for dsi_p_wuxga_10_1_bl_device
    This device is accessed even after init section is removed.
    This device can't be declared with  __initdata.
    Bug 1247591
    
    Change-Id: Iee3bba93907ed9431f690d8223f7a44f710b5f68
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/221907
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Vineel Kumar Reddy Kovvuri <vineelkumarr@nvidia.com>
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-by: Kaz Fukuoka <kfukuoka@nvidia.com>
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/panel-p-wuxga-10-1.c
+++ arch/arm/mach-tegra/panel-p-wuxga-10-1.c
@@ -514,7 +514,7 @@ static struct platform_pwm_backlight_data dsi_p_wuxga_10_1_bl_data = {
 };
 
 static struct platform_device __maybe_unused
-		dsi_p_wuxga_10_1_bl_device __initdata = {
+		dsi_p_wuxga_10_1_bl_device = {
 	.name	= "pwm-backlight",
 	.id	= -1,
 	.dev	= {

4fd3cfd7547739f8c6dc98a7dbf103122f547c85
Arto Merilainen <amerilainen@nvidia.com>
2013-07-02 14:21:56 +0300

    devfreq: Add stub functions to governor.h
    Due to the kernel update we lost devfreq stub functions from
    governor.h which effectively prevents compilation of the kernel
    when devfreq is disabled.
    
    This patch adds stub functions to governor.h to allow the kernel
    to compile when there are devfreq users and devfreq is disabled.
    
    Change-Id: Ica543ef05ffeb0ba0258363087f9427d871f8e1f
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/244668
    Reviewed-on: http://git-master/r/1160001

--- drivers/devfreq/governor.h
+++ drivers/devfreq/governor.h
@@ -40,6 +40,8 @@ static inline int update_devfreq(struct devfreq *devfreq)
 
 #endif /* !CONFIG_PM_DEVFREQ */
 
+#if defined(CONFIG_PM_DEVFREQ)
+
 extern void devfreq_monitor_start(struct devfreq *devfreq);
 extern void devfreq_monitor_stop(struct devfreq *devfreq);
 extern void devfreq_monitor_suspend(struct devfreq *devfreq);
@@ -49,5 +51,40 @@ extern void devfreq_interval_update(struct devfreq *devfreq,
 
 extern int devfreq_add_governor(struct devfreq_governor *governor);
 extern int devfreq_remove_governor(struct devfreq_governor *governor);
+
+#else /* !CONFIG_PM_DEVFREQ */
+
+static inline void devfreq_monitor_start(struct devfreq *devfreq)
+{
+}
+
+static inline void devfreq_monitor_stop(struct devfreq *devfreq)
+{
+}
+
+static inline void devfreq_monitor_suspend(struct devfreq *devfreq)
+{
+}
+
+static inline void devfreq_monitor_resume(struct devfreq *devfreq)
+{
+}
+
+static inline void devfreq_interval_update(struct devfreq *devfreq,
+					   unsigned int *delay)
+{
+}
+
+static inline int devfreq_add_governor(struct devfreq_governor *governor)
+{
+	return 0;
+}
+
+static inline int devfreq_remove_governor(struct devfreq_governor *governor)
+{
+	return 0;
+}
+
+#endif /* CONFIG_PM_DEVFREQ */
 
 #endif /* _GOVERNOR_H */

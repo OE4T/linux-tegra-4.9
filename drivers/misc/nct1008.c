1abafd83fa764ff31f13487585d65dcce5fa57b3
Hyungwoo Yang <hyungwooy@nvidia.com>
2013-07-10 14:19:36 -0700

    misc: nct1008: change names of zone devices
    Changed names of zone devices this driver creates.
    New names are based on where the device probe.
    
    nct_ext -> Tdiode
    nct_int -> Tboard
    
    Bug 1261182
    
    Change-Id: I147ae07992b3e0efa32671a47bb8c395faa24ff7
    Signed-off-by: Hyungwoo Yang <hyungwooy@nvidia.com>
    Reviewed-on: http://git-master/r/247344
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>
    (cherry picked from commit 74213e3e26148eb63c9f311a74206ba253e65ee6)
    Reviewed-on: http://git-master/r/255157
    Reviewed-by: Mandar Padmawar <mpadmawar@nvidia.com>
    Tested-by: Mandar Padmawar <mpadmawar@nvidia.com>

--- drivers/misc/nct1008.c
+++ drivers/misc/nct1008.c
@@ -1255,15 +1255,15 @@ static int nct1008_probe(struct i2c_client *client,
 			mask |= (1 << i);
 
 	if (data->plat_data.loc_name) {
-		strcpy(nct_int_name, "nct_int_");
-		strcpy(nct_ext_name, "nct_ext_");
+		strcpy(nct_int_name, "Tboard_");
+		strcpy(nct_ext_name, "Tdiode_");
 		strncat(nct_int_name, data->plat_data.loc_name,
-			(THERMAL_NAME_LENGTH - strlen("nct_int_")) - 1);
+			(THERMAL_NAME_LENGTH - strlen("Tboard_")) - 1);
 		strncat(nct_ext_name, data->plat_data.loc_name,
-			(THERMAL_NAME_LENGTH - strlen("nct_ext_")) - 1);
+			(THERMAL_NAME_LENGTH - strlen("Tdiode_")) - 1);
 	} else {
-		strcpy(nct_int_name, "nct_int");
-		strcpy(nct_ext_name, "nct_ext");
+		strcpy(nct_int_name, "Tboard");
+		strcpy(nct_ext_name, "Tdiode");
 	}
 
 	data->nct_int = thermal_zone_device_register(nct_int_name,

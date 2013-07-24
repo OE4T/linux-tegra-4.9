ce1f6624659fab33fd370ebec5cb944160bae0e9
Chao Xu <cxu@nvidia.com>
2013-07-23 17:25:08 -0700

    ARM: tegra: Add CHIMEI LVDS panel support
    Bug 1331024
    
    Change-Id: I6e112aadb1bd8c565e940af54890623f7bcfb02b
    Signed-off-by: Chao Xu <cxu@nvidia.com>
    Reviewed-on: http://git-master/r/252988
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- arch/arm/mach-tegra/board-panel.h
+++ arch/arm/mach-tegra/board-panel.h
@@ -54,6 +54,7 @@ extern struct tegra_panel dsi_a_1080p_11_6;
 extern struct tegra_panel dsi_s_wqxga_10_1;
 extern struct tegra_panel dsi_a_1080p_14_0;
 extern struct tegra_panel edp_a_1080p_14_0;
+extern struct tegra_panel lvds_c_1366_14;
 
 void tegra_dsi_resources_init(u8 dsi_instance,
 			struct resource *resources, int n_resources);

66e9cb4eb3c1cc85a9c95d0d1157d93d39a05872
Arto Merilainen <amerilainen@nvidia.com>
2013-09-12 11:02:04 +0300

    devfreq: Round frequencies in transition stats
    Transition statistics required that the new frequency was *exactly*
    as it was in the table. However, we may unexpectedly select a
    frequency that is not in the table (i.e. thermal limit forces us to
    select a bit lower frequency, the original DVFS table does not hold
    for the current PLL for some reason, etc.)
    
    This patch modifies transition table to select the best candidate
    from the frequency table.
    
    Bug 1366760
    
    Change-Id: I4329f77e733f5f1d39b9d9c3aebe3681a0760d90
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/273596
    
    Conflicts:
    	drivers/devfreq/devfreq.c

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -78,11 +78,27 @@ static int devfreq_get_freq_level(struct devfreq *devfreq, unsigned long freq)
 {
 	int lev;
 
-	for (lev = 0; lev < devfreq->profile->max_state; lev++)
-		if (freq == devfreq->profile->freq_table[lev])
-			return lev;
+	if (!devfreq->profile->max_state)
+		return -EINVAL;
 
-	return -EINVAL;
+	for (lev = 0; lev < devfreq->profile->max_state; lev++) {
+		if (devfreq->profile->freq_table[lev] >= freq) {
+			/* below minimum frequency? just return zero level */
+			if (lev == 0)
+				return 0;
+
+			/* select high freq if the freq is closer to that */
+			if ((devfreq->profile->freq_table[lev] - freq) <
+			    (freq - devfreq->profile->freq_table[lev - 1]))
+				return lev;
+
+			/* ..otherwise return the low freq */
+			return lev - 1;
+		}
+	}
+
+	/* above max freq */
+	return devfreq->profile->max_state - 1;
 }
 
 /**
@@ -1153,6 +1169,8 @@ static ssize_t trans_stat_show(struct device *dev,
 	ssize_t len;
 	int i, j;
 	unsigned int max_state = devfreq->profile->max_state;
+	int prev_freq_level;
+	unsigned long prev_freq;
 
 	if (!devfreq->stop_polling &&
 			devfreq_update_status(devfreq, devfreq->previous_freq))
@@ -1160,8 +1178,18 @@ static ssize_t trans_stat_show(struct device *dev,
 	if (max_state == 0)
 		return sprintf(buf, "Not Supported.\n");
 
+	/* round the current frequency */
+	prev_freq_level = devfreq_get_freq_level(devfreq,
+						 devfreq->previous_freq);
+
+	if (prev_freq_level < 0)
+		prev_freq = devfreq->previous_freq;
+	else
+		prev_freq = devfreq->profile->freq_table[prev_freq_level];
+
 	len = sprintf(buf, "     From  :   To\n");
 	len += sprintf(buf + len, "           :");
+
 	for (i = 0; i < max_state; i++)
 		len += sprintf(buf + len, "%10lu",
 				devfreq->profile->freq_table[i]);
@@ -1169,8 +1197,7 @@ static ssize_t trans_stat_show(struct device *dev,
 	len += sprintf(buf + len, "   time(ms)\n");
 
 	for (i = 0; i < max_state; i++) {
-		if (devfreq->profile->freq_table[i]
-					== devfreq->previous_freq) {
+		if (devfreq->profile->freq_table[i] == prev_freq) {
 			len += sprintf(buf + len, "*");
 		} else {
 			len += sprintf(buf + len, " ");

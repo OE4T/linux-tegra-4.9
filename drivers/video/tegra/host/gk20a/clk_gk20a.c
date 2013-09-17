cf35068c275d00a40081df4bd800411707335928
Kaz Fukuoka <kfukuoka@nvidia.com>
2013-09-17 13:54:16 -0700

    video: tegra: gk20a: Add barrier before udelay
    Add read barrier between write and udelay
    to make sure write is done before udelay.
    
    Change-Id: I21679bd0ef063e100ec6457fc26f7d4cb8bf53ea
    Signed-off-by: Kaz Fukuoka <kfukuoka@nvidia.com>
    Reviewed-on: http://git-master/r/278558
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    (cherry picked from commit bb63bf3cd2a58cb6e3fef40d5276903bdb630099)
    Signed-off-by: Ajay Nandakumar <anandakumarm@nvidia.com>

--- drivers/video/tegra/host/gk20a/clk_gk20a.c
+++ drivers/video/tegra/host/gk20a/clk_gk20a.c
@@ -284,6 +284,7 @@ static int clk_program_gpc_pll(struct gk20a *g, struct clk_gk20a *clk,
 		cfg = set_field(cfg, trim_sys_gpcpll_cfg_iddq_m(),
 				trim_sys_gpcpll_cfg_iddq_power_on_v());
 		gk20a_writel(g, trim_sys_gpcpll_cfg_r(), cfg);
+		gk20a_readl(g, trim_sys_gpcpll_cfg_r());
 		udelay(2);
 	}
 
@@ -755,6 +756,7 @@ static int monitor_get(void *data, u64 *val)
 
 	/* It should take about 8us to finish 100 cycle of 12MHz.
 	   But longer than 100us delay is required here. */
+	gk20a_readl(g, trim_gpc_clk_cntr_ncgpcclk_cfg_r(0));
 	udelay(2000);
 
 	count1 = gk20a_readl(g, trim_gpc_clk_cntr_ncgpcclk_cnt_r(0));

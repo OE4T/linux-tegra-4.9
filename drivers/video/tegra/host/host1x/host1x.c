063af31b3a0f06a236b722e0578a69f1c3f6711a
Terje Bergstrom <tbergstrom@nvidia.com>
2013-09-17 12:44:02 +0300

    video: tegra: host: Disable timeout in simulation
    Change-Id: I4fb67b095e42bcc87aa54ac3f0d595fdec1f2dc4
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/275694
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>

--- drivers/video/tegra/host/host1x/host1x.c
+++ drivers/video/tegra/host/host1x/host1x.c
@@ -787,8 +787,8 @@ static int nvhost_probe(struct platform_device *dev)
 	nvhost_intr_start(&host->intr, clk_get_rate(pdata->clk[0]));
 
 	nvhost_device_list_init();
-	pdata->nvhost_timeout_default =
-			CONFIG_TEGRA_GRHOST_DEFAULT_TIMEOUT;
+	pdata->nvhost_timeout_default = tegra_platform_is_linsim() ?
+			0 : CONFIG_TEGRA_GRHOST_DEFAULT_TIMEOUT;
 	nvhost_debug_init(host);
 
 	nvhost_module_idle(dev);

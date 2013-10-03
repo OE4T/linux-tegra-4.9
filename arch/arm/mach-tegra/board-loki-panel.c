da34d2e8c3a69dda1ccf6f851c7e546f850d3e56
Deepak Nibade <dnibade@nvidia.com>
2013-10-03 14:50:05 +0530

    ARM: tegra12: enable DT support for host1x
    - make board file changes to enable DT support for host1x
    - rename clocks to isp.0/vi.0/vic03.0/gk20a.0 to match with
      device names
    
    bug 1366383
    
    Change-Id: I97d8dcf1435c93be9cd81c40f101b645f8073019
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/304871
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- arch/arm/mach-tegra/board-loki-panel.c
+++ arch/arm/mach-tegra/board-loki-panel.c
@@ -45,7 +45,11 @@ struct platform_device * __init loki_host1x_init(void)
 	struct platform_device *pdev = NULL;
 
 #ifdef CONFIG_TEGRA_GRHOST
-	pdev = tegra12_register_host1x_devices();
+	if (!of_have_populated_dt())
+		pdev = tegra12_register_host1x_devices();
+	else
+		pdev = to_platform_device(bus_find_device_by_name(
+			&platform_bus_type, NULL, "host1x"));
 
 	if (!pdev) {
 		pr_err("host1x devices registration failed\n");

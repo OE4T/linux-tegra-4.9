ff00f95a26a47dc8ab6d6047b01d8d53240987f3
sharath <ssarangpur@nvidia.com>
2013-10-07 17:41:00 -0700

    tegra: dc: add hdcp reset in handle_recheck_edid_l
    Added HDCP reset logic in handle_recheck_edid_l to pass HDCP Compliance.
    
    bug 1382386
    bug 1382265
    
    Change-Id: I9e53be05f36b6dfc90bd610760e4a2bdc2970c8c
    Signed-off-by: sharath <ssarangpur@nvidia.com>
    Reviewed-on: http://git-master/r/284333
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Reviewed-on: http://git-master/r/298490
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>
    Tested-by: Jon Mayo <jmayo@nvidia.com>

--- drivers/video/tegra/dc/hdmi_state_machine.c
+++ drivers/video/tegra/dc/hdmi_state_machine.c
@@ -368,6 +368,8 @@ static void handle_recheck_edid_l(struct tegra_dc_hdmi_data *hdmi)
 		if (match) {
 			pr_info("No EDID change after HPD bounce, taking no action.\n");
 			tgt_state = HDMI_STATE_DONE_ENABLED;
+			tegra_nvhdcp_set_plug(hdmi->nvhdcp, 0);
+			tegra_nvhdcp_set_plug(hdmi->nvhdcp, 1);
 			timeout = -1;
 		} else {
 			pr_info("EDID change after HPD bounce, resetting\n");

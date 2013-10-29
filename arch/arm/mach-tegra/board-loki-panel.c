f3006388abf01f3b0a36961c5fbd1b0202826f71
Terje Bergstrom <tbergstrom@nvidia.com>
2013-10-29 05:00:05 -0700

    Revert "ARM: tegra12: enable DT support for host1x"
    This reverts commit 1a48e3a1826b6347b95aa3c1c96ab1c957a240ac.
    
    Change-Id: Ibd58be83afadda460b81def37665ab19e95b1693
    Reviewed-on: http://git-master/r/304851
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- arch/arm/mach-tegra/board-loki-panel.c
+++ arch/arm/mach-tegra/board-loki-panel.c
@@ -45,11 +45,7 @@ struct platform_device * __init loki_host1x_init(void)
 	struct platform_device *pdev = NULL;
 
 #ifdef CONFIG_TEGRA_GRHOST
-	if (!of_have_populated_dt())
-		pdev = tegra12_register_host1x_devices();
-	else
-		pdev = to_platform_device(bus_find_device_by_name(
-			&platform_bus_type, NULL, "host1x"));
+	pdev = tegra12_register_host1x_devices();
 
 	if (!pdev) {
 		pr_err("host1x devices registration failed\n");

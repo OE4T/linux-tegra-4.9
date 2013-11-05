3a4f1d579fbec3b1282b782a5b80f8f7d98244b4
Jon Mayo <jmayo@nvidia.com>
2013-11-05 12:52:05 -0800

    video: tegra: dc: ignore invalid windows
    Walk through valid_windows bitmask to skip over invalid windows.
    Mark invalid windows with a flag for easy checking.
    
    Change-Id: Idda005b57ca8503462b01ef1193c993a73432e18
    Signed-off-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-on: http://git-master/r/326796
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/dc/ext/dev.c
+++ drivers/video/tegra/dc/ext/dev.c
@@ -975,6 +975,8 @@ static int tegra_dc_ext_negotiate_bw(struct tegra_dc_ext_user *user,
 		return -1;
 
 	for (i = 0; i < win_num; i++) {
+		int idx = wins[i].index;
+
 		ret = tegra_dc_ext_pin_window(user, wins[i].buff_id,
 					      &handle, &phys_addr);
 		if (ret)
@@ -985,13 +987,13 @@ static int tegra_dc_ext_negotiate_bw(struct tegra_dc_ext_user *user,
 				handle->sgt, DMA_TO_DEVICE);
 			dma_buf_put(handle->buf);
 			kfree(handle);
-			tegra_dc_ext_set_windowattr_basic(&dc->tmp_wins[i],
+			tegra_dc_ext_set_windowattr_basic(&dc->tmp_wins[idx],
 							  &wins[i]);
 		}
 		else {
 			dc->tmp_wins[i].flags = 0;
 		}
-		dc_wins[i] = &dc->tmp_wins[i];
+		dc_wins[i] = &dc->tmp_wins[idx];
 	}
 
 	ret = tegra_dc_bandwidth_negotiate_bw(dc, dc_wins, win_num);

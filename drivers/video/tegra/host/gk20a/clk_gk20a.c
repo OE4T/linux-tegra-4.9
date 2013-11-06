9254fe7cdaac128d9d7aaa06fc1151990682ba0a
Alex Frid <afrid@nvidia.com>
2013-11-05 22:35:46 -0800

    video: tegra: gk20a: Increase GPCPLL rate limit to 2.064 GHz
    Bug 1342499
    
    Change-Id: I7d4e4ca2ab6250022247be836018097922b043fc
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/327010
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- drivers/video/tegra/host/gk20a/clk_gk20a.c
+++ drivers/video/tegra/host/gk20a/clk_gk20a.c
@@ -36,8 +36,8 @@
 
 /* from vbios PLL info table */
 struct pll_parms gpc_pll_params = {
-	144, 1824,	/* freq */
-	1000, 2000,	/* vco */
+	144, 2064,	/* freq */
+	1000, 2064,	/* vco */
 	12, 38,		/* u */
 	1, 255,		/* M */
 	8, 255,		/* N */

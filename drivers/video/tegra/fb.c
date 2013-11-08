f40ac46a056971b95548aa0f835f2137525539f8
Mike J. Chen <mjchen@google.com>
2013-11-07 17:39:04 -0800

    video: tegra: fb: Clear vscreeninfo if no monspecs
    hwcomposer recently changed to not blank and set mode if video
    mode hasn't changed (it uses FBIO_GET_VSCREENINFO).  this was
    to try to prevent blanking during early boot, when mode might
    have been already set by bootloader.
    
    But in a HDMI hotplug case, since HDMI cable removal didn't clear
    vscreeninfo, when HDMI cable is replugged, hwcomposer considers no
    mode to have changed and doesn't ever set a mode.
    
    This is one possible workaround/fix, but arguably hwcomposer should
    know that hotplug occurred and not do the check in that case.
    
    Bug 1401958
    Bug 1425201
    
    Change-Id: I4aaac56588a0deb52a22e0ecd91f5eb4ed6ab9fa
    Signed-off-by: Mike J. Chen <mjchen@google.com>
    Signed-off-by: Xia Yang <xiay@nvidia.com>
    Reviewed-on: http://git-master/r/355339
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- drivers/video/tegra/fb.c
+++ drivers/video/tegra/fb.c
@@ -601,9 +601,12 @@ void tegra_fb_update_monspecs(struct tegra_fb_info *fb_info,
 		memset(&mode, 0x0, sizeof(mode));
 
 		/*
-		 * reset video mode properties to prevent garbage being displayed on 'mode' device.
+		 * reset video mode properties to prevent garbage being
+		 * displayed on 'mode' device.
 		 */
 		fb_info->info->mode = (struct fb_videomode*) NULL;
+
+		memset(&fb_info->info->var, 0x0, sizeof(fb_info->info->var));
 
 		tegra_dc_set_mode(fb_info->win->dc, &mode);
 		mutex_unlock(&fb_info->info->lock);

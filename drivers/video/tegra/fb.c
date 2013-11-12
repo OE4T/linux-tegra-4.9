10612e6c2622ad0f55c59d64633d3c6bb30dafe4
Jon Mayo <jmayo@nvidia.com>
2013-11-12 11:25:18 -0800

    video: tegra: dc: remove android specific fb code
    The hack around the white screen issue doesn't seem to prevent the white
    screen. Removing it and replacing it with a small change to ignore
    duplicate unblanks.
    
    Change-Id: I6fe415b700d8c6e8dbbf989d39d4b4c7465ea6c9
    Signed-off-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-on: http://git-master/r/329741
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Chao Xu <cxu@nvidia.com>
    Reviewed-by: Jong Kim <jongk@nvidia.com>

--- drivers/video/tegra/fb.c
+++ drivers/video/tegra/fb.c
@@ -297,12 +297,12 @@ static int tegra_fb_blank(int blank, struct fb_info *info)
 	switch (blank) {
 	case FB_BLANK_UNBLANK:
 		dev_dbg(&tegra_fb->ndev->dev, "unblank\n");
-#if defined(CONFIG_ANDROID)
-		/* Not restoring windows to avoid white screen on android */
-		tegra_fb->win->flags &= ~TEGRA_WIN_FLAG_ENABLED;
-#else
+		if (tegra_fb->win->dc->enabled &&
+			(tegra_fb->win->flags & TEGRA_WIN_FLAG_ENABLED))
+			return 0;
 		tegra_fb->win->flags |= TEGRA_WIN_FLAG_ENABLED;
-#endif
+		tegra_fb->win->phys_addr = tegra_fb->phys_start;
+		tegra_fb->win->virt_addr = tegra_fb->info->screen_base;
 		tegra_dc_enable(tegra_fb->win->dc);
 		tegra_dc_update_windows(&tegra_fb->win, 1);
 		tegra_dc_sync_windows(&tegra_fb->win, 1);

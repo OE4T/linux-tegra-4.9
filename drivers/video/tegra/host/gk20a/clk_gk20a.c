7e5c853f1281d13c9641d21eb41e69d5d2c6fee1
Kevin Huang <kevinh@nvidia.com>
2013-12-12 09:33:18 -0800

    video: tegra: host: fix pri timeout enable
    Change-Id: Ib2c0f15ccb028e7f777e39dd516b7dc59a74f741
    Signed-off-by: Kevin Huang <kevinh@nvidia.com>
    Reviewed-on: http://git-master/r/344887
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Vince Hsu <vinceh@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/video/tegra/host/gk20a/clk_gk20a.c
+++ drivers/video/tegra/host/gk20a/clk_gk20a.c
@@ -688,7 +688,7 @@ int gk20a_init_clk_support(struct gk20a *g)
 
 	gk20a_writel(g,
 		timer_pri_timeout_r(),
-		timer_pri_timeout_period_f(0x200) ||
+		timer_pri_timeout_period_f(0x200) |
 		timer_pri_timeout_en_m());
 
 	return err;

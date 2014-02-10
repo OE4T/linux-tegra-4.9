c3b11d85e201943ea136020d6fea5e1a7c6a75a5
Deepak Nibade <dnibade@nvidia.com>
2014-02-10 17:54:02 +0530

    video: tegra: host: hard-code syncpts for t114
    - hard code syncpts for gr3d, gr2d and syncpt no. 17 for T114
    - this is to maintain user space dependency on hard-coded syncpts
    
    - syncpt no. 17 is wrongly used by some of the graphics tests which
      require this syncpt to be reserved as client_managed syncpt
    
    - keep this hard-coded part for t114 under #ifdef
      CONFIG_ARCH_TEGRA_11x_SOC
    
    Bug 1305024
    
    Change-Id: I18dc36018ca5cb95a191dd6f3eaa145d90391608
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/381961
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- include/linux/nvhost.h
+++ include/linux/nvhost.h
@@ -54,6 +54,8 @@ struct nvhost_as_moduleops;
 #define NVSYNCPT_GRAPHICS_HOST		(0)	/* t20, t30, t114, t148 */
 
 #define NVSYNCPT_AVP_0			(10)	/* t20, t30, t114, t148 */
+#define NVSYNCPT_2D_0			(18)
+#define NVSYNCPT_2D_1			(19)
 #define NVSYNCPT_3D			(22)	/* t20, t30, t114, t148 */
 #define NVSYNCPT_VBLANK0		(26)	/* t20, t30, t114, t148 */
 #define NVSYNCPT_VBLANK1		(27)	/* t20, t30, t114, t148 */

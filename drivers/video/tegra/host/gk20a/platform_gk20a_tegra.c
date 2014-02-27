a2a0ad03cef3e2bf49569fc72cc086ef82da0a49
Terje Bergstrom <tbergstrom@nvidia.com>
2014-02-27 11:14:42 +0200

    video: tegra: host: Restore T132 gk20a sync points
    Change-Id: I58b119d3e2cc8acde5d180bf600da2ba15de4c2a
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/375395
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>
    Tested-by: Juha Tukkinen <jtukkinen@nvidia.com>

--- drivers/video/tegra/host/gk20a/platform_gk20a_tegra.c
+++ drivers/video/tegra/host/gk20a/platform_gk20a_tegra.c
@@ -176,6 +176,8 @@ static struct resource gk20a_tegra_resources[] = {
 
 struct gk20a_platform t132_gk20a_tegra_platform = {
 	.nvhost = {
+		.syncpts		= {NVSYNCPT_GK20A_BASE},
+		.syncpt_base		= NVSYNCPT_GK20A_BASE,
 		.class			= NV_GRAPHICS_GPU_CLASS_ID,
 		.clocks			= {{"PLLG_ref", UINT_MAX},
 					   {"pwr", 204000000},

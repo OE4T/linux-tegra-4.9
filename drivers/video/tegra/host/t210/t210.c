3f6349c23d38bbd01696b2a39a5356802986c142
Bo Yan <byan@nvidia.com>
2014-03-18 16:30:25 -0700

    video: tegra: host: t210: add debug ops
    Change-Id: I4c0ba2b1ed2821017deec4d762a995a6d3582454
    Signed-off-by: Bo Yan <byan@nvidia.com>
    Reviewed-on: http://git-master/r/383402
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/video/tegra/host/t210/t210.c
+++ drivers/video/tegra/host/t210/t210.c
@@ -283,6 +283,7 @@ static void t210_remove_support(struct nvhost_chip_support *op)
 #include "host1x/host1x_syncpt.c"
 #include "host1x/host1x_intr.c"
 #include "host1x/host1x_actmon_t124.c"
+#include "host1x/host1x_debug.c"
 
 int nvhost_init_t210_support(struct nvhost_master *host,
        struct nvhost_chip_support *op)
@@ -297,6 +298,7 @@ int nvhost_init_t210_support(struct nvhost_master *host,
 
 	op->cdma = host1x_cdma_ops;
 	op->push_buffer = host1x_pushbuffer_ops;
+	op->debug = host1x_debug_ops;
 
 	host->sync_aperture = host->aperture + HOST1X_CHANNEL_SYNC_REG_BASE;
 	op->syncpt = host1x_syncpt_ops;

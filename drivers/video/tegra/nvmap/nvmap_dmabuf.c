0b8ae0dfca0a2ba81dd7bbf5c5e724e4c6a677e5
Arto Merilainen <amerilainen@nvidia.com>
2014-03-27 08:41:24 +0200

    video: tegra: nvmap: Export symbols
    nvmap stores some buffer specific parameters and it already
    allows accessing those parameters. This patch modifies nvmap
    so that the parameters can be read also from drivers that are
    compiled as modules.
    
    Bug 1476801
    
    Change-Id: Idfd51e25c85f8fe85891cb0df25d53f0695da401
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/387609
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_dmabuf.c
+++ drivers/video/tegra/nvmap/nvmap_dmabuf.c
@@ -746,6 +746,7 @@ int nvmap_get_dmabuf_param(struct dma_buf *dmabuf, u32 param, u64 *result)
 	info = dmabuf->priv;
 	return __nvmap_get_handle_param(NULL, info->handle, param, result);
 }
+EXPORT_SYMBOL(nvmap_get_dmabuf_param);
 
 /*
  * List detailed info for all buffers allocated.

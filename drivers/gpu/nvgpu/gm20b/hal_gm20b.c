c66d70c8699b948da5ca22501a29643c31b5348a
Kevin Huang <kevinh@nvidia.com>
2014-03-26 17:40:27 -0700

    video: tegra: host: select NETC for gm20b
    Change-Id: I6e7b5e77907033c7e7cbcc32cae200d46a3700ea
    Signed-off-by: Kevin Huang <kevinh@nvidia.com>
    Reviewed-on: http://git-master/r/387381
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/397712
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Tested-by: Bo Yan <byan@nvidia.com>

--- drivers/gpu/nvgpu/gm20b/hal_gm20b.c
+++ drivers/gpu/nvgpu/gm20b/hal_gm20b.c
@@ -23,6 +23,7 @@
 #include "fb_gm20b.h"
 #include "gm20b_gating_reglist.h"
 #include "fifo_gm20b.h"
+#include "gr_ctx_gm20b.h"
 
 struct gpu_ops gm20b_ops = {
 	.clock_gating = {
@@ -47,6 +48,7 @@ int gm20b_init_hal(struct gpu_ops *gops)
 	gm20b_init_ltc(gops);
 	gm20b_init_fb(gops);
 	gm20b_init_fifo(gops);
+	gm20b_init_gr_ctx(gops);
 	gops->name = "gm20b";
 
 	return 0;

9eb1f57ba2b7072c6c53ca9dc59b10a87d88db23
Bo Yan <byan@nvidia.com>
2014-04-14 12:03:27 -0700

    gpu: nvgpu: Add GPU driver for GM20B
    this moves GM20B driver to the new location
    
    Change-Id: I5fde14e114a8db79738a4c61849912b1ae225fb5

---
+++ drivers/gpu/nvgpu/gm20b/gr_gm20b.h
@@ -0,0 +1,33 @@
+/*
+ * GM20B GPC MMU
+ *
+ * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef _NVHOST_GM20B_GR_MMU_H
+#define _NVHOST_GM20B_GR_MMU_H
+struct gk20a;
+
+enum {
+	MAXWELL_B                 = 0xB197,
+	MAXWELL_COMPUTE_B         = 0xB1C0,
+	MAXWELL_DMA_COPY_A         = 0xB0B5,
+};
+
+#define NVB197_SET_ALPHA_CIRCULAR_BUFFER_SIZE	0x02dc
+#define NVB197_SET_CIRCULAR_BUFFER_SIZE		0x1280
+#define NVB197_SET_SHADER_EXCEPTIONS		0x1528
+#define NVB1C0_SET_SHADER_EXCEPTIONS		0x1528
+
+#define NVA297_SET_SHADER_EXCEPTIONS_ENABLE_FALSE 0
+void gm20b_init_gr(struct gpu_ops *gops);
+#endif
---
+++ drivers/gpu/nvgpu/gm20b/hal_gm20b.c
@@ -0,0 +1,51 @@
+/*
+ * GM20B Graphics
+ *
+ * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#include <linux/types.h>
+
+#include "gk20a/gk20a.h"
+
+#include "ltc_gm20b.h"
+#include "gr_gm20b.h"
+#include "ltc_gm20b.h"
+#include "fb_gm20b.h"
+#include "gm20b_gating_reglist.h"
+
+struct gpu_ops gm20b_ops = {
+	.clock_gating = {
+		.slcg_gr_load_gating_prod =
+			gr_gm20b_slcg_gr_load_gating_prod,
+		.slcg_perf_load_gating_prod =
+			gr_gm20b_slcg_perf_load_gating_prod,
+		.blcg_gr_load_gating_prod =
+			gr_gm20b_blcg_gr_load_gating_prod,
+		.pg_gr_load_gating_prod =
+			gr_gm20b_pg_gr_load_gating_prod,
+		.slcg_therm_load_gating_prod =
+			gr_gm20b_slcg_therm_load_gating_prod,
+	}
+};
+
+int gm20b_init_hal(struct gpu_ops *gops)
+{
+	*gops = gm20b_ops;
+	gm20b_init_ltc(gops);
+	gm20b_init_gr(gops);
+	gm20b_init_ltc(gops);
+	gm20b_init_fb(gops);
+	gops->name = "gm20b";
+
+	return 0;
+}

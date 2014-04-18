6cf8ca026a75975eb029285d3050e7c7eee5cc07
Sri Krishna chowdary <schowdary@nvidia.com>
2014-04-19 03:13:21 +0530

    video: tegra: nvmap: protect kernel headers from user
    rbtree.h should not be visible to user space.
    user space needs types.h is needed to identify __u32, __u64 types.
    
    Change-Id: Ia181cbce59965e850db6f9ce26f857a27b62ed10
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/398461
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Tested-by: Krishna Reddy <vdumpa@nvidia.com>

--- include/linux/nvmap.h
+++ include/linux/nvmap.h
@@ -21,8 +21,9 @@
  */
 
 #include <linux/ioctl.h>
-#include <linux/rbtree.h>
+#include <linux/types.h>
 #if defined(__KERNEL__)
+#include <linux/rbtree.h>
 #include <linux/file.h>
 #include <linux/dma-buf.h>
 #include <linux/device.h>

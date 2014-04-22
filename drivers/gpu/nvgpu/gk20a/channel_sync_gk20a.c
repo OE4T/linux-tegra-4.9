c5466deaa18b0834f7e30dac2a362025b3976120
Shridhar Rasal <srasal@nvidia.com>
2014-04-22 10:33:28 +0530

    gpu: nvgpu: disable aggresive syncpoint destroy
    Bug 1503225
    
    Change-Id: I52fd660de9bd251ceb936ad4edc34359753a0074
    Signed-off-by: Shridhar Rasal <srasal@nvidia.com>
    Reviewed-on: http://git-master/r/399460
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/channel_sync_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/channel_sync_gk20a.c
@@ -357,7 +357,7 @@ gk20a_channel_syncpt_create(struct channel_gk20a *c)
 	sp->ops.set_min_eq_max		= gk20a_channel_syncpt_set_min_eq_max;
 	sp->ops.destroy			= gk20a_channel_syncpt_destroy;
 
-	sp->ops.syncpt_aggressive_destroy = true;
+	sp->ops.syncpt_aggressive_destroy = false;
 
 	return &sp->ops;
 }

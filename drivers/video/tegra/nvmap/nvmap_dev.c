ea16b5a48987932d4e4869c940f45efcd965b7c3
Sri Krishna Chowdary <schowdary@nvidia.com>
2014-05-15 04:17:00 -0700

    Revert "video: tegra: nvmap: deal with read vs write fault"
    For nvmap managed page, page fault does not occur
    if a page is accessed for write in case a read access is
    already made. So, even reserved pages can be written by CPU.
    Prevent that from happening.
    
    This reverts commit ca19bb9c02ec86570048086c7a4603cbb7eebec.
    
    Bug 1444151
    
    Change-Id: I041c6550a399f1ea148671b089272316d5f29ce9
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/410219
    (cherry picked from commit 1cf0719abcc84762afebdcfc87a2c12ed312f177)
    Reviewed-on: http://git-master/r/418872
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_dev.c
+++ drivers/video/tegra/nvmap/nvmap_dev.c
@@ -778,12 +778,10 @@ static int nvmap_vma_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 		page = pfn_to_page(pfn);
 	} else {
 		offs >>= PAGE_SHIFT;
-		if (vmf->flags & (FAULT_FLAG_WRITE | FAULT_FLAG_MKWRITE)) {
-			if (nvmap_page_reserved(priv->handle->pgalloc.pages[offs]))
-				return VM_FAULT_SIGBUS;
-			nvmap_page_mkdirty(&priv->handle->pgalloc.pages[offs]);
-		}
+		if (nvmap_page_reserved(priv->handle->pgalloc.pages[offs]))
+			return VM_FAULT_SIGBUS;
 		page = nvmap_to_page(priv->handle->pgalloc.pages[offs]);
+		nvmap_page_mkdirty(&priv->handle->pgalloc.pages[offs]);
 	}
 
 	if (page)

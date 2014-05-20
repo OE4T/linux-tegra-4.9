dd2a6178178f91ec394eb689e5457f0f78a5daa7
Krishna Reddy <vdumpa@nvidia.com>
2014-05-20 10:38:42 -0700

    arm: tegra: nvmap: Fix incorrect declaraion of vpr and carveout memory
    resizable cma memory should be declared only when
    NVMAP_USE_CMA_FOR_CARVEOUT is enaled.
    
    Change-Id: I5ffe999d0e76245e35843b50ce71d0a0dd20e616
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/412170
    (cherry picked from commit 2c07bf5009ba801aa0cb0826b42947d2f214c6bf)
    Reviewed-on: http://git-master/r/412806

--- arch/arm/mach-tegra/board-loki-panel.c
+++ arch/arm/mach-tegra/board-loki-panel.c
@@ -460,7 +460,7 @@ int __init loki_panel_init(int board_id)
 	struct resource __maybe_unused *res;
 	struct platform_device *phost1x = NULL;
 	struct board_info bi;
-#ifdef CONFIG_TEGRA_NVMAP
+#ifdef CONFIG_NVMAP_USE_CMA_FOR_CARVEOUT
 	struct dma_declare_info vpr_dma_info;
 	struct dma_declare_info generic_dma_info;
 #endif
@@ -492,6 +492,7 @@ int __init loki_panel_init(int board_id)
 	loki_carveouts[2].base = tegra_vpr_start;
 	loki_carveouts[2].size = tegra_vpr_size;
 
+#ifdef CONFIG_NVMAP_USE_CMA_FOR_CARVEOUT
 	generic_dma_info.name = "generic";
 	generic_dma_info.base = tegra_carveout_start;
 	generic_dma_info.size = tegra_carveout_size;
@@ -503,7 +504,6 @@ int __init loki_panel_init(int board_id)
 	vpr_dma_info.size = tegra_vpr_size;
 	vpr_dma_info.resize = false;
 	vpr_dma_info.cma_dev = NULL;
-#ifdef CONFIG_NVMAP_USE_CMA_FOR_CARVEOUT
 	loki_carveouts[1].cma_dev = &tegra_generic_cma_dev;
 	loki_carveouts[1].resize = false;
 	loki_carveouts[2].cma_dev = &tegra_vpr_cma_dev;
@@ -513,7 +513,6 @@ int __init loki_panel_init(int board_id)
 	vpr_dma_info.resize = true;
 	vpr_dma_info.cma_dev = &tegra_vpr_cma_dev;
 	vpr_dma_info.notifier.ops = &vpr_dev_ops;
-#endif
 
 	if (tegra_carveout_size) {
 		err = dma_declare_coherent_resizable_cma_memory(
@@ -531,6 +530,7 @@ int __init loki_panel_init(int board_id)
 			return err;
 		}
 	}
+#endif
 
 	err = platform_device_register(&loki_nvmap_device);
 	if (err) {

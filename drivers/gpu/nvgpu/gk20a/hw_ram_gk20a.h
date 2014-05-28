bea937a74ede83ff3973b52d684ffdfabaa33767
Deepak Nibade <dnibade@nvidia.com>
2014-05-28 19:41:13 +0530

    gpu: nvgpu: add accessors for runlist ram entry
    Add accessors to modify contents of runlist ram (RAMRL) entry.
    Using these accessors we can modify a runlist entry to specify
    it as regular channel or TSG entry
    
    Bug 1470692
    
    Change-Id: If39759941ecb07af11152dbddb6fb5a67c14b26e
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/416611
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Randy Spurlock <rspurlock@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/hw_ram_gk20a.h
+++ drivers/gpu/nvgpu/gk20a/hw_ram_gk20a.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -385,5 +385,45 @@ static inline u32 ram_userd_gp_top_level_get_hi_w(void)
 static inline u32 ram_rl_entry_size_v(void)
 {
 	return 0x00000008;
+}
+static inline u32 ram_rl_entry_chid_f(u32 v)
+{
+	return (v & 0xfff) << 0;
+}
+static inline u32 ram_rl_entry_id_f(u32 v)
+{
+	return (v & 0xfff) << 0;
+}
+static inline u32 ram_rl_entry_type_f(u32 v)
+{
+	return (v & 0x1) << 13;
+}
+static inline u32 ram_rl_entry_type_chid_f(void)
+{
+	return 0x0;
+}
+static inline u32 ram_rl_entry_type_tsg_f(void)
+{
+	return 0x2000;
+}
+static inline u32 ram_rl_entry_timeslice_scale_f(u32 v)
+{
+	return (v & 0xf) << 14;
+}
+static inline u32 ram_rl_entry_timeslice_scale_3_f(void)
+{
+	return 0xc000;
+}
+static inline u32 ram_rl_entry_timeslice_timeout_f(u32 v)
+{
+	return (v & 0xff) << 18;
+}
+static inline u32 ram_rl_entry_timeslice_timeout_128_f(void)
+{
+	return 0x2000000;
+}
+static inline u32 ram_rl_entry_tsg_length_f(u32 v)
+{
+	return (v & 0x3f) << 26;
 }
 #endif

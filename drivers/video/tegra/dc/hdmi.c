c4251bfa8c48baf43410341d4d79785ac0e0a4e5
Anshuman Nath Kar <anshumank@nvidia.com>
2014-05-28 17:43:07 -0700

    video: tegra: hdmi: check dc powergating status
    Add checks for dc clockgating and powergating before
    reading or writing from dc hdmi registers
    
    Bug 200005903
    Bug 1514732
    Bug 200017536
    Bug 200018316
    Bug 200018314
    
    Change-Id: I5155af72a0529d167bf56b016d9238501c5b4229
    Signed-off-by: Anshuman Nath Kar <anshumank@nvidia.com>
    Reviewed-on: http://git-master/r/434863
    (cherry picked from commit e803fa14dc97c0da7df339185c97d4e6301832ac)
    (cherry picked from commit d54f28895c79853fa12adb3fe3551282ad8a1ba3)
    Reviewed-on: http://git-master/r/435860
    Reviewed-by: Mitch Luban <mluban@nvidia.com>
    Tested-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -421,6 +421,12 @@ unsigned long tegra_hdmi_readl(struct tegra_dc_hdmi_data *hdmi,
 					     unsigned long reg)
 {
 	unsigned long ret;
+
+	if (WARN(!tegra_is_clk_enabled(hdmi->dc->clk), "DC is clock-gated.\n") ||
+		WARN(!tegra_powergate_is_powered(hdmi->dc->powergate_id),
+			"DC is power-gated.\n"))
+		return 0;
+
 	ret = readl(hdmi->base + reg * 4);
 	trace_display_readl(hdmi->dc, ret, hdmi->base + reg * 4);
 	return ret;
@@ -429,6 +435,11 @@ unsigned long tegra_hdmi_readl(struct tegra_dc_hdmi_data *hdmi,
 void tegra_hdmi_writel(struct tegra_dc_hdmi_data *hdmi,
 				     unsigned long val, unsigned long reg)
 {
+	if (WARN(!tegra_is_clk_enabled(hdmi->dc->clk), "DC is clock-gated.\n") ||
+		WARN(!tegra_powergate_is_powered(hdmi->dc->powergate_id),
+			"DC is power-gated.\n"))
+		return;
+
 	trace_display_writel(hdmi->dc, val, hdmi->base + reg * 4);
 	writel(val, hdmi->base + reg * 4);
 }

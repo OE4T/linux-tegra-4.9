d8029baf6f55b7199316f7051b8b5cdf39c0bb67
Alex Waterman <alexw@nvidia.com>
2014-06-10 11:17:50 -0700

    video: tegra: nvmap: Move nvmap init code
    Move the nvmap init code to nvmap's directory and remove
    the board code. This is in preparation for using the device
    tree to specify the carveout structures that nvmap ends up
    creating.
    
    Bug 200008319
    
    Change-Id: Ia593714f2fd4cc1c7143df0b3c931cf01e0d611f
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: http://git-master/r/421887
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- arch/arm/mach-tegra/board-loki-panel.c
+++ arch/arm/mach-tegra/board-loki-panel.c
@@ -469,10 +469,6 @@ int __init loki_panel_init(int board_id)
 	struct resource __maybe_unused *res;
 	struct platform_device *phost1x = NULL;
 	struct board_info bi;
-#ifdef CONFIG_NVMAP_USE_CMA_FOR_CARVEOUT
-	struct dma_declare_info vpr_dma_info;
-	struct dma_declare_info generic_dma_info;
-#endif
 
 	tegra_get_board_info(&bi);
 	if ((bi.sku == BOARD_SKU_FOSTER) && (bi.board_id == BOARD_P2530)) {
@@ -494,59 +490,7 @@ int __init loki_panel_init(int board_id)
 	} else
 		loki_panel_select();
 
-#ifdef CONFIG_TEGRA_NVMAP
-	loki_carveouts[1].base = tegra_carveout_start;
-	loki_carveouts[1].size = tegra_carveout_size;
-
-	loki_carveouts[2].base = tegra_vpr_start;
-	loki_carveouts[2].size = tegra_vpr_size;
-
-#ifdef CONFIG_NVMAP_USE_CMA_FOR_CARVEOUT
-	generic_dma_info.name = "generic";
-	generic_dma_info.base = tegra_carveout_start;
-	generic_dma_info.size = tegra_carveout_size;
-	generic_dma_info.resize = false;
-	generic_dma_info.cma_dev = NULL;
-
-	vpr_dma_info.name = "vpr";
-	vpr_dma_info.base = tegra_vpr_start;
-	vpr_dma_info.size = tegra_vpr_size;
-	vpr_dma_info.resize = false;
-	vpr_dma_info.cma_dev = NULL;
-	loki_carveouts[1].cma_dev = &tegra_generic_cma_dev;
-	loki_carveouts[1].resize = false;
-	loki_carveouts[2].cma_dev = &tegra_vpr_cma_dev;
-	loki_carveouts[2].resize = true;
-
-	vpr_dma_info.size = SZ_32M;
-	vpr_dma_info.resize = true;
-	vpr_dma_info.cma_dev = &tegra_vpr_cma_dev;
-	vpr_dma_info.notifier.ops = &vpr_dev_ops;
-
-	if (tegra_carveout_size) {
-		err = dma_declare_coherent_resizable_cma_memory(
-				&tegra_generic_dev, &generic_dma_info);
-		if (err) {
-			pr_err("Generic coherent memory declaration failed\n");
-			return err;
-		}
-	}
-	if (tegra_vpr_size) {
-		err = dma_declare_coherent_resizable_cma_memory(
-				&tegra_vpr_dev, &vpr_dma_info);
-		if (err) {
-			pr_err("VPR coherent memory declaration failed\n");
-			return err;
-		}
-	}
-#endif
-
-	err = platform_device_register(&loki_nvmap_device);
-	if (err) {
-		pr_err("nvmap device registration failed\n");
-		return err;
-	}
-#endif
+	nvmap_init();
 
 	phost1x = loki_host1x_init();
 	if (!phost1x) {

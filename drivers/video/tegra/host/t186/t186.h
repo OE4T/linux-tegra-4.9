bbcf60793fc536ade157ae34286146bfa24a7818
Arto Merilainen <amerilainen@nvidia.com>
2014-07-09 19:16:39 +0300

    video: tegra: host: Check-in T186 base support
    This patch adds the platform file and required makefiles for
    enabling host1x on T18x.
    
    Bug 1525992
    
    Change-Id: If8ed1fbdd64c5a70f993cee9641c6963f55a7373
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/450211
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Shridhar Rasal <srasal@nvidia.com>

---
+++ drivers/video/tegra/host/t186/t186.h
@@ -0,0 +1,31 @@
+/*
+ * Tegra Graphics Chip support for T186
+ *
+ * Copyright (c) 2014, NVIDIA Corporation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _NVHOST_T186_H_
+#define _NVHOST_T186_H_
+
+#include "chip_support.h"
+
+/* FIXME: Update correct number of channels here */
+#define T186_NVHOST_NUMCHANNELS 12
+
+extern struct nvhost_device_data t18_host1x_info;
+
+int nvhost_init_t186_support(struct nvhost_master *host,
+			     struct nvhost_chip_support *op);
+
+#endif /* _NVHOST_T186_H_ */

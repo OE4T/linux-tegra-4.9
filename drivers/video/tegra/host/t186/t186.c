902d9d6ea7bf9356c6d56d2b7960629d5dad7e1d
Arto Merilainen <amerilainen@nvidia.com>
2014-07-09 19:16:39 +0300

    video: tegra: host: Check-in T186 base support
    This patch adds the platform file and required makefiles for
    enabling host1x on T18x.
    
    Bug 1525992
    
    Change-Id: If8ed1fbdd64c5a70f993cee9641c6963f55a7373
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/450211
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Shridhar Rasal <srasal@nvidia.com>

---
+++ drivers/video/tegra/host/t186/t186.c
@@ -0,0 +1,113 @@
+/*
+ * Tegra Graphics Init for T186 Architecture Chips
+ *
+ * Copyright (c) 2014, NVIDIA Corporation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/slab.h>
+#include <linux/io.h>
+
+#include "dev.h"
+
+#include "t186.h"
+#include "t124/t124.h"
+#include "host1x/host1x.h"
+#include "hardware_t124.h"
+#include "syncpt_t124.h"
+
+#include "chip_support.h"
+
+static struct host1x_device_info host1x04_info = {
+	.nb_channels	= T186_NVHOST_NUMCHANNELS,
+	.nb_pts		= NV_HOST1X_SYNCPT_NB_PTS,
+	.nb_mlocks	= NV_HOST1X_NB_MLOCKS,
+	.initialize_chip_support = nvhost_init_t186_support,
+};
+
+struct nvhost_device_data t18_host1x_info = {
+	.clocks		= {{"host1x", UINT_MAX}, {"actmon", UINT_MAX}, {} },
+	NVHOST_MODULE_NO_POWERGATE_IDS,
+	.private_data	= &host1x04_info,
+};
+
+#include "host1x/host1x_channel.c"
+
+static void t186_set_nvhost_chanops(struct nvhost_channel *ch)
+{
+	if (ch)
+		ch->ops = host1x_channel_ops;
+}
+
+int nvhost_init_t186_channel_support(struct nvhost_master *host,
+				     struct nvhost_chip_support *op)
+{
+	op->nvhost_dev.set_nvhost_chanops = t186_set_nvhost_chanops;
+
+	return 0;
+}
+
+static void t186_remove_support(struct nvhost_chip_support *op)
+{
+	kfree(op->priv);
+	op->priv = 0;
+}
+
+#include "host1x/host1x_cdma.c"
+#include "host1x/host1x_syncpt.c"
+#include "host1x/host1x_intr.c"
+#include "host1x/host1x_actmon_t124.c"
+#include "host1x/host1x_debug.c"
+
+int nvhost_init_t186_support(struct nvhost_master *host,
+			     struct nvhost_chip_support *op)
+{
+	int err;
+	struct t124 *t186 = 0;
+
+	op->soc_name = "tegra18x";
+
+	/* don't worry about cleaning up on failure... "remove" does it. */
+	err = nvhost_init_t186_channel_support(host, op);
+	if (err)
+		return err;
+
+	op->cdma = host1x_cdma_ops;
+	op->push_buffer = host1x_pushbuffer_ops;
+	op->debug = host1x_debug_ops;
+
+	host->sync_aperture = host->aperture + HOST1X_CHANNEL_SYNC_REG_BASE;
+	op->syncpt = host1x_syncpt_ops;
+	op->intr = host1x_intr_ops;
+	op->actmon = host1x_actmon_ops;
+
+	t186 = kzalloc(sizeof(struct t124), GFP_KERNEL);
+	if (!t186) {
+		err = -ENOMEM;
+		goto err;
+	}
+
+	t186->host = host;
+	op->priv = t186;
+	op->remove_support = t186_remove_support;
+
+	return 0;
+
+err:
+	kfree(t186);
+
+	op->priv = 0;
+	op->remove_support = 0;
+	return err;
+}

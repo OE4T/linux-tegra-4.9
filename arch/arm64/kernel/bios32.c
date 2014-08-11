4f8a194517865a2c68f9f3ab284b596e7f9287be
Vidya Sagar <vidyas@nvidia.com>
2014-08-11 11:54:53 +0530

    ARM: PCI: Export pci_common_init_dev() and pci_common_exit()
    vidyas: make similar change for ARM64
    based on http://git-master/r/#/c/449136/
    
    Bug 1496843
    
    Change-Id: I7b8f0f9d069020b26598ab811591debe8381e962
    Signed-off-by: Vidya Sagar <vidyas@nvidia.com>
    Reviewed-on: http://git-master/r/454901
    (cherry picked from commit 2aed475749e131913acf35cef6240508fdd955c8)
    Reviewed-on: http://git-master/r/1168442
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm64/kernel/bios32.c
+++ arch/arm64/kernel/bios32.c
@@ -504,6 +504,7 @@ void pci_common_init_dev(struct device *parent, struct hw_pci *hw)
 		pci_bus_add_devices(bus);
 	}
 }
+EXPORT_SYMBOL(pci_common_init_dev);
 
 void pci_common_exit(struct list_head *head)
 {
@@ -522,6 +523,7 @@ void pci_common_exit(struct list_head *head)
 		kfree(sys);
 	}
 }
+EXPORT_SYMBOL(pci_common_exit);
 
 #ifndef CONFIG_PCI_HOST_ITE8152
 void pcibios_set_master(struct pci_dev *dev)

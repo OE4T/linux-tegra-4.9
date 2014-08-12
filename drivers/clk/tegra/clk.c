01b1790afbc058c8c23a980dba1b14c4d65ad35f
Peter De Schrijver <pdeschrijver@nvidia.com>
2014-08-13 05:36:28 +0800

    clk: tegra: peripheral clk suspend resume
    Introduce support for peripheral clk context save and restore.
    
    Change-Id: I8275c50053771c92ca5453f325ed46522b6ab7ec
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1568524
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/clk/tegra/clk.c
+++ drivers/clk/tegra/clk.c
@@ -20,6 +20,7 @@
 #include <linux/delay.h>
 #include <linux/fs.h>
 #include <linux/of.h>
+#include <linux/io.h>
 #include <linux/clk/tegra.h>
 #include <linux/reset-controller.h>
 #include <linux/seq_file.h>
@@ -86,6 +87,9 @@ static int periph_banks;
 static struct clk **clks;
 static int clk_num;
 static struct clk_onecell_data clk_data;
+#ifdef CONFIG_PM_SLEEP
+static u32 *periph_ctx;
+#endif
 
 /* Handlers for SoC-specific reset lines */
 static int (*special_reset_assert)(unsigned long);
@@ -237,6 +241,63 @@ int tegra_super_cdiv_use_therm_controls(bool enable)
 }
 EXPORT_SYMBOL_GPL(tegra_super_cdiv_use_therm_controls);
 
+#ifdef CONFIG_PM_SLEEP
+void tegra_clk_periph_suspend(void __iomem *clk_base)
+{
+	int i, idx;
+
+	idx = 0;
+	for (i = 0; i < periph_banks; i++, idx++)
+		periph_ctx[idx] =
+			readl_relaxed(clk_base + periph_regs[i].rst_reg);
+
+	for (i = 0; i < periph_banks; i++, idx++)
+		periph_ctx[idx] =
+			readl_relaxed(clk_base + periph_regs[i].enb_reg);
+}
+
+void tegra_clk_periph_force_on(u32 *clks_on, int count, void __iomem *clk_base)
+{
+	int i;
+
+	WARN_ON(count != periph_banks);
+
+	for (i = 0; i < count; i++)
+		writel_relaxed(clks_on[i], clk_base + periph_regs[i].enb_reg);
+}
+
+void tegra_clk_periph_resume(void __iomem *clk_base)
+{
+	int i, idx;
+
+	idx = 0;
+	for (i = 0; i < periph_banks; i++, idx++)
+		writel_relaxed(periph_ctx[idx],
+			clk_base + periph_regs[i].rst_reg);
+
+	/* ensure all resets have propagated */
+	tegra_read_chipid();
+
+	for (i = 0; i < periph_banks; i++, idx++)
+		writel_relaxed(periph_ctx[idx],
+			clk_base + periph_regs[i].enb_reg);
+
+	/* ensure all enables have propagated */
+	tegra_read_chipid();
+}
+
+static int tegra_clk_suspend_ctx_init(int banks)
+{
+	int err = 0;
+
+	periph_ctx = kzalloc(2 * banks * sizeof(*periph_ctx), GFP_KERNEL);
+	if (!periph_ctx)
+		err = -ENOMEM;
+
+	return err;
+}
+#endif
+
 struct clk ** __init tegra_clk_init(void __iomem *regs, int num, int banks)
 {
 	clk_base = regs;
@@ -252,10 +313,20 @@ struct clk ** __init tegra_clk_init(void __iomem *regs, int num, int banks)
 	periph_banks = banks;
 
 	clks = kzalloc(num * sizeof(struct clk *), GFP_KERNEL);
-	if (!clks)
+	if (!clks) {
 		kfree(periph_clk_enb_refcnt);
+		return NULL;
+	}
 
 	clk_num = num;
+
+#ifdef CONFIG_PM_SLEEP
+	if (tegra_clk_suspend_ctx_init(banks)) {
+		kfree(periph_clk_enb_refcnt);
+		kfree(clks);
+		return NULL;
+	}
+#endif
 
 	return clks;
 }

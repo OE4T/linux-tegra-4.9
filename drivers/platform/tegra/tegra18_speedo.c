09dc1ef9642564e42a3ab1ab82ab9e85a7c2ba64
Adeel Raza <araza@nvidia.com>
2014-08-14 19:53:26 -0700

    platform: tegra: add T18x speedo file
    Add T18x speedo file which only contains stub functions so far.
    
    Bug 1509349
    
    Change-Id: I3f72ba5e37a81b1510e7366e11cdc0b192758304
    Signed-off-by: Adeel Raza <araza@nvidia.com>
    Reviewed-on: http://git-master/r/456993
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Peng Du <pdu@nvidia.com>
    Reviewed-by: Chao Xu <cxu@nvidia.com>

---
+++ drivers/platform/tegra/tegra18_speedo.c
@@ -0,0 +1,16 @@
+void tegra_init_speedo_data(void)
+{
+	/* TODO: Implement this function. */
+}
+
+int tegra_cpu_process_id(void)
+{
+	/* TODO: Implement this function. */
+	return 0;
+}
+
+int tegra_core_process_id(void)
+{
+	/* TODO: Implement this function. */
+	return 0;
+}

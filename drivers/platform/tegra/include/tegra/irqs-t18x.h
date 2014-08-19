23c5df877c3d386d64e2384ff2748a2b177433f3
Adeel Raza <araza@nvidia.com>
2014-08-19 15:43:28 -0700

    platform: tegra: add T18x irqs header
    Add irqs-t18x.h.
    
    Bug 1509349
    Bug 1539889
    Bug 1520609
    Bug 1546024
    
    Change-Id: Ibfdb66af5312a5368b6bdbc23fa2596162477c4e
    Signed-off-by: Adeel Raza <araza@nvidia.com>
    Reviewed-on: http://git-master/r/482224
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

---
+++ drivers/platform/tegra/include/tegra/irqs-t18x.h
@@ -0,0 +1,22 @@
+/*
+ * drivers/platform/tegra/include/tegra/irqs-t18x.h
+ *
+ * Copyright (C) 2014 NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __MACH_TEGRA_IRQS_T18X_H
+#define __MACH_TEGRA_IRQS_T18X_H
+
+#include "../../../../../../kernel/arch/arm64/mach-tegra/include/mach/irqs-t21x.h"
+
+#endif

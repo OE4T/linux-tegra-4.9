8ea3c85836580270adc3719240d874c4fbca758f
Krishna Reddy <vdumpa@nvidia.com>
2014-09-03 14:07:41 -0700

    mm: avoid allocating CMA memory for stack
    allocation of CMA memory for user stack would cause
    permanent migration failures and result in CMA
    allocation failures.
    Bug 1550455
    JIRA KNEXT-27
    
    Change-Id: I75ac13416dbcf1810c89641cefdd0d56726cc36a
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/496632
    Reviewed-on: http://git-master/r/1118217
    (cherry picked from commit bef61af177e19ffecc78b1aeebad4aef8dcb26e4)
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1330468
    GVS: Gerrit_Virtual_Submit

--- mm/memory.c
+++ mm/memory.c
@@ -2736,6 +2736,7 @@ static inline int check_stack_guard_page(struct vm_area_struct *vma, unsigned lo
 	return 0;
 }
 
+bool is_vma_temporary_stack(struct vm_area_struct *vma);
 /*
  * We enter with non-exclusive mmap_sem (to exclude vma changes,
  * but allow concurrent faults), and pte mapped but not yet locked.
@@ -2793,7 +2794,12 @@ static int do_anonymous_page(struct fault_env *fe)
 	/* Allocate our own private page. */
 	if (unlikely(anon_vma_prepare(vma)))
 		goto oom;
-	page = alloc_zeroed_user_highpage_movable(vma, fe->address);
+	if (vma->vm_flags & VM_LOCKED || vma->vm_flags & FAULT_FLAG_NO_CMA ||
+	    is_vma_temporary_stack(vma)) {
+		page = alloc_zeroed_user_highpage(GFP_HIGHUSER, vma, fe->address);
+	} else {
+		page = alloc_zeroed_user_highpage_movable(vma, fe->address);
+	}
 	if (!page)
 		goto oom;
 

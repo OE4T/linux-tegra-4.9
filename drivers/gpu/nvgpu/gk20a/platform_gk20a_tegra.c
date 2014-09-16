fecd58bfc942cecb4b5a64cf8096ab2c7c74e89d
Seshendra Gadagottu <sgadagottu@nvidia.com>
2014-09-16 13:04:39 -0700

    gpu: nvgpu: gm20b: enable blcg
    Enable Block Level Clock Gating power
    feature for gm20b.
    
    Bug 1552466
    
    Change-Id: Ibdd611bc2932ae9c3ce2c0d9eb847fa46a3759c7
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/499538
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/platform_gk20a_tegra.c
+++ drivers/gpu/nvgpu/gk20a/platform_gk20a_tegra.c
@@ -574,7 +574,7 @@ struct gk20a_platform gm20b_tegra_platform = {
 	/* Disable all power features for gm20b */
 	.can_railgate           = true,
 	.enable_slcg            = false,
-	.enable_blcg            = false,
+	.enable_blcg            = true,
 	.enable_elcg            = false,
 	.enable_elpg            = false,
 	.enable_aelpg           = false,

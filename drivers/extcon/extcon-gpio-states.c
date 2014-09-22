eaf11aaafb6b2d66f14f4da5ad96ae8281fb76f5
Laxman Dewangan <ldewangan@nvidia.com>
2014-09-22 18:44:00 +0530

    extcon: gpio-states: rewrite the driver
    Rewrite gpio-state based extcon driver to fix multiple issue and
    support multiple gpios.
    
    The highlight of changes are:
    - Rename gpio extcon data structure
    - Rename variable names.
    - Take multiple gpios as input.
    - Register for all input gpios.
    - Check for all gpio states and based on this, make output cable state by
      lookup table.
    
    Change-Id: I91ceb7b4709c98c279cd844dc0710d32cdee1bf7
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/501263
    (cherry picked from commit 57071910ec96e30ea7acc385d8a7e9f598084d90)
    (cherry picked from commit 26914c6a5f41d4c2a38bd688dcb6ce7727c6f71d)
    Reviewed-on: http://git-master/r/1120313
    (cherry picked from commit 943c76187c15d35ee274c69565e09acd10fe9510)
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>

--- drivers/extcon/extcon-gpio-states.c
+++ drivers/extcon/extcon-gpio-states.c
@@ -1,16 +1,13 @@
 /*
- *  drivers/extcon/extcon_gpio.c
+ * drivers/extcon/extcon-gpio-states.c
  *
- *  Single-state GPIO extcon driver based on extcon class
+ * Multiple GPIO state based based on extcon class driver.
  *
  * Author: Laxman Dewangan <ldewangan@nvidia.com>
  *
  * Based on extcon-gpio driver by
  *	Copyright (C) 2008 Google, Inc.
  *	Author: Mike Lockwood <lockwood@android.com>
- *
- * Modified by MyungJoo Ham <myungjoo.ham@samsung.com> to support extcon
- * (originally switch class is supported)
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -20,8 +17,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
-*/
+ */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -34,30 +30,39 @@
 #include <linux/extcon.h>
 #include <linux/of.h>
 #include <linux/of_gpio.h>
+#include <linux/spinlock.h>
 
 struct gpio_extcon_cables {
 	int gstate;
 	int cstate;
 };
 
-struct gpio_extcon_platform_data {
-	const char *name;
-	int gpio;
-	unsigned long debounce;
-	unsigned long irq_flags;
-	const char **cable_name;
-	int cable_count;
-	struct gpio_extcon_cables *cable_states;
-	int cable_states_count;
-};
-
-struct gpio_extcon_data {
-	struct device *dev;
+struct gpio_info {
 	int gpio;
 	int irq;
-	struct extcon_dev edev;
+};
+
+struct gpio_extcon_platform_data {
+	const char *name;
+	unsigned long debounce;
+	unsigned long irq_flags;
+	struct gpio_info *gpios;
+	int n_gpio;
+	const char **out_cable_name;
+	int n_out_cables;
+	struct gpio_extcon_cables *cable_states;
+	int n_cable_states;
+};
+
+struct gpio_extcon_info {
+	struct device *dev;
+	struct extcon_dev *edev;
 	struct delayed_work work;
 	unsigned long debounce_jiffies;
+	struct timer_list timer;
+	int timer_to_work_jiffies;
+	spinlock_t lock;
+	int *gpio_curr_state;
 	struct gpio_extcon_platform_data *pdata;
 };
 
@@ -65,38 +70,48 @@ static void gpio_extcon_work(struct work_struct *work)
 {
 	int state;
 	int cstate = -1;
-	struct gpio_extcon_data	*data =
-		container_of(to_delayed_work(work), struct gpio_extcon_data,
-			     work);
+	struct gpio_extcon_info	*gpex = container_of(to_delayed_work(work),
+					struct gpio_extcon_info, work);
+	int gstate = 0;
 	int i;
 
-	state = gpio_get_value_cansleep(data->gpio);
-	state &= 0x1;
-	for (i = 0; i < data->pdata->cable_states_count; ++i) {
-		if (data->pdata->cable_states[i].gstate == state) {
-			cstate = data->pdata->cable_states[i].cstate;
+	for (i = 0; i < gpex->pdata->n_gpio; ++i) {
+		state = gpio_get_value_cansleep(gpex->pdata->gpios[i].gpio);
+		if (state)
+			gstate |= BIT(i);
+	}
+
+	for (i = 0; i < gpex->pdata->n_cable_states; ++i) {
+		if (gpex->pdata->cable_states[i].gstate == gstate) {
+			cstate = gpex->pdata->cable_states[i].cstate;
 			break;
 		}
 	}
 
 	if (cstate == -1) {
-		dev_info(data->dev, "Cable state not found %d\n", state);
+		dev_info(gpex->dev, "Cable state not found 0x%02x\n", gstate);
 		cstate = 0;
 	}
 
-	dev_info(data->dev, "Cable state %d\n", cstate);
-	extcon_set_state(&data->edev, cstate);
+	dev_info(gpex->dev, "Cable state %d\n", cstate);
+	extcon_set_state(gpex->edev, cstate);
+}
+
+static void gpio_extcon_notifier_timer(unsigned long _data)
+{
+	struct gpio_extcon_info *gpex = (struct gpio_extcon_info *)_data;
+
+	schedule_delayed_work(&gpex->work, gpex->timer_to_work_jiffies);
 }
 
 static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
 {
-	struct gpio_extcon_data *extcon_data = dev_id;
+	struct gpio_extcon_info *gpex = dev_id;
+	unsigned long flags;
 
-	if (extcon_data->debounce_jiffies)
-		schedule_delayed_work(&extcon_data->work,
-			      extcon_data->debounce_jiffies);
-	else
-		gpio_extcon_work(&extcon_data->work.work);
+	spin_lock_irqsave(&gpex->lock, flags);
+	mod_timer(&gpex->timer, jiffies + gpex->debounce_jiffies);
+	spin_unlock_irqrestore(&gpex->lock, flags);
 
 	return IRQ_HANDLED;
 }
@@ -107,6 +122,7 @@ static struct gpio_extcon_platform_data *of_get_platform_data(
 	struct gpio_extcon_platform_data *pdata;
 	struct device_node *np = pdev->dev.of_node;
 	int gpio;
+	int n_gpio;
 	u32 pval;
 	int ret;
 	const char *names;
@@ -121,10 +137,23 @@ static struct gpio_extcon_platform_data *of_get_platform_data(
 	if (!pdata->name)
 		pdata->name = np->name;
 
-	gpio = of_get_named_gpio(np, "gpio", 0);
-	if ((gpio < 0) && (gpio != -ENOENT))
-		return ERR_PTR(gpio);
-	pdata->gpio = gpio;
+	n_gpio = of_gpio_named_count(np, "gpios");
+	if (n_gpio < 1) {
+		dev_err(&pdev->dev, "Not sufficient gpios\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	pdata->n_gpio = n_gpio;
+	pdata->gpios = devm_kzalloc(&pdev->dev,
+			sizeof(*pdata->gpios) * n_gpio, GFP_KERNEL);
+	if (!pdata->gpios)
+		return ERR_PTR(-ENOMEM);
+	for (count = 0; count < n_gpio; ++count) {
+		gpio = of_get_named_gpio(np, "gpios", count);
+		if ((gpio < 0) && (gpio != -ENOENT))
+			return ERR_PTR(gpio);
+		pdata->gpios[count].gpio = gpio;
+	}
 
 	ret = of_property_read_u32(np, "extcon-gpio,irq-flags", &pval);
 	if (!ret)
@@ -136,36 +165,40 @@ static struct gpio_extcon_platform_data *of_get_platform_data(
 	ret = of_property_read_u32(np, "extcon-gpio,debounce", &pval);
 	if (!ret)
 		pdata->debounce = pval;
+	else
+		pdata->debounce = 10;
 
-	pdata->cable_count = of_property_count_strings(np,
-					"extcon-gpio,cable-name");
-	if (pdata->cable_count <= 0) {
-		dev_err(&pdev->dev, "not found cable names\n");
+	pdata->n_out_cables = of_property_count_strings(np,
+					"extcon-gpio,out-cable-names");
+	if (pdata->n_out_cables <= 0) {
+		dev_err(&pdev->dev, "not found out cable names\n");
 		return ERR_PTR(-EINVAL);
 	}
 
-	pdata->cable_name = devm_kzalloc(&pdev->dev, (pdata->cable_count + 1) *
-				sizeof(*pdata->cable_name), GFP_KERNEL);
-	if (!pdata->cable_name)
+	pdata->out_cable_name = devm_kzalloc(&pdev->dev,
+				(pdata->n_out_cables + 1) *
+				sizeof(*pdata->out_cable_name), GFP_KERNEL);
+	if (!pdata->out_cable_name)
 		return ERR_PTR(-ENOMEM);
 	count = 0;
-	of_property_for_each_string(np, "extcon-gpio,cable-name", prop, names)
-		pdata->cable_name[count++] = names;
-	pdata->cable_name[count] = NULL;
+	of_property_for_each_string(np, "extcon-gpio,out-cable-names",
+						prop, names)
+		pdata->out_cable_name[count++] = names;
+	pdata->out_cable_name[count] = NULL;
 
-	pdata->cable_states_count = of_property_count_u32(np,
+	pdata->n_cable_states = of_property_count_u32(np,
 						"extcon-gpio,cable-states");
-	if (pdata->cable_states_count < 2) {
+	if (pdata->n_cable_states < 2) {
 		dev_err(&pdev->dev, "not found proper cable state\n");
 		return ERR_PTR(-EINVAL);
 	}
-	pdata->cable_states_count /= 2;
+	pdata->n_cable_states /= 2;
 	pdata->cable_states = devm_kzalloc(&pdev->dev,
-				(pdata->cable_states_count) *
+				(pdata->n_cable_states) *
 				sizeof(*pdata->cable_states), GFP_KERNEL);
 	if (!pdata->cable_states)
 		return ERR_PTR(-ENOMEM);
-	for (count = 0;  count < pdata->cable_states_count; ++count) {
+	for (count = 0;  count < pdata->n_cable_states; ++count) {
 		ret = of_property_read_u32_index(np, "extcon-gpio,cable-states",
 				count * 2, &pval);
 		if (!ret)
@@ -183,100 +216,121 @@ static struct gpio_extcon_platform_data *of_get_platform_data(
 static int gpio_extcon_probe(struct platform_device *pdev)
 {
 	struct gpio_extcon_platform_data *pdata = pdev->dev.platform_data;
-	struct gpio_extcon_data *extcon_data;
+	struct gpio_extcon_info *gpex;
 	int ret = 0;
+	int i;
 
 	if (!pdata && pdev->dev.of_node) {
 		pdata = of_get_platform_data(pdev);
 		if (IS_ERR(pdata))
 			return PTR_ERR(pdata);
 	}
-
 	if (!pdata)
-		return -EBUSY;
+		return -EINVAL;
 
 	if (!pdata->irq_flags) {
 		dev_err(&pdev->dev, "IRQ flag is not specified.\n");
 		return -EINVAL;
 	}
 
-	extcon_data = devm_kzalloc(&pdev->dev, sizeof(struct gpio_extcon_data),
+	gpex = devm_kzalloc(&pdev->dev, sizeof(struct gpio_extcon_info),
 				   GFP_KERNEL);
-	if (!extcon_data)
+	if (!gpex)
 		return -ENOMEM;
 
-	extcon_data->dev = &pdev->dev;
-	extcon_data->edev.name = pdata->name;
-	extcon_data->edev.dev.parent = &pdev->dev;
-	extcon_data->gpio = pdata->gpio;
-	extcon_data->debounce_jiffies = msecs_to_jiffies(pdata->debounce);
-	extcon_data->edev.supported_cable = pdata->cable_name;
-	extcon_data->pdata = pdata;
+	gpex->dev = &pdev->dev;
+	gpex->edev = devm_extcon_dev_allocate(&pdev->dev, pdata->out_cable_name);
+	if (IS_ERR(gpex->edev)) {
+		dev_err(&pdev->dev, "failed to allocate extcon device\n");
+		return -ENOMEM;
+	}
 
-	ret = extcon_dev_register(&extcon_data->edev);
+	gpex->edev->name = pdata->name;
+	gpex->debounce_jiffies = msecs_to_jiffies(pdata->debounce);
+	gpex->timer_to_work_jiffies = msecs_to_jiffies(100);
+	gpex->pdata = pdata;
+	spin_lock_init(&gpex->lock);
+
+	for (i = 0; i < gpex->pdata->n_gpio; ++i) {
+		int irq;
+
+		irq =  gpio_to_irq(gpex->pdata->gpios[i].gpio);
+		if (irq < 0) {
+			dev_err(&pdev->dev, "gpio %d to irq failed: %d\n",
+					gpex->pdata->gpios[i].gpio, irq);
+			return irq;
+		}
+		gpex->pdata->gpios[i].irq = irq;
+	}
+
+	ret = devm_extcon_dev_register(&pdev->dev, gpex->edev);
 	if (ret < 0)
 		return ret;
 
-	INIT_DELAYED_WORK(&extcon_data->work, gpio_extcon_work);
+	INIT_DELAYED_WORK(&gpex->work, gpio_extcon_work);
+	setup_timer(&gpex->timer, gpio_extcon_notifier_timer,
+			(unsigned long)gpex);
 
-	ret = devm_gpio_request_one(&pdev->dev, extcon_data->gpio, GPIOF_DIR_IN,
+	for (i = 0; i < gpex->pdata->n_gpio; ++i) {
+		int gpio = gpex->pdata->gpios[i].gpio;
+		int irq = gpex->pdata->gpios[i].irq;
+
+		ret = devm_gpio_request_one(&pdev->dev, gpio, GPIOF_DIR_IN,
 				    pdev->name);
-	if (ret < 0)
-		goto err;
+		if (ret < 0)
+			return ret;
 
-	extcon_data->irq = gpio_to_irq(extcon_data->gpio);
-	if (extcon_data->irq < 0) {
-		ret = extcon_data->irq;
-		goto err;
+		ret = devm_request_any_context_irq(&pdev->dev, irq,
+				gpio_irq_handler, pdata->irq_flags,
+				pdev->name, gpex);
+		if (ret < 0)
+			return ret;
 	}
 
-	ret = request_any_context_irq(extcon_data->irq, gpio_irq_handler,
-				      pdata->irq_flags, pdev->name,
-				      extcon_data);
-	if (ret < 0)
-		goto err;
+	platform_set_drvdata(pdev, gpex);
+	device_set_wakeup_capable(gpex->dev, true);
 
-	platform_set_drvdata(pdev, extcon_data);
-	device_set_wakeup_capable(extcon_data->dev, true);
+	/* Enable wake default */
+	device_wakeup_enable(gpex->dev);
 
 	/* Perform initial detection */
-	gpio_extcon_work(&extcon_data->work.work);
+	gpio_extcon_work(&gpex->work.work);
 	return 0;
-
-err:
-	extcon_dev_unregister(&extcon_data->edev);
-	return ret;
 }
 
 static int gpio_extcon_remove(struct platform_device *pdev)
 {
-	struct gpio_extcon_data *extcon_data = platform_get_drvdata(pdev);
+	struct gpio_extcon_info *gpex = platform_get_drvdata(pdev);
 
-	cancel_delayed_work_sync(&extcon_data->work);
-	free_irq(extcon_data->irq, extcon_data);
-	extcon_dev_unregister(&extcon_data->edev);
-
+	del_timer_sync(&gpex->timer);
+	cancel_delayed_work_sync(&gpex->work);
 	return 0;
 }
 
 #ifdef CONFIG_PM_SLEEP
 static int gpio_extcon_suspend(struct device *dev)
 {
-	struct gpio_extcon_data *extcon_data = dev_get_drvdata(dev);
+	struct gpio_extcon_info *gpex = dev_get_drvdata(dev);
+	int i;
 
-	cancel_delayed_work_sync(&extcon_data->work);
-	if (device_may_wakeup(extcon_data->dev))
-		enable_irq_wake(extcon_data->irq);
+	cancel_delayed_work_sync(&gpex->work);
+	if (device_may_wakeup(gpex->dev)) {
+		for (i = 0; i < gpex->pdata->n_gpio; ++i)
+			enable_irq_wake(gpex->pdata->gpios[i].irq);
+	}
 
 	return 0;
 }
 
 static int gpio_extcon_resume(struct device *dev)
 {
-	struct gpio_extcon_data *extcon_data = dev_get_drvdata(dev);
+	struct gpio_extcon_info *gpex = dev_get_drvdata(dev);
+	int i;
 
-	if (device_may_wakeup(extcon_data->dev))
-		disable_irq_wake(extcon_data->irq);
+	if (device_may_wakeup(gpex->dev)) {
+		for (i = 0; i < gpex->pdata->n_gpio; ++i)
+			disable_irq_wake(gpex->pdata->gpios[i].irq);
+	}
 
 	return 0;
 }

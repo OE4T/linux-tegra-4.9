371cd7b8317a5f1947212aa8785b0a9355eaf540
Pantelis Antoniou <pantoniou@nvidia.com>
2014-09-30 23:53:36 -0700

    net: tegra_hv_net: Fix copyright blurb
    Fix the copyright blurb to NV standards (tm).
    
    Bug 200035584
    
    Change-Id: I6e9da43287262aff9d0877e41cb8fe6602f580fe
    Signed-off-by: Pantelis Antoniou <pantoniou@nvidia.com>
    Reviewed-on: http://git-master/r/552650
    Reviewed-on: http://git-master/r/1198211
    Reviewed-on: http://git-master/r/1314783
    Reviewed-by: Aniruddha Banerjee <aniruddhab@nvidia.com>
    Tested-by: Aniruddha Banerjee <aniruddhab@nvidia.com>
    Reviewed-by: Nitin Sehgal <nsehgal@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Timo Alho <talho@nvidia.com>

--- drivers/net/tegra_hv_net.c
+++ drivers/net/tegra_hv_net.c
@@ -1,14 +1,13 @@
 /*
  * tegra_hv_net.c: ethernet emulation over Tegra HV
  *
- * Copyright 2014 NVidia Corporation
- *
  * Very loosely based on virtio_net.c
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Copyright (C) 2014, NVIDIA CORPORATION. All rights reserved.
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2.  This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
  */
 
 #undef DEBUG

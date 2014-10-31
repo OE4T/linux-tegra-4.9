db0e9ca84edab8658bc4614d48a241fa77d9c046
Charlie Huang <chahuang@nvidia.com>
2014-10-31 11:24:10 -0700

    drivers: media: platform: tegra: minor fixes
    fix a typo - aperature as aperture.
    
    bug 200021510
    
    Change-Id: I53b2e7e6c458edbe99d090cd166922d8fe8f3cb1
    Signed-off-by: Charlie Huang <chahuang@nvidia.com>
    Reviewed-on: http://git-master/r/592178
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>
    Tested-by: Thomas Cherry <tcherry@nvidia.com>

--- include/media/nvc_focus.h
+++ include/media/nvc_focus.h
@@ -1,18 +1,17 @@
-/* Copyright (C) 2012 NVIDIA Corporation.
+/*
+ * Copyright (c) 2012-2014 NVIDIA Corporation. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307, USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __NVC_FOCUS_H__
@@ -57,7 +56,7 @@ enum nvc_focus_sts {
 struct nvc_focus_nvc {
 	__u32 focal_length;
 	__u32 fnumber;
-	__u32 max_aperature;
+	__u32 max_aperture;
 } __packed;
 
 struct nvc_focus_cap {

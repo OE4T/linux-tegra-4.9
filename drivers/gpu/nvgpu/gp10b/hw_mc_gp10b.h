901f65c7ae5a679fa62f89f0f7d7887b5879a007
Seshendra Gadagottu <sgadagottu@nvidia.com>
2014-12-08 18:15:37 -0800

    gpu: nvgpu: gp10b: update headers
    Update replayable page fault fifo, interrupt and bar2 block
    headers.
    
    Bug 1587825
    
    Change-Id: Ifa0d3b640bdd5f3f6fbc7826c1d1edba494340df
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/661117
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gp10b/hw_mc_gp10b.h
+++ drivers/gpu/nvgpu/gp10b/hw_mc_gp10b.h
@@ -78,6 +78,10 @@ static inline u32 mc_intr_pfifo_pending_f(void)
 {
 	return 0x100;
 }
+static inline u32 mc_intr_replayable_fault_pending_f(void)
+{
+	return 0x200;
+}
 static inline u32 mc_intr_pgraph_pending_f(void)
 {
 	return 0x1000;

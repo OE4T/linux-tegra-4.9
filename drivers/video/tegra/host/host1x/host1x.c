4ed99d83f152b2665994f8e3c8cefab1bef982d8
Deepak Nibade <dnibade@nvidia.com>
2015-01-29 21:08:19 +0530

    video: tegra: host: do not allow invalid configs
    Do not allow invalid configurations/combinations of sync point
    and channel policies
    
    Bug 1525996
    
    Change-Id: I4321ffd50c488a4d35fed929c73acb9dab8706a6
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/679020
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>

--- drivers/video/tegra/host/host1x/host1x.c
+++ drivers/video/tegra/host/host1x/host1x.c
@@ -833,6 +833,17 @@ static void nvhost_virt_deinit(struct platform_device *dev)
 	}
 }
 
+static int nvhost_check_valid_config(void)
+{
+	if ((nvhost_get_channel_policy() == MAP_CHANNEL_ON_OPEN &&
+		  nvhost_get_syncpt_policy() == SYNCPT_PER_CHANNEL) ||
+	    (nvhost_get_channel_policy() == MAP_CHANNEL_ON_SUBMIT &&
+		  nvhost_get_syncpt_policy() == SYNCPT_PER_CHANNEL_INSTANCE))
+		return 0;
+
+	return -EINVAL;
+}
+
 long linsim_cl = 0;
 static int nvhost_probe(struct platform_device *dev)
 {
@@ -906,6 +917,12 @@ static int nvhost_probe(struct platform_device *dev)
 	 * by nvhost_master later */
 	memcpy(&host->info, pdata->private_data,
 			sizeof(struct host1x_device_info));
+
+	err = nvhost_check_valid_config();
+	if (err) {
+		dev_err(&dev->dev, "booting with invalid config. err:%d", err);
+		return err;
+	}
 
 	pdata->pdev = dev;
 

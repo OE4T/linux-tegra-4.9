158684f6fb4822f8f968e1d319884dc725a648a6
Aly Hirani <ahirani@nvidia.com>
2015-02-05 12:37:11 -0800

    net: usb: r8152: disable autosuspend
    We've seen some issues with the Tegra XUSB controller resuming from
    autosuspend. This change disables autosuspend until the XUSB team can
    understand the issue
    
    Bug 1604791
    Bug 200091204
    Bug 200220865
    Bug 1947539
    Bug 200338333
    
    Signed-off-by: Aly Hirani <ahirani@nvidia.com>
    Reviewed-on: http://git-master/r/681672
    (cherry picked from commit fb40b49a54b23e3d592f69170172000d3a08e6b7)
    Reviewed-on: http://git-master/r/744734
    
    Change-Id: I25af96c612c6d353bf69fe6e175552087f517d91
    Signed-off-by: Shreshtha SAHU <ssahu@nvidia.com>
    Reviewed-on: http://git-master/r/1207722
    (cherry-picked from commit 0c9f3fd5dd6d91498339849e5a9746dda992fd95 in rel-28)
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1514001
    (cherry picked from commit 0014df2cf6fcb65934fa52c62bfd9e4668abe4cb in dev-kernel-4.4)
    (swarren, re-applied change to v2.09 driver, hence c-p within same branch)
    Reviewed-on: https://git-master.nvidia.com/r/1546955
    (cherry picked from commit b57b77bd7e16102a7be40e33ca0870c1121cfa0f in dev-kernel-4.9)
    Reviewed-on: https://git-master.nvidia.com/r/1555843
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -7838,6 +7838,8 @@ static int rtl8152_probe(struct usb_interface *intf,
 
 	/* usb_enable_autosuspend(udev); */
 
+	usb_disable_autosuspend(udev);
+
 	netif_info(tp, probe, netdev, "%s\n", DRIVER_VERSION);
 	netif_info(tp, probe, netdev, "%s\n", PATENTS);
 

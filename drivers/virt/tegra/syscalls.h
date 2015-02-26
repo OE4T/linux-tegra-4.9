f76c390bc0e1750d33d08017fe07248ae0e210e8
Peter Newman <pnewman@nvidia.com>
2015-02-25 17:51:25 -0800

    t210: hv: add armv8 hypervisor support
    Add new hypercall wrappers using the ARMv8 hypervisor's calling
    convention.
    
    Bug 1597311
    
    Change-Id: I5169ff49691984d122b50c18692410a3b3c24428
    Signed-off-by: Peter Newman <pnewman@nvidia.com>
    Reviewed-on: http://git-master/r/711443
    Reviewed-on: http://git-master/r/1198236
    Reviewed-on: http://git-master/r/1314804
    Reviewed-by: Aniruddha Banerjee <aniruddhab@nvidia.com>
    Tested-by: Aniruddha Banerjee <aniruddhab@nvidia.com>
    Reviewed-by: Bahadir Balban <bbalban@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Timo Alho <talho@nvidia.com>

--- drivers/virt/tegra/syscalls.h
+++ drivers/virt/tegra/syscalls.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (C) 2014-2015, NVIDIA CORPORATION. All rights reserved.
  *
  * Hypervisor interfaces
  *
@@ -34,12 +34,16 @@
 #ifndef __VMM_SYSCALLS_H__
 #define __VMM_SYSCALLS_H__
 
-#define HVC_NR_READ_STAT	1
-#define HVC_NR_READ_IVC		2
-#define HVC_NR_READ_GID		3
-#define HVC_NR_RAISE_IRQ	4
-#define HVC_NR_READ_NGUESTS	5
-#define HVC_NR_READ_IPA_PA	6
+#define HVC_NR_READ_STAT		1
+#define HVC_NR_READ_IVC			2
+#define HVC_NR_READ_GID			3
+#define HVC_NR_RAISE_IRQ		4
+#define HVC_NR_READ_NGUESTS		5
+#define HVC_NR_READ_IPA_PA		6
+#define HVC_NR_READ_GUEST_RESET_MASK	7
+#define HVC_NR_ACK_GUEST_CLEANUP	8
+#define HVC_NR_READ_HYP_INFO		9
+#define HVC_NR_GUEST_RESET		10
 
 #define GUEST_PRIMARY		0
 #define GUEST_IVC_SERVER	0
@@ -111,6 +115,143 @@ struct hyp_ipa_pa_info {
 	uint64_t size;       /* size of pa region */
 };
 
+#ifdef CONFIG_ARM64
+
+#define _X4_X17 "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", \
+"x13", "x14", "x15", "x16", "x17"
+
+static inline int hyp_read_gid(unsigned int *gid)
+{
+	register uint64_t r0 asm("x0");
+	register uint64_t r1 asm("x1");
+
+	asm("hvc %2"
+		: "=r"(r0), "=r"(r1)
+		: "i"(HVC_NR_READ_GID)
+		: "x2", "x3", _X4_X17);
+
+	*gid = r1;
+	return (int)r0;
+}
+
+static inline int hyp_read_nguests(unsigned int *nguests)
+{
+	register uint64_t r0 asm("x0");
+	register uint64_t r1 asm("x1");
+
+	asm("hvc %2"
+		: "=r"(r0), "=r"(r1)
+		: "i"(HVC_NR_READ_NGUESTS)
+		: "x2", "x3", _X4_X17);
+
+	*nguests = r1;
+	return (int)r0;
+}
+
+static inline int hyp_read_ivc_info(uint64_t *ivc_info_page_pa)
+{
+	register uint64_t r0 asm("x0");
+	register uint64_t r1 asm("x1");
+
+	asm("hvc %2"
+		: "=r"(r0), "=r"(r1)
+		: "i"(HVC_NR_READ_IVC)
+		: "x2", "x3", _X4_X17);
+
+	*ivc_info_page_pa = r1;
+	return (int)r0;
+}
+
+static inline int hyp_read_ipa_pa_info(struct hyp_ipa_pa_info *info,
+		unsigned int guestid, uint64_t ipa)
+{
+	register uint64_t r0 asm("x0") = guestid;
+	register uint64_t r1 asm("x1") = ipa;
+	register uint64_t r2 asm("x2");
+	register uint64_t r3 asm("x3");
+
+
+	asm("hvc %4"
+		: "+r"(r0), "+r"(r1), "=r"(r2), "=r"(r3)
+		: "i"(HVC_NR_READ_IPA_PA)
+		: _X4_X17);
+
+	info->base = r1;
+	info->offset = r2;
+	info->size = r3;
+
+	return (int)r0;
+}
+
+static inline int hyp_raise_irq(unsigned int irq, unsigned int vmid)
+{
+	register uint64_t r0 asm("x0") = irq;
+	register uint64_t r1 asm("x1") = vmid;
+
+	asm volatile("hvc %1"
+		: "+r"(r0)
+		: "i"(HVC_NR_RAISE_IRQ), "r"(r1)
+		: "x2", "x3", _X4_X17);
+
+	return (int)r0;
+}
+
+static inline int hyp_read_guest_reset_mask(unsigned int *guest_reset_mask)
+{
+	register uint64_t r0 asm("x0");
+	register uint64_t r1 asm("x1");
+
+	asm("hvc %2"
+		: "=r"(r0), "=r"(r1)
+		: "i"(HVC_NR_READ_GUEST_RESET_MASK)
+		: "x2", "x3", _X4_X17);
+
+	*guest_reset_mask = r1;
+	return (int)r0;
+}
+
+static inline int hyp_ack_guest_cleanup(unsigned int vmid)
+{
+	register uint64_t r0 asm("x0") = vmid;
+
+	asm volatile("hvc %1"
+		: "+r"(r0)
+		: "i"(HVC_NR_ACK_GUEST_CLEANUP)
+		: "x1", "x2", "x3", _X4_X17);
+
+	return (int)r0;
+}
+
+static inline int hyp_read_hyp_info(uint64_t *hyp_info_page_pa)
+{
+	register uint64_t r0 asm("x0");
+	register uint64_t r1 asm("x1");
+
+	asm("hvc %2"
+		: "=r"(r0), "=r"(r1)
+		: "i"(HVC_NR_READ_HYP_INFO)
+		: "x2", "x3", _X4_X17);
+
+	*hyp_info_page_pa = r1;
+	return (int)r0;
+}
+
+static inline int hyp_guest_reset(unsigned int vmid)
+{
+	register uint64_t r0 asm("x0") = vmid;
+
+	asm volatile("hvc %1"
+		: "+r"(r0)
+		: "i"(HVC_NR_GUEST_RESET)
+		: "x1", "x2", "x3", _X4_X17);
+
+	return (int)r0;
+}
+
+#undef _X4_X17
+
+#else
+
 int hyp_read_gid(unsigned int *gid);
 int hyp_read_nguests(unsigned int *nguests);
 int hyp_read_ivc_info(uint64_t *ivc_info_page_pa);
@@ -123,6 +264,8 @@ extern int hvc_read_ivc_info(int *);
 extern int hvc_read_ipa_pa_info(void *, int guestid, uint64_t ipa);
 extern int hvc_read_nguests(void *);
 extern int hvc_raise_irq(unsigned int irq, unsigned int vmid);
+
+#endif /* CONFIG_ARCH_ARM64 */
 
 #endif /* !__ASSEMBLY__ */
 

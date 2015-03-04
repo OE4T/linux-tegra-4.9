fee388c5233dd99690fdf39673aa839981a34653
Amit Sharma (SW-TEGRA) <amisharma@nvidia.com>
2015-03-04 10:17:00 +0530

    mm: disable kswapd for normal zone only when ZRAM is disabled
    When ZRAM is enabled kswapd should not be disabled for any zone.
    
    Bug 1555561
    
    Change-Id: Iee5c8b47ace8125274fb6e9e15197e4f7aa0dbde
    Signed-off-by: Amit Sharma (SW-TEGRA) <amisharma@nvidia.com>
    Reviewed-on: http://git-master/r/713025
    Reviewed-on: http://git-master/r/723367
    (cherry picked from commit 365305cba6f33b045127511054c1bbeed750ae11)
    Reviewed-on: http://git-master/r/727523
    Reviewed-on: http://git-master/r/1115947
    (cherry picked from commit 5714b47c9823da126e8ace237a6bff893f5e27cc)
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1330464
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3500,11 +3500,18 @@ void wakeup_kswapd(struct zone *zone, int order, enum zone_type classzone_idx)
 	pg_data_t *pgdat;
 	int z;
 
+	/*
+	 * kswapd can be disabled for normal zone only when ZRAM is
+	 * disabled.
+	 */
+
+	if(!total_swap_pages) {
 #ifdef CONFIG_ZONE_DMA32
 	/* Avoid Normal zone balancing when DMA32 zone exist. */
-	if (classzone_idx == ZONE_NORMAL)
-		return;
+		if (classzone_idx == ZONE_NORMAL)
+			return;
 #endif
+	}
 
 	if (!managed_zone(zone))
 		return;

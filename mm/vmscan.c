cf68a0a71e9c2fcc2e25d674a691a9603a7b829d
Amit Sharma (SW-TEGRA) <amisharma@nvidia.com>
2015-03-04 14:56:04 +0530

    mm: disable kswapd for ZONE_HIGHMEM when ZRAM is disabled
    Bug 1555561
    
    Change-Id: Ic9a22e47e830faabb3afe66894c7d45c270b63b0
    Signed-off-by: Amit Sharma (SW-TEGRA) <amisharma@nvidia.com>
    Reviewed-on: http://git-master/r/713715
    Reviewed-on: http://git-master/r/723371
     (cherry picked from Change-Id: Ic9a22e47e830faabb3afe66894c7d45c270b63b0)
    Reviewed-on: http://git-master/r/728893
    Reviewed-on: http://git-master/r/1115948
    (cherry picked from commit da8cabe1566fd3234f4e8fe3349be922332bddb8)
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1330465
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3500,12 +3500,14 @@ void wakeup_kswapd(struct zone *zone, int order, enum zone_type classzone_idx)
 	pg_data_t *pgdat;
 	int z;
 
-	/*
-	 * kswapd can be disabled for normal zone only when ZRAM is
-	 * disabled.
-	 */
-
+	/* Check whether ZRAM is disabled. */
 	if(!total_swap_pages) {
+		/* Avoid kswapd for HIGH MEMORY ZONE. */
+#ifdef CONFIG_HIGHMEM
+                if (classzone_idx == ZONE_HIGHMEM)
+                        return;
+#endif
+
 #ifdef CONFIG_ZONE_DMA32
 	/* Avoid Normal zone balancing when DMA32 zone exist. */
 		if (classzone_idx == ZONE_NORMAL)

6d89886030136fd4b0d0f8f3538d1bd979ec9dbe
Alankrita G <alankritag@nvidia.com>
2015-03-04 12:25:06 +0530

    suspend-resume ftrace parsing script
    - Can parse new ftrace file which had different timestamp format
    
    Bug 200071658
    
    Change-Id: I358a411b944e9a988e4c09617fe6d72403a6235a
    Signed-off-by: Alankrita G <alankritag@nvidia.com>
    Reviewed-on: http://git-master/r/713725

--- scripts/parse_suspend_resume_ftrace.py
+++ scripts/parse_suspend_resume_ftrace.py
@@ -19,27 +19,27 @@ with open("ftrace.txt", "r") as file:
         if "suspend_enter[3] begin" in line:
              cycle = cycle + 1
              fp.write("*******************Cycle " +str(cycle) +" starts******************\n")
-             cycle_start_time = float(line.split(" ...1   ")[1].split(":")[0])
+             cycle_start_time = float(line.split(" ...1")[1].replace(" ","").split(":")[0])
         if "suspend_enter[3] end" in line:
-             suspend_enter_end_time = float(line.split(" ...1   ")[1].split(":")[0])
+             suspend_enter_end_time = float(line.split(" ...1")[1].replace(" ","").split(":")[0])
              fp.write("     Suspend enter took       "+str((suspend_enter_end_time-cycle_start_time)*1000000) +" us\n")
              str1=str1 + "SUSPEND_ENTER took " + str(suspend_enter_end_time-cycle_start_time) +" secs\n"
 
         if "suspend_resume: dpm" in line:
             if "begin" in line:
                 try:
-                    statestart= float(line.split(" ...1   ")[1].split(":")[0])
+                    statestart= float(line.split(" ...1")[1].replace(" ","").split(":")[0])
                     statestartname=line.split("suspend_resume: ")[1].split('[')[0]
                 except IndexError:
-                    statestart= float(line.split("N.1   ")[1].split(":")[0])
+                    statestart= float(line.split("N.1")[1].replace(" ","").split(":")[0])
                     statestartname=line.split("suspend_resume: ")[1].split('[')[0]
 
             elif "end" in line:
                 try :
-                     stateend = float(line.split(" ...1   ")[1].split(":")[0])
+                     stateend = float(line.split(" ...1")[1].replace(" ","").split(":")[0])
                      stateendname=line.split("suspend_resume: ")[1].split('[')[0]
                 except IndexError:
-                     stateend = float(line.split("N.1   ")[1].split(":")[0])
+                     stateend = float(line.split("N.1")[1].replace(" ","").split(":")[0])
                      stateendname=line.split("suspend_resume: ")[1].split('[')[0]
                 if stateendname == statestartname:
                    str1 = str1 + stateendname.upper() + " took "+str(stateend - statestart) + " sec \n";
@@ -55,21 +55,24 @@ with open("ftrace.txt", "r") as file:
 
         if "device_pm_callback_start:" in line:
             try:
-                 dpm_start = float(line.split(" ...1   ")[1].split(":")[0])
+                 dpm_start = float(line.split(" ...1")[1].replace(" ","").split(":")[0])
             except IndexError:
-                 dpm_start = float(line.split(" .N.1   ")[1].split(":")[0])
+                 dpm_start = float(line.split(" .N.1")[1].replace(" ","").split(":")[0])
             driver_sname = line.split("device_pm_callback_start: ")[1].split(",")[0]
         if "device_pm_callback_end:" in line:
             try:
-                 dpm_end = float(line.split(" ...1   ")[1].split(":")[0])
+                 dpm_end = float(line.split(" ...1")[1].replace(" ","").split(":")[0])
             except:
-                 dpm_end = float(line.split(" .N.1   ")[1].split(":")[0])
+                 dpm_end = float(line.split(" .N.1")[1].replace(" ","").split(":")[0])
             driver_ename = line.split("device_pm_callback_end: ")[1].split(",")[0]
             if driver_ename == driver_sname:
                 fp.write("      " +driver_ename.ljust(50) + " ------>       " + str((dpm_end - dpm_start)*1000000) + " us \n")
 
         if "thaw_processes[0] end" in line:
-            cycle_end_time = float(line.split(" ...1   ")[1].split(":")[0])
+            try:
+                  cycle_end_time = float(line.split(" ...1")[1].replace(" ","").split(":")[0])
+            except IndexError:
+                  cycle_end_time = float(line.split(" .N.1")[1].replace(" ","").split(":")[0])
             fp.write("********************* Cycle " +str(cycle) +" ends***************\n")
             fp.write("Cycle " + str(cycle) + " took " + str(cycle_end_time - cycle_start_time) + " sec \n")
             fp.write(str1+"\n\n\n\n")
@@ -77,63 +80,63 @@ with open("ftrace.txt", "r") as file:
         if "CPU_OFF"  in line:
             if "begin" in line:
                  try:
-                     cpuoffstart=  float(line.split("..1   ")[1].split(":")[0])
+                     cpuoffstart=  float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     cpuoffstart =float(line.split("N.1   ")[1].split(":")[0])
+                     cpuoffstart =float(line.split("N.1")[1].replace(" ","").split(":")[0])
             elif "end" in line:
                  try:
-                    cpuoffend= float(line.split("..1   ")[1].split(":")[0])
+                    cpuoffend= float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     cpuoffend =float(line.split("N.1   ")[1].split(":")[0])
+                     cpuoffend =float(line.split("N.1")[1].replace(" ","").split(":")[0])
                  str1 = str1 + "CPUOFF took " +str(cpuoffend-cpuoffstart) +" secs\n"
 
         if "machine_suspend"  in line:
             if "begin" in line:
                  try:
-                     mcsusstart=  float(line.split("..1   ")[1].split(":")[0])
+                     mcsusstart=  float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     mcsusstart =float(line.split("N.1   ")[1].split(":")[0])
+                     mcsusstart =float(line.split("N.1")[1].replace(" ","").split(":")[0])
             elif "end" in line:
                  try:
-                    mcsusend= float(line.split("..1   ")[1].split(":")[0])
+                    mcsusend= float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     mcsusend =float(line.split("N.1   ")[1].split(":")[0])
+                     mcsusend =float(line.split("N.1")[1].replace(" ","").split(":")[0])
                  str1 = str1 + "MACHINE SUSPEND  took " +str(mcsusend-mcsusstart) +" secs\n"
 
         if "CPU_ON" in line:
             if "begin" in line:
                  try:
-                     cpuonstart=  float(line.split("..1   ")[1].split(":")[0])
+                     cpuonstart=  float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     cpuonstart =float(line.split("N.1   ")[1].split(":")[0])
+                     cpuonstart =float(line.split("N.1")[1].replace(" ","").split(":")[0])
             elif "end" in line:
                  try:
-                    cpuonend= float(line.split("..1   ")[1].split(":")[0])
+                    cpuonend= float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     cpuonend =float(line.split("N.1   ")[1].split(":")[0])
+                     cpuonend =float(line.split("N.1")[1].replace(" ","").split(":")[0])
                  str1 = str1 + "CPU_ON took " +str(cpuonend-cpuonstart) +" secs\n"
 
         if "syscore_resume"  in line:
             if "begin" in line:
                  try:
-                     sysresstart=  float(line.split("..1   ")[1].split(":")[0])
+                     sysresstart=  float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     sysresstart =float(line.split("N.1   ")[1].split(":")[0])
+                     sysresstart =float(line.split("N.1")[1].replace(" ","").split(":")[0])
             elif "end" in line:
                  try:
-                    sysresend= float(line.split("..1   ")[1].split(":")[0])
+                    sysresend= float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     sysresend =float(line.split("N.1   ")[1].split(":")[0])
+                     sysresend =float(line.split("N.1")[1].replace(" ","").split(":")[0])
                  str1 = str1 + "SYSTEM CORE RESUME took " +str(sysresend-sysresstart) +" secs\n"
         if "syscore_suspend"  in line:
             if "begin" in line:
                  try:
-                     syssusstart=  float(line.split("..1   ")[1].split(":")[0])
+                     syssusstart=  float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     syssusstart =float(line.split("N.1   ")[1].split(":")[0])
+                     syssusstart =float(line.split("N.1")[1].replace(" ","").split(":")[0])
             elif "end" in line:
                  try:
-                    syssusend= float(line.split("..1   ")[1].split(":")[0])
+                    syssusend= float(line.split("..1")[1].replace(" ","").split(":")[0])
                  except IndexError:
-                     syssusend =float(line.split("N.1   ")[1].split(":")[0])
+                     syssusend =float(line.split("N.1")[1].replace(" ","").split(":")[0])
                  str1 = str1 + "SYSTEM CORE SUSPEND took " +str(syssusend-syssusstart) +" secs\n"

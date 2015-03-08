e2c8bfe23866b12e185038df7e426088b11d614e
Erik Lilliebjerg <elilliebjerg@nvidia.com>
2015-03-08 07:34:56 -0700

    sensors: Add hybrid HW/SW integration time.
    Fix ALS reporting wrong values. The new patch introduces hybrid mode capability and also applies a scaling factor.
    
    Bug 1617955
    
    Change-Id: I18f5bbe401d0b10463d578340354451e81544a7c
    Signed-off-by: Erik Lilliebjerg <elilliebjerg@nvidia.com>
    Reviewed-on: http://git-master/r/715454
    (cherry picked from commit 441b9b2257396c8f32316ece4ddaa1a5dea7e671)
    Reviewed-on: http://git-master/r/715028

--- drivers/iio/light/nvs_jsa1127.c
+++ drivers/iio/light/nvs_jsa1127.c
@@ -29,21 +29,15 @@
 
 #define JSA_VENDOR			"SolteamOpto"
 #define JSA_NAME			"jsa1127"
-#define JSA_LIGHT_VERSION		(1)
-#define JSA_LIGHT_MAX_RANGE_IVAL	(109000)
-#define JSA_LIGHT_MAX_RANGE_MICRO	(0)
-#define JSA_LIGHT_RESOLUTION_IVAL	(0)
-#define JSA_LIGHT_RESOLUTION_MICRO	(210000)
+#define JSA_LIGHT_VERSION		(2)
 #define JSA_LIGHT_MILLIAMP_IVAL		(0)
-#define JSA_LIGHT_MILLIAMP_MICRO	(90000)
+#define JSA_LIGHT_MILLIAMP_FVAL		(90000)
 #define JSA_LIGHT_SCALE_IVAL		(0)
 #define JSA_LIGHT_SCALE_MICRO		(10000)
-#define JSA_LIGHT_OFFSET_IVAL		(0)
-#define JSA_LIGHT_OFFSET_MICRO		(0)
-#define JSA_LIGHT_THRESHOLD_LO		(100)
-#define JSA_LIGHT_THRESHOLD_HI		(100)
-#define JSA_POLL_DLY_MS_MIN		(800)
+#define JSA_LIGHT_THRESHOLD_LO		(10)
+#define JSA_LIGHT_THRESHOLD_HI		(10)
 #define JSA_POLL_DLY_MS_MAX		(4000)
+#define JSA_HW_DATA_MASK		(0x7FFF)
 /* HW registers */
 #define JSA_CMD_SHUTDOWN		(0x80)
 #define JSA_CMD_EN_CONT			(0x0C)
@@ -67,7 +61,7 @@ static unsigned short jsa_i2c_addrs[] = {
 };
 
 static struct nvs_light_dynamic jsa_nld_tbl[] = {
-	{{0, 210000}, {6500,   0}, {0, 90000}, 800, 0},
+	{{6, 610000}, {212000,   0}, {0, 90000}, 800, 0},
 	{{0, 420000}, {13000,  0}, {0, 90000}, 400, 0},
 	{{0, 560000}, {18000,  0}, {0, 90000}, 300, 0},
 	{{0, 830000}, {27000,  0}, {0, 90000}, 200, 0},
@@ -83,22 +77,20 @@ struct jsa_state {
 	struct delayed_work dw;
 	struct regulator_bulk_data vreg[ARRAY_SIZE(jsa_vregs)];
 	struct nvs_light light;
-	unsigned int sts;		/* debug flags */
+	struct nvs_light_dynamic hnld[2]; /* hybrid NLD table */
+	unsigned int sts;		/* status flags */
 	unsigned int errs;		/* error count */
 	unsigned int enabled;		/* enable status */
-	bool iio_ts_en;			/* use IIO timestamps */
 	bool hw_it;			/* HW defined integration time */
+	bool hnld_it;			/* hybrid NLD table enable flag */
 	u16 i2c_addr;			/* I2C address */
 	u8 rc_cmd;			/* store for register dump */
 };
 
 
-static s64 jsa_get_time_ns(struct jsa_state *st)
+static s64 jsa_get_time_ns(void)
 {
 	struct timespec ts;
-
-	if (st->iio_ts_en)
-		return iio_get_time_ns();
 
 	ktime_get_ts(&ts);
 	return timespec_to_ns(&ts);
@@ -212,6 +204,7 @@ static int jsa_start(struct jsa_state *st)
 
 	if (st->hw_it) {
 		ret = jsa_i2c_wr(st, JSA_CMD_EN_CONT);
+
 	} else {
 		ret = jsa_i2c_wr(st, JSA_CMD_EN);
 		ret |= jsa_i2c_wr(st, JSA_CMD_START);
@@ -223,7 +216,10 @@ static unsigned int jsa_sleep(struct jsa_state *st)
 {
 	unsigned int ms;
 
-	ms = st->light.delay_us * 1000;
+	if (st->hw_it)
+		return st->light.poll_delay_ms;
+
+	ms = st->light.delay_us / 1000;
 	if (ms > st->light.poll_delay_ms) {
 		jsa_i2c_wr(st, JSA_CMD_SHUTDOWN);
 		ms -= st->light.poll_delay_ms;
@@ -238,6 +234,7 @@ static int jsa_rd(struct jsa_state *st)
 {
 	s64 ts;
 	u16 hw;
+	unsigned int i;
 	int ret;
 
 	ret = jsa_i2c_rd(st, &hw);
@@ -249,28 +246,50 @@ static int jsa_rd(struct jsa_state *st)
 		return -EINVAL;
 
 	hw &= ~(1 << JSA_VAL_VALID);
-	ts = jsa_get_time_ns(st);
+	ts = jsa_get_time_ns();
 	if (st->sts & NVS_STS_SPEW_DATA)
 		dev_info(&st->i2c->dev,
-			 "poll light hw %hu %lld  diff=%d %lldns  index=%u\n",
-			 hw, ts, hw - st->light.hw, ts - st->light.timestamp,
-			 st->light.nld_i);
+			 "%s hw: %hu %lld diff: %d %lldns nld_i(index)=%u hw_it=%x\n",
+			 __func__, hw, ts, hw - st->light.hw,
+			 ts - st->light.timestamp, st->light.nld_i, st->hw_it);
 	st->light.hw = hw;
 	st->light.timestamp = ts;
 	nvs_light_read(&st->light);
+	if (st->light.nld_i_change && st->hnld_it &&
+						   !st->light.calibration_en) {
+		/* hybrid mode SW/HW IT switching */
+		i = st->light.nld_i;
+		if (st->hnld[i].driver_data) {
+			st->hw_it = false;
+			if (i == 0) {
+				/* SW IT is for the low range */
+				st->light.hw_mask = st->hnld[i].max_range.ival;
+				st->cfg.thresh_lo = 1;
+				st->cfg.thresh_hi = 1;
+			}
+		} else {
+			st->hw_it = true;
+			st->light.hw_mask = JSA_HW_DATA_MASK;
+			st->cfg.thresh_lo = JSA_LIGHT_THRESHOLD_LO;
+			st->cfg.thresh_hi = JSA_LIGHT_THRESHOLD_HI;
+		}
+		jsa_start(st);
+	}
 	return 0;
 }
 
 static void jsa_read(struct jsa_state *st)
 {
-	unsigned int ms = st->light.poll_delay_ms;
+	unsigned int ms;
 	int ret;
 
 	st->nvs->mutex_lock(st->nvs_data);
 	if (st->enabled) {
 		if (st->hw_it) {
 			jsa_rd(st);
+			ms = st->light.poll_delay_ms;
 		} else {
+			ms = st->light.poll_delay_ms;
 			if (st->rc_cmd == JSA_CMD_START) {
 				ret = jsa_i2c_wr(st, JSA_CMD_STOP);
 				if (!ret) {
@@ -285,6 +304,10 @@ static void jsa_read(struct jsa_state *st)
 			}
 		}
 		schedule_delayed_work(&st->dw, msecs_to_jiffies(ms));
+		if (st->sts & NVS_STS_SPEW_MSG)
+			dev_info(&st->i2c->dev,
+				 "%s schedule_delayed_work=%ums\n",
+				 __func__, ms);
 	}
 	st->nvs->mutex_unlock(st->nvs_data);
 }
@@ -293,7 +316,10 @@ static void jsa_work(struct work_struct *ws)
 {
 	struct jsa_state *st = container_of((struct delayed_work *)ws,
 					    struct jsa_state, dw);
-
+	if (!(st->hw_it))
+		st->light.nld_i = 0;
+	else
+		st->light.nld_i = 1;
 	jsa_read(st);
 }
 
@@ -312,6 +338,7 @@ static int jsa_enable(void *client, int snsr_id, int enable)
 {
 	struct jsa_state *st = (struct jsa_state *)client;
 	int ret;
+	dev_info(&st->i2c->dev, "%s\n", __func__);
 
 	if (enable < 0)
 		return st->enabled;
@@ -324,10 +351,10 @@ static int jsa_enable(void *client, int snsr_id, int enable)
 			if (ret) {
 				jsa_disable(st);
 			} else {
-				st->enabled = 1;
+				st->enabled = enable;
 				schedule_delayed_work(&st->dw,
-					      msecs_to_jiffies(st->light.
-							       poll_delay_ms));
+						      usecs_to_jiffies(st->cfg.
+								delay_us_min));
 			}
 		}
 	} else {
@@ -337,15 +364,28 @@ static int jsa_enable(void *client, int snsr_id, int enable)
 }
 
 static int jsa_batch(void *client, int snsr_id, int flags,
-		     unsigned int period, unsigned int timeout)
+		     unsigned int period_us, unsigned int timeout_us)
 {
 	struct jsa_state *st = (struct jsa_state *)client;
 
-	if (timeout)
+	if (st->hnld_it && st->light.calibration_en) {
+		/* mechanism to test the hybrid SW integration time */
+		if (timeout_us) {
+			st->hw_it = false;
+			if (st->hnld[0].driver_data)
+				st->hnld[0].delay_min_ms = timeout_us;
+			else
+				st->hnld[1].delay_min_ms = timeout_us;
+		} else {
+			st->hw_it = true;
+		}
+		jsa_start(st);
+	} else if (timeout_us) {
 		/* timeout not supported (no HW FIFO) */
 		return -EINVAL;
+	}
 
-	st->light.delay_us = period;
+	st->light.delay_us = period_us;
 	return 0;
 }
 
@@ -363,10 +403,27 @@ static int jsa_regs(void *client, int snsr_id, char *buf)
 	return t;
 }
 
+static int jsa_nvs_write(void *client, int snsr_id, unsigned int nvs)
+{
+	if (nvs)
+		return -EINVAL;
+
+	return 0;
+}
+
+static int jsa_nvs_read(void *client, int snsr_id, char *buf)
+{
+	struct jsa_state *st = (struct jsa_state *)client;
+
+	return nvs_light_dbg(&st->light, buf);
+}
+
 static struct nvs_fn_dev jsa_fn_dev = {
 	.enable				= jsa_enable,
 	.batch				= jsa_batch,
 	.regs				= jsa_regs,
+	.nvs_write			= jsa_nvs_write,
+	.nvs_read			= jsa_nvs_read,
 };
 
 static int jsa_suspend(struct device *dev)
@@ -474,19 +531,10 @@ static struct sensor_cfg jsa_cfg_dflt = {
 	.part			= JSA_NAME,
 	.vendor			= JSA_VENDOR,
 	.version		= JSA_LIGHT_VERSION,
-	.max_range		= {
-		.ival		= JSA_LIGHT_MAX_RANGE_IVAL,
-		.fval		= JSA_LIGHT_MAX_RANGE_MICRO,
-	},
-	.resolution		= {
-		.ival		= JSA_LIGHT_RESOLUTION_IVAL,
-		.fval		= JSA_LIGHT_RESOLUTION_MICRO,
-	},
 	.milliamp		= {
 		.ival		= JSA_LIGHT_MILLIAMP_IVAL,
-		.fval		= JSA_LIGHT_MILLIAMP_MICRO,
+		.fval		= JSA_LIGHT_MILLIAMP_FVAL,
 	},
-	.delay_us_min		= JSA_POLL_DLY_MS_MIN * 1000,
 	.delay_us_max		= JSA_POLL_DLY_MS_MAX * 1000,
 	.scale			= {
 		.ival		= JSA_LIGHT_SCALE_IVAL,
@@ -499,36 +547,86 @@ static struct sensor_cfg jsa_cfg_dflt = {
 static int jsa_of_dt(struct jsa_state *st, struct device_node *dn)
 {
 	unsigned int i;
-
+	int ret = 0;
 	/* default NVS programmable parameters */
 	memcpy(&st->cfg, &jsa_cfg_dflt, sizeof(st->cfg));
 	st->light.cfg = &st->cfg;
-	st->light.hw_mask = 0x7FFF;
+	st->light.hw_mask = JSA_HW_DATA_MASK;
 	st->light.nld_tbl = jsa_nld_tbl;
 	/* device tree parameters */
-	if (dn)
-		/* common NVS programmable parameters */
-		st->iio_ts_en = of_property_read_bool(dn, "iio_timestamps");
 	/* common NVS parameters */
 	nvs_of_dt(dn, &st->cfg, NULL);
 	/* this device supports these programmable parameters */
-	if (nvs_light_of_dt(&st->light, dn, NULL)) {
+	ret = nvs_light_of_dt(&st->light, dn, NULL);
+	if (ret) {
 		/* default is HW IT */
 		st->light.nld_i_lo = 0;
 		st->light.nld_i_hi = 0;
 	}
 	if (st->light.nld_i_lo == st->light.nld_i_hi) {
-		/* HW IT enabled when indexes are the same */
+		/* HW IT is enabled when indexes are the same.
+		 * The values used are those in the jsa_nld_tbl pointed to by
+		 * the common index (both nld_i_lo and nld_i_hi).
+		 */
+		st->light.nld_i = st->light.nld_i_lo;
 		st->hw_it = true;
-		st->light.nld_tbl = NULL; /* disable dynamic resolution */
+		/* disable dynamic resolution */
+		st->light.nld_tbl = NULL;
+
+		/* Initialize default hybrid parameter table	*/
+		/* memcpy(&st->hnld[0], &jsa_hnld_tbl, sizeof(st->hnld[0]));*/
+		/* Overlay device-specific hybrid parameters, where necessary*/
+		if (dn) {
+			of_property_read_u32(dn, "light_hyb_maxrange_ival",
+					     &st->hnld[0].max_range.ival);
+			of_property_read_u32(dn, "light_hyb_maxrange_fval",
+					     &st->hnld[0].max_range.fval);
+			of_property_read_u32(dn, "light_hyb_resolution_ival",
+					     &st->hnld[0].resolution.ival);
+			of_property_read_u32(dn, "light_hyb_resolution_fval",
+					     &st->hnld[0].resolution.fval);
+			of_property_read_u32(dn, "light_hyb_interval",
+					     &st->hnld[0].delay_min_ms);
+		}
+
 	}
-	i = st->light.nld_i_lo;
+	i = st->light.nld_i_lo; /* use lowest resolution */
 	st->cfg.resolution.ival = jsa_nld_tbl[i].resolution.ival;
 	st->cfg.resolution.fval = jsa_nld_tbl[i].resolution.fval;
-	i = st->light.nld_i_hi;
+	st->cfg.delay_us_min = jsa_nld_tbl[i].delay_min_ms * 1000;
+	/* test if hyrbrid mode enabled */
+	if (st->hnld[0].max_range.ival || st->hnld[0].max_range.fval) {
+		/* Hybrid mode uses a both HW integration time (hw_it = true)
+		 * and SW integration time.  It's used to help obtain the low
+		 * or high range values of the fixed HW integration time.
+		 * HW IT uses values from the fixed indexed jsa_nld_tbl.
+		 * SW IT uses a custom entry in the hybrid nvs_light_dynamic
+		 * table st->hnld before or after the HW IT entry.
+		 * Therefore, this hybrid dynamic resolution/max_range table
+		 * has only two entries.
+		 */
+		st->hnld[0].milliamp.ival = JSA_LIGHT_MILLIAMP_IVAL;
+		st->hnld[0].milliamp.fval = JSA_LIGHT_MILLIAMP_FVAL;
+		st->hnld[0].driver_data = 1; /* flag the SW entry */
+		/* copy SW IT entry to other entry */
+		memcpy(&st->hnld[1], &st->hnld[0], sizeof(st->hnld[1]));
+		/* copy the HW IT to hybrid nld table in range order */
+		if (jsa_nld_tbl[st->light.nld_i].max_range.ival <
+						    st->hnld[0].max_range.ival)
+			i = 0;
+		else
+			i = 1;
+		memcpy(&st->hnld[i], &jsa_nld_tbl[st->light.nld_i],
+		       sizeof(st->hnld[i]));
+		st->light.nld_i = i;
+		st->light.nld_i_lo = 0;
+		st->light.nld_i_hi = 1;
+		st->light.nld_tbl = st->hnld;
+		st->hnld_it = true;
+	}
+	i = st->light.nld_i_hi; /* use highest range & delay */
 	st->cfg.max_range.ival = jsa_nld_tbl[i].max_range.ival;
 	st->cfg.max_range.fval = jsa_nld_tbl[i].max_range.fval;
-	st->cfg.delay_us_min = jsa_nld_tbl[i].delay_min_ms * 1000;
 	return 0;
 }
 
@@ -622,3 +720,4 @@ module_i2c_driver(jsa_driver);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("JSA1127 driver");
 MODULE_AUTHOR("NVIDIA Corporation");
+

bb51cf9ec6482b50f3020179965ef82f58d91a0a
Supriya <ssharatkumar@nvidia.com>
2015-03-10 13:57:49 +0530

    gpu: nvgpu: Skip reg read of gpc2clk
    Bug 200066741
    
    As we are just getting out of reset and this reg is not
    written before, so we dont stand the risk of loosing
    any data
    
    Change-Id: Ifc1bcaa3c224038e4e2a47882a4523f7633cb660
    Signed-off-by: Supriya <ssharatkumar@nvidia.com>
    Reviewed-on: http://git-master/r/715652
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/priv_ring_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/priv_ring_gk20a.c
@@ -26,12 +26,14 @@
 
 void gk20a_reset_priv_ring(struct gk20a *g)
 {
-	u32 data;
+	u32 data = 0;
 
 	if (tegra_platform_is_linsim())
 		return;
 
-	data = gk20a_readl(g, trim_sys_gpc2clk_out_r());
+	/* Skipping read and then writeback to this reg, as we are just getting
+	* out of reset, and before this call, the reg is not written to*/
+
 	data = set_field(data,
 			trim_sys_gpc2clk_out_bypdiv_m(),
 			trim_sys_gpc2clk_out_bypdiv_f(0));

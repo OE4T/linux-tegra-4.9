InES-HPMM/linux-l4t

video: tegra: dc: swapinterval=0 flipping
tegra_dc_update_windows will perform either VSYNC or HSYNC updates
depending on the new parameter, wait_for_vblank.

Note that the module parameter no_vsync remains; setting it will
force swapinterval=0 flipping regardless of the window settings.

HSYNC updates will poll the hardware to wait until window updates
are complete, rather than using an ISR that checks, as is done in
VSYNC flipping.

bug 970235
bug 975494
bug 1158174
bug 1405190

Change-Id: Ib0bd7fee54d96bcdf6a04a82555a6bceb57fb369
Signed-off-by: Raghavendra VK <rvk@nvidia.com>
Reviewed-on: http://git-master/r/390461
(cherry picked from commit f3a1a80cadfc0fbb0555b33a8f7ac8478e2619b4)
Reviewed-on: http://git-master/r/356157
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Chao Xu <cxu@nvidia.com>
Reviewed-by: Jon Mayo <jmayo@nvidia.com>

drivers/video/tegra/dc/dc.c
@@ -1557,7 +1557,7 @@ crc_error:
 	return crc;
 }
 
-static bool tegra_dc_windows_are_dirty(struct tegra_dc *dc)
+bool tegra_dc_windows_are_dirty(struct tegra_dc *dc, u32 win_act_req_mask)
 {
 	u32 val;
 
@@ -1565,7 +1565,7 @@ static bool tegra_dc_windows_are_dirty(struct tegra_dc *dc)
 		return false;
 
 	val = tegra_dc_readl(dc, DC_CMD_STATE_CONTROL);
-	if (val & WIN_ALL_ACT_REQ)
+	if (val & (win_act_req_mask))
 		return true;
 
 	return false;
@@ -1689,7 +1689,7 @@ static void tegra_dc_vblank(struct work_struct *work)
 	tegra_dc_get(dc);
 
 	/* Clear the V_BLANK_FLIP bit of vblank ref-count if update is clean. */
-	if (!tegra_dc_windows_are_dirty(dc))
+	if (!tegra_dc_windows_are_dirty(dc, WIN_ALL_ACT_REQ))
 		clear_bit(V_BLANK_FLIP, &dc->vblank_ref_count);
 
 	/* Update the SD brightness */
@@ -1842,7 +1842,7 @@ static void tegra_dc_vpulse2(struct work_struct *work)
 	tegra_dc_get(dc);
 
 	/* Clear the V_PULSE2_FLIP if no update */
-	if (!tegra_dc_windows_are_dirty(dc))
+	if (!tegra_dc_windows_are_dirty(dc, WIN_ALL_ACT_REQ))
 		clear_bit(V_PULSE2_FLIP, &dc->vpulse2_ref_count);
 
 	/* Update the SD brightness */
@@ -2211,6 +2211,9 @@ static int tegra_dc_init(struct tegra_dc *dc)
 	tegra_dc_writel(dc, int_enable, DC_CMD_INT_ENABLE);
 	tegra_dc_writel(dc, ALL_UF_INT(), DC_CMD_INT_MASK);
 	tegra_dc_init_vpulse2_int(dc);
+
+	tegra_dc_writel(dc, WRITE_MUX_ASSEMBLY | READ_MUX_ASSEMBLY,
+		DC_CMD_STATE_ACCESS);
 
 #if !defined(CONFIG_TEGRA_DC_BLENDER_GEN2)
 	tegra_dc_writel(dc, 0x00000000, DC_DISP_BORDER_COLOR);
@@ -2600,7 +2603,7 @@ void tegra_dc_blank(struct tegra_dc *dc)
 		dcwins[i]->flags &= ~TEGRA_WIN_FLAG_ENABLED;
 	}
 
-	tegra_dc_update_windows(dcwins, DC_N_WINDOWS, NULL);
+	tegra_dc_update_windows(dcwins, DC_N_WINDOWS, NULL, true);
 	tegra_dc_sync_windows(dcwins, DC_N_WINDOWS);
 	tegra_dc_program_bandwidth(dc, true);
 }

OE4T/linux-tegra-4.9

drivers/video/tegra/dc/dc.c
@@ -1557,7 +1557,7 @@ crc_error:
 	return crc;
 }
 
-static bool tegra_dc_windows_are_dirty(struct tegra_dc *dc)
+bool tegra_dc_windows_are_dirty(struct tegra_dc *dc, u32 win_act_req_mask)
 {
 	u32 val;
 
@@ -1565,7 +1565,7 @@ static bool tegra_dc_windows_are_dirty(struct tegra_dc *dc)
 		return false;
 
 	val = tegra_dc_readl(dc, DC_CMD_STATE_CONTROL);
-	if (val & WIN_ALL_ACT_REQ)
+	if (val & (win_act_req_mask))
 		return true;
 
 	return false;
@@ -1689,7 +1689,7 @@ static void tegra_dc_vblank(struct work_struct *work)
 	tegra_dc_get(dc);
 
 	/* Clear the V_BLANK_FLIP bit of vblank ref-count if update is clean. */
-	if (!tegra_dc_windows_are_dirty(dc))
+	if (!tegra_dc_windows_are_dirty(dc, WIN_ALL_ACT_REQ))
 		clear_bit(V_BLANK_FLIP, &dc->vblank_ref_count);
 
 	/* Update the SD brightness */
@@ -1842,7 +1842,7 @@ static void tegra_dc_vpulse2(struct work_struct *work)
 	tegra_dc_get(dc);
 
 	/* Clear the V_PULSE2_FLIP if no update */
-	if (!tegra_dc_windows_are_dirty(dc))
+	if (!tegra_dc_windows_are_dirty(dc, WIN_ALL_ACT_REQ))
 		clear_bit(V_PULSE2_FLIP, &dc->vpulse2_ref_count);
 
 	/* Update the SD brightness */
@@ -2211,6 +2211,9 @@ static int tegra_dc_init(struct tegra_dc *dc)
 	tegra_dc_writel(dc, int_enable, DC_CMD_INT_ENABLE);
 	tegra_dc_writel(dc, ALL_UF_INT(), DC_CMD_INT_MASK);
 	tegra_dc_init_vpulse2_int(dc);
+
+	tegra_dc_writel(dc, WRITE_MUX_ASSEMBLY | READ_MUX_ASSEMBLY,
+		DC_CMD_STATE_ACCESS);
 
 #if !defined(CONFIG_TEGRA_DC_BLENDER_GEN2)
 	tegra_dc_writel(dc, 0x00000000, DC_DISP_BORDER_COLOR);
@@ -2607,7 +2610,7 @@ void tegra_dc_blank(struct tegra_dc *dc, unsigned windows)
 		dcwins[nr_win++]->flags &= ~TEGRA_WIN_FLAG_ENABLED;
 	}
 
-	tegra_dc_update_windows(dcwins, nr_win, NULL);
+	tegra_dc_update_windows(dcwins, nr_win, NULL, true);
 	tegra_dc_sync_windows(dcwins, nr_win);
 	tegra_dc_program_bandwidth(dc, true);
 }
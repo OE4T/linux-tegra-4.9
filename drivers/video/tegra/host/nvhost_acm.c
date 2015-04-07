64d17591c9acb8cbc08705a652408b958257bb3b
Sumit Singh <sumsingh@nvidia.com>
2015-04-07 13:59:32 +0530

    video: tegra: host: Read pwrgate partition-id from DT
    Make modifications, so that powergate partition-id
    is always read from device tree.
    
    Bug 200105664
    
    Change-Id: I93e2a53eab146b90686356c0906c6d81c9aacead
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Reviewed-on: http://git-master/r/728416
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/video/tegra/host/nvhost_acm.c
+++ drivers/video/tegra/host/nvhost_acm.c
@@ -653,6 +653,11 @@ int nvhost_module_init(struct platform_device *dev)
 	int i = 0, err = 0;
 	struct kobj_attribute *attr = NULL;
 	struct nvhost_device_data *pdata = platform_get_drvdata(dev);
+	int partition_id = -1;
+#ifdef CONFIG_PM_GENERIC_DOMAINS_OF
+	struct device_node *dn;
+	struct generic_pm_domain *gpd;
+#endif
 
 	/* initialize clocks to known state (=enabled) */
 	pdata->num_clks = 0;
@@ -713,26 +718,38 @@ int nvhost_module_init(struct platform_device *dev)
 		IS_ENABLED(CONFIG_PM_GENERIC_DOMAINS) &&
 		pdata->can_powergate;
 
+
+#ifdef CONFIG_PM_GENERIC_DOMAINS_OF
+	gpd = dev_to_genpd(&dev->dev);
+	if (!gpd)
+		return -EINVAL;
+
+	dn = gpd->of_node;
+	of_property_read_u32(dn, "partition-id", &partition_id);
+#else
+	partition_id = pdata->powergate_id;
+#endif
+
 	/* needed to WAR MBIST issue */
 	if (pdata->poweron_toggle_slcg) {
 		pdata->toggle_slcg_notifier.notifier_call =
 			&nvhost_module_toggle_slcg;
-		if (pdata->powergate_id != -1)
-			slcg_register_notifier(pdata->powergate_id,
+		if (partition_id != -1)
+			slcg_register_notifier(partition_id,
 					       &pdata->toggle_slcg_notifier);
 	}
 
 	/* Ensure that the above or device specific MBIST WAR gets applied */
 	if (pdata->poweron_toggle_slcg || pdata->slcg_notifier_enable) {
-		do_powergate_locked(pdata->powergate_id);
-		do_unpowergate_locked(pdata->powergate_id);
+		do_powergate_locked(partition_id);
+		do_unpowergate_locked(partition_id);
 	}
 
 	/* power gate units that we can power gate */
 	if (pdata->can_powergate) {
-		do_powergate_locked(pdata->powergate_id);
+		do_powergate_locked(partition_id);
 	} else {
-		do_unpowergate_locked(pdata->powergate_id);
+		do_unpowergate_locked(partition_id);
 	}
 
 	/* set pm runtime delays */
@@ -1122,14 +1139,24 @@ static int nvhost_module_suspend(struct device *dev)
 static int nvhost_module_power_on(struct generic_pm_domain *domain)
 {
 	struct nvhost_device_data *pdata;
+	int partition_id = -1;
+#ifdef CONFIG_PM_GENERIC_DOMAINS_OF
+	struct device_node *dn;
+#endif
 
 	pdata = container_of(domain, struct nvhost_device_data, pd);
+#ifdef CONFIG_PM_GENERIC_DOMAINS_OF
+	dn = domain->of_node;
+	of_property_read_u32(dn, "partition-id", &partition_id);
+#else
+	partition_id = pdata->powergate_id;
+#endif
 
 	mutex_lock(&pdata->lock);
 	if (pdata->can_powergate) {
 		trace_nvhost_module_power_on(pdata->pdev->name,
-			pdata->powergate_id);
-		do_unpowergate_locked(pdata->powergate_id);
+			partition_id);
+		do_unpowergate_locked(partition_id);
 	}
 
 	mutex_unlock(&pdata->lock);
@@ -1140,14 +1167,24 @@ static int nvhost_module_power_on(struct generic_pm_domain *domain)
 static int nvhost_module_power_off(struct generic_pm_domain *domain)
 {
 	struct nvhost_device_data *pdata;
+	int partition_id = -1;
+#ifdef CONFIG_PM_GENERIC_DOMAINS_OF
+	struct device_node *dn;
+#endif
 
 	pdata = container_of(domain, struct nvhost_device_data, pd);
+#ifdef CONFIG_PM_GENERIC_DOMAINS_OF
+	dn = domain->of_node;
+	of_property_read_u32(dn, "partition-id", &partition_id);
+#else
+	partition_id = pdata->powergate_id;
+#endif
 
 	mutex_lock(&pdata->lock);
 	if (pdata->can_powergate) {
 		trace_nvhost_module_power_off(pdata->pdev->name,
-			pdata->powergate_id);
-		do_powergate_locked(pdata->powergate_id);
+			partition_id);
+		do_powergate_locked(partition_id);
 	}
 	mutex_unlock(&pdata->lock);
 

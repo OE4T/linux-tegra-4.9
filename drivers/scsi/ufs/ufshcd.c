20a4ccd59f039de0c43c758fe6b576961157bb66
Naveen Kumar Arepalli <naveenk@nvidia.com>
2015-04-20 14:54:54 +0530

    scsi: ufs: Increase NOP,Query Request timeout values
    -Set NOP,Query Request timeout values to 100 msecs
    -With current value 30 msecs, NOP and Query Tests are failing.
    -Added a fixme note in the comment section.
    
    Bug 200098204
    
    Change-Id: Id8bc245e8b8d5b72f58de57bd556b9a5ced7492f
    Signed-off-by: Naveen Kumar Arepalli <naveenk@nvidia.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4,6 +4,7 @@
  * This code is based on drivers/scsi/ufs/ufshcd.c
  * Copyright (C) 2011-2013 Samsung India Software Operations
  * Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
  *
  * Authors:
  *	Santosh Yaraganavi <santosh.sy@samsung.com>
@@ -54,13 +55,16 @@
 
 /* NOP OUT retries waiting for NOP IN response */
 #define NOP_OUT_RETRIES    10
-/* Timeout after 30 msecs if NOP OUT hangs without response */
-#define NOP_OUT_TIMEOUT    30 /* msecs */
+/* Fix me: Need to fineTune the Value on silicon
+ * Timeout after 100 msecs if NOP OUT hangs without response
+ */
+
+#define NOP_OUT_TIMEOUT    100 /* msecs */
 
 /* Query request retries */
 #define QUERY_REQ_RETRIES 10
 /* Query request timeout */
-#define QUERY_REQ_TIMEOUT 30 /* msec */
+#define QUERY_REQ_TIMEOUT 100 /* msec */
 /*
  * Query request timeout for fDeviceInit flag
  * fDeviceInit query response time for some devices is too large that default

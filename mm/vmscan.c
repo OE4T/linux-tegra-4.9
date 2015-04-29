272d56003273fe40c03f7220588589778e3d4e7e
Sri Krishna chowdary <schowdary@nvidia.com>
2015-04-29 10:40:15 +0530

    mm: vmscan: avoid swapping in interrupt context
    pages can be swapped out from interrupt context as well.
    zram uses zsmalloc allocator to make room for these pages.
    But zsmalloc is not made to be used from interrupt context.
    This can result in a kernel OOps.
    
    Bug 200063583
    
    Change-Id: Ia74ccbdd5dd9b0713e5e5d170a0c6f0d8b8a895b
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/737108
    Reviewed-on: http://git-master/r/739416
    (cherry picked from commit 204ca8e304a872faa045177c8d9d13bc96e75b86)
    Reviewed-on: http://git-master/r/763439
    (cherry picked from commit 74185cd61bd30b86c196c4748621877e9b6f8203)
    Reviewed-on: http://git-master/r/1159927
    (cherry picked from commit 2313359f542b610cf4f4754d84c2e11d8dd3c9a2)
    Reviewed-on: http://git-master/r/1330470
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -269,6 +269,16 @@ unsigned long lruvec_lru_size(struct lruvec *lruvec, enum lru_list lru, int zone
 
 }
 
+/* If zram is being used as swap, and zram is using zsmalloc allocator
+ * then there is a potential bug when reclaim happens from interrupt
+ * context
+ */
+static void adjust_scan_control(struct scan_control *sc)
+{
+	if (in_interrupt() && IS_ENABLED(ZSMALLOC) && total_swap_pages)
+		sc->may_swap = 0;
+}
+
 /*
  * Add a shrinker callback to be called from the vm.
  */
@@ -1312,6 +1322,7 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 	unsigned long ret, dummy1, dummy2, dummy3, dummy4, dummy5;
 	struct page *page, *next;
 	LIST_HEAD(clean_pages);
+	adjust_scan_control(&sc);
 
 	list_for_each_entry_safe(page, next, page_list, lru) {
 		if (page_is_file_cache(page) && !PageDirty(page) &&
@@ -2963,6 +2974,8 @@ unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 		.may_swap = 1,
 	};
 
+	adjust_scan_control(&sc);
+
 	/*
 	 * Do not enter reclaim if fatal signal was delivered while throttled.
 	 * 1 is returned so that the page allocator does not OOM kill at this
@@ -2999,6 +3012,8 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 		.may_swap = !noswap,
 	};
 	unsigned long lru_pages;
+
+	adjust_scan_control(&sc);
 
 	sc.gfp_mask = (gfp_mask & GFP_RECLAIM_MASK) |
 			(GFP_HIGHUSER_MOVABLE & ~GFP_RECLAIM_MASK);
@@ -3043,6 +3058,7 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 		.may_swap = may_swap,
 	};
 
+	adjust_scan_control(&sc);
 	/*
 	 * Unlike direct reclaim via alloc_pages(), memcg's reclaim doesn't
 	 * take care of from where we get pages. So the node where we start the
@@ -3213,6 +3229,7 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int classzone_idx)
 		.may_unmap = 1,
 		.may_swap = 1,
 	};
+	adjust_scan_control(&sc);
 	count_vm_event(PAGEOUTRUN);
 
 	do {
@@ -3566,6 +3583,7 @@ unsigned long shrink_all_memory(unsigned long nr_to_reclaim)
 	struct task_struct *p = current;
 	unsigned long nr_reclaimed;
 
+	adjust_scan_control(&sc);
 	p->flags |= PF_MEMALLOC;
 	lockdep_set_current_reclaim_state(sc.gfp_mask);
 	reclaim_state.reclaimed_slab = 0;
@@ -3751,6 +3769,7 @@ static int __node_reclaim(struct pglist_data *pgdat, gfp_t gfp_mask, unsigned in
 		.reclaim_idx = classzone_idx,
 	};
 
+	adjust_scan_control(&sc);
 	cond_resched();
 	/*
 	 * We need to be able to allocate from the reserves for RECLAIM_UNMAP

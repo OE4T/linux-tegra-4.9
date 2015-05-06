75f5119996a13452fb7218a3e6876270233de840
Deepak Nibade <dnibade@nvidia.com>
2015-05-06 18:06:41 +0530

    video: tegra: host: use host1x_readl()/writel() for sync registers
    For T18x, sync_aperture = host->aperture (which was not the case
    for pre-T18x)
    
    hence, to access sync registers we can use host1x_readl()/writel()
    instead of host1x_sync_readl()/writel()
    
    Bug 1611482
    
    Change-Id: I5322fe894c90715e18a010442cf0c26f7833748a
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/741243
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>

--- drivers/video/tegra/host/host1x/host1x_syncpt_prot_t186.c
+++ drivers/video/tegra/host/host1x/host1x_syncpt_prot_t186.c
@@ -27,20 +27,20 @@ static void t186_syncpt_reset(struct nvhost_syncpt *sp, u32 id)
 	int min = nvhost_syncpt_read_min(sp, id);
 
 	/* move syncpoint to VM1 */
-	host1x_sync_writel(dev->dev, (host1x_sync_syncpt_vm_0_r() + id * 4), 1);
+	host1x_writel(dev->dev, (host1x_sync_syncpt_vm_0_r() + id * 4), 1);
 
 	/* reserve it for channel 63 */
-	host1x_sync_writel(dev->dev,
+	host1x_writel(dev->dev,
 		(host1x_sync_syncpt_ch_app_0_r() + id * 4),
 		host1x_sync_syncpt_ch_app_0_syncpt_ch_f(63));
 
 	/* enable protection */
-	host1x_sync_writel(dev->dev,
+	host1x_writel(dev->dev,
 		host1x_sync_syncpt_prot_en_0_r(),
 		host1x_sync_syncpt_prot_en_0_ch_en_f(1));
 
 	/* restore current min value */
-	host1x_sync_writel(dev->dev, (host1x_sync_syncpt_0_r() + id * 4), min);
+	host1x_writel(dev->dev, (host1x_sync_syncpt_0_r() + id * 4), min);
 
 }
 
@@ -49,7 +49,7 @@ static int t186_syncpt_mark_used(struct nvhost_syncpt *sp,
 {
 	struct nvhost_master *dev = syncpt_to_dev(sp);
 
-	host1x_sync_writel(dev->dev,
+	host1x_writel(dev->dev,
 		(host1x_sync_syncpt_ch_app_0_r() + syncptid * 4),
 		host1x_sync_syncpt_ch_app_0_syncpt_ch_f(chid));
 	return 0;
@@ -59,7 +59,7 @@ static int t186_syncpt_mark_unused(struct nvhost_syncpt *sp, u32 syncptid)
 {
 	struct nvhost_master *dev = syncpt_to_dev(sp);
 
-	host1x_sync_writel(dev->dev,
+	host1x_writel(dev->dev,
 		(host1x_sync_syncpt_ch_app_0_r() + syncptid * 4),
 		host1x_sync_syncpt_ch_app_0_syncpt_ch_f(0xff));
 	return 0;

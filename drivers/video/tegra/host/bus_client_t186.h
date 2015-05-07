cf061b55b31506789e7a624a087950e853a7356d
Deepak Nibade <dnibade@nvidia.com>
2015-05-07 14:09:59 +0530

    video: tegra: host: define APIs for hypervisor register access
    Define below APIs to read/write registers from Hypervisor range :
    
    u32 host1x_hypervisor_readl(struct platform_device *pdev, u32 r)
    void host1x_hypervisor_writel(struct platform_device *pdev,
    u32 r, u32 v)
    
    For now, use aperture[0] for hypervisor register access.
    Once register space (and Host1x aperture) is split, these APIs
    will switch to use aperture[1]
    
    Bug 1611482
    
    Change-Id: I707f892b43e94790c048b72e4d8cde3726126d4b
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/741244
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>

---
+++ drivers/video/tegra/host/bus_client_t186.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015, NVIDIA Corporation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __NVHOST_BUS_CLIENT_T186_H
+#define __NVHOST_BUS_CLIENT_T186_H
+
+#include "bus_client.h"
+
+void host1x_hypervisor_writel(struct platform_device *pdev, u32 r, u32 v)
+{
+	void __iomem *aperture = get_aperture(pdev, 0);
+
+	if (aperture) {
+		nvhost_dbg(dbg_reg, " d=%s r=0x%x v=0x%x", pdev->name, r, v);
+		writel(v, aperture + r);
+	}
+}
+
+u32 host1x_hypervisor_readl(struct platform_device *pdev, u32 r)
+{
+	void __iomem *aperture = get_aperture(pdev, 0);
+	u32 v = 0;
+
+	if (aperture) {
+		nvhost_dbg(dbg_reg, " d=%s r=0x%x", pdev->name, r);
+		v = readl(aperture + r);
+		nvhost_dbg(dbg_reg, " d=%s r=0x%x v=0x%x", pdev->name, r, v);
+	}
+
+	return v;
+}
+
+#endif /* __NVHOST_BUS_CLIENT_T186_H */

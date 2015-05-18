e3d9f61162ea08df0aa7820d12a1c8831ad38cc2
Ishan Mittal <imittal@nvidia.com>
2015-05-18 11:31:19 +0530

    video: tegra: dc: simplify lut load logic for gcc
    Always initialize cmu first, and simplify error handling. Otherwise,
    with -fprofile-arcs (for code coverage), gcc would wrongly deduce that
    cmu could be uninitialized in this function.
    
    Change-Id: Id088a62d611d043b901e0aaf27b87bd4150ff461
    Signed-off-by: Konsta Holtta <kholtta@nvidia.com>
    Reviewed-on: http://git-master/r/730760
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>
    Reviewed-by: Vinod Gopalakrishnakurup <vinodg@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>
    
    Conflicts:
    	drivers/video/tegra/dc/ext/dev.c

--- drivers/video/tegra/dc/ext/dev.c
+++ drivers/video/tegra/dc/ext/dev.c
@@ -1247,8 +1247,8 @@ static int tegra_dc_ext_get_cmu_v2(struct tegra_dc_ext_user *user,
 {
 	int i;
 	struct tegra_dc *dc = user->ext->dc;
-	struct tegra_dc_cmu *cmu;
-	struct tegra_dc_lut *cmu_lut;
+	struct tegra_dc_cmu *cmu = dc->pdata->cmu;
+	struct tegra_dc_lut *cmu_lut = &dc->cmu;
 
 	if (custom_value && dc->pdata->cmu) {
 		cmu = dc->pdata->cmu;

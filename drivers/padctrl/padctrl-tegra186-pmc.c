3ed1c8bb4f3315dd4e20babd1c611d3fa011540f
Shardar Shariff Md <smohammed@nvidia.com>
2015-05-20 20:15:00 +0530

    padctrl: t186: Implement pad control for tegra PMC
    Add tegra PMC padcontrol driver for Tegra 186
    
    Bug 200088606
    
    Change-Id: I512ba3ae40e3ef59f8d89b7920c34a2397f10dab
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: http://git-master/r/744996
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

---
+++ drivers/padctrl/padctrl-tegra186-pmc.c
@@ -0,0 +1,299 @@
+/*
+ * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#include <linux/padctrl/padctrl.h>
+#include <linux/tegra-pmc.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+#include <dt-bindings/padctrl/tegra186-pads.h>
+
+struct tegra186_pmc_pads {
+	const char *pad_name;
+	int pad_id;
+	int bit_position;
+};
+
+struct tegra186_pmc_padcontrl {
+	struct padctrl_dev *pad_dev;
+};
+
+#define TEGRA_186_PADS(_name, _id, _bit)		\
+{							\
+	.pad_name = _name,				\
+	.pad_id = TEGRA186_IO_PAD_##_id,		\
+	.bit_position = _bit,				\
+}
+
+static struct tegra186_pmc_pads tegra186_pads[] = {
+	TEGRA_186_PADS("ddr-dvi", DDR_DVI, 0),
+	TEGRA_186_PADS("ddr-gmi", DDR_GMI, 1),
+	TEGRA_186_PADS("ddr-sdmmc2", DDR_SDMMC2, 2),
+	TEGRA_186_PADS("ddr-spi", DDR_SPI, 3),
+	TEGRA_186_PADS("ufs", UFS, 1),
+	TEGRA_186_PADS("mem", MEM, 2),
+	TEGRA_186_PADS("mem1", MEM1, 3),
+	TEGRA_186_PADS("dbg", DBG, 4),
+	TEGRA_186_PADS("spi", SPI, 5),
+	TEGRA_186_PADS("ao-hv", AO_HV, 0),
+	TEGRA_186_PADS("audio-hv", AUDIO_HV, 1),
+	TEGRA_186_PADS("dmic-hv", DMIC_HV, 2),
+	TEGRA_186_PADS("gpio-hv", GPIO_HV, 3),
+	TEGRA_186_PADS("sdmmc1-hv", SDMMC1_HV, 4),
+	TEGRA_186_PADS("sdmmc2-hv", SDMMC2_HV, 5),
+	TEGRA_186_PADS("sdmmc3-hv", SDMMC3_HV, 6),
+};
+
+static int tegra186_pmc_padctrl_set_voltage(struct padctrl_dev *pad_dev,
+		int pad_id, u32 voltage)
+{
+	u32 offset;
+	int val;
+	int i;
+
+	if ((voltage != 1200000) && (voltage != 1800000)
+			&& (voltage != 3300000))
+		return -EINVAL;
+
+	for (i = 0; i < ARRAY_SIZE(tegra186_pads); ++i) {
+		if (tegra186_pads[i].pad_id == pad_id)
+			break;
+	}
+
+	if (i == ARRAY_SIZE(tegra186_pads))
+		return -EINVAL;
+
+	offset = BIT(tegra186_pads[i].bit_position);
+	if (i <= TEGRA186_IO_PAD_DDR_SPI) {
+		val = (voltage == 1800000) ? offset : 0;
+		tegra186_pmc_pwr_ddr_update(offset, val);
+	} else if (i <= TEGRA186_IO_PAD_SPI) {
+		val = (voltage == 1800000) ? offset : 0;
+		tegra186_pmc_pwr_e18V_update(offset, val);
+	} else {
+		val = (voltage == 3300000) ? offset : 0;
+		tegra186_pmc_pwr_e33V_update(offset, val);
+	}
+	udelay(100);
+	return 0;
+}
+
+static int tegra186_pmc_padctl_get_voltage(struct padctrl_dev *pad_dev,
+		int pad_id, u32 *voltage)
+{
+	unsigned int pwrdet_mask;
+	u32 offset;
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(tegra186_pads); ++i) {
+		if (tegra186_pads[i].pad_id == pad_id)
+			break;
+	}
+
+	if (i == ARRAY_SIZE(tegra186_pads))
+		return -EINVAL;
+
+	offset = BIT(tegra186_pads[i].bit_position);
+	if (i <= TEGRA186_IO_PAD_DDR_SPI) {
+		pwrdet_mask = tegra186_pmc_pwr_ddr_get();
+		if (pwrdet_mask & offset)
+			*voltage = 1800000UL;
+		else
+			*voltage = 1200000UL;
+	} else if (i <= TEGRA186_IO_PAD_SPI) {
+		pwrdet_mask = tegra186_pmc_pwr_e18V_get();
+		if (pwrdet_mask & offset)
+			*voltage = 1800000UL;
+		else
+			*voltage = 1200000UL;
+	} else {
+		pwrdet_mask = tegra186_pmc_pwr_e33V_get();
+		if (pwrdet_mask & offset)
+			*voltage = 3300000UL;
+		else
+			*voltage = 1800000UL;
+	}
+
+	return 0;
+}
+
+static struct padctrl_ops tegra186_pmc_padctrl_ops = {
+	.set_voltage = &tegra186_pmc_padctrl_set_voltage,
+	.get_voltage = &tegra186_pmc_padctl_get_voltage,
+};
+
+static struct padctrl_desc tegra186_pmc_padctrl_desc = {
+	.name = "tegra-pmc-padctrl",
+	.ops = &tegra186_pmc_padctrl_ops,
+};
+
+static int tegra186_pmc_parse_io_pad_init(struct device_node *np,
+		struct padctrl_dev *pad_dev)
+{
+	struct device_node *pad_np, *child;
+	u32 pval;
+	int pad_id;
+	const char *pad_name, *name;
+	int n_config;
+	u32 *volt_configs;
+	int i, index, vcount;
+	int ret;
+
+	pad_np = of_get_child_by_name(np, "io-pad-defaults");
+	if (!pad_np)
+		return 0;
+
+	pr_info("PMC: configuring io pad defaults\n");
+	n_config = of_get_child_count(pad_np);
+	if (!n_config)
+		return 0;
+	n_config *= 2;
+
+	volt_configs = kzalloc(n_config * sizeof(*volt_configs), GFP_KERNEL);
+	if (!volt_configs)
+		return -ENOMEM;
+
+	vcount = 0;
+	for_each_child_of_node(pad_np, child) {
+		/* Ignore the nodes if disabled */
+		ret = of_device_is_available(child);
+		if (!ret)
+			continue;
+		name = of_get_property(child, "nvidia,pad-name", NULL);
+		if (!name)
+			name = child->name;
+
+		for (i = 0; i < ARRAY_SIZE(tegra186_pads); ++i) {
+			if (strcmp(name, tegra186_pads[i].pad_name))
+				continue;
+			ret = of_property_read_u32(child,
+				"nvidia,io-pad-init-voltage", &pval);
+			if (!ret) {
+				volt_configs[vcount] = i;
+				volt_configs[vcount + 1] = pval;
+				vcount += 2;
+			}
+		}
+	}
+
+	for (i = 0; i < vcount/2; ++i) {
+		index = i * 2;
+		if (!volt_configs[index + 1])
+			continue;
+		pad_id = tegra186_pads[volt_configs[index]].pad_id;
+		pad_name = tegra186_pads[volt_configs[index]].pad_name;
+
+		ret = tegra186_pmc_padctrl_set_voltage(pad_dev,
+				pad_id, volt_configs[index + 1]);
+		if (ret < 0) {
+			pr_warn("PMC: IO pad %s voltage config failed: %d\n",
+					pad_name, ret);
+			WARN_ON(1);
+		} else {
+			pr_info("PMC: IO pad %s voltage is %d\n",
+					pad_name, volt_configs[index + 1]);
+		}
+	}
+
+	kfree(volt_configs);
+	return 0;
+}
+
+int tegra186_pmc_padctrl_init(struct device *dev, struct device_node *np)
+{
+	struct tegra186_pmc_padcontrl *pmc_padctrl;
+	struct padctrl_config config = { };
+	int ret;
+
+	pmc_padctrl = kzalloc(sizeof(*pmc_padctrl), GFP_KERNEL);
+	if (!pmc_padctrl) {
+		pr_err("Mem allocation for pmc_padctrl failed\n");
+		return -ENOMEM;
+	}
+
+	config.of_node = (dev && dev->of_node) ? dev->of_node : np;
+	pmc_padctrl->pad_dev = padctrl_register(dev, &tegra186_pmc_padctrl_desc,
+					&config);
+	if (IS_ERR(pmc_padctrl->pad_dev)) {
+		ret = PTR_ERR(pmc_padctrl->pad_dev);
+		pr_err("T186 padctrl driver init failed: %d\n", ret);
+		kfree(pmc_padctrl);
+		return ret;
+	}
+	padctrl_set_drvdata(pmc_padctrl->pad_dev, pmc_padctrl);
+	tegra186_pmc_parse_io_pad_init(config.of_node,
+				pmc_padctrl->pad_dev);
+
+	pr_info("T186 pmc padctrl driver initialized\n");
+	return 0;
+}
+
+#ifdef  CONFIG_DEBUG_FS
+
+#include <linux/debugfs.h>
+#include <linux/seq_file.h>
+
+static int dbg_io_pad_show(struct seq_file *s, void *unused)
+{
+	unsigned int pwrdet_mask;
+	u32 offset;
+	int i;
+	unsigned long voltage;
+
+	for (i = 0; i < ARRAY_SIZE(tegra186_pads); ++i) {
+		offset = BIT(tegra186_pads[i].bit_position);
+		if (i <= TEGRA186_IO_PAD_DDR_SPI) {
+			pwrdet_mask = tegra186_pmc_pwr_ddr_get();
+			if (pwrdet_mask & offset)
+				voltage = 1800000UL;
+			else
+				voltage = 1200000UL;
+		} else if (i <= TEGRA186_IO_PAD_SPI) {
+			pwrdet_mask = tegra186_pmc_pwr_e18V_get();
+			if (pwrdet_mask & offset)
+				voltage = 1800000UL;
+			else
+				voltage = 1200000UL;
+		} else {
+			pwrdet_mask = tegra186_pmc_pwr_e33V_get();
+			if (pwrdet_mask & offset)
+				voltage = 3300000UL;
+			else
+				voltage = 1800000UL;
+		}
+		seq_printf(s, "PMC: IO pad %s voltage %lu\n",
+			tegra186_pads[i].pad_name, voltage);
+	}
+
+	return 0;
+}
+static int dbg_io_pad_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, dbg_io_pad_show, &inode->i_private);
+}
+
+static const struct file_operations debug_fops = {
+	.open	   = dbg_io_pad_open,
+	.read	   = seq_read,
+	.llseek	 = seq_lseek,
+	.release	= single_release,
+};
+
+static int __init tegra_io_pad_debuginit(void)
+{
+	(void)debugfs_create_file("tegra_io_pad", S_IRUGO,
+				NULL, NULL, &debug_fops);
+	return 0;
+}
+late_initcall(tegra_io_pad_debuginit);
+
+#endif
---
+++ include/dt-bindings/padctrl/tegra186-pads.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef _DT_BINDINGS_PADCTRL_TEGRA186_PADS_H
+#define _DT_BINDINGS_PADCTRL_TEGRA186_PADS_H
+
+#define TEGRA186_IO_PAD_DDR_DVI		0
+#define TEGRA186_IO_PAD_DDR_GMI		1
+#define TEGRA186_IO_PAD_DDR_SDMMC2	2
+#define TEGRA186_IO_PAD_DDR_SPI		3
+#define TEGRA186_IO_PAD_UFS		4
+#define TEGRA186_IO_PAD_MEM		5
+#define TEGRA186_IO_PAD_MEM1		6
+#define TEGRA186_IO_PAD_DBG		7
+#define TEGRA186_IO_PAD_SPI		8
+#define TEGRA186_IO_PAD_AO_HV		9
+#define TEGRA186_IO_PAD_AUDIO_HV	10
+#define TEGRA186_IO_PAD_DMIC_HV		11
+#define TEGRA186_IO_PAD_GPIO_HV		12
+#define TEGRA186_IO_PAD_SDMMC1_HV	14
+#define TEGRA186_IO_PAD_SDMMC2_HV	15
+#define TEGRA186_IO_PAD_SDMMC3_HV	16
+
+#define IO_PAD_VOLTAGE_1_2V	1200000
+#define IO_PAD_VOLTAGE_1_8V	1800000
+#define IO_PAD_VOLTAGE_3_3V	3300000
+
+#endif

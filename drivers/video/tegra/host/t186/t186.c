29e786453178504bf630aa8a3fdacc9fea06ce6c
Deepak Nibade <dnibade@nvidia.com>
2015-06-22 18:48:25 +0530

    video: tegra: host: define host1xb device
    Add nvhost_device_data for host1xb
    
    Bug 1611482
    
    Change-Id: Ib298c491b9ba01cb8bbce2a8f443bfc46949cd86
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/760758
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-by: Shridhar Rasal <srasal@nvidia.com>

--- drivers/video/tegra/host/t186/t186.c
+++ drivers/video/tegra/host/t186/t186.c
@@ -57,6 +57,27 @@ struct nvhost_device_data t18_host1x_info = {
 	.private_data		= &host1x04_info,
 };
 
+static struct host1x_device_info host1xb04_info = {
+	.nb_channels	= T186_NVHOST_NUMCHANNELS,
+	.ch_base	= 0,
+	.ch_limit	= T186_NVHOST_NUMCHANNELS,
+	.nb_mlocks	= NV_HOST1X_NB_MLOCKS,
+	.initialize_chip_support = nvhost_init_t186_support,
+	.nb_hw_pts	= NV_HOST1X_SYNCPT_NB_PTS,
+	.nb_pts		= NV_HOST1X_SYNCPT_NB_PTS,
+	.pts_base	= 0,
+	.pts_limit	= NV_HOST1X_SYNCPT_NB_PTS,
+	.syncpt_policy	= SYNCPT_PER_CHANNEL_INSTANCE,
+	.channel_policy	= MAP_CHANNEL_ON_SUBMIT,
+};
+
+struct nvhost_device_data t18_host1xb_info = {
+	.clocks			= {{"host1x", UINT_MAX},
+				   {"actmon", UINT_MAX}, {} },
+	NVHOST_MODULE_NO_POWERGATE_ID,
+	.private_data		= &host1xb04_info,
+};
+
 #ifdef CONFIG_TEGRA_GRHOST_ISP
 struct nvhost_device_data t18_isp_info = {
 	.num_channels		= 1,

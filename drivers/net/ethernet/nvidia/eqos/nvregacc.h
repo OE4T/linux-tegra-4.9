4b8c2dc1aa8ab36e6d96f9e32d9e8c9e7922d5d1
Dave Lim <dlim@nvidia.com>
2015-07-15 17:35:17 -0700

    net: tegra: Changes to support multi-irqs
    Bug 1653977
    
    Change-Id: I336893f459cdfca33ea8161d7cc30de04453660e
    Signed-off-by: Dave Lim <dlim@nvidia.com>
    Signed-off-by: Ajay Gupta <ajayg@nvidia.com>
    Reviewed-on: http://git-master/r/781366
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Anirban Ray <aray@nvidia.com>
    Reviewed-on: http://git-master/r/783210
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

--- drivers/net/ethernet/nvidia/eqos/nvregacc.h
+++ drivers/net/ethernet/nvidia/eqos/nvregacc.h
@@ -44,4 +44,269 @@
 		data = ((data >> 30) & CLK_CRTL0_TX_CLK_Mask);\
 } while(0)
 
+#define VIRT_INTR_CH0_CRTL0_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x8600))
+
+#define VIRT_INTR_CH0_CRTL0_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH0_CRTL0_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH0_CRTL0_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH0_CRTL0_RgOffAddr);\
+} while(0)
+
+/*#define  VIRT_INTR_CH0_CRTL0_TX_Mask (ULONG)(~(~0<<(1))) << (0)))*/
+
+#define VIRT_INTR_CH0_CRTL0_TX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH0_CRTL0_TX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (0)))*/
+
+#define VIRT_INTR_CH0_CRTL0_TX_Wr_Mask (ULONG)(0xfffffffe)
+
+#define VIRT_INTR_CH0_CRTL0_TX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH0_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH0_CRTL0_TX_Wr_Mask) | ((data & VIRT_INTR_CH0_CRTL0_TX_Mask)<<0));\
+		VIRT_INTR_CH0_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH0_CRTL0_TX_UdfRd(data) do {\
+		VIRT_INTR_CH0_CRTL0_RgRd(data);\
+		data = ((data >> 0) & VIRT_INTR_CH0_CRTL0_TX_Mask);\
+} while(0)
+
+/*#define  VIRT_INTR_CH0_CRTL0_RX_Mask (ULONG)(~(~0<<(1))) << (1)))*/
+
+#define VIRT_INTR_CH0_CRTL0_RX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH0_CRTL0_RX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (1)))*/
+
+#define VIRT_INTR_CH0_CRTL0_RX_Wr_Mask (ULONG)(0xfffffffd)
+
+#define VIRT_INTR_CH0_CRTL0_RX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH0_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH0_CRTL0_RX_Wr_Mask) | ((data & VIRT_INTR_CH0_CRTL0_RX_Mask)<<1));\
+		VIRT_INTR_CH0_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH0_CRTL0_RX_UdfRd(data) do {\
+		VIRT_INTR_CH0_CRTL0_RgRd(data);\
+		data = ((data >> 1) & VIRT_INTR_CH0_CRTL0_RX_Mask);\
+} while(0)
+
+
+#define VIRT_INTR_CH1_CRTL0_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x8608))
+
+#define VIRT_INTR_CH1_CRTL0_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH1_CRTL0_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH1_CRTL0_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH1_CRTL0_RgOffAddr);\
+} while(0)
+
+/*#define  VIRT_INTR_CH1_CRTL0_TX_Mask (ULONG)(~(~0<<(1))) << (0)))*/
+
+#define VIRT_INTR_CH1_CRTL0_TX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH1_CRTL0_TX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (0)))*/
+
+#define VIRT_INTR_CH1_CRTL0_TX_Wr_Mask (ULONG)(0xfffffffe)
+
+#define VIRT_INTR_CH1_CRTL0_TX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH1_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH1_CRTL0_TX_Wr_Mask) | ((data & VIRT_INTR_CH1_CRTL0_TX_Mask)<<0));\
+		VIRT_INTR_CH1_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH1_CRTL0_TX_UdfRd(data) do {\
+		VIRT_INTR_CH1_CRTL0_RgRd(data);\
+		data = ((data >> 0) & VIRT_INTR_CH1_CRTL0_TX_Mask);\
+} while(0)
+
+/*#define  VIRT_INTR_CH1_CRTL0_RX_Mask (ULONG)(~(~0<<(1))) << (1)))*/
+
+#define VIRT_INTR_CH1_CRTL0_RX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH1_CRTL0_RX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (1)))*/
+
+#define VIRT_INTR_CH1_CRTL0_RX_Wr_Mask (ULONG)(0xfffffffd)
+
+#define VIRT_INTR_CH1_CRTL0_RX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH1_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH1_CRTL0_RX_Wr_Mask) | ((data & VIRT_INTR_CH1_CRTL0_RX_Mask)<<1));\
+		VIRT_INTR_CH1_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH1_CRTL0_RX_UdfRd(data) do {\
+		VIRT_INTR_CH1_CRTL0_RgRd(data);\
+		data = ((data >> 1) & VIRT_INTR_CH1_CRTL0_RX_Mask);\
+} while(0)
+
+
+#define VIRT_INTR_CH2_CRTL0_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x8610))
+
+#define VIRT_INTR_CH2_CRTL0_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH2_CRTL0_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH2_CRTL0_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH2_CRTL0_RgOffAddr);\
+} while(0)
+
+/*#define  VIRT_INTR_CH2_CRTL0_TX_Mask (ULONG)(~(~0<<(1))) << (0)))*/
+
+#define VIRT_INTR_CH2_CRTL0_TX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH2_CRTL0_TX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (0)))*/
+
+#define VIRT_INTR_CH2_CRTL0_TX_Wr_Mask (ULONG)(0xfffffffe)
+
+#define VIRT_INTR_CH2_CRTL0_TX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH2_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH2_CRTL0_TX_Wr_Mask) | ((data & VIRT_INTR_CH2_CRTL0_TX_Mask)<<0));\
+		VIRT_INTR_CH2_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH2_CRTL0_TX_UdfRd(data) do {\
+		VIRT_INTR_CH2_CRTL0_RgRd(data);\
+		data = ((data >> 0) & VIRT_INTR_CH2_CRTL0_TX_Mask);\
+} while(0)
+
+/*#define  VIRT_INTR_CH2_CRTL0_RX_Mask (ULONG)(~(~0<<(1))) << (1)))*/
+
+#define VIRT_INTR_CH2_CRTL0_RX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH2_CRTL0_RX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (1)))*/
+
+#define VIRT_INTR_CH2_CRTL0_RX_Wr_Mask (ULONG)(0xfffffffd)
+
+#define VIRT_INTR_CH2_CRTL0_RX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH2_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH2_CRTL0_RX_Wr_Mask) | ((data & VIRT_INTR_CH2_CRTL0_RX_Mask)<<1));\
+		VIRT_INTR_CH2_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH2_CRTL0_RX_UdfRd(data) do {\
+		VIRT_INTR_CH2_CRTL0_RgRd(data);\
+		data = ((data >> 1) & VIRT_INTR_CH2_CRTL0_RX_Mask);\
+} while(0)
+
+
+#define VIRT_INTR_CH3_CRTL0_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x8618))
+
+#define VIRT_INTR_CH3_CRTL0_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH3_CRTL0_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH3_CRTL0_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH3_CRTL0_RgOffAddr);\
+} while(0)
+
+/*#define  VIRT_INTR_CH3_CRTL0_TX_Mask (ULONG)(~(~0<<(1))) << (0)))*/
+
+#define VIRT_INTR_CH3_CRTL0_TX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH3_CRTL0_TX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (0)))*/
+
+#define VIRT_INTR_CH3_CRTL0_TX_Wr_Mask (ULONG)(0xfffffffe)
+
+#define VIRT_INTR_CH3_CRTL0_TX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH3_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH3_CRTL0_TX_Wr_Mask) | ((data & VIRT_INTR_CH3_CRTL0_TX_Mask)<<0));\
+		VIRT_INTR_CH3_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH3_CRTL0_TX_UdfRd(data) do {\
+		VIRT_INTR_CH3_CRTL0_RgRd(data);\
+		data = ((data >> 0) & VIRT_INTR_CH3_CRTL0_TX_Mask);\
+} while(0)
+
+/*#define  VIRT_INTR_CH3_CRTL0_RX_Mask (ULONG)(~(~0<<(1))) << (1)))*/
+
+#define VIRT_INTR_CH3_CRTL0_RX_Mask (ULONG)(0x1)
+
+/*#define VIRT_INTR_CH3_CRTL0_RX_Wr_Mask (unsigned long)(~((~(~0 << (1))) << (1)))*/
+
+#define VIRT_INTR_CH3_CRTL0_RX_Wr_Mask (ULONG)(0xfffffffd)
+
+#define VIRT_INTR_CH3_CRTL0_RX_UdfWr(data) do {\
+		ULONG v;\
+		VIRT_INTR_CH3_CRTL0_RgRd(v);\
+		v = ((v & VIRT_INTR_CH3_CRTL0_RX_Wr_Mask) | ((data & VIRT_INTR_CH3_CRTL0_RX_Mask)<<1));\
+		VIRT_INTR_CH3_CRTL0_RgWr(v);\
+} while(0)
+
+#define VIRT_INTR_CH3_CRTL0_RX_UdfRd(data) do {\
+		VIRT_INTR_CH3_CRTL0_RgRd(data);\
+		data = ((data >> 1) & VIRT_INTR_CH3_CRTL0_RX_Mask);\
+} while(0)
+
+
+#define VIRT_INTR_CH0_STAT_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x8604))
+
+#define VIRT_INTR_CH0_STAT_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH0_STAT_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH0_STAT_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH0_STAT_RgOffAddr);\
+} while(0)
+
+
+#define VIRT_INTR_CH1_STAT_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x860c))
+
+#define VIRT_INTR_CH1_STAT_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH1_STAT_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH1_STAT_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH1_STAT_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH2_STAT_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x8614))
+
+#define VIRT_INTR_CH2_STAT_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH2_STAT_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH2_STAT_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH2_STAT_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH3_STAT_RgOffAddr ((volatile ULONG *)(BASE_ADDRESS + 0x861c))
+
+#define VIRT_INTR_CH3_STAT_RgWr(data) do {\
+		iowrite32(data, (void *)VIRT_INTR_CH3_STAT_RgOffAddr);\
+} while(0)
+
+#define VIRT_INTR_CH3_STAT_RgRd(data) do {\
+		(data) = ioread32((void *)VIRT_INTR_CH3_STAT_RgOffAddr);\
+} while(0)
+
+
+#define VIRT_INTR_CH_CRTL_TX_Wr_Mask (ULONG)(1 << 0)
+#define VIRT_INTR_CH_CRTL_RX_Wr_Mask (ULONG)(1 << 1)
+
+#define VIRT_INTR_CH_CRTL_RgWr(chan, data) do {\
+		iowrite32(data, (void *)(volatile ULONG*)(BASE_ADDRESS + 0x8600 + (chan * 8)));\
+} while(0)
+
+#define VIRT_INTR_CH_CRTL_RgRd(chan, data) do {\
+		(data) = ioread32((void *)(volatile ULONG*)(BASE_ADDRESS+0x8600 + (chan * 8)));\
+} while(0)
+
+#define VIRT_INTR_CH_STAT_RgWr(chan, data) do {\
+		iowrite32(data, (void *)(volatile ULONG*)(BASE_ADDRESS+0x8604 + (chan * 8)));\
+} while(0)
+
+#define VIRT_INTR_CH_STAT_RgRd(chan, data) do {\
+		(data) = ioread32((void *)(volatile ULONG*)(BASE_ADDRESS+0x8604 + (chan * 8)));\
+} while(0)
+
 #endif

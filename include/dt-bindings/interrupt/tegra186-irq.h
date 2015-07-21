c280a254263177883c2a9864d860b8f8622f2d3c
Shardar Shariff Md <smohammed@nvidia.com>
2015-07-22 01:42:03 +0530

    irq: tegra: Add irq num definitions for T186
    Add interrupt number definitions for Tegra 186
    
    Change-Id: I42c7dd058c895715b09f920d23d79c45de5ba404
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: http://git-master/r/773075
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

---
+++ include/dt-bindings/interrupt/tegra186-irq.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef _DT_BINDINGS_INTERRUPT_TEGRA186_IRQ_H
+#define _DT_BINDINGS_INTERRUPT_TEGRA186_IRQ_H
+
+#define TEGRA186_IRQ_I2C1			25
+#define TEGRA186_IRQ_I2C2			26
+#define TEGRA186_IRQ_I2C3			27
+#define TEGRA186_IRQ_I2C4			28
+#define TEGRA186_IRQ_I2C5			29
+#define TEGRA186_IRQ_I2C6			30
+#define TEGRA186_IRQ_I2C7			31
+#define TEGRA186_IRQ_I2C8			32
+#define TEGRA186_IRQ_I2C9			33
+
+#endif

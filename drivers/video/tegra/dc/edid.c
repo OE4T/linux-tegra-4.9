9f3793982375db87c51b425519da88dbd5bcf637
Aly Hirani <ahirani@nvidia.com>
2015-08-18 02:48:42 -0700

    video: tegra: dc: enable 1000/1001 modes
    This change enables the 1000/1001 modes for T210. On T210, we have a fractional
    divider available for DC. This allows us to generate pclks almost closely
    accurate to the common 1000/1001 modes such as 23.976, 29.97 and 59.94 Hz.
    
    This change:
    
    1. Adds a new VMODE flag to signify modes which are * 1000 / 1001
    2. Filters exclusively for 24/30/60 Hz modes in the EDID parser, duplicates
       these modes and changes them to be the 1000 / 1001 modes
    3. Changes the dc pclk calculation to take into account these modes during the
       rounding
    
    Bug 200111432
    
    Change-Id: I2cc762b2338ce5662d85891859327015e88b670b
    Signed-off-by: Aly Hirani <ahirani@nvidia.com>
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: http://git-master/r/799342
    (cherry picked from commit fc19c750fd8ac492ba5d9cd9e561f627c80f4add)
    Reviewed-on: http://git-master/r/927763
    Reviewed-by: Mitch Luban <mluban@nvidia.com>
    Tested-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/tegra/dc/edid.c
+++ drivers/video/tegra/dc/edid.c
@@ -735,6 +735,37 @@ int tegra_edid_get_monspecs(struct tegra_edid *edid, struct fb_monspecs *specs)
 		}
 	}
 
+#ifdef CONFIG_ARCH_TEGRA_21x_SOC
+	/* T210 supports fractional divider and hence can support the * 1000 / 1001 modes.
+	   For now, only enable support for 24, 30 and 60 Hz modes */
+	{
+		struct fb_videomode *m;
+		struct fb_videomode frac_modes[10];
+		u8 frac_n = 0;
+
+		for (j = 0; j < specs->modedb_len; ++j) {
+			if ((tegra_dc_calc_fb_refresh(&specs->modedb[j]) == 24000 ||
+			    tegra_dc_calc_fb_refresh(&specs->modedb[j]) == 30000 ||
+			    tegra_dc_calc_fb_refresh(&specs->modedb[j]) == 60000) &&
+		        frac_n < ARRAY_SIZE(frac_modes)) {
+				memcpy(&frac_modes[frac_n], &specs->modedb[j], sizeof(struct fb_videomode));
+				frac_modes[frac_n].pixclock = frac_modes[frac_n].pixclock * 1000 / 1001;
+				frac_modes[frac_n].vmode |= FB_VMODE_1000DIV1001;
+				frac_n++;
+			}
+		}
+
+		m = kzalloc((specs->modedb_len + frac_n) * sizeof(struct fb_videomode), GFP_KERNEL);
+		if (m) {
+			memcpy(m, specs->modedb, specs->modedb_len * sizeof(struct fb_videomode));
+			memcpy(&m[specs->modedb_len], frac_modes, frac_n * sizeof(struct fb_videomode));
+			kfree(specs->modedb);
+			specs->modedb = m;
+			specs->modedb_len += frac_n;
+		}
+	}
+#endif
+
 	new_data->dc_edid.len = i * 128;
 
 	mutex_lock(&edid->lock);

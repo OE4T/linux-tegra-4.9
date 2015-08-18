InES-HPMM/linux-l4t

tegra: dc: add support for adobeRGB color space
Support adde for adobeRGB color space in kernel.

Bug 1644175

Change-Id: I5aa0fb4818b30eb84a07d8d6f4f5bc41a616e346
Signed-off-by: Arun Swain <arswain@nvidia.com>
Reviewed-on: http://git-master/r/740307
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

include/video/tegra_dc_ext.h
@@ -460,6 +460,9 @@ struct tegra_dc_ext_feature {
 #define TEGRA_DC_EXT_SET_CMU_ALIGNED \
 	_IOW('D', 0x16, struct tegra_dc_ext_cmu)
 
+#define TEGRA_DC_EXT_GET_CMU_ADBRGB\
+	_IOR('D', 0x18, struct tegra_dc_ext_cmu)
+
 enum tegra_dc_ext_control_output_type {
 	TEGRA_DC_EXT_DSI,
 	TEGRA_DC_EXT_LVDS,

OE4T/linux-tegra-4.9

Support added for adobeRGB color space in kernel.
(cherry picked from commit 2fa48d598b18813fd13ff95c6673451a89aacab4)
Reviewed-on: http://git-master/r/780618
Reviewed-by: Mitch Luban <mluban@nvidia.com>
Tested-by: Mitch Luban <mluban@nvidia.com>

include/video/tegra_dc_ext.h
@@ -650,6 +650,9 @@ struct tegra_dc_ext_feature {
 #define TEGRA_DC_EXT_SET_PROPOSED_BW_3 \
 	_IOR('D', 0x1B, struct tegra_dc_ext_flip_3)
 
+#define TEGRA_DC_EXT_GET_CMU_ADBRGB\
+	_IOR('D', 0x1C, struct tegra_dc_ext_cmu)
+
 enum tegra_dc_ext_control_output_type {
 	TEGRA_DC_EXT_DSI,
 	TEGRA_DC_EXT_LVDS,
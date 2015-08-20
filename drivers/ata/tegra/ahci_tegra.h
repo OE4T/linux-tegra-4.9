248125599911384233e777a192104feca5e3aa5c
Preetham Chandru R <pchandru@nvidia.com>
2015-08-20 16:53:08 +0530

    ata: ahci_tegra: Add suspend and resume
    Bug 200129957
    
    Signed-off-by: Preetham Chandru R <pchandru@nvidia.com>
    Change-Id: I32c314c492106925823ef33f10804465c559e198
    Reviewed-on: http://git-master/r/1127295
    (cherry picked from commit 4e4398fdb3389e893624214a2cd371d9787c1c4f)

--- drivers/ata/tegra/ahci_tegra.h
+++ drivers/ata/tegra/ahci_tegra.h
@@ -118,6 +118,9 @@
 #define T_SATA0_INDEX_NONE_SELECTED                     0
 #define T_SATA0_INDEX_CH1				BIT(0)
 
+#define T_SATA0_CFG_POWER_GATE				0x4AC
+#define T_SATA0_CFG_POWER_GATE_SSTS_RESTORED		BIT(23)
+
 
 /* AHCI registers */
 
@@ -165,6 +168,7 @@ struct tegra_ahci_priv {
 	struct clk		   *pllp_clk; /* sata_oob clk parent */
 	struct clk		   *pllp_uphy_clk; /* sata_clk parent */
 	bool			   devslp_override;
+	bool			   devslp_pinmux_override;
 };
 
 struct tegra_ahci_ops {
@@ -237,91 +241,6 @@ static u32 pg_restore_bar5_bkdr_port_registers[] = {
 	0x794,  /* BKDR of NV_PROJ__SATA0_CHX_AHCI_PORT_PXSIG */
 	0x798   /* BKDR of NV_PROJ__SATA0_CHX_AHCI_PORT_PXSSTS */
 };
-
-static u32 pg_save_config_registers[] = {
-	0x004,  /* T_SATA0_CFG_1 */
-	0x00C,  /* T_SATA0_CFG_3 */
-	0x024,  /* T_SATA0_CFG_9 */
-	0x028,  /* T_SATA0_CFG_10 */
-	0x030,  /* T_SATA0_CFG_12 */
-	0x034,  /* T_SATA0_CFG_13 */
-	0x038,  /* T_SATA0_CFG_14 */
-	0x03C,  /* T_SATA0_CFG_15 */
-	0x040,  /* T_SATA0_CFG_16 */
-	0x044,  /* T_SATA0_CFG_17 */
-	0x048,  /* T_SATA0_CFG_18 */
-	0x0B0,  /* T_SATA0_MSI_CTRL */
-	0x0B4,  /* T_SATA0_MSI_ADDR1 */
-	0x0B8,  /* T_SATA0_MSI_ADDR2 */
-	0x0BC,  /* T_SATA0_MSI_DATA */
-	0x0C0,  /* T_SATA0_MSI_QUEUE */
-	0x0EC,  /* T_SATA0_MSI_MAP */
-	0x124,  /* T_SATA0_CFG_PHY_POWER */
-	0x128,  /* T_SATA0_CFG_PHY_POWER_1 */
-	0x12C,  /* T_SATA0_CFG_PHY_1 */
-	0x174,  /* T_SATA0_CFG_LINK_0 */
-	0x178,  /* T_SATA0_CFG_LINK_1 */
-	0x1D0,  /* MCP_SATA0_CFG_TRANS_0 */
-	0x238,  /* T_SATA0_ALPM_CTRL */
-	0x30C,  /* T_SATA0_AHCI_HBA_CYA_0 */
-	0x320,  /* T_SATA0_AHCI_HBA_SPARE_1 */
-	0x324,  /* T_SATA0_AHCI_HBA_SPARE_2 */
-	0x328,  /* T_SATA0_AHCI_HBA_DYN_CLK_CLAMP */
-	0x32C,  /* T_SATA0_AHCI_CFG_ERR_CTRL */
-	0x338,  /* T_SATA0_AHCI_HBA_CYA_1 */
-	0x340,  /* T_SATA0_AHCI_HBA_PRE_STAGING_CONTROL */
-	0x430,  /* T_SATA0_CFG_FPCI_0 */
-	0x494,  /* T_SATA0_CFG_ESATA_CTRL */
-	0x4A0,  /* T_SATA0_CYA1 */
-	0x4B0,  /* T_SATA0_CFG_GLUE */
-	0x534,  /* T_SATA0_PHY_CTRL */
-	0x540,  /* T_SATA0_CTRL */
-	0x550,  /* T_SATA0_DBG0 */
-	0x554   /* T_SATA0_LOW_POWER_COUNT */
-};
-
-static u32 pg_save_config_port_registers[] = {
-	/* Save and restore per port */
-	/* need to have port selected */
-	0x530,  /* T_SATA0_CHXCFG1 */
-	0x684,  /* T_SATA0_CHX_MISC */
-	0x700,  /* T_SATA0_CHXCFG3 */
-	0x704,  /* T_SATA0_CHXCFG4_CHX */
-	0x690,  /* T_SATA0_CHX_PHY_CTRL1_GEN1 */
-	0x694,  /* T_SATA0_CHX_PHY_CTRL1_GEN2 */
-	0x698,  /* T_SATA0_CHX_PHY_CTRL1_GEN3 */
-	0x69C,  /* T_SATA0_CHX_PHY_CTRL_2 */
-	0x6B0,  /* T_SATA0_CHX_PHY_CTRL_3 */
-	0x6B4,  /* T_SATA0_CHX_PHY_CTRL_4 */
-	0x6B8,  /* T_SATA0_CHX_PHY_CTRL_5 */
-	0x6BC,  /* T_SATA0_CHX_PHY_CTRL_6 */
-	0x714,  /* T_SATA0_PRBS_CHX - OP */
-	0x750,  /* T_SATA0_CHX_LINK0 */
-	0x7F0   /* T_SATA0_CHX_GLUE */
-};
-
-static u32 pg_save_ipfs_registers[] = {
-	0x094,  /* SATA_FPCI_BAR5_0 */
-	0x0C0,  /* SATA_MSI_BAR_SZ_0 */
-	0x0C4,  /* SATA_MSI_AXI_BAR_ST_0 */
-	0x0C8,  /* SATA_MSI_FPCI_BAR_ST_0 */
-	0x140,  /* SATA_MSI_EN_VEC0_0 */
-	0x144,  /* SATA_MSI_EN_VEC1_0 */
-	0x148,  /* SATA_MSI_EN_VEC2_0 */
-	0x14C,  /* SATA_MSI_EN_VEC3_0 */
-	0x150,  /* SATA_MSI_EN_VEC4_0 */
-	0x154,  /* SATA_MSI_EN_VEC5_0 */
-	0x158,  /* SATA_MSI_EN_VEC6_0 */
-	0x15C,  /* SATA_MSI_EN_VEC7_0 */
-	0x180,  /* SATA_CONFIGURATION_0 */
-	0x184,  /* SATA_FPCI_ERROR_MASKS_0 */
-	0x188,  /* SATA_INTR_MASK_0 */
-	0x1A0,  /* SATA_CFG_REVID_0 */
-	0x198,  /* SATA_IPFS_INTR_ENABLE_0 */
-	0x1BC,  /* SATA_CLKGATE_HYSTERSIS_0 */
-	0x1DC   /* SATA_SATA_MCCIF_FIFOCTRL_0 */
-};
-
 static inline void tegra_ahci_save_regs(u32 **save_addr,
 		void __iomem *reg_base,
 		u32 reg_array[],

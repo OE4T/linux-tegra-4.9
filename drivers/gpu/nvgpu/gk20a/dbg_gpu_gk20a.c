1de62619720b86f07338402d367e278ca820c32d
Deepak Nibade <dnibade@nvidia.com>
2015-09-09 22:03:56 +0530

    gpu: nvgpu: add missing slcg_ltc_load_gating_prod
    Add missing slcg_ltc_load_gating_prod() call in
    dbg_set_powergate(POWERGATE_ENABLE) path
    
    Also, re-order POWERGATE_ENABLE operations in opposite
    order of POWERGATE_DISABLE
    
    Bug 1679372
    
    Change-Id: Ib72a0b80929e2dee2cf88a6d3d0f96d61c02307b
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/796459
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sandarbh Jain <sanjain@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/dbg_gpu_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/dbg_gpu_gk20a.c
@@ -663,13 +663,15 @@ static int dbg_set_powergate(struct dbg_session_gk20a *dbg_s,
 		    --g->dbg_powergating_disabled_refcount == 0) {
 
 			g->elcg_enabled = true;
-			gr_gk20a_init_elcg_mode(g, ELCG_AUTO, ENGINE_GR_GK20A);
 			gr_gk20a_init_elcg_mode(g, ELCG_AUTO, ENGINE_CE2_GK20A);
+			gr_gk20a_init_elcg_mode(g, ELCG_AUTO, ENGINE_GR_GK20A);
 			gr_gk20a_init_blcg_mode(g, BLCG_AUTO, ENGINE_GR_GK20A);
 
-			g->ops.clock_gating.slcg_gr_load_gating_prod(g,
+			g->ops.clock_gating.slcg_ltc_load_gating_prod(g,
 					g->slcg_enabled);
 			g->ops.clock_gating.slcg_perf_load_gating_prod(g,
+					g->slcg_enabled);
+			g->ops.clock_gating.slcg_gr_load_gating_prod(g,
 					g->slcg_enabled);
 
 			if (support_gk20a_pmu(g->dev))

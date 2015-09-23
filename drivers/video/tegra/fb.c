80716769fc7a27238e1f14996c32f550cb1c0bd0
Naveen Kumar S <nkumars@nvidia.com>
2015-09-23 14:21:20 +0530

    video: tegra: set dc blank on display timeout
    While unblanking FB, all available windows are sync'd again only
    if dc is in blanked state. Hence, set dc blank state to true
    when dc is disabled during display timeout.
    
    bug 200136625
    
    Change-Id: I05bbf793b9c3fdd220589509dade0d5e64368832
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: http://git-master/r/807015
    (cherry picked from commit 8bbd4f727196c4717b676d472141be7aa2836e40)
    Reviewed-on: http://git-master/r/820894
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- drivers/video/tegra/fb.c
+++ drivers/video/tegra/fb.c
@@ -353,6 +353,7 @@ static int tegra_fb_blank(int blank, struct fb_info *info)
 		if (!dc->suspended && dc->enabled)
 			tegra_fb->curr_xoffset = -1;
 		tegra_dc_disable(dc);
+		dc->blanked = true;
 		return 0;
 #endif
 	default:

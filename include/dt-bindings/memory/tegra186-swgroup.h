77e86bca9fa9d681748fb98af9226cefe3e0b0cf
Alex Waterman <alexw@nvidia.com>
2015-09-30 14:03:20 -0700

    dt-bindings: memory: Update tegra18 SMMU bindings
    Update Tegra's ARM SMMU bindings. This adds the SID defines which are
    needed for migrating away from the Tegra SMMU bindings.
    
    Change-Id: If385f83b4590687088cb69c1d238e197b076038b
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: http://git-master/r/812437
    (cherry picked from commit df6db4e4b9f61a9f45ebe89c8c51eecb3e9b45b4)
    Reviewed-on: http://git-master/r/831514
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- include/dt-bindings/memory/tegra186-swgroup.h
+++ include/dt-bindings/memory/tegra186-swgroup.h
@@ -12,11 +12,30 @@
 #define TEGRA_SWGROUP_SE2		61
 #define TEGRA_SWGROUP_SE3		62
 
-#define TEGRA_SID_GPCDMA_0	46	/* Same as SWGROUP_AXIS */
-#define TEGRA_SID_GPCDMA_1	63
-#define TEGRA_SID_GPCDMA_2	65
-#define TEGRA_SID_GPCDMA_3	66
-#define TEGRA_SID_GPCDMA_4	67
-#define TEGRA_SID_GPCDMA_5	68
-#define TEGRA_SID_GPCDMA_6	69
-#define TEGRA_SID_GPCDMA_7	70
+/*
+ * This is the t18x specific component of the new SID dt-binding.
+ */
+#define TEGRA_SID_NVCSI		0x2	/* 2 */
+#define TEGRA_SID_SE2		0xe	/* 14 */
+#define TEGRA_SID_SE3		0xf	/* 15 */
+#define TEGRA_SID_SCE		0x1f	/* 31 */
+
+/* The GPC DMA clients. */
+#define TEGRA_SID_GPCDMA_0	0x20	/* 32 */
+#define TEGRA_SID_GPCDMA_1	0x21	/* 33 */
+#define TEGRA_SID_GPCDMA_2	0x22	/* 34 */
+#define TEGRA_SID_GPCDMA_3	0x23	/* 35 */
+#define TEGRA_SID_GPCDMA_4	0x24	/* 36 */
+#define TEGRA_SID_GPCDMA_5	0x25	/* 37 */
+#define TEGRA_SID_GPCDMA_6	0x26	/* 38 */
+#define TEGRA_SID_GPCDMA_7	0x27	/* 39 */
+
+/* Host1x virtualization clients. */
+#define TEGRA_SID_HOST1X_CTX0	0x38	/* 56 */
+#define TEGRA_SID_HOST1X_CTX1	0x39	/* 57 */
+#define TEGRA_SID_HOST1X_CTX2	0x3a	/* 58 */
+#define TEGRA_SID_HOST1X_CTX3	0x3b	/* 59 */
+#define TEGRA_SID_HOST1X_CTX4	0x3c	/* 60 */
+#define TEGRA_SID_HOST1X_CTX5	0x3d	/* 61 */
+#define TEGRA_SID_HOST1X_CTX6	0x3e	/* 62 */
+#define TEGRA_SID_HOST1X_CTX7	0x3f	/* 63 */

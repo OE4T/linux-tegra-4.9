5873d76f950c9e71a766bd25a0825b0786400098
Pavan Kunapuli <pkunapuli@nvidia.com>
2015-11-30 16:20:14 +0530

    ARM: tegra: panel: E3320 A01 support in Sharp 8' panel driver
    Adding support for powering up and powering down Sharp 8' panel driver
    when used with E3320 A00 and A01 display connectors.
    
    Using outp,outn regulators for vmm,vpp regulator control and cleaned up
    I2C related code from the panel driver.
    
    Bug 1695392
    
    Change-Id: I06f910dc4c83932a136c9afee1ffcac0a58f8329
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/839308
    Reviewed-by: Daniel Solomon <daniels@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/panel-s-wuxga-8-0.c
+++ arch/arm/mach-tegra/panel-s-wuxga-8-0.c
@@ -29,105 +29,12 @@
 static bool reg_requested;
 static struct regulator *avdd_lcd_3v0;
 static struct regulator *dvdd_lcd_1v8;
-static struct regulator *avdd_lcd_1v2;
-static struct regulator *vdd_ts_3v3;
-static struct regulator *vdd_ts_1v8;
 static struct regulator *vpp_lcd;
 static struct regulator *vmm_lcd;
 static struct device *dc_dev;
 static u16 en_panel_rst;
-static u16 en_panel;
 static u16 en_backlight;
-static bool en_vmm_vpp_i2c_config;
-static struct i2c_client *dsi_s_vmm_vpp_i2c_client;
-
-enum i2c_transfer_type {
-	I2C_WRITE,
-	I2C_READ,
-};
-
-static u8 dsi_s_vmm_vpp_configure[][2] = {
-	{0x03, 0xF2},
-	{0x01, 0x07},
-	{0x01, 0x0F},
-};
-
-static struct i2c_board_info i2c_vmm_vpp_pdata = {
-	.type = "vmm-vpp-adapter",
-	.addr = 0x21,
-};
-
-static struct i2c_driver dsi_s_vmm_vpp_i2c_slave_driver = {
-	.driver = {
-		.name = "vmm-vpp-adapter",
-	},
-};
-
-static struct i2c_client *init_e3320_i2c_slave(struct device *dev, struct i2c_board_info *pdata,
-	struct i2c_driver *i2c_slave_driver)
-{
-	struct i2c_adapter *adapter;
-	struct i2c_client *client;
-	int bus = 0;
-	int err = 0;
-	adapter = i2c_get_adapter(bus);
-	if (!adapter) {
-		pr_err("e3320: Can't get adapter for I2C bus\n");
-		err = -EBUSY;
-		goto err;
-	}
-
-	client = i2c_new_device(adapter, pdata);
-	i2c_put_adapter(adapter);
-	if (!client) {
-		pr_err("e3320: Can't add i2c slave device...\n");
-		err = -EBUSY;
-		goto err;
-	}
-
-	err = i2c_add_driver(i2c_slave_driver);
-	if (err) {
-		pr_err("e3320: Can't add i2c slave driver!\n");
-		err = -EBUSY;
-		goto err_free;
-	}
-	return client;
-err_free:
-	i2c_unregister_device(client);
-err:
-	return ERR_PTR(err);
-
-}
-
-static int dsi_s_e3320_i2c_configure(struct i2c_client *client, u8 data[][2],
-		u8 num_of_xfers)
-{
-	struct i2c_msg *i2c_msg_xfer;
-	u32 cnt;
-	int err = 0;
-
-	i2c_msg_xfer = kzalloc(num_of_xfers * sizeof(*i2c_msg_xfer),
-		GFP_KERNEL);
-	if (!i2c_msg_xfer)
-		return -ENOMEM;
-
-	for (cnt = 0; cnt < num_of_xfers; cnt++) {
-		i2c_msg_xfer[cnt].addr = client->addr;
-		i2c_msg_xfer[cnt].flags = I2C_WRITE;
-		i2c_msg_xfer[cnt].len = 2;
-		i2c_msg_xfer[cnt].buf = data[cnt];
-	}
-
-	for (cnt = 0; cnt < num_of_xfers; cnt++) {
-		err = i2c_transfer(client->adapter, &i2c_msg_xfer[cnt], 1);
-		if (err < 0)
-			break;
-		msleep(10);
-	}
-
-	kfree(i2c_msg_xfer);
-	return err;
-}
+static bool vmm_vpp_fixed_regulators;
 
 static int dsi_s_wuxga_8_0_regulator_get(struct device *dev)
 {
@@ -152,54 +59,20 @@ static int dsi_s_wuxga_8_0_regulator_get(struct device *dev)
 		goto fail;
 	}
 
-	avdd_lcd_1v2 = regulator_get(dev, "avdd_lcd_1v2");
-	if (IS_ERR_OR_NULL(avdd_lcd_1v2)) {
-		pr_err("avdd_lcd_1v2 regulator get failed\n");
-		err = PTR_ERR(avdd_lcd_1v2);
-		avdd_lcd_1v2 = NULL;
+	vpp_lcd = regulator_get(dev, "outp");
+	if (IS_ERR_OR_NULL(vpp_lcd)) {
+		pr_err("vpp_lcd regulator get failed\n");
+		err = PTR_ERR(vpp_lcd);
+		vpp_lcd = NULL;
 		goto fail;
 	}
 
-	if (en_vmm_vpp_i2c_config) {
-		vdd_ts_3v3 = regulator_get(dev, "vdd_ts_3v3");
-		if (IS_ERR_OR_NULL(vdd_ts_3v3)) {
-			pr_err("vdd_ts_3v3 regulator get failed\n");
-			err = PTR_ERR(vdd_ts_3v3);
-			vdd_ts_3v3 = NULL;
-			goto fail;
-		}
-
-		vdd_ts_1v8 = regulator_get(dev, "vdd_ts_1v8");
-		if (IS_ERR_OR_NULL(vdd_ts_1v8)) {
-			pr_err("vdd_ts_1v8 regulator get failed\n");
-			err = PTR_ERR(vdd_ts_1v8);
-			vdd_ts_1v8 = NULL;
-			goto fail;
-		}
-		dsi_s_vmm_vpp_i2c_client = init_e3320_i2c_slave(dev, &i2c_vmm_vpp_pdata,
-			&dsi_s_vmm_vpp_i2c_slave_driver);
-		if (IS_ERR_OR_NULL(dsi_s_vmm_vpp_i2c_client)) {
-			pr_err("e3320: Failed to get vmm vpp i2c adapter client\n");
-			dsi_s_vmm_vpp_i2c_client = NULL;
-			err = -EINVAL;
-			goto fail;
-		}
-	} else {
-		vpp_lcd = regulator_get(dev, "outp");
-		if (IS_ERR_OR_NULL(vpp_lcd)) {
-			pr_err("vpp_lcd regulator get failed\n");
-			err = PTR_ERR(vpp_lcd);
-			vpp_lcd = NULL;
-			goto fail;
-		}
-
-		vmm_lcd = regulator_get(dev, "outn");
-		if (IS_ERR_OR_NULL(vmm_lcd)) {
-			pr_err("vmm_lcd regulator get failed\n");
-			err = PTR_ERR(vmm_lcd);
-			vmm_lcd = NULL;
-			goto fail;
-		}
+	vmm_lcd = regulator_get(dev, "outn");
+	if (IS_ERR_OR_NULL(vmm_lcd)) {
+		pr_err("vmm_lcd regulator get failed\n");
+		err = PTR_ERR(vmm_lcd);
+		vmm_lcd = NULL;
+		goto fail;
 	}
 
 	reg_requested = true;
@@ -218,7 +91,7 @@ static int dsi_s_wuxga_8_0_enable(struct device *dev)
 		pr_err("display regulator dt check failed\n");
 		goto fail;
 	} else {
-		en_vmm_vpp_i2c_config = panel_of.en_vmm_vpp_i2c_config;
+		vmm_vpp_fixed_regulators = panel_of.en_vmm_vpp_i2c_config;
 	}
 
 	err = dsi_s_wuxga_8_0_regulator_get(dev);
@@ -245,12 +118,6 @@ static int dsi_s_wuxga_8_0_enable(struct device *dev)
 	else
 		pr_err("display backlight enable gpio invalid\n");
 
-	if (gpio_is_valid(panel_of.panel_gpio[TEGRA_GPIO_PANEL_EN]))
-		en_panel = panel_of.panel_gpio[TEGRA_GPIO_PANEL_EN];
-	else {
-		pr_err("display panel en invalid. enable vmm/vpp with i2c\n");
-	}
-
 	if (dvdd_lcd_1v8) {
 		err = regulator_enable(dvdd_lcd_1v8);
 		if (err < 0) {
@@ -259,7 +126,7 @@ static int dsi_s_wuxga_8_0_enable(struct device *dev)
 		}
 	}
 
-	usleep_range(500, 1500);
+	usleep_range(1000, 1500);
 
 	if (avdd_lcd_3v0) {
 		err = regulator_enable(avdd_lcd_3v0);
@@ -269,90 +136,46 @@ static int dsi_s_wuxga_8_0_enable(struct device *dev)
 		}
 	}
 
-	usleep_range(500, 1500);
+	usleep_range(3000, 3500);
+
 	if (gpio_is_valid(en_backlight))
 		gpio_direction_output(en_backlight, 1);
-	usleep_range(500, 1500);
 
-	if (en_vmm_vpp_i2c_config) {
-		if (avdd_lcd_1v2) {
-			err = regulator_enable(avdd_lcd_1v2);
-			if (err < 0) {
-				pr_err("avdd_lcd_1v2 regulator enable failed\n");
-				goto fail;
-			}
-		}
-
-		usleep_range(500, 1500);
-
-		/*
-		 * To avoid touch panel power on sequence violation, first
-		 * enable 1.8V rail and finally enable 3.3V rail.
-		 */
-		if (vdd_ts_1v8) {
-			err = regulator_enable(vdd_ts_1v8);
-			if (err < 0) {
-				pr_err("vdd_ts_1v8 regulator enable failed\n");
-				goto fail;
-			}
-		}
-
-		usleep_range(500, 6000);
-
-		if (vdd_ts_3v3) {
-			err = regulator_enable(vdd_ts_3v3);
-			if (err < 0) {
-				pr_err("vdd_ts_3v3 regulator enable failed\n");
-				goto fail;
-			}
-		}
-
-		usleep_range(500, 1500);
-
-
-		/* VMM and VPP Driving high */
-		err = dsi_s_e3320_i2c_configure(dsi_s_vmm_vpp_i2c_client, dsi_s_vmm_vpp_configure,
-			ARRAY_SIZE(dsi_s_vmm_vpp_configure));
+	if (vpp_lcd) {
+		err = regulator_enable(vpp_lcd);
 		if (err < 0) {
-			pr_err("e3320: Failed to configure vmm vpp driving %d\n", err);
+			pr_err("vpp_lcd regulator enable failed\n");
 			goto fail;
 		}
 
-		if (gpio_is_valid(en_panel))
-			gpio_direction_output(en_panel, 1);
-	} else {
-		if (vpp_lcd) {
-			err = regulator_enable(vpp_lcd);
-			if (err < 0) {
-				pr_err("vpp_lcd regulator enable failed\n");
-				goto fail;
-			}
-
+		if (!vmm_vpp_fixed_regulators) {
 			err = regulator_set_voltage(vpp_lcd, 5500000, 5500000);
 			if (err < 0) {
 				pr_err("vpp_lcd regulator failed changing voltage\n");
 				goto fail;
 			}
 		}
+	}
 
-		usleep_range(500, 1500);
+	usleep_range(2000, 2500);
 
-		if (vmm_lcd) {
-			err = regulator_enable(vmm_lcd);
-			if (err < 0) {
-				pr_err("vmm_lcd regulator enable failed\n");
-				goto fail;
-			}
+	if (vmm_lcd) {
+		err = regulator_enable(vmm_lcd);
+		if (err < 0) {
+			pr_err("vmm_lcd regulator enable failed\n");
+			goto fail;
+		}
 
+		if (!vmm_vpp_fixed_regulators) {
 			err = regulator_set_voltage(vmm_lcd, 5500000, 5500000);
 			if (err < 0) {
 				pr_err("vmm_lcd regulator failed changing voltage\n");
 				goto fail;
 			}
 		}
-
-		usleep_range(7000, 8000);
 	}
+
+	usleep_range(15000, 15500);
 
 #if DSI_PANEL_RESET
 	if (!tegra_dc_initialized(dev)) {
@@ -389,38 +212,20 @@ static int dsi_s_wuxga_8_0_disable(struct device *dev)
 	} else
 		pr_err("ERROR! display reset gpio invalid\n");
 
-	if (gpio_is_valid(en_panel))
-		gpio_direction_output(en_panel, 0);
-
 	if (vmm_lcd)
 		regulator_disable(vmm_lcd);
 
-	usleep_range(1000, 2000);
+	usleep_range(2000, 2500);
 
 	if (vpp_lcd)
 		regulator_disable(vpp_lcd);
 
-	usleep_range(1500, 2000);
-
-	if (vdd_ts_1v8)
-		regulator_disable(vdd_ts_1v8);
-
-	usleep_range(1500, 2000);
-
-	if (vdd_ts_3v3)
-		regulator_disable(vdd_ts_3v3);
-
-	usleep_range(1500, 2000);
-
-	if (avdd_lcd_1v2)
-		regulator_disable(avdd_lcd_1v2);
-
-	usleep_range(1500, 2000);
+	msleep(125);
 
 	if (avdd_lcd_3v0)
 		regulator_disable(avdd_lcd_3v0);
 
-	usleep_range(1500, 2000);
+	msleep(780);
 
 	if (dvdd_lcd_1v8)
 		regulator_disable(dvdd_lcd_1v8);
@@ -430,6 +235,7 @@ static int dsi_s_wuxga_8_0_disable(struct device *dev)
 	msleep(140);
 
 	dc_dev = NULL;
+
 	return 0;
 }
 

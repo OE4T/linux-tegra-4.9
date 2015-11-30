d760512ade757355aadc882942808b72127cd397
Terje Bergstrom <tbergstrom@nvidia.com>
2015-11-30 09:30:22 -0800

    gpu: nvgpu: Recreate HW headers
    Add gradual slowdown registers, and fix names for L2 flush registers.
    
    Change-Id: If085c4febef494ae299d2147ca5201cd373bee0b
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/839369
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Seshendra Gadagottu <sgadagottu@nvidia.com>

--- drivers/gpu/nvgpu/gp10b/hw_pwr_gp10b.h
+++ drivers/gpu/nvgpu/gp10b/hw_pwr_gp10b.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2015, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,

8db05322e7b4b2132785a3c20599f5b76cf66dc1
Pekka Pessi <ppessi@nvidia.com>
2015-12-02 19:43:02 +0200

    platform: tegra: mc: do not override SID from SCE
    Bug 1707771
    
    Signed-off-by: Pekka Pessi <ppessi@nvidia.com>
    Change-Id: Ie8e2c2b652437314027cbe6bd2b9ce278353a40f
    Reviewed-on: http://git-master/r/843755
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-by: Jihoon Bang <jbang@nvidia.com>

--- drivers/platform/tegra/mc/mc-sid.c
+++ drivers/platform/tegra/mc/mc-sid.c
@@ -1,7 +1,7 @@
 /*
  * MC StreamID configuration
  *
- * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2016 NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -449,7 +449,7 @@ static struct sid_to_oids sid_to_oids[] = {
 			SCEDMAR,
 			SCEDMAW,
 		},
-		.ord = OVERRIDE,
+		.ord = NO_OVERRIDE,
 		.name = "SCE",
 	},
 	{

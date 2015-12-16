4224b72e9616ffed6fe05caec3b64396536e69a4
Nitin Kumbhar <nkumbhar@nvidia.com>
2015-12-16 10:48:32 +0530

    platform: tegra: mc-sid: add NO_OVERRIDE for APE
    Replace OVERRIDE with NO_OVERRIDE in mc override flag for
    APE clients - APER, APEW, APEDMAR and APEDMAW. APE ACAST and
    ADAST are setup to specify required APESID or PhySID.
    
    Bug 200120400
    
    Change-Id: I4278d27f530b039f69689d6782604a2c0bb750fd
    Signed-off-by: Nitin Kumbhar <nkumbhar@nvidia.com>
    Reviewed-on: http://git-master/r/925237
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit

--- drivers/platform/tegra/mc/mc-sid.c
+++ drivers/platform/tegra/mc/mc-sid.c
@@ -1,7 +1,7 @@
 /*
  * MC StreamID configuration
  *
- * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2016, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -343,7 +343,7 @@ static struct sid_to_oids sid_to_oids[] = {
 			APEDMAR,
 			APEDMAW,
 		},
-		.ord = OVERRIDE,
+		.ord = NO_OVERRIDE,
 		.name = "APE",
 	},
 	{

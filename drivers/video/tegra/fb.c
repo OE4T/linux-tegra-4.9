73e6b1b4187e1a832f94099f6b9ebfc7de3a1c2a
Naveen Kumar S <nkumars@nvidia.com>
2016-01-05 18:07:08 +0530

    l4t: video: tegra: fb_blank cursor suspend/resume
    Cursor stays visible during fb normal-blank. Added cursor
    suspend and resume support during fb normal-blank and
    unblank for L4T.
    
    bug 200146453
    
    Change-Id: I7b862319664be6a2b419e9ec456412c45e9cd172
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: http://git-master/r/930314
    (cherry picked from commit 237dde58d896275ee4e1921574bee9dc5ee0f9d5)
    Reviewed-on: http://git-master/r/930923
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/tegra/fb.c
+++ drivers/video/tegra/fb.c
@@ -322,6 +322,9 @@ static int tegra_fb_blank(int blank, struct fb_info *info)
 	case FB_BLANK_UNBLANK:
 		dev_info(&tegra_fb->ndev->dev, "unblank\n");
 		tegra_dc_enable(dc);
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+		tegra_dc_cursor_resume(dc);
+#endif
 		if (!dc->suspended && dc->blanked &&
 		    !tegra_dc_restore(dc)) {
 			struct tegra_dc_win *win = &tegra_fb->win;
@@ -339,6 +342,9 @@ static int tegra_fb_blank(int blank, struct fb_info *info)
 		if (dc->enabled)
 			tegra_fb->curr_xoffset = -1;
 		dc->blanked = true;
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+		tegra_dc_cursor_suspend(dc);
+#endif
 		tegra_dc_blank(dc, BLANK_ALL);
 		return 0;
 

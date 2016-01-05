b8ff52f677643604ec8a54b1c0934f37db590d71
navneet kumar <navneetk@nvidia.com>
2016-01-04 16:44:13 -0800

    thermal: of: remove unnecessary NULL checks
    As a part of Coverity defects, remove unnecessary NULL checks
    
    Coverity DefectId's:
    13212: Array compared against zero
    13213: Array compared against zero
    
    Bug 200116059
    
    Change-Id: I215b68bd20cc8ee63ea9c9406c370015778e2231
    Signed-off-by: navneet kumar <navneetk@nvidia.com>
    Reviewed-on: http://git-master/r/928838
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Timo Alho <talho@nvidia.com>

--- drivers/thermal/of-thermal.c
+++ drivers/thermal/of-thermal.c
@@ -220,8 +220,7 @@ static int of_thermal_bind(struct thermal_zone_device *thermal,
 	for (i = 0; i < data->num_tbps; i++) {
 		struct __thermal_bind_params *tbp = data->tbps + i;
 
-		if ((tbp->cooling_device == cdev->np) ||
-			(tbp->type && cdev->type &&
+		if ((tbp->cooling_device == cdev->np) || (tbp->type &&
 			!strncmp(tbp->type, cdev->type, THERMAL_NAME_LENGTH))) {
 			int ret;
 
@@ -251,8 +250,7 @@ static int of_thermal_unbind(struct thermal_zone_device *thermal,
 	for (i = 0; i < data->num_tbps; i++) {
 		struct __thermal_bind_params *tbp = data->tbps + i;
 
-		if ((tbp->cooling_device == cdev->np) ||
-			(tbp->type && cdev->type &&
+		if ((tbp->cooling_device == cdev->np) || (tbp->type &&
 			!strncmp(tbp->type, cdev->type, THERMAL_NAME_LENGTH))) {
 			int ret;
 

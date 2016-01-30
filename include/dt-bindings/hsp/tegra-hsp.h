47b8925a70587d1e81b92eda0fa4dbdf39c42214
Songhee Baek <sbaek@nvidia.com>
2016-01-29 16:30:28 -0800

    dt-bindings: hsp: Add DB for APE
    JIRA EN-631
    Bug 1724933
    
    Change-Id: I7aa20cd540d3a8dfd0e7bfc0edde34c7c2ecda42
    Signed-off-by: Songhee Baek <sbaek@nvidia.com>
    Reviewed-on: http://git-master/r/1000322
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Pekka Pessi <ppessi@nvidia.com>
    Reviewed-by: Mika Liljeberg <mliljeberg@nvidia.com>

--- include/dt-bindings/hsp/tegra-hsp.h
+++ include/dt-bindings/hsp/tegra-hsp.h
@@ -1,7 +1,7 @@
 /*
  * This header provides macros for Tegra IVC-based IPC bindings.
  *
- * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2016, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -23,9 +23,11 @@
 #define TEGRA_HSP_PROC_BPMP		19
 #define TEGRA_HSP_PROC_SPE		20
 #define TEGRA_HSP_PROC_SCE		21
+#define TEGRA_HSP_PROC_APE		27
 
 #define TEGRA_HSP_DB_BPMP		3
 #define TEGRA_HSP_DB_SPE		4
 #define TEGRA_HSP_DB_SCE		5
+#define TEGRA_HSP_DB_APE		6
 
 #endif

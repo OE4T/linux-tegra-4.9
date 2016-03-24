d5544f856c37da1a5c5382e74d9817751aca3f99
Ken Chang <kenc@nvidia.com>
2016-03-24 12:56:16 +0800

    drivers: media: camera: enable use_fh_prio
    Set V4L2_FL_USE_FH_PRIO to enable v4l2 framework to handle
    VIDIOC_G/S_PRIORITY ioctls.
    
    Bug 200184254
    
    Change-Id: Icedae53962bcf942d4a58efc8e2c85825397aaee
    Signed-off-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    Reviewed-on: http://git-master/r/1115295
    (cherry picked from commit 2de13a41fa1e1f253db78477133f310b236cc9d2)
    Reviewed-on: http://git-master/r/1153842
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jihoon Bang <jbang@nvidia.com>

--- drivers/media/platform/tegra/camera/channel.c
+++ drivers/media/platform/tegra/camera/channel.c
@@ -1500,6 +1500,8 @@ static int tegra_channel_init(struct tegra_mc_vi *vi, unsigned int index)
 	chan->video.ctrl_handler = &chan->ctrl_handler;
 	chan->video.lock = &chan->video_lock;
 
+	set_bit(V4L2_FL_USE_FH_PRIO, &chan->video.flags);
+
 	video_set_drvdata(&chan->video, chan);
 
 #if defined(CONFIG_VIDEOBUF2_DMA_CONTIG)

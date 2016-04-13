ff983430f3dfff3ed9a5af658c5a664fc28a0e9e
Shardar Shariff Md <smohammed@nvidia.com>
2016-04-13 14:59:56 +0530

    serial: tegra: add support to ignore read
    Add support to ignore read characters if
    CREAD flag is not set.
    
    Bug 1735481
    
    Change-Id: I6b99e3fab7363890d44fe839a829aaceddbe411e
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: http://git-master/r/1126074
    Reviewed-on: http://git-master/r/1131958
    (cherry picked from commit 232d21d792d958be2b646a18d760670b6277740a)

--- drivers/tty/serial/serial-tegra.c
+++ drivers/tty/serial/serial-tegra.c
@@ -610,6 +610,9 @@ static void tegra_uart_handle_rx_pio(struct tegra_uart_port *tup,
 		ch = (unsigned char) tegra_uart_read(tup, UART_RX);
 		tup->uport.icount.rx++;
 
+		if (tup->uport.ignore_status_mask & UART_LSR_DR)
+			continue;
+
 		if (!uart_handle_sysrq_char(&tup->uport, ch) && tty)
 			tty_insert_flip_char(tty, ch, flag);
 	} while (1);
@@ -630,6 +633,10 @@ static void tegra_uart_copy_rx_to_tty(struct tegra_uart_port *tup,
 		dev_err(tup->uport.dev, "No tty port\n");
 		return;
 	}
+
+	if (tup->uport.ignore_status_mask & UART_LSR_DR)
+		return;
+
 	dma_sync_single_for_cpu(tup->uport.dev, tup->rx_dma_buf_phys,
 				TEGRA_UART_RX_DMA_BUFFER_SIZE, DMA_FROM_DEVICE);
 	copied = tty_insert_flip_string(tty,
@@ -1274,6 +1281,11 @@ static void tegra_uart_set_termios(struct uart_port *u,
 	/* Reenable interrupt */
 	tegra_uart_write(tup, tup->ier_shadow, UART_IER);
 	tegra_uart_read(tup, UART_IER);
+
+	tup->uport.ignore_status_mask = 0;
+	/* Ignore all characters if CREAD is not set */
+	if ((termios->c_cflag & CREAD) == 0)
+		tup->uport.ignore_status_mask |= UART_LSR_DR;
 
 	spin_unlock_irqrestore(&u->lock, flags);
 }

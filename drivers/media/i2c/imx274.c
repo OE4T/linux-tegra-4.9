d55c90d94c4d000b59ca37b07fe6c71c2c772768
Bhanu Murthy V <bmurthyv@nvidia.com>
2016-04-25 12:56:38 -0700

    drivers: media: i2c: IMX274 input status
    Add g_input_status video op and return the status
    based on the pw->state value.
    
    Bug 200184502
    
    Change-Id: Id8ac224d1dad679c2fb7280758b2a5df0c6f3877
    Signed-off-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    Reviewed-on: http://git-master/r/1132077
    (cherry picked from commit ef3f07d48d2b863bccdd82113064c35ca11233f0)
    Reviewed-on: http://git-master/r/1147741
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jihoon Bang <jbang@nvidia.com>

--- drivers/media/i2c/imx274.c
+++ drivers/media/i2c/imx274.c
@@ -505,6 +505,17 @@ static int imx274_set_fmt(struct v4l2_subdev *sd,
 	return ret;
 }
 
+static int imx274_g_input_status(struct v4l2_subdev *sd, u32 *status)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	struct camera_common_data *s_data = to_camera_common_data(client);
+	struct imx274 *priv = (struct imx274 *)s_data->priv;
+	struct camera_common_power_rail *pw = &priv->power;
+
+	*status = pw->state == SWITCH_ON;
+	return 0;
+}
+
 static struct v4l2_subdev_video_ops imx274_subdev_video_ops = {
 	.s_stream	= imx274_s_stream,
 	.s_mbus_fmt	= camera_common_s_fmt,
@@ -512,6 +523,7 @@ static struct v4l2_subdev_video_ops imx274_subdev_video_ops = {
 	.try_mbus_fmt	= camera_common_try_fmt,
 	.enum_mbus_fmt	= camera_common_enum_fmt,
 	.g_mbus_config	= camera_common_g_mbus_config,
+	.g_input_status	= imx274_g_input_status,
 };
 
 static struct v4l2_subdev_core_ops imx274_subdev_core_ops = {

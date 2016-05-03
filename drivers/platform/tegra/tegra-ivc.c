8ef22b17fdebbd7f459baa5eeac919848aaabfdb
Vlad Buzov <vbuzov@nvidia.com>
2016-05-02 17:41:37 -0700

    ivc: fix incorrect counter reset
    We must not reset locally owned counters when a local
    end in the ESTABLISHED state observes the remote end in
    the ACK state. Otherwise, data loss would result.
    
    Fix this condition by being explicit in the handling of
    state transitions. Now, we handle each state transition
    individually as opposed to generally.
    
    Bug 1757686
    
    Change-Id: I9d35f1bafcbb9148fff97223f6071ca8fac7119c
    Reviewed-on: http://git-master/r/1139988
    (cherry picked from commit 46ea2cc294c8edd8c04c3dd8ff4f0131346630de)
    Reviewed-on: http://git-master/r/1146931
    Reviewed-on: http://git-master/r/1198264
    Signed-off-by: Aniruddha Banerjee <aniruddhab@nvidia.com>
    Reviewed-on: http://git-master/r/1323005
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Timo Alho <talho@nvidia.com>

--- drivers/platform/tegra/tegra-ivc.c
+++ drivers/platform/tegra/tegra-ivc.c
@@ -722,11 +722,34 @@ int tegra_ivc_channel_notified(struct ivc *ivc)
 		 */
 		ivc->notify(ivc);
 
-	} else if (ivc->tx_channel->state == ivc_state_ack) {
+	} else if (ivc->tx_channel->state == ivc_state_ack &&
+			peer_state == ivc_state_established) {
 		/*
 		 * At this point, we have observed the peer to be in either
 		 * the ACK or ESTABLISHED state. Next, order observation of
 		 * peer state before storing to tx_channel.
+		 */
+		ivc_rmb();
+
+		/*
+		 * Move to ESTABLISHED state. We know that we have previously
+		 * cleared our counters, and we know that the remote end has
+		 * cleared its counters, so it is safe to start writing/reading
+		 * on this channel.
+		 */
+		ivc->tx_channel->state = ivc_state_established;
+		ivc_flush_counter(ivc, ivc->tx_handle +
+				offsetof(struct ivc_channel_header, w_count));
+
+		/*
+		 * There is no need to notify the remote end. The peer is
+		 * already in the ESTABLISHED state.
+		 */
+
+	} else if (ivc->tx_channel->state == ivc_state_ack &&
+			peer_state == ivc_state_ack) {
+		/*
+		 * Order observation of peer state before storing to tx_channel.
 		 */
 		ivc_rmb();
 

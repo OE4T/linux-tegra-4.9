205559cf31212af1c3c602f4889421748a433416
Terje Bergstrom <tbergstrom@nvidia.com>
2016-05-12 10:19:22 -0700

    gpu: nvgpu: Remove setting op set_max_ways_evict_last
    Do not set op set_max_ways_evict_last. It gets removed from
    ltc_gk20a.c, and it's never called in gm20b and beyond anyway.
    
    Change-Id: Ib8851057810aa8ddf2088c9e9245e4caf469bddf
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/1146882

--- drivers/gpu/nvgpu/gp10b/ltc_gp10b.c
+++ drivers/gpu/nvgpu/gp10b/ltc_gp10b.c
@@ -206,7 +206,6 @@ static void gp10b_ltc_init_fs_state(struct gk20a *g)
 void gp10b_init_ltc(struct gpu_ops *gops)
 {
 	gops->ltc.determine_L2_size_bytes = gp10b_determine_L2_size_bytes;
-	gops->ltc.set_max_ways_evict_last = gk20a_ltc_set_max_ways_evict_last;
 	gops->ltc.set_zbc_color_entry = gk20a_ltc_set_zbc_color_entry;
 	gops->ltc.set_zbc_depth_entry = gk20a_ltc_set_zbc_depth_entry;
 	gops->ltc.init_cbc = gk20a_ltc_init_cbc;

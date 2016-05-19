6a6f4564a6717559043ab184e79c646010c26ef5
Sai Gurrappadi <sgurrappadi@nvidia.com>
2016-05-18 18:41:17 -0700

    cpufreq: schedutil: Only update rate limit on freq. changes
    Updating the rate limit timestamp regardless of whether the actual
    frequency changed or not causes frequency requests to get throttled
    instead of just the actual frequency changes being throttled.
    
    Fix this by only updating the rate limit time stamp when the actual
    requested frequency as clipped to the table changes.
    
    Based on a patch by Steve Muckle <steve.muckle@linaro.org>.
    
    TPM-955
    
    Conflicts:
    	kernel/sched/cpufreq_schedutil.c
    
    (cherry picked from commit c0454b39a2c462d57e1fc69e5248ed69c82a0a5b)
    
    Change-Id: I740dd93d2bd43a89777a942bd99e7fb15b8aa1c3
    Signed-off-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    Reviewed-on: http://git-master/r/1164516
    Reviewed-on: http://git-master/r/1479502
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -92,8 +92,6 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
 
-	sg_policy->last_freq_update_time = time;
-
 	if (policy->fast_switch_enabled) {
 		if (sg_policy->next_freq == next_freq) {
 			trace_cpu_frequency(policy->cur, smp_processor_id());
@@ -106,10 +104,12 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 
 		policy->cur = next_freq;
 		trace_cpu_frequency(next_freq, smp_processor_id());
+		sg_policy->last_freq_update_time = time;
 	} else if (sg_policy->next_freq != next_freq) {
 		sg_policy->next_freq = next_freq;
 		sg_policy->work_in_progress = true;
 		irq_work_queue(&sg_policy->irq_work);
+		sg_policy->last_freq_update_time = time;
 	}
 }
 

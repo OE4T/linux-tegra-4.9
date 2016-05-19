77d19570f480733393ecc36219052ce9fe3298da
Sri Krishna chowdary <schowdary@nvidia.com>
2016-05-19 17:17:33 +0530

    tegra: gpcdma: remove unused functions
    This is found while compiling kernel using clang
    
    bug 1745660
    
    Change-Id: I895bba3573ffb3b24542d07565f8fed3f7132d5f
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1150420
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>

--- drivers/dma/tegra186-gpc-dma.c
+++ drivers/dma/tegra186-gpc-dma.c
@@ -273,16 +273,6 @@ struct tegra_dma {
 	struct tegra_dma_channel channels[0];
 };
 
-static inline void tdma_write(struct tegra_dma *tdma, u32 reg, u32 val)
-{
-	writel(val, tdma->base_addr + reg);
-}
-
-static inline u32 tdma_read(struct tegra_dma *tdma, u32 reg)
-{
-	return readl(tdma->base_addr + reg);
-}
-
 static inline void tdc_write(struct tegra_dma_channel *tdc,
 		u32 reg, u32 val)
 {

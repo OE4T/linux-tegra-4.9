79938b59dcf1849a05cd9333e1f80a3632010f41
Remi Denis-Courmont <remid@nvidia.com>
2016-05-31 17:04:04 +0300

    rtcpu: simplify legacy sysfs mbox driver
    Signed-off-by: Remi Denis-Courmont <remid@nvidia.com>
    Change-Id: Ib994dddebf63facf84c476656aacf3b1abe61f1c
    Reviewed-on: http://git-master/r/1156560
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jan Solanti <jsolanti@nvidia.com>
    Reviewed-by: Pekka Pessi <ppessi@nvidia.com>
    Reviewed-by: Mika Liljeberg <mliljeberg@nvidia.com>

--- drivers/platform/tegra/rtcpu/tegra-sysfs-mbox.c
+++ drivers/platform/tegra/rtcpu/tegra-sysfs-mbox.c
@@ -25,43 +25,30 @@
 
 #define NV(p) "nvidia," #p
 
-struct sysfs_mbox {
-	struct device *legacy;
-	bool has_data;
-	u8 data[];
-};
-
-static void sysfs_mbox_notify(struct tegra_ivc_channel *chan)
+static ssize_t sysfs_mbox_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
 {
-	struct sysfs_mbox *tsm = tegra_ivc_channel_get_drvdata(chan);
+	struct tegra_ivc_channel *chan = dev_get_drvdata(dev);
+	size_t length = chan->ivc.frame_size;
+	ssize_t count = 0;
 
-	while (tegra_ivc_can_read(&chan->ivc)) {
+	dev_err(dev, "mbox is deprecated\n");
+
+	while ((count + length < PAGE_SIZE) &&
+		tegra_ivc_can_read(&chan->ivc)) {
 		const void *data = tegra_ivc_read_get_next_frame(&chan->ivc);
-		size_t length = chan->ivc.frame_size;
 
 		dev_dbg(&chan->dev, "rx msg\n");
 		print_hex_dump(KERN_DEBUG, "", DUMP_PREFIX_OFFSET,
 				16, 1, data, length, true);
 
-		memcpy(tsm->data, data, length);
-		tsm->has_data = true;
+		memcpy(buf, data, length);
 		tegra_ivc_read_advance(&chan->ivc);
+		buf += length;
+		count += length;
 	}
-}
 
-static ssize_t sysfs_mbox_show(struct device *dev,
-	struct device_attribute *attr, char *buf)
-{
-	struct tegra_ivc_channel *chan = dev_get_drvdata(dev);
-	struct sysfs_mbox *tsm = tegra_ivc_channel_get_drvdata(chan);
-
-	if (!tsm->has_data)
-		return 0;
-
-	dev_err(dev, "mbox is deprecated\n");
-	memcpy(buf, tsm->data, chan->ivc.frame_size);
-	tsm->has_data = false;
-	return chan->ivc.frame_size;
+	return count;
 }
 
 static ssize_t sysfs_mbox_store(struct device *dev,
@@ -87,8 +74,6 @@ static const DEVICE_ATTR(mbox, 0600, sysfs_mbox_show, sysfs_mbox_store);
 
 /* This is a quick-and-dirty hack driver. Do not extend. */
 static const struct of_device_id sysfs_mbox_of_match[] = {
-	{ .compatible = "nvidia,tegra186-camera-ivc-protocol-echo",
-		.data = "rtcpu-echo" },
 	{ .compatible = "nvidia,tegra186-camera-ivc-protocol-mods",
 		.data = "sce-mods" },
 	{ },
@@ -97,11 +82,9 @@ static const struct of_device_id sysfs_mbox_of_match[] = {
 /* Tegra IVC channel device */
 static int sysfs_mbox_probe(struct tegra_ivc_channel *chan)
 {
-	struct sysfs_mbox *tsm;
 	struct device *legacy;
 	const struct of_device_id *id = of_match_device(sysfs_mbox_of_match,
 							&chan->dev);
-	size_t frame_size = chan->ivc.frame_size;
 	int err;
 
 	if (id == NULL)
@@ -109,19 +92,13 @@ static int sysfs_mbox_probe(struct tegra_ivc_channel *chan)
 
 	BUG_ON(id->data == NULL);
 
-	tsm = devm_kmalloc(&chan->dev, sizeof(*tsm) + frame_size, GFP_KERNEL);
-	if (unlikely(tsm == NULL))
-		return -ENOMEM;
-
-	tegra_ivc_channel_set_drvdata(chan, tsm);
-	tsm->has_data = false;
 	legacy = root_device_register(id->data);
 
 	if (IS_ERR(legacy))
 		return PTR_ERR(legacy);
 
 	dev_set_drvdata(legacy, chan);
-	tsm->legacy = legacy;
+	tegra_ivc_channel_set_drvdata(chan, legacy);
 
 	err = sysfs_create_link(&legacy->kobj, &chan->dev.kobj, "ivc-channel");
 	if (err)
@@ -145,8 +122,7 @@ err1:
 
 static void sysfs_mbox_remove(struct tegra_ivc_channel *chan)
 {
-	struct sysfs_mbox *tsm = tegra_ivc_channel_get_drvdata(chan);
-	struct device *legacy = tsm->legacy;
+	struct device *legacy = tegra_ivc_channel_get_drvdata(chan);
 
 	device_remove_file(legacy, &dev_attr_mbox);
 	sysfs_delete_link(&chan->dev.kobj, &legacy->kobj, "legacy");
@@ -157,7 +133,6 @@ static void sysfs_mbox_remove(struct tegra_ivc_channel *chan)
 static const struct tegra_ivc_channel_ops tegra_ivc_channel_sysfs_ops = {
 	.probe	= sysfs_mbox_probe,
 	.remove	= sysfs_mbox_remove,
-	.notify	= sysfs_mbox_notify,
 };
 
 static struct tegra_ivc_driver sysfs_mbox_driver = {

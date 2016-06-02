731489a698770fc2fbb2ec4bb5b09f01bafe072a
Pekka Pessi <ppessi@nvidia.com>
2016-06-02 14:55:00 +0300

    camera: rtcpu: update debugfs
    Add nodes for ASTs and rtos/state. Add more timestamps to ping results.
    
    Jira CRTC-124
    Bug 1699448
    
    Change-Id: Ie7ff06a32158f63cdd32a7979227f8b29e0391b3
    Signed-off-by: Pekka Pessi <ppessi@nvidia.com>
    Reviewed-on: http://git-master/r/1159513
    Tested-by: Remi Denis-Courmont <remid@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Remi Denis-Courmont <remid@nvidia.com>
    Reviewed-by: Mika Liljeberg <mliljeberg@nvidia.com>

--- drivers/platform/tegra/rtcpu/debug.c
+++ drivers/platform/tegra/rtcpu/debug.c
@@ -14,16 +14,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "camrtc-dbg-messages.h"
+
 #include <linux/debugfs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/slab.h>
+#include <linux/tegra_ast.h>
 #include <linux/tegra-camera-rtcpu.h>
 #include <linux/tegra-ivc.h>
 #include <linux/tegra-ivc-bus.h>
-
-#include "camrtc-dbg-messages.h"
 
 struct camrtc_debug {
 	struct mutex mutex;
@@ -34,6 +35,8 @@ struct camrtc_debug {
 		u32 mods_loops;
 	} parameters;
 };
+
+#define NV(x) "nvidia," #x
 
 /* Get a camera-rtcpu device */
 static struct device *camrtc_get_device(struct tegra_ivc_channel *ch)
@@ -77,7 +80,7 @@ static int camrtc_show_reboot(struct seq_file *file, void *data)
 	return 0;
 }
 
-DEFINE_SEQ_FOPS(fops_reboot, camrtc_show_reboot);
+DEFINE_SEQ_FOPS(camrtc_dbgfs_fops_reboot, camrtc_show_reboot);
 
 static int show_halt(void *data, u64 *val)
 {
@@ -102,7 +105,8 @@ static int store_halt(void *data, u64 val)
 	return tegra_camrtc_set_halt(rce_dev, val != 0);
 }
 
-DEFINE_SIMPLE_ATTRIBUTE(fops_halt, show_halt, store_halt, "%lld\n");
+DEFINE_SIMPLE_ATTRIBUTE(camrtc_dbgfs_fops_halt,
+			show_halt, store_halt, "%lld\n");
 
 static int camrtc_show_reset(struct seq_file *file, void *data)
 {
@@ -116,7 +120,7 @@ static int camrtc_show_reset(struct seq_file *file, void *data)
 	return 0;
 }
 
-DEFINE_SEQ_FOPS(fops_reset, camrtc_show_reset);
+DEFINE_SEQ_FOPS(camrtc_dbgfs_fops_reset, camrtc_show_reset);
 
 static void camrtc_debug_notify(struct tegra_ivc_channel *ch)
 {
@@ -196,22 +200,34 @@ static int camrtc_show_ping(struct seq_file *file, void *data)
 		.req_type = CAMRTC_REQ_PING,
 	};
 	struct camrtc_dbg_response resp;
-	union ktime sent, recv;
+	u64 sent, recv, tsc;
 	int ret = 0;
 
-	sent = ktime_get_raw();
-	req.data.ping_data.ts_req = sent.tv64;
+	sent = sched_clock();
+	req.data.ping_data.ts_req = sent;
 
 	ret = camrtc_ivc_dbg_xact(ch, &req, &resp, 0);
-	if (ret == 0) {
-		recv = ktime_get_raw();
-		seq_printf(file, "roundtrip=%llu\n", recv.tv64 - sent.tv64);
-	}
+	if (ret)
+		return ret;
 
-	return ret;
+	recv = sched_clock();
+	tsc = resp.data.ping_data.ts_resp;
+	seq_printf(file,
+		"roundtrip=%llu.%03llu us "
+		"(sent=%llu.%09llu recv=%llu.%09llu)\n",
+		(recv - sent) / 1000, (recv - sent) % 1000,
+		sent / 1000000000, sent % 1000000000,
+		recv / 1000000000, recv % 1000000000);
+	seq_printf(file,
+		"rtcpu tsc=%llu.%09llu offset=%llu.%09llu\n",
+		tsc / (1000000000 / 32), tsc % (1000000000 / 32),
+		(tsc * 32ULL - sent) / 1000000000,
+		(tsc * 32ULL - sent) % 1000000000);
+
+	return 0;
 }
 
-DEFINE_SEQ_FOPS(fops_ping, camrtc_show_ping);
+DEFINE_SEQ_FOPS(camrtc_dbgfs_fops_ping, camrtc_show_ping);
 
 static int camrtc_show_mods_result(struct seq_file *file, void *data)
 {
@@ -221,7 +237,7 @@ static int camrtc_show_mods_result(struct seq_file *file, void *data)
 		.req_type = CAMRTC_REQ_MODS_TEST,
 	};
 	struct camrtc_dbg_response resp;
-	int ret = 0;
+	int ret;
 	unsigned long timeout = crd->parameters.completion_timeout;
 	u32 loops = crd->parameters.mods_loops;
 
@@ -234,76 +250,185 @@ static int camrtc_show_mods_result(struct seq_file *file, void *data)
 	return ret;
 }
 
-DEFINE_SEQ_FOPS(fops_mods_result, camrtc_show_mods_result);
+DEFINE_SEQ_FOPS(camrtc_dbgfs_fops_mods_result, camrtc_show_mods_result);
 
-struct camrtc_dbgfs_node {
-	char *name;
-	mode_t mode;
-	const struct file_operations *fops;
+static int camrtc_dbgfs_show_freertos_state(struct seq_file *file, void *data)
+{
+	struct tegra_ivc_channel *ch = file->private;
+	struct camrtc_dbg_request req = {
+		.req_type = CAMRTC_REQ_RTOS_STATE,
+	};
+	struct camrtc_dbg_response resp;
+	int ret = 0;
+
+	ret = camrtc_ivc_dbg_xact(ch, &req, &resp, 0);
+	if (ret == 0) {
+		seq_printf(file, "%.*s",
+			(int) sizeof(resp.data.rtos_state_data.rtos_state),
+			resp.data.rtos_state_data.rtos_state);
+	}
+
+	return ret;
+}
+
+DEFINE_SEQ_FOPS(camrtc_dbgfs_fops_freertos_state,
+		camrtc_dbgfs_show_freertos_state);
+
+static void camrtc_dbgfs_show_ast_region(struct seq_file *file,
+				void __iomem *ast[],
+				int ast_dma,
+				u32 index)
+{
+	void __iomem *base = ast[ast_dma];
+	struct tegra_ast_region_info info;
+
+	tegra_ast_get_region_info(base, index, &info);
+
+	seq_printf(file, "ast%u region %u %s\n", ast_dma, index,
+		info.enabled ? "enabled" : "disabled");
+
+	if (!info.enabled)
+		return;
+
+	seq_printf(file,
+		"\tslave=0x%llx\n"
+		"\tmaster=0x%llx\n"
+		"\tsize=0x%llx\n"
+		"\tlock=%u snoop=%u non_secure=%u ns_passthru=%u\n"
+		"\tcarveout_id=%u carveout_al=%u\n"
+		"\tvpr_rd=%u vpr_wr=%u vpr_passthru=%u\n"
+		"\tvm_index=%u physical=%u\n"
+		"\tstream_id=%u enabled=%u\n",
+		info.slave, info.master, info.mask + 1,
+		info.lock, info.snoop,
+		info.non_secure, info.ns_passthru,
+		info.carveout_id, info.carveout_al,
+		info.vpr_rd, info.vpr_wr, info.vpr_passthru,
+		info.vm_index, info.physical,
+		info.stream_id, info.stream_id_enabled);
+}
+
+struct camrtc_dbgfs_ast_node
+{
+	struct tegra_ivc_channel *ch;
+	uint8_t dma;
+	uint8_t mask;
 };
 
-static const struct camrtc_dbgfs_node rce_nodes[] = {
-	{
-		.name = "reboot",
-		.mode = S_IRUGO,
-		.fops = &fops_reboot,
-	},
-	{
-		.name = "reset",
-		.mode = S_IRUGO,
-		.fops = &fops_reset,
-	},
-	{
-		.name = "halt",
-		.mode = S_IRUGO | S_IWUSR,
-		.fops = &fops_halt,
-	},
-	{
-		.name = "ping",
-		.mode = S_IRUGO,
-		.fops = &fops_ping,
-	},
-	{
-		.name = "mods",
-	},
-	{
-		.name = "result",
-		.mode = S_IRUGO,
-		.fops = &fops_mods_result,
-	},
-};
+static int camrtc_dbgfs_show_ast(struct seq_file *file,
+				void *data)
+{
+	struct camrtc_dbgfs_ast_node *node = file->private;
+	struct tegra_ivc_channel *ch = node->ch;
+	struct device *rce_dev = camrtc_get_device(ch);
+	void __iomem *ast[2];
+	int i, ret;
+
+	BUG_ON(node->dma >= ARRAY_SIZE(ast));
+
+	ret = tegra_ast_map(rce_dev, NV(ast), ARRAY_SIZE(ast), ast);
+	if (ret) {
+		dev_err(&ch->dev, "RTCPU ASTs not found: %d\n", ret);
+		return ret;
+	}
+
+	for (i = 0; i <= 7; i++) {
+		if (!(node->mask & BIT(i)))
+			continue;
+
+		camrtc_dbgfs_show_ast_region(file, ast, node->dma, i);
+
+		if (node->mask & (node->mask - 1)) /* are multiple bits set? */
+			seq_puts(file, "\n");
+	}
+
+	tegra_ast_unmap(rce_dev, ARRAY_SIZE(ast), ast);
+
+	return 0;
+}
+
+DEFINE_SEQ_FOPS(camrtc_dbgfs_fops_ast, camrtc_dbgfs_show_ast);
 
 static int camrtc_debug_populate(struct tegra_ivc_channel *ch)
 {
 	struct camrtc_debug *crd = tegra_ivc_channel_get_drvdata(ch);
 	struct dentry *dir;
-	int i;
+	struct camrtc_dbgfs_ast_node *ast_nodes;
+	unsigned dma, region;
 
 	crd->root = dir = debugfs_create_dir("camrtc", NULL);
 	if (dir == NULL)
 		return -ENOMEM;
 
-	if (!debugfs_create_u32("timeout", S_IRUGO|S_IWUSR, dir,
-				&crd->parameters.completion_timeout))
+	if (!debugfs_create_file("reboot", S_IRUGO, dir, ch,
+			&camrtc_dbgfs_fops_reboot))
+		goto error;
+	if (!debugfs_create_file("reset", S_IRUGO, dir, ch,
+			&camrtc_dbgfs_fops_reset))
+		goto error;
+	if (!debugfs_create_file("halt", S_IRUGO | S_IWUSR, dir, ch,
+			&camrtc_dbgfs_fops_halt))
+		goto error;
+	if (!debugfs_create_file("ping", S_IRUGO, dir, ch,
+			&camrtc_dbgfs_fops_ping))
+		goto error;
+	if (!debugfs_create_u32("timeout", S_IRUGO | S_IWUSR, dir,
+			&crd->parameters.completion_timeout))
 		goto error;
 
-	for (i = 0; i < ARRAY_SIZE(rce_nodes); i++) {
-		const struct camrtc_dbgfs_node *n = &rce_nodes[i];
-		struct dentry *d;
+	dir = debugfs_create_dir("mods", crd->root);
+	if (!dir)
+		goto error;
+	if (!debugfs_create_u32("loops", S_IRUGO | S_IWUSR, dir,
+			&crd->parameters.mods_loops))
+		goto error;
+	if (!debugfs_create_file("result", S_IRUGO, dir, ch,
+			&camrtc_dbgfs_fops_mods_result))
+		goto error;
 
-		if (n->fops == NULL)
-			d = dir = debugfs_create_dir(n->name, crd->root);
-		else
-			d = debugfs_create_file(n->name,
-				n->mode, dir, ch, n->fops);
+	dir = debugfs_create_dir("rtos", crd->root);
+	if (!dir)
+		goto error;
+	if (!debugfs_create_file("state", S_IRUGO, dir, ch,
+			&camrtc_dbgfs_fops_freertos_state))
+		goto error;
 
-		if (IS_ERR_OR_NULL(d))
-			return PTR_ERR(d);
+	ast_nodes = devm_kzalloc(&ch->dev, 18 * sizeof(*ast_nodes),
+					GFP_KERNEL);
+	if (unlikely(ast_nodes == NULL))
+		goto error;
+
+	for (dma = 0; dma <= 1; dma++) {
+		dir = debugfs_create_dir(dma ? "ast1" : "ast0", crd->root);
+		if (dir == NULL)
+			goto error;
+
+		ast_nodes->ch = ch;
+		ast_nodes->dma = dma;
+		ast_nodes->mask = 0xff;
+
+		if (!debugfs_create_file("all", S_IRUGO, dir, ast_nodes,
+						&camrtc_dbgfs_fops_ast))
+			goto error;
+
+		ast_nodes++;
+
+		for (region = 0; region < 8; region++) {
+			char name[8];
+
+			snprintf(name, sizeof name, "%u", region);
+
+			ast_nodes->ch = ch;
+			ast_nodes->dma = dma;
+			ast_nodes->mask = BIT(region);
+
+			if (!debugfs_create_file(name, S_IRUGO, dir, ast_nodes,
+						&camrtc_dbgfs_fops_ast))
+				goto error;
+
+			ast_nodes++;
+		}
 	}
-
-	if (!debugfs_create_u32("loops", S_IRUGO|S_IWUSR, dir,
-				&crd->parameters.mods_loops))
-		goto error;
 
 	return 0;
 error:
@@ -315,8 +440,6 @@ static int camrtc_debug_probe(struct tegra_ivc_channel *ch)
 {
 	struct device *dev = &ch->dev;
 	struct camrtc_debug *crd;
-
-	dev_info(dev, "probing");
 
 	BUG_ON(ch->ivc.frame_size < sizeof(struct camrtc_dbg_request));
 	BUG_ON(ch->ivc.frame_size < sizeof(struct camrtc_dbg_response));
@@ -335,8 +458,6 @@ static int camrtc_debug_probe(struct tegra_ivc_channel *ch)
 
 	if (camrtc_debug_populate(ch))
 		return -ENOMEM;
-
-	dev_info(dev, "probed OK");
 
 	return 0;
 }

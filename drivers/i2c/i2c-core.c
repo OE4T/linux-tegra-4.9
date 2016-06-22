d6e048c780d659d1dac146bdbc2a5b9411cdc6c9
Shardar Shariff Md <smohammed@nvidia.com>
2016-06-23 02:30:00 +0530

    i2c: core: skip prod-settings node during i2c device registration
    skip prod-settings node during i2c device registration as
    prod-settings is not i2c device
    
    Bug 200211824
    
    This patch is rebase of below original commit:
    319160d i2c: core: skip prod-settings node during i2c device registration
    
    Change-Id: I91230b9bf84873f534d2f971044b47262d37cd9e
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: http://git-master/r/1169690
    (cherry picked from commit 7d983143be400cd81e3df303ff0db3148ff41f79)

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -1732,6 +1732,8 @@ static void of_i2c_register_devices(struct i2c_adapter *adap)
 		bus = of_node_get(adap->dev.of_node);
 
 	for_each_available_child_of_node(bus, node) {
+		if (!strcmp(node->name, "prod-settings"))
+			continue;
 		if (of_node_test_and_set_flag(node, OF_POPULATED))
 			continue;
 

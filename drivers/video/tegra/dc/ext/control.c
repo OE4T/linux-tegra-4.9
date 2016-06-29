663b2cdc86d679c1131b0edcd20fa08f1d10a48a
Naveen Kumar S <nkumars@nvidia.com>
2016-06-29 10:42:23 +0530

    video: tegra: dc: ext: add API to detect userspace
    Defining an API to detect if userspace is active. Userspace entities
    register as users under EXT Control structure. This API checks the
    list of registered users to determine if any userspace entity is active.
    
    bug 200205112
    
    Change-Id: I5da99f67e5bd905d89403766378885fcd3cfaa3f
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: http://git-master/r/1173049
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/dc/ext/control.c
+++ drivers/video/tegra/dc/ext/control.c
@@ -47,6 +47,11 @@ int tegra_dc_ext_process_hotplug(int output)
 	return tegra_dc_ext_queue_hotplug(&g_control, output);
 }
 
+bool tegra_dc_ext_is_userspace_active(void)
+{
+	return !list_empty(&g_control.users);
+}
+
 int tegra_dc_ext_process_vblank(int output, ktime_t timestamp)
 {
 	return tegra_dc_ext_queue_vblank(&g_control, output, timestamp);

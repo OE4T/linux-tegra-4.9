d822dceac1fbbee99e9a07ea60439b751b9c2291
Daniel Solomon <daniels@nvidia.com>
2016-07-02 17:07:43 -0700

    video: tegra: dc: fix sparse warning
    $TOP/kernel/display/drivers/video/tegra/dc/hdmivrr.h:98:6: warning:
    symbol 'tegra_hdmivrr_update_monspecs' was not declared. Should it be
    static?
    $TOP/kernel/display/drivers/video/tegra/dc/hdmivrr.h:101:6: warning:
    symbol '_tegra_hdmivrr_activate' was not declared. Should it be static?
    
    Bug 200088648
    
    Change-Id: I9197dc1a74400b2d2fc8100fd596fc4f1bf9639d
    Signed-off-by: Daniel Solomon <daniels@nvidia.com>
    Reviewed-on: http://git-master/r/1174784
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/tegra/dc/hdmivrr.h
+++ drivers/video/tegra/dc/hdmivrr.h
@@ -94,11 +94,14 @@ int tegra_hdmi_vrr_init(struct tegra_hdmi *hdmi);
 void te_authenticate_vrr(u8 *buf_ptr, u32 buflen);
 void _tegra_hdmivrr_activate(struct tegra_hdmi *hdmi, bool activate);
 #else
-int tegra_hdmivrr_setup(struct tegra_hdmi *hdmi) { return -EPROTONOSUPPORT; }
-void tegra_hdmivrr_update_monspecs(struct tegra_dc *dc,
+static inline int tegra_hdmivrr_setup(struct tegra_hdmi *hdmi)
+{ return -EPROTONOSUPPORT; }
+static inline void tegra_hdmivrr_update_monspecs(struct tegra_dc *dc,
 	struct list_head *head) { return; }
-int tegra_hdmi_vrr_init(struct tegra_hdmi *hdmi) { return -EPROTONOSUPPORT; }
-void _tegra_hdmivrr_activate(struct tegra_hdmi *hdmi, bool activate)
+static inline int tegra_hdmi_vrr_init(struct tegra_hdmi *hdmi)
+{ return -EPROTONOSUPPORT; }
+static inline void _tegra_hdmivrr_activate(struct tegra_hdmi *hdmi,
+	bool activate)
 { return; }
 #endif
 

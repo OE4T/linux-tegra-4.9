32fe739f68e6307062a8a3f535be41208c97766d
Seema Khowala <seemaj@nvidia.com>
2016-07-07 13:47:48 -0700

    gpu: nvgpu: gv11b: updated hal and gpu_ops
    Added init_fb, init_fifo and get_litter_values.
    Also initialized read_ptimer
    
    Bug 1735760
    
    Change-Id: Ia1665d60392e9cc58db4cc3f292597aeaea0c718
    Signed-off-by: Seema Khowala <seemaj@nvidia.com>
    Reviewed-on: http://git-master/r/1177174
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Tested-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-by: Adeel Raza <araza@nvidia.com>

--- drivers/gpu/nvgpu/gv11b/hal_gv11b.c
+++ drivers/gpu/nvgpu/gv11b/hal_gv11b.c
@@ -31,13 +31,95 @@
 #include "gv11b/mm_gv11b.h"
 #include "gv11b/pmu_gv11b.h"
 #include "gv11b/therm_gv11b.h"
+#include "gv11b/fb_gv11b.h"
+#include "gv11b/fifo_gv11b.h"
+#include "gv11b/hw_proj_gv11b.h"
 
 #include "gm20b/gr_gm20b.h"
+
+static struct gpu_ops gv11b_ops;
+
+static int gv11b_get_litter_value(struct gk20a *g,
+		enum nvgpu_litter_value value)
+{
+	int ret = EINVAL;
+	switch (value) {
+	case GPU_LIT_NUM_GPCS:
+		ret = proj_scal_litter_num_gpcs_v();
+		break;
+	case GPU_LIT_NUM_PES_PER_GPC:
+		ret = proj_scal_litter_num_pes_per_gpc_v();
+		break;
+	case GPU_LIT_NUM_ZCULL_BANKS:
+		ret = proj_scal_litter_num_zcull_banks_v();
+		break;
+	case GPU_LIT_NUM_TPC_PER_GPC:
+		ret = proj_scal_litter_num_tpc_per_gpc_v();
+		break;
+	case GPU_LIT_NUM_FBPS:
+		ret = proj_scal_litter_num_fbps_v();
+		break;
+	case GPU_LIT_GPC_BASE:
+		ret = proj_gpc_base_v();
+		break;
+	case GPU_LIT_GPC_STRIDE:
+		ret = proj_gpc_stride_v();
+		break;
+	case GPU_LIT_GPC_SHARED_BASE:
+		ret = proj_gpc_shared_base_v();
+		break;
+	case GPU_LIT_TPC_IN_GPC_BASE:
+		ret = proj_tpc_in_gpc_base_v();
+		break;
+	case GPU_LIT_TPC_IN_GPC_STRIDE:
+		ret = proj_tpc_in_gpc_stride_v();
+		break;
+	case GPU_LIT_TPC_IN_GPC_SHARED_BASE:
+		ret = proj_tpc_in_gpc_shared_base_v();
+		break;
+	case GPU_LIT_PPC_IN_GPC_STRIDE:
+		ret = proj_ppc_in_gpc_stride_v();
+		break;
+	case GPU_LIT_ROP_BASE:
+		ret = proj_rop_base_v();
+		break;
+	case GPU_LIT_ROP_STRIDE:
+		ret = proj_rop_stride_v();
+		break;
+	case GPU_LIT_ROP_SHARED_BASE:
+		ret = proj_rop_shared_base_v();
+		break;
+	case GPU_LIT_HOST_NUM_ENGINES:
+		ret = proj_host_num_engines_v();
+		break;
+	case GPU_LIT_HOST_NUM_PBDMA:
+		ret = proj_host_num_pbdma_v();
+		break;
+	case GPU_LIT_LTC_STRIDE:
+		ret = proj_ltc_stride_v();
+		break;
+	case GPU_LIT_LTS_STRIDE:
+		ret = proj_lts_stride_v();
+		break;
+	case GPU_LIT_NUM_FBPAS:
+		ret = proj_scal_litter_num_fbpas_v();
+		break;
+	case GPU_LIT_FBPA_STRIDE:
+		ret = proj_fbpa_stride_v();
+		break;
+	default:
+		break;
+	}
+
+	return ret;
+}
 
 int gv11b_init_hal(struct gk20a *g)
 {
 	struct gpu_ops *gops = &g->ops;
 	struct nvgpu_gpu_characteristics *c = &g->gpu_characteristics;
+
+	*gops = gv11b_ops;
 
 	/* boot in non-secure modes for time beeing */
 	gops->privsecurity = 0;
@@ -47,14 +129,18 @@ int gv11b_init_hal(struct gk20a *g)
 	gv11b_init_ltc(gops);
 	gv11b_init_gr(gops);
 	gv11b_init_fecs_trace_ops(gops);
+	gv11b_init_fb(gops);
+	gv11b_init_fifo(gops);
 	gv11b_init_ce(gops);
 	gv11b_init_mm(gops);
 	gv11b_init_gr_ctx(gops);
 	gv11b_init_pmu_ops(gops);
-        gk20a_init_debug_ops(gops);
+	gk20a_init_debug_ops(gops);
 	gv11b_init_therm_ops(gops);
 	gops->name = "gv11b";
 	gops->chip_init_gpu_characteristics = gv11b_init_gpu_characteristics;
+	gops->get_litter_value = gv11b_get_litter_value;
+	gops->read_ptimer = gk20a_read_ptimer;
 
 	c->twod_class = FERMI_TWOD_A;
 	c->threed_class = VOLTA_A;

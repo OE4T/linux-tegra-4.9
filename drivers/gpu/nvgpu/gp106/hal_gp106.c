d2b67f1ad606733a63d8261e36068e5bd1f96cdc
David Nieto <dmartineznie@nvidia.com>
2016-08-08 03:13:37 -0700

    gpu: nvgpu: add debugfs to dump clocks
    * Removed unused registers from headers
    * Added counter based MCLK
    * Removed hardcoding
    
    JIRA DNVGPU-98
    
    Change-Id: Idffcd7fc17024582b41c29371a2295df8f0c206b
    Signed-off-by: David Nieto <dmartineznie@nvidia.com>
    Reviewed-on: http://git-master/r/1204019
    (cherry picked from commit 48dfa41a641c3adbc4d25a35f418cf73b08d5e8c)
    Reviewed-on: http://git-master/r/1227264
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gp106/hal_gp106.c
+++ drivers/gpu/nvgpu/gp106/hal_gp106.c
@@ -37,7 +37,7 @@
 #include "gm20b/gr_gm20b.h"
 #include "gm20b/fifo_gm20b.h"
 #include "gm20b/pmu_gm20b.h"
-#include "gm20b/clk_gm20b.h"
+#include "gp106/clk_gp106.h"
 
 #include "gp106/mm_gp106.h"
 #include "gp106/pmu_gp106.h"
@@ -156,6 +156,7 @@ int gp106_init_hal(struct gk20a *g)
 	gp106_init_pmu_ops(gops);
 	gk20a_init_debug_ops(gops);
 	gk20a_init_dbg_session_ops(gops);
+	gp106_init_clk_ops(gops);
 	gp10b_init_regops(gops);
 	gp10b_init_cde_ops(gops);
 	gk20a_init_tsg_ops(gops);

cfb01d86ef76eaa3b79a5f7480e8ef97c7ff4f59
Wenjia Zhou <wenjiaz@nvidia.com>
2016-08-11 17:11:57 -0700

    media:platform:tegra:mipical: Add T18x support
    - Port common mipical driver to k4.4.
    - Add T18x support
    - Fix/update driver code
    
    Bug 1686313
    Bug 1702917
    
    Change-Id: Ib774709fc63470704be88c99e1a35571c9f1c44d
    Signed-off-by: Wenjia Zhou <wenjiaz@nvidia.com>
    Reviewed-on: http://git-master/r/1171307

--- drivers/media/platform/tegra/mipical/mipi_cal_t18x.h
+++ drivers/media/platform/tegra/mipical/mipi_cal_t18x.h
@@ -13,14 +13,16 @@
  * GNU General Public License for more details.
  *
  */
-#define MIPI_CAL_CTRL		0x00
+
+#define MIPI_CAL_MODE		0x00
+#define MIPI_CAL_CTRL		0x04
 #define		NOISE_FLT	(0xf << 26)
 #define		PRESCALE	(0x3 << 24)
 #define		CLKEN_OVR	(1 << 4)
 #define		AUTOCAL_EN	(1 << 1)
 #define		STARTCAL	(1 << 0)
-#define MIPI_CAL_AUTOCAL_CTRL0	0x04
-#define CIL_MIPI_CAL_STATUS	0x08
+#define MIPI_CAL_AUTOCAL_CTRL0	0x08
+#define CIL_MIPI_CAL_STATUS	0x0c
 #define		CAL_DONE_DSID   (1 << 31)
 #define		CAL_DONE_DSIC	(1 << 30)
 #define		CAL_DONE_DSIB   (1 << 29)
@@ -33,8 +35,8 @@
 #define		CAL_DONE_CSIA   (1 << 20)
 #define		CAL_DONE	(1 << 16)
 #define		CAL_ACTIVE	(1 << 0)
-#define CIL_MIPI_CAL_STATUS_2	0x0c
-#define CILA_MIPI_CAL_CONFIG	0x14
+#define CIL_MIPI_CAL_STATUS_2	0x10
+#define CILA_MIPI_CAL_CONFIG	0x18
 #define		OVERIDEA	(1 << 30)
 #define		OVERIDEA_SHIFT	30
 #define		SELA		(1 << 21)
@@ -42,17 +44,17 @@
 #define		TERMOSA_CLK_SHIFT	11
 #define		TERMOSA		0x1f
 #define		TERMOSA_SHIFT	0
-#define CILB_MIPI_CAL_CONFIG	0x18
+#define CILB_MIPI_CAL_CONFIG	0x1c
 #define		SELB		(1 << 21)
-#define CILC_MIPI_CAL_CONFIG	0x1c
+#define CILC_MIPI_CAL_CONFIG	0x20
 #define		SELC		(1 << 21)
-#define CILD_MIPI_CAL_CONFIG	0x20
+#define CILD_MIPI_CAL_CONFIG	0x24
 #define		SELD		(1 << 21)
-#define CILE_MIPI_CAL_CONFIG	0x24
+#define CILE_MIPI_CAL_CONFIG	0x28
 #define		SELE		(1 << 21)
-#define CILF_MIPI_CAL_CONFIG	0x28
+#define CILF_MIPI_CAL_CONFIG	0x2c
 #define		SELF		(1 << 21)
-#define DSIA_MIPI_CAL_CONFIG	0x38
+#define DSIA_MIPI_CAL_CONFIG	0x3c
 #define		OVERIDEDSIA	(1 << 30)
 #define		OVERIDEDSIA_SHIFT 30
 #define		SELDSIA		(1 << 21)
@@ -62,35 +64,35 @@
 #define		HSPUOSDSIA_SHIFT 8
 #define		TERMOSDSIA	0x1f
 #define		TERMOSDSIA_SHIFT 0
-#define DSIB_MIPI_CAL_CONFIG	0x3c
+#define DSIB_MIPI_CAL_CONFIG	0x40
 #define		SELDSIB		(1 << 21)
 #define         HSPUOSDSIB	(0x1f << 8)
 #define		HSPUOSDSIB_SHIFT 8
-#define DSIC_MIPI_CAL_CONFIG	0x40
+#define DSIC_MIPI_CAL_CONFIG	0x44
 #define		SELDSIC		(1 << 21)
 #define         HSPUOSDSIC	(0x1f << 8)
 #define		HSPUOSDSIC_SHIFT 8
-#define DSID_MIPI_CAL_CONFIG	0x44
+#define DSID_MIPI_CAL_CONFIG	0x48
 #define		SELDSID		(1 << 21)
 #define         HSPUOSDSID	(0x1f << 8)
 #define		HSPUOSDSID_SHIFT 8
-#define MIPI_BIAS_PAD_CFG0	0x58
+#define MIPI_BIAS_PAD_CFG0	0x5c
 #define		E_VCLAMP_REF	(1 << 0)
 #define		E_VCLAMP_REF_SHIFT	0
 #define		PDVCLAMP	(1 << 1)
 #define		PDVCLAMP_SHIFT	1
-#define MIPI_BIAS_PAD_CFG1	0x5c
+#define MIPI_BIAS_PAD_CFG1	0x60
 #define		PAD_DRIV_UP_REF (0x7 << 8)
 #define		PAD_DRIV_UP_REF_SHIFT 8
 #define		PAD_DRIV_DN_REF (0x7 << 16)
 #define		PAD_DRIV_DN_REF_SHIFT 16
-#define MIPI_BIAS_PAD_CFG2	0x60
+#define MIPI_BIAS_PAD_CFG2	0x64
 #define		PDVREG		(1 << 1)
 #define		PAD_VAUXP_LEVEL (0x7 << 4)
 #define		PAD_VAUXP_LEVEL_SHIFT 4
 #define		PAD_VCLAMP_LEVEL (0x7 << 16)
 #define		PAD_VCLAMP_LEVEL_SHIFT 16
-#define DSIA_MIPI_CAL_CONFIG_2	0x64
+#define DSIA_MIPI_CAL_CONFIG_2	0x68
 #define		CLKOVERIDEDSIA	(1 << 30)
 #define		CLKOVERIDEDSIA_SHIFT 30
 #define		CLKSELDSIA	(1 << 21)
@@ -100,15 +102,15 @@
 #define		HSCLKPDOSDSIA_SHIFT 8
 #define		HSCLKPUOSDSIA	0x1f
 #define		HSCLKPUOSDSIA_SHIFT 0
-#define DSIB_MIPI_CAL_CONFIG_2	0x68
+#define DSIB_MIPI_CAL_CONFIG_2	0x6c
 #define		CLKSELDSIB	(1 << 21)
 #define		HSCLKPUOSDSIB	0x1f
 #define		HSCLKPUOSDSIB_SHIFT 0
-#define DSIC_MIPI_CAL_CONFIG_2	0x70
+#define DSIC_MIPI_CAL_CONFIG_2	0x74
 #define		CLKSELDSIC	(1 << 21)
 #define		HSCLKPUOSDSIC	0x1f
 #define		HSCLKPUOSDSIC_SHIFT 0
-#define DSID_MIPI_CAL_CONFIG_2	0x74
+#define DSID_MIPI_CAL_CONFIG_2	0x78
 #define		CLKSELDSID	(1 << 21)
 #define		HSCLKPUOSDSID	0x1f
 #define		HSCLKPUOSDSID_SHIFT 0

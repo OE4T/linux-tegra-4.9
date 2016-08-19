41ba1e8e4e1f9c5f74c19b3932cf4142a9f4b513
Pekka Pessi <ppessi@nvidia.com>
2016-08-19 14:27:54 +0300

    arm64: t186: add Stream IDs for camera CPUs
    Add TEGRA_SID_RCE and TEGRA_SID_RCE_1X for camera CPUs.
    
    Jira CRTC-467
    Bug 1799032
    
    Change-Id: Ib8df330be520afc27d0f0ba05c160f14fe423481
    Signed-off-by: Pekka Pessi <ppessi@nvidia.com>
    Reviewed-on: http://git-master/r/1205104
    Reviewed-by: Jihoon Bang <jbang@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mika Liljeberg <mliljeberg@nvidia.com>

--- include/dt-bindings/memory/tegra186-swgroup.h
+++ include/dt-bindings/memory/tegra186-swgroup.h
@@ -20,6 +20,12 @@
 #define TEGRA_SID_APE_1		0x28	/* 40 */
 #define TEGRA_SID_APE_2		0x29	/* 41 */
 
+/* The Camera RTCPU. */
+#define TEGRA_SID_RCE		0x2a	/* 42 */
+
+/* The Camera RTCPU on Host1x address space. */
+#define TEGRA_SID_RCE_1X	0x2b	/* 43 */
+
 /* Host1x virtualization clients. */
 #define TEGRA_SID_HOST1X_CTX0	0x38	/* 56 */
 #define TEGRA_SID_HOST1X_CTX1	0x39	/* 57 */

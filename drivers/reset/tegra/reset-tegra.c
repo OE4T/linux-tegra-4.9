4a4e2ba4b1577baf598d42f61ef0cdcad3528a5e
Sivaram Nair <sivaramn@nvidia.com>
2016-08-24 16:07:53 -0700

    reset: tegra: make the driver more generic
    and enable selectable probe points
    
    Bug 1787987
    
    Change-Id: If5cf701625cbf57d02fae4532636a370ac01f44e
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: http://git-master/r/1209434
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Timo Alho <talho@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/reset/tegra/reset-tegra.c
+++ drivers/reset/tegra/reset-tegra.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2016, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -16,11 +16,11 @@
 
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/reset-controller.h>
 #include <linux/slab.h>
 #include <soc/tegra/tegra_bpmp.h>
-
 #include <dt-bindings/reset/tegra186-reset.h>
 #include "reset.h"
 
@@ -29,33 +29,44 @@ static int tegra18x_reset_probe(struct platform_device *pdev)
 	return bpmp_register_reset(TEGRA186_RESET_SIZE, pdev);
 }
 
-static const struct of_device_id tegra18x_reset_match[] = {
-	{ .compatible = "nvidia,tegra18x-car", },
+static int tegra_reset_probe(struct platform_device *pdev)
+{
+	int (*probe)(struct platform_device *);
+
+	probe = of_device_get_match_data(&pdev->dev);
+	if (!probe)
+		return -ENODEV;
+
+	return probe(pdev);
+}
+
+static const struct of_device_id tegra_reset_match[] = {
+	{ .compatible = "nvidia,tegra18x-car", .data = tegra18x_reset_probe },
 	{ },
 };
 
-static struct platform_driver tegra18x_reset_driver = {
-	.probe = tegra18x_reset_probe,
+static struct platform_driver tegra_reset_driver = {
+	.probe = tegra_reset_probe,
 	.driver = {
-		.name		= "tegra18x-reset",
+		.name		= "tegra-reset",
 		.owner		= THIS_MODULE,
-		.of_match_table	= tegra18x_reset_match,
+		.of_match_table	= tegra_reset_match,
 	},
 };
 
-static int __init tegra18x_reset_driver_init(void)
+static int __init tegra_reset_driver_init(void)
 {
-	return platform_driver_register(&tegra18x_reset_driver);
+	return platform_driver_register(&tegra_reset_driver);
 }
 
-static void __exit tegra18x_reset_driver_exit(void)
+static void __exit tegra_reset_driver_exit(void)
 {
-	platform_driver_unregister(&tegra18x_reset_driver);
+	platform_driver_unregister(&tegra_reset_driver);
 }
 
-arch_initcall(tegra18x_reset_driver_init);
-module_exit(tegra18x_reset_driver_exit);
+arch_initcall(tegra_reset_driver_init);
+module_exit(tegra_reset_driver_exit);
 
 MODULE_AUTHOR("Peter De Schrijver <pdeschrijver@nvidia.com>");
-MODULE_DESCRIPTION("Tegra18x Controller Driver");
+MODULE_DESCRIPTION("Tegra Reset Controller Driver");
 MODULE_LICENSE("GPL");

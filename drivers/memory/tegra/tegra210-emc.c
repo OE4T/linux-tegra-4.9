952164979202742b80db9f448659b73fd879361b
David Pu <dpu@nvidia.com>
2016-09-01 13:56:40 -0700

    memory: tegra: fix build error when DEBUG_FS=n
    fix below build error by avoid calling tegra_emc_debug_init
    if CONFIG_DEBUG_FS is not set.
    
    tegra210-emc.c: In function 'tegra210_emc_probe':
    tegra210-emc.c:2186:2: error: implicit declaration of function
    'tegra_emc_debug_init' [-Werror=implicit-function-declaration]
      tegra_emc_debug_init();
      ^
    cc1: some warnings being treated as errors
    make[5]: *** [drivers/memory/tegra/tegra210-emc.o] Error 1
    make[4]: *** [drivers/memory/tegra] Error 2
    make[3]: *** [drivers/memory] Error 2
    make[2]: *** [drivers] Error 2
    
    Bug 1778001
    
    Change-Id: Icfde2aa10836356508feff8792e631f112e7d85f
    Signed-off-by: David Pu <dpu@nvidia.com>
    Reviewed-on: http://git-master/r/1213152
    (cherry picked from commit a5199ff9e3df70b013628a3a42b4940562b696b5)

--- drivers/memory/tegra/tegra210-emc.c
+++ drivers/memory/tegra/tegra210-emc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2016, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -2182,8 +2182,9 @@ static int tegra210_emc_probe(struct platform_device *pdev)
 		return ret;
 
 	tegra_emc_init_done = true;
-
+#ifdef CONFIG_DEBUG_FS
 	tegra_emc_debug_init();
+#endif
 
 	return 0;
 }

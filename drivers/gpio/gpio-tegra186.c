749044635b83051d252e574072ae0f876a5463da
Laxman Dewangan <ldewangan@nvidia.com>
2016-09-22 18:02:31 +0530

    gpio: t186: Make wakeup table for GPIO locally
    Keep the gpio to wake number conversion table locally so that
    it can be split in two parts when aon and non-aon gpio drivers
    are enabled.
    
    bug 200237366
    
    Change-Id: I4d71f635de40f313d2230175a36bf37811f6cbf5
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/1225233
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/gpio/gpio-tegra186.c
+++ drivers/gpio/gpio-tegra186.c
@@ -34,6 +34,7 @@
 #include <linux/pm.h>
 #include <linux/irqchip/tegra.h>
 #include <linux/version.h>
+#include <dt-bindings/gpio/tegra186-gpio.h>
 
 #define GPIO_ENB_CONFIG_REG	0x00
 #define GPIO_ENB_BIT		BIT(0)
@@ -128,6 +129,105 @@ static u32 tegra186_gpio_port_pin_count[MAX_PORTS] = {
 	6, 6, 5, 4, 6, 8, 8, 8, 7, 4, 8, 2, 4, 0, 3, 5,
 };
 
+static int tegra186_gpio_wakes[] = {
+	TEGRA_GPIO(A, 6),		/* wake0 */
+	TEGRA_GPIO(A, 2),		/* wake1 */
+	TEGRA_GPIO(A, 5),		/* wake2 */
+	TEGRA_GPIO(D, 3),		/* wake3 */
+	TEGRA_GPIO(E, 3),		/* wake4 */
+	TEGRA_GPIO(G, 3),		/* wake5 */
+	-EINVAL,			/* wake6 */
+	TEGRA_GPIO(B, 3),		/* wake7 */
+	TEGRA_GPIO(B, 5),		/* wake8 */
+	TEGRA_GPIO(C, 0),		/* wake9 */
+	TEGRA_GPIO(S, 2),		/* wake10 */
+	TEGRA_GPIO(H, 2),		/* wake11 */
+	TEGRA_GPIO(J, 5),		/* wake12 */
+	TEGRA_GPIO(J, 6),		/* wake13 */
+	TEGRA_GPIO(J, 7),		/* wake14 */
+	TEGRA_GPIO(K, 0),		/* wake15 */
+	TEGRA_GPIO(Q, 1),		/* wake16 */
+	TEGRA_GPIO(F, 4),		/* wake17 */
+	TEGRA_GPIO(M, 5),		/* wake18 */
+	TEGRA_GPIO(P, 0),		/* wake19 */
+	TEGRA_GPIO(P, 2),		/* wake20 */
+	TEGRA_GPIO(P, 1),		/* wake21 */
+	TEGRA_GPIO(O, 3),		/* wake22 */
+	TEGRA_GPIO(R, 5),		/* wake23 */
+	-EINVAL,			/* wake24 */
+	TEGRA_GPIO(S, 3),		/* wake25 */
+	TEGRA_GPIO(S, 4),		/* wake26 */
+	TEGRA_GPIO(S, 1),		/* wake27 */
+	TEGRA_GPIO(F, 2),		/* wake28 */
+	TEGRA_GPIO(FF, 0),		/* wake29 */
+	TEGRA_GPIO(FF, 4),		/* wake30 */
+	TEGRA_GPIO(C, 6),		/* wake31 */
+	TEGRA_GPIO(W, 2),		/* wake32 */
+	TEGRA_GPIO(W, 5),		/* wake33 */
+	TEGRA_GPIO(W, 1),		/* wake34 */
+	TEGRA_GPIO(V, 0),		/* wake35 */
+	TEGRA_GPIO(V, 1),		/* wake36 */
+	TEGRA_GPIO(V, 2),		/* wake37 */
+	TEGRA_GPIO(V, 3),		/* wake38 */
+	TEGRA_GPIO(V, 4),		/* wake39 */
+	TEGRA_GPIO(V, 5),		/* wake40 */
+	TEGRA_GPIO(EE, 0),		/* wake41 */
+	TEGRA_GPIO(Z, 1),		/* wake42 */
+	TEGRA_GPIO(Z, 3),		/* wake43 */
+	TEGRA_GPIO(AA, 0),		/* wake44 */
+	TEGRA_GPIO(AA, 1),		/* wake45 */
+	TEGRA_GPIO(AA, 2),		/* wake46 */
+	TEGRA_GPIO(AA, 3),		/* wake47 */
+	TEGRA_GPIO(AA, 4),		/* wake48 */
+	TEGRA_GPIO(AA, 5),		/* wake49 */
+	TEGRA_GPIO(AA, 6),		/* wake50 */
+	TEGRA_GPIO(AA, 7),		/* wake51 */
+	TEGRA_GPIO(X, 3),		/* wake52 */
+	TEGRA_GPIO(X, 7),		/* wake53 */
+	TEGRA_GPIO(Y, 0),		/* wake54 */
+	TEGRA_GPIO(Y, 1),		/* wake55 */
+	TEGRA_GPIO(Y, 2),		/* wake56 */
+	TEGRA_GPIO(Y, 5),		/* wake57 */
+	TEGRA_GPIO(Y, 6),		/* wake58 */
+	TEGRA_GPIO(L, 1),		/* wake59 */
+	TEGRA_GPIO(L, 3),		/* wake60 */
+	TEGRA_GPIO(L, 4),		/* wake61 */
+	TEGRA_GPIO(L, 5),		/* wake62 */
+	TEGRA_GPIO(I, 4),		/* wake63 */
+	TEGRA_GPIO(I, 6),		/* wake64 */
+	TEGRA_GPIO(Z, 0),		/* wake65 */
+	TEGRA_GPIO(Z, 2),		/* wake66 */
+	TEGRA_GPIO(FF, 1),		/* wake67 */
+	TEGRA_GPIO(FF, 2),		/* wake68 */
+	TEGRA_GPIO(FF, 3),		/* wake69 */
+	TEGRA_GPIO(H, 3),		/* wake70 */
+	TEGRA_GPIO(P, 5),		/* wake71 */
+	-EINVAL,		/* wake72 */
+	-EINVAL,		/* wake73 */
+	-EINVAL,		/* wake74 */
+	-EINVAL,		/* wake75 */
+	-EINVAL,		/* wake76 */
+	-EINVAL,		/* wake77 */
+	-EINVAL,		/* wake78 */
+	-EINVAL,		/* wake79 */
+	-EINVAL,		/* wake80 */
+	-EINVAL,		/* wake81 */
+	-EINVAL,		/* wake82 */
+	-EINVAL,		/* wake83 */
+	-EINVAL,		/* wake84 */
+	-EINVAL,		/* wake85 */
+	-EINVAL,		/* wake86 */
+	-EINVAL,		/* wake87 */
+	-EINVAL,		/* wake88 */
+	-EINVAL,		/* wake89 */
+	-EINVAL,		/* wake90 */
+	-EINVAL,		/* wake91 */
+	-EINVAL,		/* wake92 */
+	-EINVAL,		/* wake93 */
+	-EINVAL,		/* wake94 */
+	-EINVAL,		/* wake95 */
+};
+
 static u32 address_map[32][2];
 static u32 tegra_gpio_bank_count;
 static struct tegra_gpio_controller
@@ -147,6 +247,20 @@ static inline u32 controller_index(u32 gpio)
 		}
 	}
 	return -1;
+}
+
+static int tegra186_gpio_to_wake(int gpio)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(tegra186_gpio_wakes); i++) {
+		if (tegra186_gpio_wakes[i] == gpio) {
+			pr_info("gpio wake%d for gpio=%d\n", i, gpio);
+			return i;
+		}
+	}
+
+	return -EINVAL;
 }
 
 static inline u32 tegra_gpio_readl(u32 gpio, u32 reg_offset)
@@ -378,7 +492,7 @@ static int tegra_gpio_irq_set_type(struct irq_data *d, unsigned int type)
 	u32 lvl_type = 0;
 	u32 trg_type = 0;
 	u32 val;
-	u32 wake = tegra_gpio_to_wake(d->hwirq);
+	int wake = tegra186_gpio_to_wake(d->hwirq);
 
 	switch (type & IRQ_TYPE_SENSE_MASK) {
 	case IRQ_TYPE_EDGE_RISING:
@@ -425,17 +539,20 @@ static int tegra_gpio_irq_set_type(struct irq_data *d, unsigned int type)
 	else if (type & (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_EDGE_RISING))
 		irq_set_handler_locked(d, handle_edge_irq);
 
-	tegra_pm_irq_set_wake_type(wake, type);
+	if (wake >= 0)
+		tegra_pm_irq_set_wake_type(wake, type);
 
 	return 0;
 }
 
 static int tegra_gpio_irq_set_wake(struct irq_data *d, unsigned int enable)
 {
-	int ret = 0;
-	int wake;
+	int ret;
+	int wake = tegra186_gpio_to_wake(d->hwirq);
 
-	wake = tegra_gpio_to_wake(d->hwirq);
+	if (wake < 0)
+		return wake;
+
 	ret = tegra_pm_irq_set_wake(wake, enable);
 	if (ret)
 		pr_err("Failed gpio lp0 %s for irq=%d, error=%d\n",
@@ -657,6 +774,9 @@ static int tegra_gpio_probe(struct platform_device *pdev)
 					tegra_gpio_irq_handler, tg_cont);
 	}
 
+	tegra_pm_update_gpio_wakeup_table(tegra_gpio_chip.base,
+					  tegra186_gpio_wakes,
+					  ARRAY_SIZE(tegra186_gpio_wakes));
 	return 0;
 }
 

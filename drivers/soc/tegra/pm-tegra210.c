5c3113e6bdd592ef8d55108142b01d66d9db3950
Joseph Lo <josephl@nvidia.com>
2016-09-29 15:41:55 +0800

    soc/tegra: CC4 initialization for Tegra210
    Check the CC4 property from the Tegra210 CPU idle node to set up the CC4
    retention voltage.
    
    Bug 200237195
    
    Change-Id: Id3d8c23aa753d61d3437204cd542d47a243912b4
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Reviewed-on: http://git-master/r/1231789
    (cherry picked from commit 47d2096f7382c433b2fc5306d63601c875ffb4c5)
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1749091
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/soc/tegra/pm-tegra210.c
+++ drivers/soc/tegra/pm-tegra210.c
@@ -17,9 +17,79 @@
 #include <linux/kernel.h>
 #include <linux/cpu.h>
 #include <linux/init.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/regulator/consumer.h>
+#include <linux/syscore_ops.h>
 
+#include <soc/tegra/flowctrl.h>
 #include <soc/tegra/fuse.h>
 #include <soc/tegra/tegra_bpmp.h>
+
+struct tegra210_pm_data {
+	bool cc4_no_retention;
+};
+static struct tegra210_pm_data t210_pm_data;
+
+static void do_cc4_init(void)
+{
+	flowctrl_update(FLOW_CTLR_CC4_HVC_CONTROL,
+			2 << 3 | FLOW_CTRL_CC4_HVC_ENABLE);
+	flowctrl_update(FLOW_CTRL_CC4_RETENTION_CONTROL, 2 << 3);
+	flowctrl_update(FLOW_CTRL_CC4_HVC_RETRY, 2);
+}
+
+static struct syscore_ops cc4_syscore_ops = {
+	.restore = do_cc4_init,
+	.resume = do_cc4_init
+};
+
+static int tegra210_cpuidle_cc4_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct regulator *reg;
+	uint32_t uv;
+	int r;
+
+	/* T210 BPMP supports CC4 retention only with max77621 or ovr2. */
+	t210_pm_data.cc4_no_retention = of_property_read_bool(dev->of_node,
+							"cc4-no-retention");
+
+	/* If cc4-microvolt is not found, assume not max77621 */
+	if (of_property_read_u32(dev->of_node, "cc4-microvolt", &uv))
+		goto out;
+
+	reg = regulator_get(dev, "vdd-cpu");
+	if (IS_ERR(reg)) {
+		dev_err(dev, "vdd-cpu regulator get failed\n");
+		return PTR_ERR(reg);
+	}
+
+	r = regulator_set_sleep_voltage(reg, uv - 100000, uv + 100000);
+	if (r)
+		dev_err(dev, "failed to set retention voltage: %d\n", r);
+
+	dev_info(dev, "retention voltage is %u uv\n", uv);
+out:
+	do_cc4_init();
+	register_syscore_ops(&cc4_syscore_ops);
+
+	return 0;
+}
+
+static const struct of_device_id tegra210_cpuidle_of[] = {
+	{ .compatible = "nvidia,tegra210-cpuidle" },
+	{}
+};
+
+static struct platform_driver tegra210_cpuidle_driver = {
+	.probe = tegra210_cpuidle_cc4_probe,
+	.driver = {
+		.owner = THIS_MODULE,
+		.name = "cpuidle-tegra210",
+		.of_match_table = tegra210_cpuidle_of,
+	}
+};
 
 /* Change CPU tolerance level according to hotplug state */
 static int tegra_bpmp_tolerate_idle(int cpu, int ccxtl, int scxtl)
@@ -62,6 +132,7 @@ static int __init tegra210_pm_init(void)
 		goto out;
 
 	register_cpu_notifier(&tegra210_cpu_nb);
+	platform_driver_register(&tegra210_cpuidle_driver);
 
 out:
 	return 0;

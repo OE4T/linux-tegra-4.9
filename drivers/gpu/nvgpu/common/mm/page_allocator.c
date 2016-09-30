24e8ee192a02ff75a569f8c734efa9b4b5267fc8
Alex Waterman <alexw@nvidia.com>
2016-09-30 16:46:54 -0700

    gpu: nvgpu: Fix call to wrong free function
    Fix a mistake in which the wrong free call is used.
    
    Bug 1799159
    Bug 1823380
    
    Change-Id: I3b60949cabbdb6b4d193c6687657cad606462687
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: http://git-master/r/1283142
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/gpu/nvgpu/common/mm/page_allocator.c
+++ drivers/gpu/nvgpu/common/mm/page_allocator.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -150,7 +150,7 @@ static void __nvgpu_free_pages(struct nvgpu_page_allocator *a,
 		kfree(chunk);
 	}
 
-	kfree(alloc);
+	kmem_cache_free(page_alloc_cache, alloc);
 }
 
 static int __insert_page_alloc(struct nvgpu_page_allocator *a,

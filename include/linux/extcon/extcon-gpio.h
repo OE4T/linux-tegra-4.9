00bfd772f06d772619ef7b1fa516ff51a66f9280
Hyong Bin Kim <hyongbink@nvidia.com>
2016-10-05 21:45:51 +0900

    extcon: gpio: add device tree support for extcon
    Add Device tree support for extcon-gpio driver and
    add support for default connection state based on
    platform if there is no provision of connecting
    through gpio.
    
    Bug 200240416
    Bug 1935963
    
    Change-Id: I43ef21ebdb9dc23ee6f8aa404de9e06fb744a15c
    Signed-off-by: Hyong Bin Kim <hyongbink@nvidia.com>
    Reviewed-on: http://git-master/r/1231974
    (cherry picked from commit 80ca8c90318fac8866d4624d16cfc2c50ad9103c)
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    (cherry picked from commit 957fd3a12274736970f375481b79ba676d67f01f)
    Reviewed-on: http://git-master/r/1495586
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- include/linux/extcon/extcon-gpio.h
+++ include/linux/extcon/extcon-gpio.h
@@ -24,22 +24,26 @@
 
 /**
  * struct gpio_extcon_pdata - A simple GPIO-controlled extcon device.
- * @extcon_id:		The unique id of specific external connector.
+ * @name:		The name of this GPIO extcon device.
+ * @supported_cable:	Array of supported cable names ending with EXTCON_NONE.
  * @gpio:		Corresponding GPIO.
  * @gpio_active_low:	Boolean describing whether gpio active state is 1 or 0
  *			If true, low state of gpio means active.
  *			If false, high state of gpio means active.
  * @debounce:		Debounce time for GPIO IRQ in ms.
  * @irq_flags:		IRQ Flags (e.g., IRQF_TRIGGER_LOW).
+ * @default_state: Default state of the connection if gpio is not provided.
  * @check_on_resume:	Boolean describing whether to check the state of gpio
  *			while resuming from sleep.
  */
 struct gpio_extcon_pdata {
-	unsigned int extcon_id;
-	unsigned gpio;
+	const char *name;
+	unsigned int supported_cable[2];
+	int gpio;
 	bool gpio_active_low;
 	unsigned long debounce;
 	unsigned long irq_flags;
+	bool default_state;
 
 	bool check_on_resume;
 };

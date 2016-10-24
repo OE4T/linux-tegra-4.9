aa2f31bf58e1208e57f96f2be30ca2a3b9c301d5
Pritesh Raithatha <praithatha@nvidia.com>
2016-10-24 16:33:35 +0530

    dt-bindings: memory: add sid for nvdla0, nvdla1, pva0 and pva1
    Bug 200249245
    Bug 200248433
    Bug 200249455
    
    Change-Id: Ie9b22d96804ca4044bf4854b492748552937f0bf
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: http://git-master/r/1242703
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

---
+++ include/dt-bindings/memory/tegra194-swgroup.h
@@ -0,0 +1,8 @@
+/*
+ * This is the t19x specific component of the new SID dt-binding.
+ */
+
+#define TEGRA_SID_NVDLA0	0x34	/* 52 */
+#define TEGRA_SID_NVDLA1	0x35	/* 53 */
+#define TEGRA_SID_PVA0		0x36	/* 54 */
+#define TEGRA_SID_PVA1		0x37	/* 55 */

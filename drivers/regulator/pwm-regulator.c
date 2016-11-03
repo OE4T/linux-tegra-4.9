89ebc60943c4d815684ecd2c1b8929a03591f3ff
Venkat Reddy Talla <vreddytalla@nvidia.com>
2016-11-03 18:10:24 +0530

    regulator: pwm: add support to set voltage ramp time
    adding support to set voltage ramp time for the regulator
    output voltage to stabilise after being set to a new value.
    
    Bug 200233943
    
    Change-Id: I699f59ba28a7c5bbafdb26079b674197e20c9cb9
    Reviewed-on: http://git-master/r/1247473
    (cherry picked from commit 2cdc57e69029a187637060d95f09dad4ed117087)
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1551356
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/regulator/pwm-regulator.c
+++ drivers/regulator/pwm-regulator.c
@@ -47,6 +47,9 @@ struct pwm_regulator_data {
 
 	/* Enable GPIO */
 	struct gpio_desc *enb_gpio;
+
+	/* Voltage ramp time */
+	u32 voltage_ramp_time;
 };
 
 struct pwm_voltages {
@@ -202,6 +205,8 @@ static int pwm_regulator_set_voltage(struct regulator_dev *rdev,
 	int ret;
 
 	pwm_init_state(drvdata->pwm, &pstate);
+	dev_info(&rdev->dev, "%s() is called with %d:%d\n",
+				__func__, req_min_uV, req_max_uV);
 
 	/*
 	 * The dutycycle for min_uV might be greater than the one for max_uV.
@@ -231,6 +236,14 @@ static int pwm_regulator_set_voltage(struct regulator_dev *rdev,
 	}
 
 	return 0;
+}
+
+static int pwm_regulator_set_voltage_time_sel(struct regulator_dev *rdev,
+				unsigned int old_uV, unsigned int new_uV)
+{
+	struct pwm_regulator_data *drvdata = rdev_get_drvdata(rdev);
+
+	return drvdata->voltage_ramp_time;
 }
 
 static struct regulator_ops pwm_regulator_voltage_table_ops = {
@@ -305,6 +318,11 @@ static int pwm_regulator_init_continuous(struct platform_device *pdev,
 
 	memcpy(&drvdata->ops, &pwm_regulator_voltage_continuous_ops,
 	       sizeof(drvdata->ops));
+
+	if (!of_property_read_u32(pdev->dev.of_node, "voltage-time-sel",
+				&drvdata->voltage_ramp_time))
+		drvdata->ops.set_voltage_time_sel = pwm_regulator_set_voltage_time_sel;
+
 	drvdata->desc.ops = &drvdata->ops;
 	drvdata->desc.continuous_voltage_range = true;
 
@@ -395,6 +413,7 @@ static int pwm_regulator_probe(struct platform_device *pdev)
 		return ret;
 	}
 
+	dev_info(&pdev->dev, "PWM regulator registration passed\n");
 	return 0;
 }
 

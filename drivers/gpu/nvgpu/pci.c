4c449f0ec0c9393e0e5695e3d0c8077d7772840c
Mahantesh Kumbar <mkumbar@nvidia.com>
2016-11-09 19:40:28 +0530

    gpu: nvgpu: enable RPPG
    - enable RPPG support to engage/disengage RPPG
    if selected PSTATE supports
    
    JIRA DNVGPU-71
    
    Change-Id: I188c8e6235a25ed8c342aef42b39d48cc6378db3
    Signed-off-by: Mahantesh Kumbar <mkumbar@nvidia.com>
    Reviewed-on: http://git-master/r/1250509
    (cherry picked from commit a6ef8204538bb94a53a09e569e727c02dab7fdd0)
    Reviewed-on: http://git-master/r/1270973
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/pci.c
+++ drivers/gpu/nvgpu/pci.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -61,10 +61,12 @@ static struct gk20a_platform nvgpu_pci_device[] = {
 	/* power management configuration */
 	.railgate_delay		= 500,
 	.can_railgate		= false,
-	.can_elpg = false,
+	.can_elpg = true,
+	.enable_elpg = true,
 	.enable_elcg = false,
 	.enable_slcg = true,
 	.enable_blcg = true,
+	.enable_mscg = false,
 	.default_pri_timeout = 0x3ff,
 
 	.disable_aspm = true,
@@ -92,10 +94,12 @@ static struct gk20a_platform nvgpu_pci_device[] = {
 	/* power management configuration */
 	.railgate_delay		= 500,
 	.can_railgate		= false,
-	.can_elpg = false,
+	.can_elpg = true,
+	.enable_elpg = true,
 	.enable_elcg = false,
 	.enable_slcg = true,
 	.enable_blcg = true,
+	.enable_mscg = false,
 	.default_pri_timeout = 0x3ff,
 
 	.disable_aspm = true,
@@ -123,10 +127,12 @@ static struct gk20a_platform nvgpu_pci_device[] = {
 	/* power management configuration */
 	.railgate_delay		= 500,
 	.can_railgate		= false,
-	.can_elpg = false,
+	.can_elpg = true,
+	.enable_elpg = true,
 	.enable_elcg = false,
 	.enable_slcg = true,
 	.enable_blcg = true,
+	.enable_mscg = false,
 	.default_pri_timeout = 0x3ff,
 
 	.disable_aspm = true,

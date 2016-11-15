cde68617f8504759000bd7a69f47e8d9dd1e2d60
Liang Cheng <licheng@nvidia.com>
2016-11-15 11:54:17 -0600

    serial: combined_uart: add the input support
    Tegra combined uart is designed to support the two way communication
    between SPE and all other clients through mailbox. Currently the driver
    at the client ``Linux'' side has the support of sending message to SPE.
    
    This change addresses the support of receiving message from SPE. Unlike
    sending messages, receiving message is handled through interrupt. SPE
    drops the message in Linux's mailbox, and set the TAG bit to trigger
    interrupt. Linux consumes the packet and writes ack to SPE's mailbox.
    
    Linux just puts the received messages in tty's flip buffer right now.
    Useful features, such as executing command supplied by SPE, come next
    after discussion with the owners of other modules.
    
    Bug 1527617
    
    Change-Id: If011bab2bbdcd7384a9bf846d139726431693b32
    Signed-off-by: Liang Cheng <licheng@nvidia.com>
    Reviewed-on: http://git-master/r/1261745
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Adeel Raza <araza@nvidia.com>
    Reviewed-by: Mustafa Bilgen <mbilgen@nvidia.com>
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

--- drivers/tty/serial/tegra-combined-uart.c
+++ drivers/tty/serial/tegra-combined-uart.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -16,11 +16,18 @@
 
 #include <linux/serial_core.h>
 #include <linux/console.h>
+#include <linux/tty_flip.h>
 #include <linux/platform_device.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/of_irq.h>
+#include <linux/irqreturn.h>
 #include <asm/io.h>
+
+#define HSP_INT_IE_0		0x100
+#define TOP0_SHARED_MBOX0	0x0
+#define MBOX0_FULL_BIT		8
 
 #define PKT_RCVD_BIT		26
 #define NUM_BYTES_FIELD_BIT	27
@@ -28,8 +35,9 @@
 #define FIRST_PKT_BIT		30
 #define INTR_TRIGGER_BIT	31
 
-static u32 __iomem *top0_mbox_reg;
-static u32 __iomem *spe_mbox_reg;
+static u8 __iomem *top0_mbox01_base;
+static u8 __iomem *spe_mbox_reg;
+static u8 __iomem *top0_cmn_base;
 
 static struct uart_port tegra_combined_uart_port;
 static struct console tegra_combined_uart_console;
@@ -61,6 +69,7 @@ static void tegra_combined_uart_start_tx(struct uart_port *port)
 		return;
 
 	tegra_combined_uart_console_write(NULL, (char *)tail, count);
+	xmit->tail = (xmit->tail + count) & (UART_XMIT_SIZE - 1);
 }
 
 /*
@@ -114,11 +123,12 @@ static void tegra_combined_uart_console_write(struct console *co,
 		 * Wait for SPE to ACK that it received the current packet.
 		 * TODO: Add support for interrupt based operation.
 		 */
-		while ((readl(top0_mbox_reg) & BIT(PKT_RCVD_BIT)) == 0)
+		while (!(readl(top0_mbox01_base + TOP0_SHARED_MBOX0)
+					& BIT(PKT_RCVD_BIT)))
 			cpu_relax();
 
 		/* Clear the ACK */
-		writel(0, top0_mbox_reg);
+		writel(0, top0_mbox01_base + TOP0_SHARED_MBOX0);
 	}
 }
 
@@ -137,6 +147,58 @@ static int __init tegra_combined_uart_console_setup(struct console *co,
 				bits, flow);
 }
 
+static irqreturn_t tegra_combined_uart_rx(int irq, void *dev_id)
+{
+	int i;
+	char ch = 0;
+	int bytes;
+	u32 reg_val;
+	struct tty_port *port = &((tegra_combined_uart_port.state)->port);
+
+	/* read the mailbox register: top0_shared_mbox0 */
+	reg_val = readl(top0_mbox01_base + TOP0_SHARED_MBOX0);
+	if (reg_val & BIT(PKT_RCVD_BIT)) {
+		pr_err("%s: ACK received but was expecting data\n", __func__);
+		return IRQ_HANDLED;
+	}
+
+	bytes = (reg_val >> NUM_BYTES_FIELD_BIT) & 0x3;
+	for (i = 0; i < bytes; i++) {
+		ch = (reg_val >> i * 8) & 0xFF;
+		tty_insert_flip_char(port, ch, TTY_NORMAL);
+	}
+	if (reg_val & BIT(LAST_PKT_BIT))
+		tty_flip_buffer_push(port);
+
+	/* clear the mailbox register: top0_shared_mbox0 */
+	writel(0, top0_mbox01_base + TOP0_SHARED_MBOX0);
+	/* ack by writing to spe mailbox register */
+	writel(BIT(PKT_RCVD_BIT) | BIT(INTR_TRIGGER_BIT), spe_mbox_reg);
+
+	return IRQ_HANDLED;
+}
+
+static int tegra_combined_uart_startup(struct uart_port *port)
+{
+	int ret;
+	u32 reg_val;
+	/* allocate IRQ */
+	ret = request_irq(tegra_combined_uart_port.irq, tegra_combined_uart_rx,
+		0, "combined_uart rx", port);
+	if (ret) {
+		pr_err("%s: request_irq error\n", __func__);
+		return ret;
+	}
+	/*
+	 * enable interrupt
+	 * WARNING: HSP_INT_IE_0 is not protected for RMW.
+	 */
+	reg_val = readl(top0_cmn_base + HSP_INT_IE_0);
+	reg_val |= (1 << MBOX0_FULL_BIT);
+	writel(reg_val, top0_cmn_base + HSP_INT_IE_0);
+	return ret;
+}
+
 static int tegra_combined_uart_probe(struct platform_device *pdev)
 {
 	int ret;
@@ -145,30 +207,57 @@ static int tegra_combined_uart_probe(struct platform_device *pdev)
 	if (!np)
 		return -ENODEV;
 
-	top0_mbox_reg =	(u32 *)(of_io_request_and_map(np, 0,
-				"Tegra Combined UART TOP0_HSP Linux mailbox"));
-	if (IS_ERR(top0_mbox_reg))
-		return PTR_ERR(top0_mbox_reg);
+	top0_mbox01_base = (u8 *)(of_io_request_and_map(np, 0,
+		    "Tegra Combined UART TOP0_HSP Linux mailbox"));
+	if (IS_ERR(top0_mbox01_base))
+		return PTR_ERR(top0_mbox01_base);
 
-	spe_mbox_reg = (u32 *)(of_io_request_and_map(np, 1,
-					"Tegra Combined UART SPE mailbox"));
-	if (IS_ERR(spe_mbox_reg))
-		return PTR_ERR(spe_mbox_reg);
+	spe_mbox_reg = (u8 *)(of_io_request_and_map(np, 1,
+		    "Tegra Combined UART SPE mailbox"));
+	if (IS_ERR(spe_mbox_reg)) {
+		ret = PTR_ERR(spe_mbox_reg);
+		goto err_mapping;
+	}
+
+	top0_cmn_base = (u8 *)(of_io_request_and_map(np, 2,
+		    "Tegra Combined UART TOP0_HSP Linux mailbox interrrupt"));
+	if (IS_ERR(top0_cmn_base)) {
+		ret = PTR_ERR(top0_cmn_base);
+		goto err_mapping;
+	}
 
 	ret = uart_register_driver(&tegra_combined_uart_driver);
 	if (ret < 0) {
 		pr_err("%s: Could not register driver\n", __func__);
-		return ret;
+		goto err_mapping;
+	}
+
+	tegra_combined_uart_port.irq = irq_of_parse_and_map(np, 0);
+	if (!tegra_combined_uart_port.irq) {
+		pr_err("%s: Failed to get irq from device tree\n", __func__);
+		ret = -EINVAL;
+		goto err_irq;
 	}
 
 	ret = uart_add_one_port(&tegra_combined_uart_driver,
 				&tegra_combined_uart_port);
 	if (ret < 0) {
 		pr_err("%s: Failed to add uart port\n", __func__);
-		return ret;
+		goto err_irq;
 	}
 
-	return 0;
+	return ret;
+
+err_irq:
+	uart_unregister_driver(&tegra_combined_uart_driver);
+err_mapping:
+	if (spe_mbox_reg != NULL && !IS_ERR(spe_mbox_reg))
+		iounmap(spe_mbox_reg);
+	if (top0_mbox01_base != NULL && !IS_ERR(top0_mbox01_base))
+		iounmap(top0_mbox01_base);
+	if (top0_cmn_base != NULL && !IS_ERR(top0_cmn_base))
+		iounmap(top0_cmn_base);
+	return ret;
 }
 
 static int tegra_combined_uart_remove(struct platform_device *pdev)
@@ -177,7 +266,8 @@ static int tegra_combined_uart_remove(struct platform_device *pdev)
 				&tegra_combined_uart_port);
 	uart_unregister_driver(&tegra_combined_uart_driver);
 	iounmap(spe_mbox_reg);
-	iounmap(top0_mbox_reg);
+	iounmap(top0_mbox01_base);
+	iounmap(top0_cmn_base);
 	return 0;
 }
 
@@ -208,7 +298,7 @@ static struct uart_ops tegra_combined_uart_ops = {
 	.start_tx	= tegra_combined_uart_start_tx,
 	.stop_rx	= (void (*)(struct uart_port *)) &uart_null_func,
 	.break_ctl	= (void (*)(struct uart_port *, int)) &uart_null_func,
-	.startup	= uart_null_func,
+	.startup	= tegra_combined_uart_startup,
 	.shutdown	= (void (*)(struct uart_port *)) &uart_null_func,
 	.set_termios	= (void (*)(struct uart_port *,
 					struct ktermios *,

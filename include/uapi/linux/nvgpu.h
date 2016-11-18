f2f06852c3ec6b1fe644707f03e1e2818cee1d27
Thomas Fleury <tfleury@nvidia.com>
2016-11-18 09:11:45 -0800

    gpu: nvgpu: alarm/event numbering
    Remove hole in alarm/event numbering
    
    Jira DNVGPU-186
    
    Change-Id: I5c71c08d345a734c27ff75a04ab18ee4746e47fa
    Signed-off-by: Thomas Fleury <tfleury@nvidia.com>
    Reviewed-on: http://git-master/r/1256413
    (cherry picked from commit 7b068e588712980f1d33c14e692db24068b3ef56)
    Reviewed-on: http://git-master/r/1267158
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- include/uapi/linux/nvgpu.h
+++ include/uapi/linux/nvgpu.h
@@ -861,21 +861,21 @@ struct nvgpu_gpu_get_temperature_args {
 
 /* Recoverable alarms (POLLPRI) */
 /* Alarm when target frequency on any session is not possible */
-#define NVGPU_GPU_EVENT_ALARM_TARGET_VF_NOT_POSSIBLE		2
+#define NVGPU_GPU_EVENT_ALARM_TARGET_VF_NOT_POSSIBLE		1
 /* Alarm when target frequency on current session is not possible */
-#define NVGPU_GPU_EVENT_ALARM_LOCAL_TARGET_VF_NOT_POSSIBLE	3
+#define NVGPU_GPU_EVENT_ALARM_LOCAL_TARGET_VF_NOT_POSSIBLE	2
 /* Alarm when Clock Arbiter failed */
-#define NVGPU_GPU_EVENT_ALARM_CLOCK_ARBITER_FAILED		4
+#define NVGPU_GPU_EVENT_ALARM_CLOCK_ARBITER_FAILED		3
 /* Alarm when VF table update failed */
-#define NVGPU_GPU_EVENT_ALARM_VF_TABLE_UPDATE_FAILED		5
+#define NVGPU_GPU_EVENT_ALARM_VF_TABLE_UPDATE_FAILED		4
 /* Alarm on thermal condition */
-#define NVGPU_GPU_EVENT_ALARM_THERMAL_ABOVE_THRESHOLD		6
+#define NVGPU_GPU_EVENT_ALARM_THERMAL_ABOVE_THRESHOLD		5
 /* Alarm on power condition */
-#define NVGPU_GPU_EVENT_ALARM_POWER_ABOVE_THRESHOLD		7
+#define NVGPU_GPU_EVENT_ALARM_POWER_ABOVE_THRESHOLD		6
 
 /* Non recoverable alarm (POLLUP) */
 /* Alarm on GPU shutdown/fall from bus */
-#define NVGPU_GPU_EVENT_ALARM_GPU_LOST				8
+#define NVGPU_GPU_EVENT_ALARM_GPU_LOST				7
 
 struct nvgpu_gpu_event_info {
 	__u32 event_id;		/* NVGPU_GPU_EVENT_* */

331762a7e78924296dacb4c1465e67679d4b57f7
seshendra Gadagottu <sgadagottu@nvidia.com>
2016-11-22 10:31:42 -0800

    gpu: nvgpu: gv11b: implement mm_setup_hw
    Reuse gk20a_mm_setup_hw for gv11b.
    
    JIRA GV11B-21
    
    Change-Id: I5141dbb8088799a8bd5df55469bc371b63497e96
    Signed-off-by: seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/1254939
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gv11b/mm_gv11b.c
+++ drivers/gpu/nvgpu/gv11b/mm_gv11b.c
@@ -46,4 +46,5 @@ void gv11b_init_mm(struct gpu_ops *gops)
 	gops->mm.bar1_bind = NULL;
 	gops->mm.is_bar1_supported = gv11b_mm_is_bar1_supported;
 	gops->mm.init_inst_block = gv11b_init_inst_block;
+	gops->mm.init_mm_setup_hw = gk20a_init_mm_setup_hw;
 }

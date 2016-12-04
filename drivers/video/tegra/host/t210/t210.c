6ca4a317f10f92598719a671d524624bd993a782
Frank Chen <frankc@nvidia.com>
2016-12-03 22:45:07 -0800

    video: tegra: host: Update ispb clock name
    Change ispb clock name from "isp" to "ispb",
    if common clock framework is used.
    
    Bug 1798951
    
    Change-Id: I43dfd6c9412ff2c0d23b757eee03a3a4dfeeb690
    Signed-off-by: Frank Chen <frankc@nvidia.com>
    Reviewed-on: http://git-master/r/1264508
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Wenjia Zhou <wenjiaz@nvidia.com>
    Reviewed-by: Jihoon Bang <jbang@nvidia.com>

--- drivers/video/tegra/host/t210/t210.c
+++ drivers/video/tegra/host/t210/t210.c
@@ -117,7 +117,11 @@ struct nvhost_device_data t21_ispb_info = {
 	.autosuspend_delay	= 500,
 	.can_powergate		= true,
 	.clocks			= {
+#ifdef CONFIG_COMMON_CLK
+		{ "ispb", UINT_MAX, 0, TEGRA_MC_CLIENT_ISPB },
+#else
 		{ "isp", UINT_MAX, 0, TEGRA_MC_CLIENT_ISPB },
+#endif
 		{"emc", 0, NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER} },
 	.finalize_poweron	= nvhost_isp_t210_finalize_poweron,
 	.prepare_poweroff	= nvhost_isp_t124_prepare_poweroff,

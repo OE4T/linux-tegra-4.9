c4188e36013f28701bb3e95ff54ee4e11fd45baa
Sivaram Nair <sivaramn@nvidia.com>
2016-12-07 15:14:49 -0800

    firmware: tegra: combine t186 mail ops files
    The mail ops functions for t186 are now spread among four different
    files: bpmp_virt.c, mail_native_ivc.c, mail_virt_ivc.c and mail_t186.c.
    This is making it more difficult to get rid of the chip specic defconfig
    flags. Combine the files by moving the functions from other three files
    into mail_t186.c.
    
    As a positive side effect, we are removing use of TEGRA_HV_MANAGER
    config flag (even though this is not a chip specic flag). We will use
    device tree compatibility string to select the appropriate mail ops.
    
    Bug 200257382
    
    Change-Id: I8d41b135eaff24b09efad0adb8444b6343b47aa1
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: http://git-master/r/1266907

--- drivers/firmware/tegra/mail_t186.h
+++ drivers/firmware/tegra/mail_t186.h
@@ -16,21 +16,6 @@
 
 #include <linux/kernel.h>
 
-#define CPU_0_TO_BPMP_CH		0
-#define CPU_1_TO_BPMP_CH		1
-#define CPU_2_TO_BPMP_CH		2
-#define CPU_3_TO_BPMP_CH		3
-#define CPU_4_TO_BPMP_CH		4
-#define CPU_5_TO_BPMP_CH		5
-#define CPU_NA_0_TO_BPMP_CH		6
-#define CPU_NA_1_TO_BPMP_CH		7
-#define CPU_NA_2_TO_BPMP_CH		8
-#define CPU_NA_3_TO_BPMP_CH		9
-#define CPU_NA_4_TO_BPMP_CH		10
-#define CPU_NA_5_TO_BPMP_CH		11
-#define CPU_NA_6_TO_BPMP_CH		12
-#define BPMP_TO_CPU_CH			13
-
 struct ivc;
 
 struct mail_ops {
@@ -43,14 +28,5 @@ struct mail_ops {
 	void (*resume)(void);
 	void (*ring_doorbell)(int ch);
 };
-
-struct mail_ops *native_mail_ops(void);
-
-#ifdef CONFIG_TEGRA_HV_MANAGER
-struct mail_ops *virt_mail_ops(void);
-struct tegra_hv_ivm_cookie *virt_get_mempool(uint32_t *mempool);
-#else
-static inline struct mail_ops *virt_mail_ops(void) { return NULL; }
-#endif
 
 #endif

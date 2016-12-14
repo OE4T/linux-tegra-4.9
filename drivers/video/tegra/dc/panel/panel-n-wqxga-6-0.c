f0eab82c3db2d087f3187d056dfceb28a903f53d
Ishwarya Balaji Gururajan <igururajan@nvidia.com>
2016-12-13 16:34:59 -0800

    tegra: dc: panel: fix coverity SIZEOF_MISMATCH
    fix wrong argument size in dsi_i2c_panel_volt_en
    
    Coverity ID 20189
    
    Bug 200192413
    
    Change-Id: Ic67f94c42dbf184f8819598280b1b36375703d71
    Signed-off-by: Ishwarya Balaji Gururajan <igururajan@nvidia.com>
    Reviewed-on: http://git-master/r/1270564
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Daniel Solomon <daniels@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/tegra/dc/panel/panel-n-wqxga-6-0.c
+++ drivers/video/tegra/dc/panel/panel-n-wqxga-6-0.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2015-2017, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -55,7 +55,7 @@ static int dsi_i2c_panel_volt_en(struct i2c_client *client)
 	u32 cnt;
 	int err;
 
-	i2c_msg_transfer = kzalloc(num_of_xfers * sizeof(i2c_msg_transfer),
+	i2c_msg_transfer = kcalloc(num_of_xfers, sizeof(struct i2c_msg),
 		GFP_KERNEL);
 	if (!i2c_msg_transfer)
 		return -ENOMEM;

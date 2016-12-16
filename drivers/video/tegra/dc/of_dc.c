26df871817dc1c12a2e18768f5bd06cad268accf
Ishwarya Balaji Gururajan <igururajan@nvidia.com>
2016-12-15 23:38:15 -0800

    tegra: dc: fix coverity DEADCODE in of_dc.c
    Coverity ID 33376
    
    Bug 1839145
    
    Change-Id: Id99cfef7bcfb68ecc284b786c1053b6b6ddf1364
    Signed-off-by: Ishwarya Balaji Gururajan <igururajan@nvidia.com>
    Reviewed-on: http://git-master/r/1272434
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/dc/of_dc.c
+++ drivers/video/tegra/dc/of_dc.c
@@ -1,7 +1,7 @@
 /*
  * drivers/video/tegra/dc/of_dc.c
  *
- * Copyright (c) 2013-2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2013-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -2267,8 +2267,6 @@ static int dc_hdmi_out_disable(struct device *dev)
 	if (!dev)
 		return -EINVAL;
 	ndev = to_platform_device(dev);
-	if (!ndev)
-		return -EINVAL;
 
 	dc = platform_get_drvdata(ndev);
 	hdmi = tegra_dc_get_outdata(dc);

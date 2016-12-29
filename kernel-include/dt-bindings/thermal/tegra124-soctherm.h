c99c24564222962c3dd4f21b22cc37c110f570bf
Venkat Reddy Talla <vreddytalla@nvidia.com>
2016-12-29 12:13:38 +0530

    tegra: dt-bindings: copy dts binding content to hw repo
    All dts files present in hw repos use dt binding header
    from the HW repos, not from kernel repos, copying
    dts binding content merged in kernel repos to hw repo
    to align with kernel repo dt-binding content.
    
    Change-Id: I15f47ef0e247d76b514d727dac5110ca5da6f81c
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-on: http://git-master/r/1278049
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Timo Alho <talho@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- kernel-include/dt-bindings/thermal/tegra124-soctherm.h
+++ kernel-include/dt-bindings/thermal/tegra124-soctherm.h
@@ -1,4 +1,18 @@
 /*
+ * Copyright (c) 2014 - 2016, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Author:
+ *	Mikko Perttunen <mperttunen@nvidia.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * This header provides constants for binding nvidia,tegra124-soctherm.
  */
 
@@ -6,8 +20,9 @@
 #define _DT_BINDINGS_THERMAL_TEGRA124_SOCTHERM_H
 
 #define TEGRA124_SOCTHERM_SENSOR_CPU 0
-#define TEGRA124_SOCTHERM_SENSOR_MEM 1
-#define TEGRA124_SOCTHERM_SENSOR_GPU 2
+#define TEGRA124_SOCTHERM_SENSOR_GPU 1
+#define TEGRA124_SOCTHERM_SENSOR_MEM 2
 #define TEGRA124_SOCTHERM_SENSOR_PLLX 3
+#define TEGRA124_SOCTHERM_SENSOR_NUM 4
 
 #endif

afbbd9edd40db8b7a09964ac682a70d39d7d3e71
Pritesh Raithatha <praithatha@nvidia.com>
2016-12-30 15:48:30 +0530

    dt-binding: t19x: define SIDs for new clients
    Bug 200249245
    
    Change-Id: I124e2d17fcd9d988c09221d6caa3bb92085d3591
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: http://git-master/r/1278662
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- kernel-include/dt-bindings/memory/tegra194-swgroup.h
+++ kernel-include/dt-bindings/memory/tegra194-swgroup.h
@@ -2,7 +2,21 @@
  * This is the t19x specific component of the new SID dt-binding.
  */
 
-#define TEGRA_SID_NVDLA0	0x34	/* 52 */
-#define TEGRA_SID_NVDLA1	0x35	/* 53 */
-#define TEGRA_SID_PVA0		0x36	/* 54 */
-#define TEGRA_SID_PVA1		0x37	/* 55 */
+#define TEGRA_SID_MIU		0x50	/* 80 */
+
+#define TEGRA_SID_NVDLA0	0x51	/* 81 */
+#define TEGRA_SID_NVDLA1	0x52	/* 82 */
+
+#define TEGRA_SID_PVA0		0x53	/* 83 */
+#define TEGRA_SID_PVA1		0x54	/* 84 */
+
+#define TEGRA_SID_NVENC1	0x55	/* 85 */
+
+#define TEGRA_SID_PCIE0		0x56	/* 86 */
+#define TEGRA_SID_PCIE1		0x57	/* 87 */
+#define TEGRA_SID_PCIE2		0x58	/* 88 */
+#define TEGRA_SID_PCIE3		0x59	/* 89 */
+#define TEGRA_SID_PCIE4		0x5A	/* 90 */
+#define TEGRA_SID_PCIE5		0x5B	/* 91 */
+
+#define TEGRA_SID_NVDEC1	0x5C	/* 92 */

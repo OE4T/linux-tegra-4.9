49c511adde4273646f3b0d4d5f31557abb1e6b89
Sivaram Nair <sivaramn@nvidia.com>
2017-01-03 10:05:23 -0800

    soc: tegra: use ABI defs from bpmp_t210_abi.h
    We are going to remove the T210 specific ABI defines from tegra_bpmp.h.
    Instead, we should be using the ABI defines from bpmp_t210_abi.h.
    Therefore, add the right include header.
    
    Bug 200257382
    
    Change-Id: I400d9cfea076298ea92671ea449537e152888d3b
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: http://git-master/r/1279657
    (cherry picked from commit f5444e1e61f9228511014765f407070be372f527)
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1749095
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Joseph Lo <josephl@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/soc/tegra/pm-tegra210.c
+++ drivers/soc/tegra/pm-tegra210.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -25,7 +25,7 @@
 #include <linux/regulator/consumer.h>
 #include <linux/spinlock.h>
 #include <linux/syscore_ops.h>
-
+#include <soc/tegra/bpmp_t210_abi.h>
 #include <soc/tegra/flowctrl.h>
 #include <soc/tegra/fuse.h>
 #include <soc/tegra/tegra_bpmp.h>

6d28a75d6023ec680f690c4cfdec056d4d9e9e60
Laxman Dewangan <ldewangan@nvidia.com>
2017-01-03 17:24:00 +0530

    watchdog: tegra: Use soc/tegra/pmc.h for PMC header
    Use the soc/tegra/pmc.h for PMC header instead of
    linux/tegra-pmc.h as this will get removed part of
    single PMC driver for all SOCs.
    
    Bug 200260700
    
    Change-Id: I62b225f4ceefc7633e8472954a5fdc28ffd8ca68
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/1279563
    Reviewed-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/watchdog/tegra_wdt_t18x.c
+++ drivers/watchdog/tegra_wdt_t18x.c
@@ -38,8 +38,8 @@
 #include <linux/uaccess.h>
 #include <linux/watchdog.h>
 #include <linux/tegra-soc.h>
-#include <linux/tegra-pmc.h>
 #include <linux/tegra_wdt_t18x.h>
+#include <soc/tegra/pmc.h>
 
 /* minimum and maximum watchdog trigger periods, in seconds */
 #define MIN_WDT_PERIOD	5

7fd02cf343b8ba527865b64512fd684eb4bab313
Terje Bergstrom <tbergstrom@nvidia.com>
2017-01-04 12:37:02 -0800

    gpu: nvgpu: Don't recursively #include ctrlvolt.h
    ctrlvolt.h #includes ctrlperf.h, which recursively #includes
    ctrlvolt.h without needing anything from it. Remove the #include
    to improve built time.
    
    Bug 200192125
    
    Change-Id: I8c917533127e6d42aae6ad1401860c1f5d110d81
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/1280345
    GVS: Gerrit_Virtual_Submit

--- drivers/gpu/nvgpu/ctrl/ctrlperf.h
+++ drivers/gpu/nvgpu/ctrl/ctrlperf.h
@@ -1,7 +1,7 @@
 /*
  * general p state infrastructure
  *
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -14,8 +14,6 @@
  */
 #ifndef _ctrlperf_h_
 #define _ctrlperf_h_
-
-#include "ctrlvolt.h"
 
 struct ctrl_perf_volt_rail_list_item {
 	u8 volt_domain;

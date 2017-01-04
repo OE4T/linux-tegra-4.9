67fc4629892ab60d6335752b93d883ef690f1ea4
Terje Bergstrom <tbergstrom@nvidia.com>
2017-01-04 12:47:37 -0800

    gpu: nvgpu: Don't recursively #include gk20a.h
    gk20a.h includes pmu_gk20a.h, which #includes gpmuifboardobj.h,
    which recursively #includes gk20a.h without needing anything from it.
    Remove the #include to improve built time.
    
    gpmuifboardobj.h also includes pmu_gk20a.h, when it actually needs a
    definition from pmu_common.h. Change the #include to prevent another
    recursive #include.
    
    Bug 200192125
    
    Change-Id: Idb8c13bceeae1a60a4efa466c07e78cb15ffa1fb
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/1280348
    GVS: Gerrit_Virtual_Submit

--- drivers/gpu/nvgpu/pmuif/gpmuifboardobj.h
+++ drivers/gpu/nvgpu/pmuif/gpmuifboardobj.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+* Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms and conditions of the GNU General Public License,
@@ -13,8 +13,7 @@
 #ifndef _GPMUIFBOARDOBJ_H_
 #define _GPMUIFBOARDOBJ_H_
 
-#include "gk20a/gk20a.h"
-#include "gk20a/pmu_gk20a.h"
+#include "gk20a/pmu_common.h"
 #include "ctrl/ctrlboardobj.h"
 
 /*

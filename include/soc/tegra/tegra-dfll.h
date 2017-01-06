c402a2a852856e8b20f658b26e0e01093cd8b0a5
Jon Hunter <jonathanh@nvidia.com>
2017-01-06 14:14:03 +0000

    clk: tegra: Add helper functions to get DFLL minimum voltages
    Add helper functions to get the minimum and thermal floor voltages
    for the DFLL.
    
    Bug 1811732
    
    Change-Id: I27e9f1ebefedcbb82aebadae9e600abd4cb8ebbd
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: http://git-master/r/1281371
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- include/soc/tegra/tegra-dfll.h
+++ include/soc/tegra/tegra-dfll.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2016, NVIDIA Corporation. All rights reserved.
+ * Copyright (c) 2014-2017, NVIDIA Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -38,4 +38,6 @@ extern int tegra_dfll_count_thermal_states(struct tegra_dfll *td,
 			enum tegra_dfll_thermal_type type);
 u32 tegra_dfll_get_thermal_floor_mv(void);
 u32 tegra_dfll_get_thermal_cap_mv(void);
+u32 tegra_dfll_get_thermal_floor(int index);
+u32 tegra_dfll_get_min_millivolts(void);
 #endif

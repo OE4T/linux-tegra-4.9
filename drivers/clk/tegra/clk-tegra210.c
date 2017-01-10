c7731cb31294982a29191eda30bf3d7b675c5b74
Peter De Schrijver <pdeschrijver@nvidia.com>
2017-01-10 13:59:32 +0200

    clk: tegra: add qspi_out clock
    This clock controls the QSPI_CLK_DIV2_SEL bit of
    CLK_RST_CONTROLLER_CLK_SOURCE_QSPI. The modelling is the same as for Tegra186.
    
    bug 200267756
    
    Change-Id: I7baac33f21e04dfee54d3b5c8763ca2789607c26
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1528714
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/clk/tegra/clk-tegra210.c
+++ drivers/clk/tegra/clk-tegra210.c
@@ -2721,6 +2721,10 @@ static __init void tegra210_periph_clk_init(void __iomem *clk_base,
 	clks[TEGRA210_CLK_ACLK] = clk;
 
 	tegra_periph_clk_init(clk_base, pmc_base, tegra210_clks, &pll_p_params);
+
+	clk = clk_register_divider(NULL, "qspi_out", "qspi", 0,
+				   clk_base + 0x6c4, 8, 1, 0, NULL);
+	clks[TEGRA210_CLK_QSPI_OUT] = clk;
 }
 
 static void __init tegra210_pll_init(void __iomem *clk_base,

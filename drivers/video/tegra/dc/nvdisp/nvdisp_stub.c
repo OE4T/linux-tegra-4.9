144de811606a065226e46365532d21998b6096bf
Laxman Dewangan <ldewangan@nvidia.com>
2017-01-17 16:31:05 +0530

    video: dc: get rid of framebuffer parser APIs
    Remove the functions which implement the parsing of kernel
    command line for the FB memory for FB, FB2, FB3 and FB_LUT.
    
    These are already implemented in the soc/tegra/common.c and
    enabled for all Tegra platforms.
    
    Change-Id: I6dc706b988123bb6eff3fedcf95e6c3ed98c76ef
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/1291566
    Reviewed-by: Ujwal Patel <ujwalp@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/dc/nvdisp/nvdisp_stub.c
+++ drivers/video/tegra/dc/nvdisp/nvdisp_stub.c
@@ -1,7 +1,7 @@
 /*
  * drivers/video/tegra/dc/nvdisplay/nvdis_stub.c
  *
- * Copyright (c) 2014-2016, NVIDIA CORPORATION, All rights reserved.
+ * Copyright (c) 2014-2017, NVIDIA CORPORATION, All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -35,18 +35,10 @@
 #include "board-panel.h"
 #include "dc_priv.h"
 #include <linux/platform_data/lp855x.h>
+#include <soc/tegra/common.h>
 
 const struct disp_client *tegra_la_disp_clients_info;
 atomic_t sd_brightness = ATOMIC_INIT(255);
-phys_addr_t tegra_bootloader_fb_start;
-phys_addr_t tegra_bootloader_fb_size;
-phys_addr_t tegra_bootloader_fb2_start;
-phys_addr_t tegra_bootloader_fb2_size;
-phys_addr_t tegra_bootloader_fb3_start;
-phys_addr_t tegra_bootloader_fb3_size;
-phys_addr_t tegra_bootloader_lut_start;
-phys_addr_t tegra_bootloader_lut_size;
-
 EXPORT_SYMBOL(sd_brightness);
 
 int tegra_is_clk_enabled(struct clk *c)
@@ -134,153 +126,6 @@ int nvdisp_register_backlight_notifier(struct tegra_dc *dc)
 			backlight_device_register_notifier(bl_device, &i2c_nb);
 	}
 	return 0;
-}
-
-static int __init tegra_bootloader_fb_arg(char *options)
-{
-	char *p = options;
-
-	tegra_bootloader_fb_size = memparse(p, &p);
-	if (*p == '@')
-		tegra_bootloader_fb_start = memparse(p+1, &p);
-
-	pr_info("Found tegra_fbmem: %08llx@%08llx\n",
-		(u64)tegra_bootloader_fb_size, (u64)tegra_bootloader_fb_start);
-
-	if (tegra_bootloader_fb_size) {
-		tegra_bootloader_fb_size = PAGE_ALIGN(tegra_bootloader_fb_size);
-
-		if (memblock_reserve(tegra_bootloader_fb_start,
-				tegra_bootloader_fb_size)) {
-			pr_err("Failed to reserve bootloader fb %08llx@%08llx\n",
-				(u64)tegra_bootloader_fb_size,
-				(u64)tegra_bootloader_fb_start);
-			tegra_bootloader_fb_start = 0;
-			tegra_bootloader_fb_size = 0;
-		}
-	}
-
-	return 0;
-}
-early_param("tegra_fbmem", tegra_bootloader_fb_arg);
-
-static int __init tegra_bootloader_fb2_arg(char *options)
-{
-	char *p = options;
-
-	tegra_bootloader_fb2_size = memparse(p, &p);
-	if (*p == '@')
-		tegra_bootloader_fb2_start = memparse(p+1, &p);
-
-	pr_info("Found tegra_fbmem2: %08llx@%08llx\n",
-		(u64)tegra_bootloader_fb2_size,
-		(u64)tegra_bootloader_fb2_start);
-
-	if (tegra_bootloader_fb2_size) {
-		tegra_bootloader_fb2_size =
-				PAGE_ALIGN(tegra_bootloader_fb2_size);
-		if (memblock_reserve(tegra_bootloader_fb2_start,
-				tegra_bootloader_fb2_size)) {
-			pr_err("Failed to reserve bootloader fb2 %08llx@%08llx\n",
-				(u64)tegra_bootloader_fb2_size,
-				(u64)tegra_bootloader_fb2_start);
-			tegra_bootloader_fb2_start = 0;
-			tegra_bootloader_fb2_size = 0;
-		}
-	}
-
-	return 0;
-}
-early_param("tegra_fbmem2", tegra_bootloader_fb2_arg);
-
-static int __init tegra_bootloader_fb3_arg(char *options)
-{
-	char *p = options;
-
-	tegra_bootloader_fb3_size = memparse(p, &p);
-	if (*p == '@')
-		tegra_bootloader_fb3_start = memparse(p+1, &p);
-
-	pr_info("Found tegra_fbmem3: %08llx@%08llx\n",
-		(u64)tegra_bootloader_fb3_size,
-		(u64)tegra_bootloader_fb3_start);
-
-	if (tegra_bootloader_fb3_size) {
-		tegra_bootloader_fb3_size =
-				PAGE_ALIGN(tegra_bootloader_fb3_size);
-		if (memblock_reserve(tegra_bootloader_fb3_start,
-				tegra_bootloader_fb3_size)) {
-			pr_err("Failed to reserve bootloader fb3 %08llx@%08llx\n",
-				(u64)tegra_bootloader_fb3_size,
-				(u64)tegra_bootloader_fb3_start);
-			tegra_bootloader_fb3_start = 0;
-			tegra_bootloader_fb3_size = 0;
-		}
-	}
-
-	return 0;
-}
-early_param("tegra_fbmem3", tegra_bootloader_fb3_arg);
-
-static int __init tegra_bootloader_lut_arg(char *options)
-{
-	char *p = options;
-
-	tegra_bootloader_lut_size = memparse(p, &p);
-	if (*p == '@')
-		tegra_bootloader_lut_start = memparse(p+1, &p);
-
-	pr_info("Found lut_mem: %08llx@%08llx\n",
-		(u64)tegra_bootloader_lut_size,
-		(u64)tegra_bootloader_lut_start);
-
-	if (tegra_bootloader_lut_size) {
-		tegra_bootloader_lut_size =
-				PAGE_ALIGN(tegra_bootloader_lut_size);
-		if (memblock_reserve(tegra_bootloader_lut_start,
-				tegra_bootloader_lut_size)) {
-			pr_err("Failed to reserve bootloader lut_mem %08llx@%08llx\n",
-				(u64)tegra_bootloader_lut_size,
-				(u64)tegra_bootloader_lut_start);
-			tegra_bootloader_lut_start = 0;
-			tegra_bootloader_lut_size = 0;
-		}
-	}
-
-	return 0;
-}
-early_param("lut_mem", tegra_bootloader_lut_arg);
-
-void tegra_get_fb_resource(struct resource *fb_res, int instance)
-{
-	if (!tegra_is_bl_display_initialized(instance)) {
-		fb_res->start = 0;
-		fb_res->end = 0;
-	} else {
-		switch (instance) {
-		case 0:
-			fb_res->start =
-				(resource_size_t) tegra_bootloader_fb_start;
-			fb_res->end = fb_res->start +
-				(resource_size_t) tegra_bootloader_fb_size - 1;
-			break;
-		case 1:
-			fb_res->start =
-				(resource_size_t) tegra_bootloader_fb2_start;
-			fb_res->end = fb_res->start +
-				(resource_size_t) tegra_bootloader_fb2_size - 1;
-			break;
-		case 2:
-			fb_res->start =
-				(resource_size_t) tegra_bootloader_fb3_start;
-			fb_res->end = fb_res->start +
-				(resource_size_t) tegra_bootloader_fb3_size - 1;
-			break;
-		default:
-			pr_err("Could not find DC instance %d\n", instance);
-			break;
-		}
-	}
 }
 
 static int disp_fb_linear_set(void)
@@ -431,26 +276,6 @@ static void tegra_panel_register_ops(struct tegra_dc_out *dc_out,
 }
 
 extern struct tegra_panel_ops panel_sim_ops;
-
-/* Fix T18x kernel-only build - Cloned from common.c */
-/* returns true if bl initialized the display */
-bool tegra_is_bl_display_initialized(int instance)
-{
-	/* display initialized implies non-zero
-	 * fb size is passed from bl to kernel
-	 */
-	switch (instance) {
-	case 0:
-		return tegra_bootloader_fb_start && tegra_bootloader_fb_size;
-	case 1:
-		return tegra_bootloader_fb2_start && tegra_bootloader_fb2_size;
-	case 2:
-		return tegra_bootloader_fb3_start && tegra_bootloader_fb3_size;
-	default:
-		pr_err("Could not find DC instance %d\n", instance);
-		return false;
-	}
-}
 
 /* Clone from board-panel.c */
 struct device_node *tegra_primary_panel_get_dt_node(

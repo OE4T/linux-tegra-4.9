a74cd5b1820a8d84c060427d61b0dffbb45147be
Gigon Bae <gbae@nvidia.com>
2017-01-26 00:36:04 -0800

    v4l2: Increase event subscription queue size
    Increase the size of event subscription queue to 2.
    
    If the internal event queue for a particular subscribed event
    becomes full, then the oldest event in that queue will be dropped.
    Since we need both the on and off signals to our subscription,
    it is good to increase the queue to 2.
    
    Bug 1787461
    Bug 1824769
    
    Change-Id: Ic38517639448b10e7723d18ae91e1c6e793a0563
    Signed-off-by: Gigon Bae <gbae@nvidia.com>
    Reviewed-on: http://git-master/r/1294535
    Reviewed-on: http://git-master/r/1469821
    Reviewed-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    Reviewed-by: Jihoon Bang <jbang@nvidia.com>

--- drivers/media/v4l2-core/v4l2-ctrls.c
+++ drivers/media/v4l2-core/v4l2-ctrls.c
@@ -3409,7 +3409,7 @@ int v4l2_ctrl_subscribe_event(struct v4l2_fh *fh,
 				const struct v4l2_event_subscription *sub)
 {
 	if (sub->type == V4L2_EVENT_CTRL)
-		return v4l2_event_subscribe(fh, sub, 0, &v4l2_ctrl_sub_ev_ops);
+		return v4l2_event_subscribe(fh, sub, 2, &v4l2_ctrl_sub_ev_ops);
 	return -EINVAL;
 }
 EXPORT_SYMBOL(v4l2_ctrl_subscribe_event);

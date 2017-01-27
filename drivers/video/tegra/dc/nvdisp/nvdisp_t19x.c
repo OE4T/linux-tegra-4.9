2fdd687c89f14716b818703684478c47c1f68fae
Ujwal Patel <ujwalp@nvidia.com>
2017-01-26 16:35:34 -0800

    t19x: dc: add t19x display source code
    Source code pertaining to t19x display should reside under
    t19x repository. Create directory structure and source file names
    such that if t19x is collapsed into main kernel repository then
    it would not face conflict.
    
    TDS-1549
    
    Change-Id: Ib556c39789f6ecc5d19e8c0bcfd914977f4d7b8f
    Signed-off-by: Ujwal Patel <ujwalp@nvidia.com>
    Reviewed-on: http://git-master/r/1294902
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Aron Wong <awong@nvidia.com>
    Reviewed-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

---
+++ drivers/video/tegra/dc/nvdisp/nvdisp_t19x.c
@@ -0,0 +1,27 @@
+/*
+ * drivers/video/tegra/dc/nvdisp/nvdisp_t19x.c
+ *
+ * Copyright (c) 2017, NVIDIA CORPORATION, All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include "dc_priv.h"
+
+void tegra_dc_populate_t19x_hw_data(struct tegra_dc_hw_data *hw_data)
+{
+	if (!hw_data)
+		return;
+
+	hw_data->nheads = 4;
+	hw_data->nwins = 6;
+	hw_data->valid = true;
+}

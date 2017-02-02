4685489ab40c27aa5088eb00be3ca27a675374c9
Krishna Sitaraman <ksitaraman@nvidia.com>
2017-02-01 17:45:50 -0800

    t194: mce: Add nvg header file
    This patch adds the initial nvg header file for use by kernel.
    The header contains information for communication with the mce in
    ccplex through nvg interface.
    
    JIRA TPM-1062
    
    Change-Id: Ib14685ab1a051e883f6f8183a92cbf48d545f6c1
    Signed-off-by: Krishna Sitaraman <ksitaraman@nvidia.com>
    Reviewed-on: http://git-master/r/1297644
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

---
+++ include/linux/t194_nvg.h
@@ -0,0 +1,215 @@
+#ifndef T194_NVG_H
+#define T194_NVG_H
+
+/*
+ * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+/**
+ * t194_nvg.h - Header for the NVIDIA Generic interface (NVG).
+ * Official documentation for this interface is included as part
+ * of the T194 TRM.
+ */
+
+enum {
+	TEGRA_NVG_VERSION_MAJOR = 6,
+	TEGRA_NVG_VERSION_MINOR = 0,
+};
+
+typedef enum {
+	TEGRA_NVG_CHANNEL_VERSION = 0,
+	TEGRA_NVG_CHANNEL_POWER_PERF = 1,
+	TEGRA_NVG_CHANNEL_POWER_MODES = 2,
+	TEGRA_NVG_CHANNEL_WAKE_TIME = 3,
+	TEGRA_NVG_CHANNEL_CSTATE_INFO = 4,
+	TEGRA_NVG_CHANNEL_CROSSOVER_C6_LOWER_BOUND = 5,
+	TEGRA_NVG_CHANNEL_CROSSOVER_CC6_LOWER_BOUND = 6,
+	/* Value 7 reserved */
+	TEGRA_NVG_CHANNEL_CROSSOVER_CG7_LOWER_BOUND = 8,
+	/* Value 9 reserved */
+	TEGRA_NVG_CHANNEL_CSTATE_STAT_QUERY_REQUEST = 10,
+	TEGRA_NVG_CHANNEL_CSTATE_STAT_QUERY_VALUE = 11,
+	/* Values 12-42 reserved */
+	TEGRA_NVG_CHANNEL_IS_SC7_ALLOWED = 43,
+	TEGRA_NVG_CHANNEL_ONLINE_CORE = 44,
+	TEGRA_NVG_CHANNEL_CC3_CTRL = 45,
+	TEGRA_NVG_CHANNEL_UPDATE_CCPLEX_GSC = 50,
+	TEGRA_NVG_CHANNEL_CCPLEX_CACHE_INVAL = 51,
+	/* 52 FREQ FEEDBACK */
+	TEGRA_NVG_CHANNEL_HSM_ERROR_CTRL = 53,
+	TEGRA_NVG_CHANNEL_SECURITY_CONFIG = 54,
+	TEGRA_NVG_CHANNEL_LAST_INDEX,
+} tegra_nvg_channel_id_t;
+
+typedef enum {
+	/* Value 0 reserved */
+	NVG_STAT_QUERY_SC7_ENTRIES = 1,
+	/* Values 2-5 reserved */
+	NVG_STAT_QUERY_CC6_ENTRIES = 6,
+	NVG_STAT_QUERY_CG7_ENTRIES = 7,
+	/* Values 8-9 reserved */
+	NVG_STAT_QUERY_C6_ENTRIES = 10,
+	/* Values 11-13 reserved */
+	NVG_STAT_QUERY_C7_ENTRIES = 14,
+	/* Values 15-31 reserved */
+	NVG_STAT_QUERY_SC7_RESIDENCY_SUM = 32,
+	NVG_STAT_QUERY_CC6_RESIDENCY_SUM = 41,
+	NVG_STAT_QUERY_CG7_RESIDENCY_SUM = 46,
+	NVG_STAT_QUERY_C6_RESIDENCY_SUM = 51,
+	NVG_STAT_QUERY_C7_RESIDENCY_SUM = 56,
+} tegra_nvg_stat_query_t;
+
+typedef enum {
+	TEGRA_NVG_CORE_C0 = 0,
+	TEGRA_NVG_CORE_C1 = 1,
+	TEGRA_NVG_CORE_C6 = 6,
+	TEGRA_NVG_CORE_C7 = 7,
+	TEGRA_NVG_CORE_WARMRSTREQ = 8,
+} tegra_nvg_core_sleep_state_t;
+
+typedef enum {
+	TEGRA_NVG_CLUSTER_CC0 = 0,
+	TEGRA_NVG_CLUSTER_CC6 = 6,
+} tegra_nvg_cluster_sleep_state_t;
+
+typedef enum {
+	TEGRA_NVG_CCPLEX_CG0 = 0,
+	TEGRA_NVG_CCPLEX_CG7 = 1,
+} tegra_nvg_cluster_group_sleep_state_t;
+
+typedef enum {
+	TEGRA_NVG_SYSTEM_SC0 = 0,
+	TEGRA_NVG_SYSTEM_SC7 = 7,
+	TEGRA_NVG_SYSTEM_SC8 = 8,
+} tegra_nvg_system_sleep_state_t;
+
+/* ------------------- */
+/* NVG Data subformats */
+/* ------------------- */
+
+typedef union {
+	uint64_t flat;
+	struct nvg_version_channel_t {
+		uint64_t minor_version : 32;
+		uint64_t major_version : 32;
+	} bits;
+} nvg_version_data_t;
+
+typedef union nvg_channel_1_data_u {
+	uint64_t flat;
+	struct nvg_channel_1_data_s {
+		uint64_t perf_per_watt_mode : 1;
+		uint64_t reserved_63_1 : 63;
+	} bits;
+} nvg_channel_1_data_t;
+
+typedef union nvg_channel_2_data_u {
+	uint64_t flat;
+	struct nvg_channel_2_data_s {
+		uint64_t reserved_1_0 : 2;
+		uint64_t battery_saver_mode : 1;
+		uint64_t reserved_63_3 : 61;
+	} bits;
+} nvg_channel_2_data_t;
+
+typedef union {
+	uint64_t flat;
+	struct nvg_wake_time_channel_t {
+		uint64_t wake_time : 32;
+		uint64_t reserved_63_32 : 32;
+	} bits;
+} nvg_wake_time_channel_t;
+
+typedef union {
+	uint64_t flat;
+	struct nvg_cstate_info_channel_t {
+		uint64_t cluster_state : 3;
+		uint64_t reserved_6_3 : 4;
+		uint64_t update_cluster : 1;
+		uint64_t cg_cstate : 3;
+		uint64_t reserved_14_11 : 4;
+		uint64_t update_cg : 1;
+		uint64_t system_cstate : 4;
+		uint64_t reserved_22_20 : 3;
+		uint64_t update_system : 1;
+		uint64_t reserved_30_24 : 7;
+		uint64_t update_wake_mask : 1;
+		uint64_t wake_mask : 32;
+	} bits;
+} nvg_cstate_info_channel_t;
+
+typedef union {
+	uint64_t flat;
+	struct nvg_lower_bound_channel_t {
+		uint64_t crossover_value : 32;
+		uint64_t reserved_63_32 : 32;
+	} bits;
+} nvg_lower_bound_channel_t;
+
+
+typedef union {
+	uint64_t flat;
+	struct nvg_cstate_stat_query_channel_t {
+		uint64_t unit_id : 4;
+		uint64_t reserved_15_4 : 12;
+		uint64_t stat_id : 16;
+		uint64_t reserved_63_32 : 32;
+	} bits;
+} nvg_cstate_stat_query_channel_t;
+
+typedef union {
+	uint64_t flat;
+	struct nvg_is_sc7_allowed_channel_t {
+		uint64_t is_sc7_allowed : 1;
+		uint64_t reserved_63_32 : 63;
+	} bits;
+} nvg_is_sc7_allowed_channel_t;
+
+
+typedef union {
+	uint64_t flat;
+	struct nvg_core_online_channel_t {
+		uint64_t core_id : 4;
+		uint64_t reserved_63_4 : 60;
+	} bits;
+} nvg_core_online_channel_t;
+
+
+typedef union {
+	uint64_t flat;
+	struct nvg_cc3_control_channel_t {
+		uint64_t freq_req : 8;
+		uint64_t reserved_30_8 : 23;
+		uint64_t enable : 1;
+		uint64_t reserved_63_32 : 32;
+	} bits;
+} nvg_cc3_control_channel_t;
+
+typedef union {
+	uint64_t flat;
+	struct nvg_update_gsc_channel_t {
+		uint64_t gsc_enum : 16;
+		uint64_t reserved_63_16 : 48;
+	} bits;
+} nvg_update_gsc_channel_t;
+
+typedef union {
+	uint64_t flat;
+	struct nvg_cache_inval_channel_t {
+		uint64_t cache_clean : 1;
+		uint64_t cache_clean_inval : 1;
+		uint64_t cache_clean_inval_tr : 1;
+		uint64_t reserved_63_3 : 61;
+	} bits;
+} nvg_cache_inval_channel_t;
+
+#endif /* T194_NVG_H */

ab1783a5e123b4a729c63d909f1799252f56f63e
Srikar Srimath Tirumala <srikars@nvidia.com>
2017-02-02 17:26:54 -0800

    include: tegra: throttle: remove unused var
    Remove unused code and CONFIG_ARCH_TEGRA_21x_SOC references.
    
    Bug 200273953
    
    Change-Id: I99d8b75316cef2a8ca641a8e27a11ba019f1f15d
    Signed-off-by: Srikar Srimath Tirumala <srikars@nvidia.com>
    Reviewed-on: http://git-master/r/1298357
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    (cherry picked from commit bced4a2d9fb47b145fad221fc74169d790636649)
    Reviewed-on: https://git-master.nvidia.com/r/1557307
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- include/linux/tegra_throttle.h
+++ include/linux/tegra_throttle.h
@@ -1,7 +1,7 @@
 /*
  * include/linux/tegra_throttle.h
  *
- * Copyright (c) 2010-2014 NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2010-2017 NVIDIA CORPORATION. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -32,30 +32,11 @@ struct tegra_cooling_device {
 #define NO_CAP			(ULONG_MAX) /* no cap */
 #define CPU_THROT_LOW		0 /* lowest throttle freq. only used for CPU */
 
-#if defined(CONFIG_ARCH_TEGRA_12x_SOC) || defined(CONFIG_ARCH_TEGRA_21x_SOC)
-#define GBUS_CNT		1
-#else
-#define GBUS_CNT		0
-#endif
-
-#ifdef CONFIG_TEGRA_GPU_DVFS
-#define CBUS_CNT		2
-#else
-#define CBUS_CNT		1
-#endif
-
-/* cpu, gpu(0|1), cbus(1|2), sclk, emc */
-#define NUM_OF_CAP_FREQS	(1 + GBUS_CNT + CBUS_CNT + 1 + 1)
-
 #ifdef CONFIG_TEGRA_THERMAL_THROTTLE
 bool tegra_is_throttling(int *count);
-unsigned long tegra_throttle_governor_speed(unsigned long requested_speed);
 #else
 static inline bool tegra_is_throttling(int *count)
 { return false; }
-static inline unsigned long tegra_throttle_governor_speed(
-	unsigned long requested_speed)
-{ return requested_speed; }
 #endif /* CONFIG_TEGRA_THERMAL_THROTTLE */
 
 #endif	/* __TEGRA_THROTTLE_H */

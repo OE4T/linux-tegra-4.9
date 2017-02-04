98710964d7670ede178ed9618529dd73a4696ea0
Krishna Reddy <vdumpa@nvidia.com>
2017-02-03 16:50:27 -0800

    Revert "include: dt-bindings: add camera StreamIDs"
    This reverts commit 8e9f22ab11bdd439ca1d14652b394acd88c1319d.
    
    Change-Id: I623ea2014b948a6ecf89448f4265f7c7642d0885
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/1299022
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Damian Halas <dhalas@nvidia.com>
    Tested-by: Damian Halas <dhalas@nvidia.com>

--- include/dt-bindings/memory/tegra194-swgroup.h
+++ include/dt-bindings/memory/tegra194-swgroup.h
@@ -2,15 +2,6 @@
  * This is the t19x specific component of the new SID dt-binding.
  */
 
-#define TEGRA_SID_RCE		0x2a	/* 42 */
-#define TEGRA_SID_RCE_1X	0x2b	/* 43 */
-
-#define TEGRA_SID_RCE_VM1	0x2a	/* 42 */
-#define TEGRA_SID_RCE_VM1_1X	0x2b	/* 43 */
-
-#define TEGRA_SID_RCE_VM2	0x2d	/* 45 */
-#define TEGRA_SID_RCE_VM2_1X	0x2e	/* 46 */
-
 #define TEGRA_SID_MIU		0x50	/* 80 */
 
 #define TEGRA_SID_NVDLA0	0x51	/* 81 */
@@ -29,11 +20,3 @@
 #define TEGRA_SID_PCIE5		0x5B	/* 91 */
 
 #define TEGRA_SID_NVDEC1	0x5C	/* 92 */
-
-/* Falcon task descriptors */
-#define TEGRA_SID_RCE_VM0	0x5D	/* 93 */
-#define TEGRA_SID_VIFALC	0x5E	/* 94 */
-#define TEGRA_SID_ISPFALC	0x5F	/* 95 */
-
-#define TEGRA_SID_RCE_VM3	0x60	/* 96 */
-#define TEGRA_SID_RCE_VM3_1X	0x61	/* 97 */

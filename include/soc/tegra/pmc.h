madisongh/linux-tegra

padctrl: tegra: Add generic IO pad control driver
Add generic Tegra IO Pad control driver for all Tegra SOCs.
This driver use the tegra PMC APIs and implement the padcontrol
operations.

This helps in unifying PMC driver which implement all register
access and APIs provided via the IO pad names.

bug 200259694

Change-Id: Id4fbfad2763500f1d1d06de5f3823eee5fd9f88f
Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
Reviewed-on: http://git-master/r/1284789
Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

include/soc/tegra/pmc.h
@@ -299,6 +299,15 @@ void tegra_pmc_lock_thermal_shutdown(void);
 int tegra210_pmc_padctrl_init(struct device *dev, struct device_node *np);
 int tegra186_pmc_padctrl_init(struct device *dev, struct device_node *np);
 
+#if defined(CONFIG_ARCH_OR_PLATFORM_TEGRA)
+int tegra_io_pads_padctrl_init(struct device *dev);
+#else
+int tegra_io_pads_padctrl_init(struct device *dev)
+{
+	return 0;
+}
+#endif
+
 static inline int tegra_pmc_padctrl_init(struct device *dev,
 					 struct device_node *np)
 {

OE4T/linux-tegra-4.9

Bug 200259694
(cherry picked from commit ff3ecabec633f280f1fed77a224d4f2a6acf91a8)
Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>

include/soc/tegra/pmc.h
@@ -296,6 +296,15 @@ void tegra_pmc_lock_thermal_shutdown(void);
 int tegra210_pmc_padctrl_init(struct device *dev, struct device_node *np);
 int tegra186_pmc_padctrl_init(struct device *dev, struct device_node *np);
 
+#if defined(CONFIG_ARCH_TEGRA)
+int tegra_io_pads_padctrl_init(struct device *dev);
+#else
+int tegra_io_pads_padctrl_init(struct device *dev)
+{
+	return 0;
+}
+#endif
+
 static inline int tegra_pmc_padctrl_init(struct device *dev,
 					 struct device_node *np)
 {
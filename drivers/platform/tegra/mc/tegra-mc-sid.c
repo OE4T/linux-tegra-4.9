1cbcd79991d006eb9d654d074ea1a0a216ead644
Shawn Joo <sjoo@nvidia.com>
2017-02-08 10:53:58 +0900

    platform: tegra: mc: fix build err when unset CONFIG_DEBUG_FS
    replace tegra_mc_sid_create_debugfs with tegra_mc_sid_debugfs
    to fix build error when CONFIG_DEBUG_FS is unset
    
    Bug 200276656
    
    Change-Id: Ic96d2878baf3cfa707ae4e7ce4684233b073dddb
    Signed-off-by: Shawn Joo <sjoo@nvidia.com>
    Reviewed-on: http://git-master/r/1301046
    (cherry picked from commit 8c75ada7a6f666c67e039e7ddca23ca46f6cfa62)
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: https://git-master/r/1512333
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/platform/tegra/mc/tegra-mc-sid.c
+++ drivers/platform/tegra/mc/tegra-mc-sid.c
@@ -1,7 +1,7 @@
 /*
  * MC StreamID configuration
  *
- * Copyright (c) 2015-2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -197,7 +197,7 @@ static void tegra_mc_sid_remove_debugfs(void)
 	debugfs_remove_recursive(mc_sid->debugfs_root);
 }
 #else
-static inline void tegra_mc_sid_debugfs(void)
+static inline void tegra_mc_sid_create_debugfs(void)
 {
 }
 static void tegra_mc_sid_remove_debugfs(void)

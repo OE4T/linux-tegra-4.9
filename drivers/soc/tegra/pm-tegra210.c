b2f5e58145c9bf44f62914bec08cd9105105a820
Jon Mayo <jmayo@nvidia.com>
2017-02-23 13:24:31 -0800

    soc/tegra: pm-tegra210: wrap with CONFIG_PM_SLEEP
    When PM_SLEEP is not enabled, this file shouldn't be compiled.
    This change fixes a build error for missing resume_device_irq().
    
    Change-Id: Ia7aa413f363c58c05709ddbaad975b65020da2fa
    Signed-off-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-on: http://git-master/r/1310451
    (cherry picked from commit 9400e489339f25838aad3717d0539c3c75757f83)

--- drivers/soc/tegra/pm-tegra210.c
+++ drivers/soc/tegra/pm-tegra210.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#ifdef CONFIG_PM_SLEEP
 #include <linux/kernel.h>
 #include <linux/cpu.h>
 #include <linux/cpu_pm.h>
@@ -562,3 +562,4 @@ out:
 	return 0;
 }
 device_initcall(tegra210_pm_init);
+#endif

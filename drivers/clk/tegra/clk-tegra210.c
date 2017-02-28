b197e8f2d7a99f339906d7df37e8f77cfee7d7ab
Chun Xu <chunx@nvidia.com>
2017-02-28 14:50:37 +0800

    clk: tegra-cec: set pclk default refcount to 1
    Enable pclk and increment the refcount by one.
    It will never be disabled.
    
    Bug 200275121
    
    Change-Id: I9b1a5b08488e3c442a91ebdafa6e9a05d68bea75
    Signed-off-by: Chun Xu <chunx@nvidia.com>
    Reviewed-on: http://git-master/r/1312338
    (cherry picked from commit 61647e3c68ce0e90e64636ba03429cf59ce5fbba)
    Reviewed-on: https://git-master.nvidia.com/r/1531064
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/clk/tegra/clk-tegra210.c
+++ drivers/clk/tegra/clk-tegra210.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2017 NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -3044,6 +3044,7 @@ static struct tegra_clk_init_table init_table[] __initdata = {
 	{ TEGRA210_CLK_PLL_U_OUT1, TEGRA210_CLK_CLK_MAX, 48000000, 1 },
 	{ TEGRA210_CLK_PLL_U_OUT2, TEGRA210_CLK_CLK_MAX, 60000000, 1 },
 	{ TEGRA210_CLK_PLL_C4, TEGRA210_CLK_CLK_MAX, 1000*1000*1000, 0 },
+	{ TEGRA210_CLK_PCLK, TEGRA210_CLK_CLK_MAX, 0, 1 },
 	/* This MUST be the last entry. */
 	{ TEGRA210_CLK_CLK_MAX, TEGRA210_CLK_CLK_MAX, 0, 0 },
 };

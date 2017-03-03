e45d9013d34e16a908312714716cc1c441eff85f
Aniruddha TVS Rao <anrao@nvidia.com>
2017-03-03 17:24:11 +0530

    fs: ext4: Avoid uninitialized array
    blocks can remain uninitialized for a possible set of branches.
    Fixed by initializing blocks to 0.
    
    Coverity ID 38476
    
    Bug 200314122
    
    Change-Id: Ic47edcf0d417b86f1a59b94623c10bee316779d8
    Signed-off-by: Aniruddha TVS Rao <anrao@nvidia.com>
    Reviewed-on: http://git-master/r/1314927
    (cherry picked from commit 0fd9293f89ea1576683f5899d1d66d671cec07e5)
    Reviewed-on: http://git-master/r/1496466
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- fs/ext4/readpage.c
+++ fs/ext4/readpage.c
@@ -155,6 +155,7 @@ int ext4_mpage_readpages(struct address_space *mapping,
 	unsigned relative_block = 0;
 	struct ext4_map_blocks map;
 
+	memset(blocks, 0x0, MAX_BUF_PER_PAGE);
 	map.m_pblk = 0;
 	map.m_lblk = 0;
 	map.m_len = 0;

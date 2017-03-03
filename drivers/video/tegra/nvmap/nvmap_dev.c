3f93e7baa7950750fd9a8eabb45ab075d9bd2aa7
Sri Krishna chowdary <schowdary@nvidia.com>
2017-03-03 19:14:08 +0530

    video: tegra: nvmap: Export nvmap_dev
    Exporting nvmap_dev will help test modules use it.
    
    bug 200285489
    
    Change-Id: Id1d68ec67b23313508e9b1daa550a90d35a2adac
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1314977
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_dev.c
+++ drivers/video/tegra/nvmap/nvmap_dev.c
@@ -3,7 +3,7 @@
  *
  * User-space interface to nvmap
  *
- * Copyright (c) 2011-2016, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2011-2017, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -63,6 +63,7 @@
 #define NVMAP_CARVEOUT_KILLER_RETRY_TIME 100 /* msecs */
 
 struct nvmap_device *nvmap_dev;
+EXPORT_SYMBOL(nvmap_dev);
 struct nvmap_stats nvmap_stats;
 
 static struct device_dma_parameters nvmap_dma_parameters = {

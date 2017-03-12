0f8b2051782b0011b876e5f985c3abed7bfea505
Ujwal Patel <ujwalp@nvidia.com>
2017-03-12 11:32:57 -0700

    t19x: dc: add nsors and dc version
    In order to scale the driver for multiple displays, add
    number of SORs per SOC and DC version in the HW data.
    
    TDS-1543
    
    Change-Id: Ia76c355fdd2718ef753e41ef2fde827e8c2d08c7
    Signed-off-by: Ujwal Patel <ujwalp@nvidia.com>
    Reviewed-on: http://git-master/r/1319527
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-by: Santosh Galma <galmar@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Aron Wong <awong@nvidia.com>

--- drivers/video/tegra/dc/nvdisp/nvdisp_t19x.c
+++ drivers/video/tegra/dc/nvdisp/nvdisp_t19x.c
@@ -23,5 +23,7 @@ void tegra_dc_populate_t19x_hw_data(struct tegra_dc_hw_data *hw_data)
 
 	hw_data->nheads = 4;
 	hw_data->nwins = 6;
+	hw_data->nsors = 4;
 	hw_data->valid = true;
+	hw_data->version = TEGRA_DC_HW_T19x;
 }

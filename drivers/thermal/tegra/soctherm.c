4774e4a6d8ddff9ed7e7516a5fd97de0a4792d63
Srikar Srimath Tirumala <srikars@nvidia.com>
2017-03-16 16:40:22 -0700

    thermal: tegra: add support for GPU hw-throttle
    Add support to trigger pulse skippers on the GPU when a HOT trip
    point is triggered. The pulse skippers can be signalled to throttle
    at lo, medium and high depths\levels.
    
    Also allow the throttle depth to be configured through DT and
    introduce a new DT binding for the same.
    
    Bug 200233003
    
    [ This is a cherry-pick from K4.4 to K4.9. It sovled some merge conflicts
      as the driver now has CCROC related code in K4.9. It also changed those
      TEGRA_SOCTHERM_THROT_LEVEL_* macros to match the downstream definations
      in the devicetree and older kernels. -- Nicolin ]
    
    Change-Id: I775135aadfa1343688ae5253113025dd9ca56cf4
    Signed-off-by: Srikar Srimath Tirumala <srikars@nvidia.com>
    Reviewed-on: http://git-master/r/1322663
    (cherry picked from commit 1a268d66d01faafaeee3a781124b05d2df1e5148)
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/thermal/tegra/soctherm.c
+++ drivers/thermal/tegra/soctherm.c
@@ -201,6 +201,14 @@
 
 /* get dividend from the depth */
 #define THROT_DEPTH_DIVIDEND(depth)	((256 * (100 - (depth)) / 100) - 1)
+/* gk20a nv_therm interface N:3 Mapping. Levels defined in tegra124-sochterm.h
+ * level	vector
+ * NONE		3'b000
+ * LOW		3'b001
+ * MED		3'b011
+ * HIGH		3'b111
+*/
+#define THROT_LEVEL_TO_DEPTH(level)		((0x1 << (level)) - 1)
 
 /* get THROT_PSKIP_xxx offset per LIGHT/HEAVY throt and CPU/GPU dev */
 #define THROT_OFFSET			0x30
@@ -280,6 +288,7 @@ struct soctherm_throt_cfg {
 	u8 priority;
 	u8 cpu_throt_level;
 	u32 cpu_throt_depth;
+	u32 gpu_throt_level;
 	struct thermal_cooling_device *cdev;
 	bool init;
 };
@@ -1671,6 +1680,41 @@ static int soctherm_hw_pllx_offsets_parse(struct platform_device *pdev)
 	return 0;
 }
 
+static int soctherm_throt_cfg_parse(struct device *dev,
+				struct device_node *np,
+				struct soctherm_throt_cfg *stc)
+{
+	struct tegra_soctherm *ts = dev_get_drvdata(dev);
+	int r, r1;
+	u32 val;
+
+	r = of_property_read_u32(np, "nvidia,priority", &val);
+	if (r) {
+		dev_err(dev, "throttle-cfg: %s: inv priority\n", stc->name);
+		return -EINVAL;
+	}
+
+	stc->priority = val;
+	r = of_property_read_u32(np, ts->soc->use_ccroc ?
+				 "nvidia,cpu-throt-level" :
+				 "nvidia,cpu-throt-percent", &val);
+	if (!r && ts->soc->use_ccroc)
+		stc->cpu_throt_level = val;
+	else if (!r && val <= 100)
+		stc->cpu_throt_depth = val;
+
+	r1 = of_property_read_u32(np, "nvidia,gpu-throt-level", &val);
+	if (!r1 && val <= TEGRA_SOCTHERM_THROT_LEVEL_HIGH)
+		stc->gpu_throt_level = val;
+
+	if (r && r1) {
+		dev_err(dev, "throttle-cfg: %s: no throt prop\n", stc->name);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 /**
  * soctherm_init_hw_throt_cdev() - Parse the HW throttle configurations
  * and register them as cooling devices.
@@ -1679,10 +1723,9 @@ static void soctherm_init_hw_throt_cdev(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct tegra_soctherm *ts = dev_get_drvdata(dev);
-	struct device_node *np_stc, *np_stcc;
+	struct device_node *np, *c;
 	const char *name;
-	u32 val;
-	int i, r;
+	int i, err;
 
 	for (i = 0; i < THROTTLE_SIZE; i++) {
 		ts->throt_cfgs[i].name = throt_names[i];
@@ -1690,64 +1733,32 @@ static void soctherm_init_hw_throt_cdev(struct platform_device *pdev)
 		ts->throt_cfgs[i].init = false;
 	}
 
-	np_stc = of_get_child_by_name(dev->of_node, "throttle-cfgs");
-	if (!np_stc) {
-		dev_info(dev,
-			 "throttle-cfg: no throttle-cfgs - not enabling\n");
+	np = of_get_child_by_name(dev->of_node, "throttle-cfgs");
+	if (!np) {
+		dev_info(dev, "throttle-cfg: no throttle-cfgs\n");
 		return;
 	}
 
-	for_each_child_of_node(np_stc, np_stcc) {
+	for_each_child_of_node(np, c) {
 		struct soctherm_throt_cfg *stc;
 		struct thermal_cooling_device *tcd;
 
-		name = np_stcc->name;
+		name = c->name;
 		stc = find_throttle_cfg_by_name(ts, name);
 		if (!stc) {
-			dev_err(dev,
-				"throttle-cfg: could not find %s\n", name);
+			dev_err(dev, "throttle-cfg: could not find %s\n", name);
 			continue;
 		}
 
-		r = of_property_read_u32(np_stcc, "nvidia,priority", &val);
-		if (r) {
-			dev_info(dev,
-				 "throttle-cfg: %s: missing priority\n", name);
+		err = soctherm_throt_cfg_parse(dev, c, stc);
+		if (err)
 			continue;
-		}
-		stc->priority = val;
 
-		if (ts->soc->use_ccroc) {
-			r = of_property_read_u32(np_stcc,
-						 "nvidia,cpu-throt-level",
-						 &val);
-			if (r) {
-				dev_info(dev,
-					 "throttle-cfg: %s: missing cpu-throt-level\n",
-					 name);
-				continue;
-			}
-			stc->cpu_throt_level = val;
-		} else {
-			r = of_property_read_u32(np_stcc,
-						 "nvidia,cpu-throt-percent",
-						 &val);
-			if (r) {
-				dev_info(dev,
-					 "throttle-cfg: %s: missing cpu-throt-percent\n",
-					 name);
-				continue;
-			}
-			stc->cpu_throt_depth = val;
-		}
-
-		tcd = thermal_of_cooling_device_register(np_stcc,
-							 (char *)name, ts,
+		tcd = thermal_of_cooling_device_register(c, (char *)name, ts,
 							 &throt_cooling_ops);
-		of_node_put(np_stcc);
+		of_node_put(c);
 		if (IS_ERR_OR_NULL(tcd)) {
-			dev_err(dev,
-				"throttle-cfg: %s: failed to register cooling device\n",
+			dev_err(dev, "throttle-cfg: %s: cdev register failed\n",
 				name);
 			continue;
 		}
@@ -1756,7 +1767,7 @@ static void soctherm_init_hw_throt_cdev(struct platform_device *pdev)
 		stc->init = true;
 	}
 
-	of_node_put(np_stc);
+	of_node_put(np);
 }
 
 /**
@@ -1850,6 +1861,28 @@ static void throttlectl_cpu_level_select(struct tegra_soctherm *ts,
 }
 
 /**
+ * throttlectl_gpu_level() - selects throttling level for GPU
+ * @throt: the LIGHT/HEAVY of throttle event id
+ *
+ * This function programs soctherm's interface to GK20a NV_THERM to select
+ * pre-configured "Low", "Medium" or "Heavy" throttle levels.
+ *
+ * Return: boolean true if HW was programmed
+ */
+static void throttlectl_gpu_level(struct tegra_soctherm *ts,
+					enum soctherm_throttle_id throt)
+{
+	u32 r, depth, throt_vect;
+
+	depth = ts->throt_cfgs[throt].gpu_throt_level;
+	throt_vect = THROT_LEVEL_TO_DEPTH(depth);
+	r = readl(ts->regs + THROT_PSKIP_CTRL(throt, THROTTLE_DEV_GPU));
+	r = REG_SET_MASK(r, THROT_PSKIP_CTRL_ENABLE_MASK, 1);
+	r = REG_SET_MASK(r, THROT_PSKIP_CTRL_VECT_GPU_MASK, throt_vect);
+	writel(r, ts->regs + THROT_PSKIP_CTRL(throt, THROTTLE_DEV_GPU));
+}
+
+/**
  * throttlectl_cpu_mn() - program CPU pulse skipper configuration
  * @throt: the LIGHT/HEAVY of throttle event id
  *
@@ -1863,8 +1896,7 @@ static void throttlectl_cpu_level_select(struct tegra_soctherm *ts,
 static void throttlectl_cpu_mn(struct tegra_soctherm *ts,
 			       enum soctherm_throttle_id throt)
 {
-	u32 r;
-	int depth;
+	u32 r, depth;
 	u8 dividend;
 
 	depth = ts->throt_cfgs[throt].cpu_throt_depth;
@@ -1903,6 +1935,7 @@ static void soctherm_throttle_program(struct tegra_soctherm *ts,
 		throttlectl_cpu_level_select(ts, throt);
 	else
 		throttlectl_cpu_mn(ts, throt);
+	throttlectl_gpu_level(ts, throt);
 
 	r = REG_SET_MASK(0, THROT_PRIORITY_LITE_PRIO_MASK, stc.priority);
 	writel(r, ts->regs + THROT_PRIORITY_CTRL(throt));

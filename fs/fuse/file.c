4a97501b7a9c693e86830c7c289d06e3319e6d23
Aniruddha TVS Rao <anrao@nvidia.com>
2017-03-17 15:05:56 +0530

    fs: fuse: Avoid parameter hide
    local variable "fc" hides function argument "fc".
    Rename "fc" with "temp_fc" to fix the issue.
    
    Coverity ID 29684
    
    Bug 200314122
    
    Change-Id: Ie0c0e006767f6b329747ee1658fbe6ad1d4e7b26
    Signed-off-by: Aniruddha TVS Rao <anrao@nvidia.com>
    Reviewed-on: http://git-master/r/1322971
    (cherry picked from commit c09725a01836dda61363c9dac3a69649ba8fd899)
    Reviewed-on: http://git-master/r/1496498
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1538,7 +1538,7 @@ static void fuse_writepage_end(struct fuse_conn *fc, struct fuse_req *req)
 	mapping_set_error(inode->i_mapping, req->out.h.error);
 	spin_lock(&fc->lock);
 	while (req->misc.write.next) {
-		struct fuse_conn *fc = get_fuse_conn(inode);
+		struct fuse_conn *temp_fc = get_fuse_conn(inode);
 		struct fuse_write_in *inarg = &req->misc.write.in;
 		struct fuse_req *next = req->misc.write.next;
 		req->misc.write.next = next->misc.write.next;
@@ -1569,7 +1569,7 @@ static void fuse_writepage_end(struct fuse_conn *fc, struct fuse_req *req)
 		 * no invocations of fuse_writepage_end() while we're in
 		 * fuse_set_nowrite..fuse_release_nowrite section.
 		 */
-		fuse_send_writepage(fc, next, inarg->offset + inarg->size);
+		fuse_send_writepage(temp_fc, next, inarg->offset + inarg->size);
 	}
 	fi->writectr--;
 	fuse_writepage_finish(fc, req);

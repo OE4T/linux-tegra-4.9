be803ff604beb1fa8aac7a1052705c0b6cfac1be
Aniruddha TVS Rao <anrao@nvidia.com>
2017-03-20 15:35:01 +0530

    drivers: mtd: build: Avoid out of bounds write
    string pointed by tokens[0] may exceed the length of array p->name
    and lead to out of bounds write.
    Add a check for the length of string at tokens[0] to fix the issue.
    
    Coverity ID 30855
    
    Bug 200192581
    
    Change-Id: I5d825f5d6167e4913082d96812f48bf2f4ce7d3b
    Signed-off-by: Aniruddha TVS Rao <anrao@nvidia.com>
    Reviewed-on: http://git-master/r/1324223
    (cherry picked from commit 372781b73e76bd9ef9c572c7aa9767fccd9132a4)
    Reviewed-on: https://git-master.nvidia.com/r/1528634
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/mtd/ubi/build.c
+++ drivers/mtd/ubi/build.c
@@ -1433,8 +1433,13 @@ static int __init ubi_mtd_param_parse(const char *val, struct kernel_param *kp)
 	}
 
 	p = &mtd_dev_param[mtd_devs];
-	strcpy(&p->name[0], tokens[0]);
 
+	if (strlen(tokens[0]) > MTD_PARAM_LEN_MAX) {
+		pr_err("length of tokens[0] exceeds destination p->name[0]\n");
+		return -EINVAL;
+	}
+
+	strcpy(&p->name[0], tokens[0]);
 	token = tokens[1];
 	if (token) {
 		p->vid_hdr_offs = bytes_str_to_int(token);

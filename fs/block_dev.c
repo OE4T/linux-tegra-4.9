6b0fc0c49316f4d055f05f8b9e5f1542a9af61b5
Jan Kara <jack@suse.cz>
2017-03-23 01:37:00 +0100

    block: Fix oops in locked_inode_to_wb_and_lock_list()
    When block device is closed, we call inode_detach_wb() in __blkdev_put()
    which sets inode->i_wb to NULL. That is contrary to expectations that
    inode->i_wb stays valid once set during the whole inode's lifetime and
    leads to oops in wb_get() in locked_inode_to_wb_and_lock_list() because
    inode_to_wb() returned NULL.
    
    The reason why we called inode_detach_wb() is not valid anymore though.
    BDI is guaranteed to stay along until we call bdi_put() from
    bdev_evict_inode() so we can postpone calling inode_detach_wb() to that
    moment.
    
    Also add a warning to catch if someone uses inode_detach_wb() in a
    dangerous way.
    
    Reported-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit f759741d9d913eb57784a94b9bca78b376fc26a9)
    
    Change-Id: I19067a22690021a5404ba3290bcb9900d84c2e9f
    Signed-off-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1575888
    Tested-by: Naman Patel <namanp@nvidia.com>
    Reviewed-by: Venkata Jagadish <vjagadish@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -626,6 +626,8 @@ static void bdev_evict_inode(struct inode *inode)
 	spin_lock(&bdev_lock);
 	list_del_init(&bdev->bd_list);
 	spin_unlock(&bdev_lock);
+	/* Detach inode from wb early as bdi_put() may free bdi->wb */
+	inode_detach_wb(inode);
 }
 
 static const struct super_operations bdev_sops = {
@@ -1584,12 +1586,6 @@ static void __blkdev_put(struct block_device *bdev, fmode_t mode, int for_part)
 		kill_bdev(bdev);
 
 		bdev_write_inode(bdev);
-		/*
-		 * Detaching bdev inode from its wb in __destroy_inode()
-		 * is too late: the queue which embeds its bdi (along with
-		 * root wb) can be gone as soon as we put_disk() below.
-		 */
-		inode_detach_wb(bdev->bd_inode);
 	}
 	if (bdev->bd_contains == bdev) {
 		if (disk->fops->release)

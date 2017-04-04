0198e5f7c3722e68c395e0a4958217aef20178df
Aniruddha TVS Rao <anrao@nvidia.com>
2017-04-04 12:27:32 +0530

    block: blk-map: Avoid use after free
    mapped_bio dereferences pointer to freed memory and attempts
    to free it again.
    Remove unnecessary assignment and free statements to avoid undefined
    behaviour.
    
    Coverity ID 38491
    
    Bug 200314096
    
    Change-Id: Iea67c5dbe675bec6e68cac4f34602fa56a6e2d38
    Signed-off-by: Aniruddha TVS Rao <anrao@nvidia.com>
    Reviewed-on: http://git-master/r/1454617
    (cherry picked from commit 8ca4d9fd68e746cade6933c2d460561530847897)
    Reviewed-on: http://git-master/r/1496553
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- block/blk-map.c
+++ block/blk-map.c
@@ -183,13 +183,10 @@ int blk_rq_unmap_user(struct bio *bio)
 		if (unlikely(bio_flagged(bio, BIO_BOUNCED)))
 			mapped_bio = bio->bi_private;
 
+		bio = bio->bi_next;
 		ret2 = __blk_rq_unmap_user(mapped_bio);
 		if (ret2 && !ret)
 			ret = ret2;
-
-		mapped_bio = bio;
-		bio = bio->bi_next;
-		bio_put(mapped_bio);
 	}
 
 	return ret;

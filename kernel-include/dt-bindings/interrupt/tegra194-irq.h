892c3d23cb9d68503acec5bca31ddf85f3ea524e
Venkat Reddy Talla <vreddytalla@nvidia.com>
2017-04-04 17:01:00 +0530

    DT: tegra19: remove tegra186 references from T19x
    Remove tegra186 powergate and IRQ references from Tegra194 soc
    dts files, create/use tegra194 specific dt-bindigs for tegra194
    platforms.
    
    Change-Id: Id7f9a9a237de7ca377dcf1778057f9f050dd0068
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-on: http://git-master/r/1454803
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Shardar Mohammed <smohammed@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

---
+++ kernel-include/dt-bindings/interrupt/tegra194-irq.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef _DT_BINDINGS_INTERRUPT_TEGRA194_IRQ_H
+#define _DT_BINDINGS_INTERRUPT_TEGRA194_IRQ_H
+
+#define TEGRA194_IRQ_I2C1			25
+#define TEGRA194_IRQ_I2C2			26
+#define TEGRA194_IRQ_I2C3			27
+#define TEGRA194_IRQ_I2C4			28
+#define TEGRA194_IRQ_I2C5			29
+#define TEGRA194_IRQ_I2C6			30
+#define TEGRA194_IRQ_I2C7			31
+#define TEGRA194_IRQ_I2C8			32
+#define TEGRA194_IRQ_I2C9			33
+#define TEGRA194_IRQ_UFSHC			44
+#define TEGRA194_IRQ_UARTA			112
+#define TEGRA194_IRQ_UARTB			113
+#define TEGRA194_IRQ_UARTC			114
+#define TEGRA194_IRQ_UARTD			115
+#define TEGRA194_IRQ_UARTE			116
+#define TEGRA194_IRQ_UARTF			117
+#define TEGRA194_IRQ_UARTG			118
+#define TEGRA194_IRQ_SATA			197
+#define TEGRA194_IRQ_ACTMON			210
+#endif

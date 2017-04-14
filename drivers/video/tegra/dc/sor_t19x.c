3311e0e918a09c25d9e3105f5d7218ff37ac0301
Shu Zhong <shuz@nvidia.com>
2017-04-13 17:06:28 -0700

    t19x: dc: add SOR register header and source
    For T19x, the following kinds of SOR register changes
    need to be taken care of:
    A) Existing registers with a new base offset
    B) Existing registers with new fields
    C) Existing registers with added field enum values
    D) New registers
    
    This change adds a new T19x SOR register definition
    header - "sor_regs_t19x.h" - which contains all the
    registers that fall into the four above groups,
    except for:
    - Existing registers which we currently don't expose
      for T21x/T18x
    - Registers whose fields and/or enum values were semantically
      renamed
    
    This change also forks off any T19x SOR programming changes
    into a new "sor_t19x.c" file, so that everything is
    logically grouped.
    
    TDS-2129
    
    Change-Id: I1cfa7f45f6d572c09f1a252c866d2f5a466df1f7
    Signed-off-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-on: http://git-master/r/1462876
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ drivers/video/tegra/dc/sor_t19x.c
@@ -0,0 +1,97 @@
+/*
+ * drivers/video/tegra/dc/sor_t19x.c
+ *
+ * Copyright (c) 2017, NVIDIA CORPORATION, All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include "dc_priv.h"
+#include "hw_nvdisp_nvdisp.h"
+#include "nvdisp.h"
+#include "sor.h"
+#include "sor_regs_t19x.h"
+
+inline u32 nv_sor_head_state0_t19x(u32 i)
+{
+	return NV_HEAD_STATE0_T19X(i);
+}
+
+inline u32 nv_sor_head_state1_t19x(u32 i)
+{
+	return NV_HEAD_STATE1_T19X(i);
+}
+
+inline u32 nv_sor_head_state2_t19x(u32 i)
+{
+	return NV_HEAD_STATE2_T19X(i);
+}
+
+inline u32 nv_sor_head_state3_t19x(u32 i)
+{
+	return NV_HEAD_STATE3_T19X(i);
+}
+
+inline u32 nv_sor_head_state4_t19x(u32 i)
+{
+	return NV_HEAD_STATE4_T19X(i);
+}
+
+inline u32 nv_sor_head_state5_t19x(u32 i)
+{
+	return NV_HEAD_STATE5_T19X(i);
+}
+
+inline u32 nv_sor_pll0_t19x(void)
+{
+	return NV_SOR_PLL0_T19X;
+}
+
+inline u32 nv_sor_pll1_t19x(void)
+{
+	return NV_SOR_PLL1_T19X;
+}
+
+inline u32 nv_sor_pll2_t19x(void)
+{
+	return NV_SOR_PLL2_T19X;
+}
+
+inline u32 nv_sor_pll3_t19x(void)
+{
+	return NV_SOR_PLL3_T19X;
+}
+
+inline u32 nv_sor_pll4_t19x(void)
+{
+	return NV_SOR_PLL4_T19X;
+}
+
+inline u32 nv_sor_dp_padctl_t19x(u32 i)
+{
+	return NV_SOR_DP_PADCTL_T19X(i);
+}
+
+inline u32 nv_sor_dp_misc1_override_t19x(void)
+{
+	return NV_SOR_DP_MISC1_OVERRIDE_T19X;
+}
+
+inline u32 nv_sor_dp_misc1_bit6_t19x(void)
+{
+	return NV_SOR_DP_MISC1_BIT6_T19X;
+}
+
+inline u32 nv_sor_dp_int_enable_t19x(void)
+{
+	return NV_SOR_DP_INT_ENABLE_T19X;
+}
+

9064172617fec21063bb645ad0a524c5fe9ffd71
Laxman Dewangan <ldewangan@nvidia.com>
2017-04-17 16:20:38 +0530

    Revert "pwm: tegra: Use DIV_ROUND_CLOSEST_ULL() instead of local implementation"
    Reverting this change to align with mainline.
    
    This reverts commit 39779b302e852ba32c038ddd280547d9c82f1f9c.
    
    Change-Id: Iecf6df2a928089ebea9cb324dda8326e1e79a5f9
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/1463858
    GVS: Gerrit_Virtual_Submit

--- drivers/pwm/pwm-tegra.c
+++ drivers/pwm/pwm-tegra.c
@@ -134,7 +134,8 @@ static int tegra_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 	 * nearest integer during division.
 	 */
 	c *= (1 << PWM_DUTY_WIDTH);
-	c = DIV_ROUND_CLOSEST_ULL(c, period_ns);
+	c += period_ns / 2;
+	do_div(c, period_ns);
 
 	val = (u32)c << PWM_DUTY_SHIFT;
 
@@ -151,8 +152,10 @@ static int tegra_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 	rate = clk_get_rate(pc->clk) >> PWM_DUTY_WIDTH;
 
 	/* Consider two digit precision in PWM_SCALE_WIDTH rate calculation */
-	ns100 *= precision;
-	hz = DIV_ROUND_CLOSEST_ULL(ns100, period_ns);
+	hz = ns100 * precision;
+	hz += period_ns / 2;
+	do_div(hz, period_ns);
+
 	rate = DIV_ROUND_CLOSEST((rate * precision), hz);
 
 	/*

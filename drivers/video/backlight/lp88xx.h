52ddcacf6a07c97c5d13977b361e4be28552cdf7
Ishwarya Balaji Gururajan <igururajan@nvidia.com>
2017-04-24 13:55:18 -0700

    backlight: lp88x: add support for low-persistence
    1) add nvsr unlock functionality.
    2) add support for nvsr brightness control.
    3) read display mode parameters from panel display timings node on DT.
    4) read settle-time from DT for timer-offsets. If flash-time is not
       specified on DT, read flash-time as (vertical blank period -
       settle time)
    
    Jira TDS-2087
    Jira TDS-2309
    
    Change-Id: Ief2e0e74180fada89aeadf9c196a3e9e0261c948
    Signed-off-by: Ishwarya Balaji Gururajan <igururajan@nvidia.com>
    Reviewed-on: http://git-master/r/1468853
    (cherry picked from commit 00980c3644e41c0cad87a5473ee9d4c654264c47)
    Reviewed-on: https://git-master.nvidia.com/r/1535174
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/backlight/lp88xx.h
+++ drivers/video/backlight/lp88xx.h
@@ -3,6 +3,8 @@
  *
  * Copyright 2016 Texas Instruments
  *
+ * Copyright (c) 2017, NVIDIA CORPORATION, All rights reserved.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
@@ -12,8 +14,11 @@
 #define __BL_LP88XX_H
 
 #include <linux/pwm.h>
+#include <drm/drm_fixed.h>
 
-#define EEPROM_NUM_ADDR	22
+#define EEPROM_NUM_ADDR			22
+#define LP88XX_NUM_REGIONS		6
+#define LP88XX_NUM_BRT_LEVELS		6
 
 enum lp88xx_chip_id {
 	LP8580,
@@ -40,9 +45,16 @@ struct lp88xx {
 	u16 max_dev_brt;
 	struct eeprom_datum default_eeprom[EEPROM_NUM_ADDR];
 	int eeprom_count;
+	int led_strings_used;		/* Number of LED strings used */
 	u32 max_input_brt;
+	u16 brt_levels[LP88XX_NUM_BRT_LEVELS];	/* Brightness levels */
+	u32 pwm_settle_time_int; /* settle time in pwm clock ticks */
+	u32 pwm_flash_time_int; /* flash time in pwm clock tickcs */
+	u32 frame_duration_ms;
 	enum lp88xx_chip_id chip_id;
 	bool eeprom_lock;
+	bool nvsr_lock;
+	bool low_persistence;
 };
 
 extern int lp88xx_common_probe(struct device *dev, struct lp88xx *lp);
--- drivers/video/backlight/lp88xx_core.c
+++ drivers/video/backlight/lp88xx_core.c
@@ -3,6 +3,8 @@
  *
  * Copyright 2016 Texas Instruments
  *
+ * Copyright (c) 2017, NVIDIA CORPORATION, All rights reserved.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
@@ -21,7 +23,6 @@
 
 #include "lp88xx.h"
 
-#define LP88XX_NUM_REGIONS		6
 #define DEFAULT_BL_NAME			"lcd-bl"
 #define LP88XX_MAX_INT_STATUS		3
 #define LP88XX_INT_REG_OFFSET		2
@@ -35,19 +36,33 @@
 #define LP88XX_PWM_MODE			0
 #define LP88XX_REGISTER_MODE		2
 #define LP88XX_BL_EN			BIT(8)
+#define LP88XX_NVSR_EN			BIT(11)
 
 /* LP8580 */
 #define LP8580_REG_BRT_BASE			0x26
 #define LP8580_REG_GROUP1			0x2e
 #define LP8580_REG_GROUP2			0x30
+#define LP8580_REG_TIMING_CONFIG1		0x34
+#define LP8580_FRAME_TIMING_MASK	0xE000
+#define LP8580_EXTERNAL_VSYNC		BIT(15)
+#define LP8580_INTERNAL_VSYNC		BIT(14)
+#define LP8580_LEDX_BX				0x130
+#define LP8580_LEDX_BX_DEFAULT		0xFFF8
+#define LP8580_LED_BX_OFFSET		0x02
+#define LP8580_LEDX_OFFSET		0x0C
+#define LP8580_TIMER_T0				0x100
+#define LP8580_TIMER_T1				0x102
+#define LP8580_TIMER_T2				0x104
+
+#define LP8580_TIMER_T0_OFFSET		0x02
 #define LP8580_MAX_BRIGHTNESS		0x1fff
 
 /* EEPROM */
-#define LP88XX_EEPROM_CONTROL_1		0x270
-#define LP88XX_EEPROM_CONTROL_2		0x272
-#define LP88XX_EEPROM_CONTROL_3		0x274
-#define LP88XX_EEPROM_CONTROL_4		0x276
-#define LP88XX_EEPROM_UNLOCK		0x278
+#define LP88XX_EEPROM_CONTROL_1			0x270
+#define LP88XX_EEPROM_CONTROL_2			0x272
+#define LP88XX_EEPROM_CONTROL_3			0x274
+#define LP88XX_EEPROM_CONTROL_4			0x276
+#define LP88XX_EEPROM_UNLOCK			0x278
 #define LP88XX_EEPROM_UNLOCK_SEQ1	0x1234
 #define LP88XX_EEPROM_UNLOCK_SEQ2	0x5678
 #define LP88XX_EEPROM_UNLOCK_SEQ3	0x9abc
@@ -56,6 +71,18 @@
 #define LP88XX_START_EEPROM_PROG	BIT(2)
 #define LP88XX_EEPROM0_ADDR			0x0000
 #define LP88XX_EEPROM1_ADDR			0x0001
+
+/* PWM */
+#define LP88XX_INTERNAL_PLL_FREQ	20000000
+#define LP88XX_PWM_FREQ_CONFIG			0x24
+
+/* NVSR */
+#define LP88XX_NVSR_LOCK4			0x68
+#define LP88XX_NVSR_LOCK5			0x6a
+#define LP88XX_NVSR_UNLOCK_SEQ1		0x4356
+#define LP88XX_NVSR_UNLOCK_SEQ2		0x9a5f
+#define LP88XX_NVSR_UNLOCK_SEQ3		0x93da
+#define LP88XX_DEFAULT_FRAME_DURATION	17
 
 /* LP88xx */
 #define LP88XX_REG_BRT_BASE			0x28
@@ -278,6 +305,49 @@ lock_eeprom:
 	return ret;
 }
 
+static int lp88xx_unlock_nvsr(struct lp88xx *lp)
+{
+	int ret = 0;
+	u16 val = 0;
+
+	if (!lp->nvsr_lock)
+		return ret;
+
+	ret = lp88xx_reg_read(lp, LP88XX_NVSR_LOCK4, &val);
+	if (ret < 0)
+		return ret;
+
+	val ^= LP88XX_NVSR_UNLOCK_SEQ1;
+
+	ret = lp88xx_reg_write(lp, LP88XX_NVSR_LOCK5, val);
+	if (ret < 0)
+		return ret;
+
+	ret = lp88xx_reg_read(lp, LP88XX_NVSR_LOCK4, &val);
+	if (ret < 0)
+		return ret;
+
+	val ^= LP88XX_NVSR_UNLOCK_SEQ2;
+
+	ret = lp88xx_reg_write(lp, LP88XX_NVSR_LOCK5, val);
+	if (ret < 0)
+		return ret;
+
+	ret = lp88xx_reg_read(lp, LP88XX_NVSR_LOCK4, &val);
+	if (ret < 0)
+		return ret;
+
+	val ^= LP88XX_NVSR_UNLOCK_SEQ3;
+
+	ret = lp88xx_reg_write(lp, LP88XX_NVSR_LOCK5, val);
+	if (ret < 0)
+		return ret;
+
+	lp->nvsr_lock = false;
+
+	return ret;
+}
+
 static int lp88xx_reg_update(struct lp88xx *lp, u16 reg, u16 mask, u16 val)
 {
 	int ret;
@@ -349,13 +419,107 @@ static int lp88xx_bl_on(struct lp88xx *lp, int on)
 	return lp88xx_reg_update(lp, LP88XX_REG_BL_MODE, LP88XX_BL_EN, val);
 }
 
+static int lp88xx_nvsr_brightness_control_enable(struct lp88xx *lp)
+{
+	int ret = 0, i = 0, j = 0;
+
+	/* Configure frame timing based on external VSYNC input */
+	lp88xx_reg_update(lp, LP8580_REG_TIMING_CONFIG1,
+			LP8580_FRAME_TIMING_MASK, LP8580_EXTERNAL_VSYNC);
+
+	for (i = 0; i < lp->led_strings_used; i++) {
+
+		/* Assign grouping to LED strings */
+		lp88xx_update_region(lp, i, (i * 1));
+
+		/* Change brightness levels for all regions */
+		for (j = 0; j < LP88XX_NUM_BRT_LEVELS; j++) {
+			lp88xx_reg_write(lp, LP8580_LEDX_BX +
+				(j * LP8580_LED_BX_OFFSET) +
+				(i * LP8580_LEDX_OFFSET), lp->brt_levels[j]);
+		}
+	}
+
+	/* In effect, we flash from settle time to (settle time + flash time) */
+	lp88xx_reg_write(lp, LP8580_TIMER_T1, lp->pwm_settle_time_int);
+	lp88xx_reg_write(lp, LP8580_TIMER_T2, lp->pwm_flash_time_int +
+						lp->pwm_settle_time_int);
+
+	/* Enable NVSR and set sel_nvsr_mode to Mode 1 */
+	lp88xx_reg_update(lp, LP88XX_REG_BL_MODE, LP88XX_NVSR_EN,
+			LP88XX_NVSR_EN);
+
+	return ret;
+}
+
+static int lp88xx_nvsr_brightness_control_disable(struct lp88xx *lp)
+{
+	int ret = 0, i = 0, j = 0;
+
+	for (i = 0; i < lp->led_strings_used; i++) {
+
+		/* Reset brightness levels for all regions */
+		for (j = 0; j < LP88XX_NUM_BRT_LEVELS; j++) {
+			lp88xx_reg_write(lp, LP8580_LEDX_BX +
+				(j * LP8580_LED_BX_OFFSET) +
+				(i * LP8580_LEDX_OFFSET),
+				LP8580_LEDX_BX_DEFAULT);
+		}
+
+		/* Assign grouping to LED strings */
+		lp88xx_update_region(lp, i, false);
+	}
+
+	/* Reset timer - Common for all LED strings */
+	for (j = 0; j < LP88XX_NUM_BRT_LEVELS; j++) {
+		lp88xx_reg_write(lp, LP8580_TIMER_T0 +
+				(j * LP8580_TIMER_T0_OFFSET), 0);
+	}
+
+	/* Wait for the frame duration */
+	msleep(lp->frame_duration_ms);
+
+	/* Change frame timing back to internal VSYNC */
+	lp88xx_reg_update(lp, LP8580_REG_TIMING_CONFIG1,
+			LP8580_FRAME_TIMING_MASK, LP8580_INTERNAL_VSYNC);
+
+	/* Disable NVSR mode */
+	lp88xx_reg_update(lp, LP88XX_REG_BL_MODE, LP88XX_NVSR_EN, 0);
+
+	return ret;
+}
+
+static fixed20_12 lp88xx_get_pwm_clock_period(struct lp88xx *lp)
+{
+	u16 pwm_counter_length = 0;
+	u32 pwm_clk_freq = 0;
+	int ret = 0;
+	fixed20_12 fconst, freq;
+	fixed20_12 pwm_clk_period_us = dfixed_init(0);
+
+	ret = lp88xx_reg_read(lp, LP88XX_PWM_FREQ_CONFIG, &pwm_counter_length);
+	if (ret < 0)
+		return pwm_clk_period_us;
+
+	pwm_clk_freq = LP88XX_INTERNAL_PLL_FREQ / (pwm_counter_length + 1);
+
+	fconst.full = dfixed_const(1000000);
+	freq.full = dfixed_const(pwm_clk_freq);
+	pwm_clk_period_us.full = dfixed_div(fconst, freq);
+
+	dev_dbg(lp->dev, "pwm clk period is %d.%dus\n",
+		dfixed_trunc(pwm_clk_period_us),
+		dfixed_frac(pwm_clk_period_us) * 1000 / ((1 << 12) - 1));
+	return pwm_clk_period_us;
+}
+
 static int lp88xx_bl_update_status(struct backlight_device *bldev)
 {
 	struct lp88xx_bl *bl = bl_get_data(bldev);
 	struct lp88xx *lp = bl->lp;
 	u32 val = bldev->props.brightness;
 	int duty;
-	int ret;
+	int ret = 0;
 
 	val = val * lp->max_dev_brt / lp->max_input_brt;
 
@@ -363,6 +527,20 @@ static int lp88xx_bl_update_status(struct backlight_device *bldev)
 		lp88xx_bl_on(lp, 1);
 	else
 		lp88xx_bl_on(lp, 0);
+
+	/* If low-persistence mode is set */
+	if ((bldev->props.state & BL_CORE_LPMODE) && !lp->low_persistence) {
+		lp88xx_nvsr_brightness_control_enable(lp);
+		dev_dbg(lp->dev, "Enabling low-persistence mode\n");
+		lp->low_persistence = true;
+	}
+
+	/* If low-persistence mode is unset */
+	if (!(bldev->props.state & BL_CORE_LPMODE) && lp->low_persistence) {
+		lp88xx_nvsr_brightness_control_disable(lp);
+		dev_dbg(lp->dev, "Disabling low-persistence mode\n");
+		lp->low_persistence = false;
+	}
 
 	/* PWM mode */
 	if (lp->pwm) {
@@ -394,13 +572,148 @@ static const struct backlight_ops lp88xx_bl_ops = {
 	.update_status = lp88xx_bl_update_status,
 };
 
+static int lp88xx_get_lp_params(struct lp88xx *lp)
+{
+	int ret = 0, num_levels = 0;
+	struct device_node *timings_np, *mode_np;
+	const __be32 *num_levels_list;
+	u32 temp = 0;
+	u32 pclk, refresh_rate;
+	u32 h_back_porch, v_back_porch, h_front_porch, v_front_porch;
+	u32 h_sync_width, v_sync_width, h_active, v_active, h_total, v_total;
+	fixed20_12 per_line_us, fixed_refresh_rate, pwm_clk_period_us;
+	fixed20_12 fixed_v_total, time_conv, fixed_temp, frame_duration_ms;
+	fixed20_12 v_blank_period_us, v_blank_period_ms, v_blank_period;
+	fixed20_12 settle_time_ms, settle_time_us, pwm_settle_time;
+	fixed20_12 flash_time_ms, flash_time_us, pwm_flash_time;
+
+	pwm_clk_period_us = lp88xx_get_pwm_clock_period(lp);
+	if (!dfixed_trunc(pwm_clk_period_us) &&
+	    !dfixed_frac(pwm_clk_period_us))  {
+		dev_warn(lp->dev, "Failed to get pwm clock period %d\n",
+				ret);
+		return ret;
+	}
+	/* Parse panel timings */
+	timings_np = of_parse_phandle(lp->dev->of_node, "panel-timings", 0);
+	if (IS_ERR_OR_NULL(timings_np)) {
+		dev_err(lp->dev, "cannot find panel timings\n");
+		return PTR_ERR(timings_np);
+	}
+
+	mode_np = of_get_next_child(timings_np, NULL);
+
+	of_property_read_u32(mode_np, "clock-frequency", &pclk);
+	of_property_read_u32(mode_np, "hsync-len", &h_sync_width);
+	of_property_read_u32(mode_np, "vsync-len", &v_sync_width);
+	of_property_read_u32(mode_np, "hback-porch", &h_back_porch);
+	of_property_read_u32(mode_np, "vback-porch", &v_back_porch);
+	of_property_read_u32(mode_np, "hactive", &h_active);
+	of_property_read_u32(mode_np, "vactive", &v_active);
+	of_property_read_u32(mode_np, "hfront-porch", &h_front_porch);
+	of_property_read_u32(mode_np, "vfront-porch", &v_front_porch);
+
+	num_levels_list = of_get_property(lp->dev->of_node, "brt-levels",
+						&num_levels);
+	if (!num_levels_list) {
+		dev_err(lp->dev, "warning: using default region brt\n");
+		memset(lp->brt_levels, LP8580_LEDX_BX_DEFAULT,
+			sizeof(lp->brt_levels));
+	} else {
+		num_levels /= sizeof(*num_levels_list);
+		ret = of_property_read_u16_array(lp->dev->of_node, "brt-levels",
+				lp->brt_levels, num_levels);
+		if (ret) {
+			dev_err(lp->dev, "Failed to get region brt: %d\n", ret);
+			return ret;
+		}
+	}
+
+	h_total = h_sync_width + h_back_porch + h_active + h_front_porch;
+	v_total = v_sync_width + v_back_porch + v_active + v_front_porch;
+	refresh_rate = pclk / (h_total * v_total);
+
+	/* For s to ms, ms to us and vice-versa conversion */
+	time_conv.full = dfixed_const(1000);
+	fixed_refresh_rate.full = dfixed_const(refresh_rate);
+
+	frame_duration_ms.full = dfixed_div(time_conv, fixed_refresh_rate);
+	fixed_v_total.full = dfixed_const(v_total);
+	v_blank_period.full = dfixed_const(v_back_porch + v_front_porch);
+
+	/* Convert duration per line to us for better accuracy */
+	fixed_temp.full = dfixed_mul(frame_duration_ms, time_conv);
+	per_line_us.full = dfixed_div(fixed_temp, fixed_v_total);
+
+	/* vertical blank period */
+	fixed_temp.full = dfixed_mul(per_line_us, v_blank_period);
+	v_blank_period_ms.full = dfixed_div(fixed_temp, time_conv);
+
+	dev_dbg(lp->dev, "frame_duration_ms is %d.%dms\n",
+			dfixed_trunc(frame_duration_ms),
+			dfixed_frac(frame_duration_ms) *
+					1000 / ((1 << 12) - 1));
+	dev_dbg(lp->dev, "per_line_us is %d.%dus\n",
+			dfixed_trunc(per_line_us),
+			dfixed_frac(per_line_us) * 1000 / ((1 << 12) - 1));
+	dev_dbg(lp->dev, "v_blank_period_ms is %d.%dms\n",
+			dfixed_trunc(v_blank_period_ms),
+			dfixed_frac(v_blank_period_ms) *
+					1000 / ((1 << 12) - 1));
+
+	ret = of_property_read_u32(lp->dev->of_node, "settle-time-us", &temp);
+	if (ret) {
+		dev_err(lp->dev, "cannot find settle time\n");
+		return ret;
+	}
+	settle_time_us.full = dfixed_const(temp);
+	settle_time_ms.full = dfixed_div(settle_time_us, time_conv);
+	v_blank_period_us.full = dfixed_mul(v_blank_period_ms, time_conv);
+
+	/* Flash time is optional.
+	 * If not specified, use (vblank - settle time) as flash time.
+	 */
+	if (of_property_read_u32(lp->dev->of_node, "flash-time-us", &temp)) {
+		dev_info(lp->dev, "use (vblank - settle time) as flash time\n");
+		flash_time_us.full = dfixed_const(
+					dfixed_trunc(v_blank_period_us) -
+					dfixed_trunc(settle_time_us));
+		flash_time_ms.full = dfixed_div(flash_time_us, time_conv);
+	} else {
+		flash_time_us.full = dfixed_const(temp);
+		flash_time_ms.full = dfixed_div(flash_time_us, time_conv);
+	}
+
+	/* Timer offsets are counted as PWM clock periods */
+	pwm_settle_time.full = dfixed_div(settle_time_us, pwm_clk_period_us);
+	pwm_flash_time.full = dfixed_div(flash_time_us, pwm_clk_period_us);
+
+	/* We are writing only the integer part into timer offset registers */
+	lp->pwm_settle_time_int = dfixed_trunc(pwm_settle_time);
+	lp->pwm_flash_time_int = dfixed_trunc(pwm_flash_time);
+	lp->frame_duration_ms = dfixed_trunc(frame_duration_ms);
+
+	dev_dbg(lp->dev, "settle time is %d.%dms, pwm settle time is %d\n",
+		dfixed_trunc(settle_time_ms),
+		dfixed_frac(settle_time_ms) * 100 / ((1 << 12) - 1),
+		lp->pwm_settle_time_int);
+	dev_dbg(lp->dev, "flash time is %d.%dms, pwm flash time is %d\n",
+		dfixed_trunc(flash_time_ms),
+		dfixed_frac(flash_time_ms) * 100 / ((1 << 12) - 1),
+		lp->pwm_flash_time_int);
+
+	return ret;
+}
+
 static int lp88xx_add_bl_device(struct lp88xx *lp, int id)
 {
 	struct lp88xx_bl *bl;
 	struct device *dev = lp->dev;
 	struct backlight_properties props;
 	char name[64];
+	int ret = 0;
 	const char *pname;
+	u32 temp = 0;
 	unsigned int reg_brt[] = {
 		[LP88XX_REGION_BASE] =
 			lp->chip_id ? LP88XX_REG_BRT_BASE : LP8580_REG_BRT_BASE,
@@ -425,6 +738,28 @@ static int lp88xx_add_bl_device(struct lp88xx *lp, int id)
 		snprintf(name, sizeof(name), "%s", pname);
 	else
 		snprintf(name, sizeof(name), "%s:%d", DEFAULT_BL_NAME, id);
+
+	memset(&props, 0, sizeof(props));
+
+	if (of_property_read_u32(dev->of_node, "low-persistence", &temp))
+		props.low_persistence_capable = false;
+	else
+		props.low_persistence_capable = (bool) temp;
+
+	if (props.low_persistence_capable) {
+		/* Unlock NVSR for enabling NVSR brightness mode */
+		ret = lp88xx_unlock_nvsr(lp);
+		if (ret < 0) {
+			dev_err(dev, "Failed to unlock nvsr %d\n", ret);
+			return ret;
+		}
+	}
+
+	if (props.low_persistence_capable) {
+		/* Get low-persistence related params */
+		if (lp88xx_get_lp_params(lp))
+			return ret;
+	}
 
 	props.type = BACKLIGHT_PLATFORM;
 	props.max_brightness = lp->max_input_brt;
@@ -573,6 +908,8 @@ int lp88xx_common_probe(struct device *dev, struct lp88xx *lp)
 		lp->chip_id = LP88XX;
 
 	lp->eeprom_lock = true;
+	lp->nvsr_lock = true;
+	lp->low_persistence = false;
 
 	ret = lp88xx_reg_read(lp, LP88XX_REG_CAP2, &val);
 	if (ret) {
@@ -587,6 +924,12 @@ int lp88xx_common_probe(struct device *dev, struct lp88xx *lp)
 	if (of_property_read_u32(dev->of_node, "max-input-brt",
 		&lp->max_input_brt))
 		lp->max_input_brt = lp->max_dev_brt;
+
+	if (of_property_read_u32(dev->of_node, "led-strings-used",
+					&lp->led_strings_used)) {
+		dev_warn(dev, "warning: using default led-strings-used\n");
+		lp->led_strings_used = LP88XX_NUM_REGIONS;
+	}
 
 	of_property_for_each_u32(dev->of_node, "default-eeprom", prop, p, u)
 		eeprom_count++;

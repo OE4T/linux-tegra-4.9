592bc947bb088720081c12639836b326e0f377e2
Sri Krishna chowdary <schowdary@nvidia.com>
2017-04-25 19:39:34 +0530

    video: tegra: nvmap: update copyright text
    Update copyright text as recommended by GPLv2
    
    bug 200302395
    
    Change-Id: I18d720ba152df43532ef16bb7bb0e95f00f683ea
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1469581
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_cache_t19x.c
+++ drivers/video/tegra/nvmap/nvmap_cache_t19x.c
@@ -1,7 +1,7 @@
 /*
  * drivers/video/tegra/nvmap/nvmap_cache_t19x.c
  *
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -11,9 +11,6 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #define pr_fmt(fmt)	"nvmap: %s() " fmt, __func__

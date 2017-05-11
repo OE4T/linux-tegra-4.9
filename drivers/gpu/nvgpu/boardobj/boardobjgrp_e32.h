677edcd6ec16298722b1402d92bd493e68bc0299
Deepak Nibade <dnibade@nvidia.com>
2017-05-11 14:46:32 +0530

    gpu: nvgpu: fix recursion in included headers from boardobjgrp_e32.h
    We have this recursion in headers as pointed
    by Coverity
    gk20a/gk20a.h" includes itself: gk20a.h -> clk.h -> clk_vin.h ->
    boardobjgrp_e32.h -> gk20a.h
    
    Fix this by removing gk20a.h header from boardobjgrp_e32.h
    
    Also remove unused header gk20a.h from boardobjgrp_e255.h too
    
    Coverity id : 2513065
    Bug 200291879
    
    Change-Id: I4bee04f064dfb8b02026de9b5a07429b1f48d0a0
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/1479883
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/gpu/nvgpu/boardobj/boardobjgrp_e32.h
+++ drivers/gpu/nvgpu/boardobj/boardobjgrp_e32.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -14,7 +14,6 @@
 #ifndef _BOARDOBJGRP_E32_H_
 #define _BOARDOBJGRP_E32_H_
 
-#include "gk20a/gk20a.h"
 #include "gk20a/pmu_gk20a.h"
 #include "ctrl/ctrlboardobj.h"
 #include "boardobj.h"

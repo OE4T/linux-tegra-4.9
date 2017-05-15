81bbca46969bb7ae499c34ef60076000460e9a57
Krishna Reddy <vdumpa@nvidia.com>
2017-05-15 10:50:40 -0700

    platform: tegra: mc: move t21x specific mcerr code to t21x file
    Bug 1921015
    
    Change-Id: I232f33e3610b87b3af8b7954ed3f049a3d4e8beb
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/1475734
    Reviewed-on: http://git-master/r/1482097
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>

--- drivers/platform/tegra/mc/mcerr-t21.c
+++ drivers/platform/tegra/mc/mcerr-t21.c
@@ -18,6 +18,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#define pr_fmt(fmt) "mc-err: " fmt
+
+#include <linux/of.h>
+#include <linux/moduleparam.h>
 #include <linux/platform/tegra/mcerr.h>
 #include <dt-bindings/memory/tegra-swgroup.h>
 
@@ -202,11 +206,275 @@ static const char *t210_intr_info[] = {
 };
 
 /*
+ * Some platforms report SMMU errors via the SMMU driver.
+ */
+static const char *const smmu_page_attrib[] = {
+	"nr-nw-s",
+	"nr-nw-ns",
+	"nr-wr-s",
+	"nr-wr-ns",
+	"rd-nw-s",
+	"rd-nw-ns",
+	"rd-wr-s",
+	"rd-wr-ns"
+};
+
+/*
+ * Table of known errors and their interrupt signatures.
+ */
+static const struct mc_error mc_errors[] = {
+	MC_ERR(MC_INT_DECERR_EMEM,
+	       "EMEM address decode error",
+	       0, MC_ERR_STATUS, MC_ERR_ADR),
+	MC_ERR(MC_INT_DECERR_VPR,
+	       "MC request violates VPR requirements",
+	       E_VPR, MC_ERR_VPR_STATUS, MC_ERR_VPR_ADR),
+	MC_ERR(MC_INT_SECURITY_VIOLATION,
+	       "non secure access to secure region",
+	       0, MC_ERR_STATUS, MC_ERR_ADR),
+	MC_ERR(MC_INT_DECERR_EMEM | MC_INT_SECURITY_VIOLATION,
+	       "non secure access to secure region",
+	       0, MC_ERR_STATUS, MC_ERR_ADR),
+	MC_ERR(MC_INT_SECERR_SEC,
+	       "MC request violated SEC carveout requirements",
+	       0, MC_ERR_SEC_STATUS, MC_ERR_SEC_ADR),
+
+	/*
+	 * SMMU related faults.
+	 */
+	MC_ERR(MC_INT_INVALID_SMMU_PAGE,
+	       "SMMU address translation fault",
+	       E_SMMU, MC_ERR_STATUS, MC_ERR_ADR),
+	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_DECERR_EMEM,
+	       "EMEM decode error on PDE or PTE entry",
+	       E_SMMU, MC_ERR_STATUS, MC_ERR_ADR),
+	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_SECERR_SEC,
+	       "secure SMMU address translation fault",
+	       E_SMMU, MC_ERR_SEC_STATUS, MC_ERR_SEC_ADR),
+	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_DECERR_VPR,
+	       "VPR SMMU address translation fault",
+	       E_VPR | E_SMMU, MC_ERR_VPR_STATUS, MC_ERR_VPR_ADR),
+	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_DECERR_VPR |
+	       MC_INT_DECERR_EMEM,
+	       "EMEM decode error on PDE or PTE entry on VPR context",
+	       E_VPR | E_SMMU, MC_ERR_VPR_STATUS, MC_ERR_VPR_ADR),
+
+	/*
+	 * MTS access violation.
+	 */
+	MC_ERR(MC_INT_DECERR_MTS,
+	       "MTS carveout access violation",
+	       0, MC_ERR_MTS_STATUS, MC_ERR_MTS_ADR),
+
+	/*
+	 * Generalized carveouts.
+	 */
+	MC_ERR(MC_INT_DECERR_GENERALIZED_CARVEOUT,
+	       "GSC access violation", 0,
+	       MC_ERR_GENERALIZED_CARVEOUT_STATUS,
+	       MC_ERR_GENERALIZED_CARVEOUT_ADR),
+	MC_ERR(MC_INT_DECERR_GENERALIZED_CARVEOUT | MC_INT_DECERR_EMEM,
+	       "EMEM GSC access violation", 0,
+	       MC_ERR_GENERALIZED_CARVEOUT_STATUS,
+	       MC_ERR_GENERALIZED_CARVEOUT_ADR),
+
+	/*
+	 * Miscellaneous errors.
+	 */
+	MC_ERR(MC_INT_INVALID_APB_ASID_UPDATE,
+	       "invalid APB ASID update", 0,
+	       MC_ERR_STATUS, MC_ERR_ADR),
+
+	/* NULL terminate. */
+	MC_ERR(0, NULL, 0, 0, 0),
+};
+
+static int arb_intr_mma_set(const char *arg, const struct kernel_param *kp);
+static int arb_intr_mma_get(char *buff, const struct kernel_param *kp);
+
+static struct arb_emem_intr_info arb_intr_info = {
+	.lock = __SPIN_LOCK_UNLOCKED(arb_intr_info.lock),
+};
+static int arb_intr_count;
+
+static struct kernel_param_ops arb_intr_mma_ops = {
+	.get = arb_intr_mma_get,
+	.set = arb_intr_mma_set,
+};
+
+module_param_cb(arb_intr_mma_in_ms, &arb_intr_mma_ops,
+		&arb_intr_info.arb_intr_mma, S_IRUGO | S_IWUSR);
+module_param(arb_intr_count, int, S_IRUGO | S_IWUSR);
+
+static int arb_intr_mma_set(const char *arg, const struct kernel_param *kp)
+{
+	int ret;
+	unsigned long flags;
+
+	spin_lock_irqsave(&arb_intr_info.lock, flags);
+	ret = param_set_int(arg, kp);
+	spin_unlock_irqrestore(&arb_intr_info.lock, flags);
+	return ret;
+}
+
+static int arb_intr_mma_get(char *buff, const struct kernel_param *kp)
+{
+	return param_get_int(buff, kp);
+}
+
+static const struct mc_error *mcerr_default_info(u32 intr)
+{
+	const struct mc_error *err;
+
+	for (err = mc_errors; err->sig && err->msg; err++) {
+		if (intr != err->sig)
+			continue;
+		return err;
+	}
+
+	return NULL;
+}
+
+void __weak smmu_dump_pagetable(int swgid, dma_addr_t addr)
+{
+}
+
+/*
+ * This will print at least 8 hex digits for address. If the address is bigger
+ * then more digits will be printed but the full 16 hex digits for a 64 bit
+ * address will not get printed by the current code.
+ */
+static void mcerr_default_print(const struct mc_error *err,
+				const struct mc_client *client,
+				u32 status, phys_addr_t addr,
+				int secure, int rw, const char *smmu_info)
+{
+	if (smmu_info)
+		smmu_dump_pagetable(client->swgid, addr);
+
+	if (err->flags & E_VPR)
+		mcerr_pr("vpr base=%x:%x, size=%x, ctrl=%x, override:(%x, %x, %x, %x)\n",
+			 mc_readl(MC_VIDEO_PROTECT_BOM_ADR_HI),
+			 mc_readl(MC_VIDEO_PROTECT_BOM),
+			 mc_readl(MC_VIDEO_PROTECT_SIZE_MB),
+			 mc_readl(MC_VIDEO_PROTECT_REG_CTRL),
+			 mc_readl(MC_VIDEO_PROTECT_VPR_OVERRIDE),
+			 mc_readl(MC_VIDEO_PROTECT_VPR_OVERRIDE1),
+			 mc_readl(MC_VIDEO_PROTECT_GPU_OVERRIDE_0),
+			 mc_readl(MC_VIDEO_PROTECT_GPU_OVERRIDE_1));
+
+	mcerr_pr("(%d) %s: %s\n", client->swgid, client->name, err->msg);
+	mcerr_pr("  status = 0x%08x; addr = 0x%08llx\n", status,
+		 (long long unsigned int)addr);
+	mcerr_pr("  secure: %s, access-type: %s, SMMU fault: %s\n",
+		secure ? "yes" : "no", rw ? "write" : "read",
+		smmu_info ? smmu_info : "none");
+}
+
+static void arb_intr(void)
+{
+	u64 time;
+	u32 time_diff_ms;
+	unsigned long flags;
+
+	spin_lock_irqsave(&arb_intr_info.lock, flags);
+	arb_intr_count++;
+	time = sched_clock();
+	time_diff_ms = (time - arb_intr_info.time) >> 20;
+	arb_intr_info.time = time;
+	arb_intr_info.arb_intr_mma =
+		((MMA_HISTORY_SAMPLES - 1) * time_diff_ms +
+		 arb_intr_info.arb_intr_mma) / MMA_HISTORY_SAMPLES;
+	spin_unlock_irqrestore(&arb_intr_info.lock, flags);
+}
+
+static void clear_interrupt(unsigned int irq)
+{
+	mc_writel(0x00033F40, MC_INTSTATUS);
+}
+
+static void mcerr_info_update(struct mc_client *c, u32 stat)
+{
+	int i;
+
+	for (i = 0; i < (sizeof(stat) * 8); i++) {
+		if (stat & (1 << i))
+			c->intr_counts[i]++;
+	}
+}
+
+static void log_mcerr_fault(unsigned int irq)
+{
+	struct mc_client *client;
+	const struct mc_error *fault;
+	const char *smmu_info;
+	phys_addr_t addr;
+	u32 status, write, secure, client_id;
+	int src_chan = MC_BROADCAST_CHANNEL;
+	u32 intstatus = mc_int_mask &
+			__mc_readl(src_chan, MC_INTSTATUS);
+
+
+	if (intstatus & MC_INT_ARBITRATION_EMEM) {
+		arb_intr();
+		if (intstatus == MC_INT_ARBITRATION_EMEM)
+			return;
+		intstatus &= ~MC_INT_ARBITRATION_EMEM;
+	}
+
+	fault = mcerr_default_info(intstatus & mc_int_mask);
+	if (WARN(!fault, "Unknown error! intr sig: 0x%08x\n",
+		 intstatus & mc_int_mask))
+		return;
+
+	if (fault->flags & E_NO_STATUS) {
+		mcerr_pr("MC fault - no status: %s\n", fault->msg);
+		return;
+	}
+
+	status = __mc_readl(src_chan, fault->stat_reg);
+	addr = __mc_readl(src_chan, fault->addr_reg);
+
+	if (fault->flags & E_TWO_STATUS) {
+		mcerr_pr("MC fault - %s\n", fault->msg);
+		mcerr_pr("status: 0x%08x status2: 0x%08llx\n",
+			status, addr);
+		return;
+	}
+
+	secure = !!(status & MC_ERR_STATUS_SECURE);
+	write = !!(status & MC_ERR_STATUS_WRITE);
+	client_id = status & 0xff;
+	client = &mc_clients[client_id <= mc_client_last
+				     ? client_id : mc_client_last];
+
+	mcerr_info_update(client, intstatus & mc_int_mask);
+
+	/*
+	 * LPAE: make sure we get the extra 2 physical address bits available
+	 * and pass them down to the printing function.
+	 */
+	addr |= (((phys_addr_t)(status & MC_ERR_STATUS_ADR_HI)) << 12);
+
+	if (fault->flags & E_SMMU)
+		smmu_info = smmu_page_attrib[MC_ERR_SMMU_BITS(status)];
+	else
+		smmu_info = NULL;
+
+	mcerr_default_print(fault, client, status, addr, secure, write,
+				  smmu_info);
+}
+
+/*
  * Set up chip specific functions and data for handling this particular chip's
  * error decoding and logging.
  */
 void mcerr_chip_specific_setup(struct mcerr_chip_specific *spec)
 {
+	spec->nr_clients = ARRAY_SIZE(mc_clients);
+	spec->intr_descriptions = t210_intr_info;
+	spec->clear_interrupt = clear_interrupt;
+	spec->log_mcerr_fault = log_mcerr_fault;
 	spec->nr_clients = ARRAY_SIZE(mc_clients);
 	spec->intr_descriptions = t210_intr_info;
 	return;
--- drivers/platform/tegra/mc/mcerr.c
+++ drivers/platform/tegra/mc/mcerr.c
@@ -41,232 +41,22 @@
 
 static bool mcerr_throttle_enabled = true;
 u32  mcerr_silenced;
-
-static int arb_intr_mma_set(const char *arg, const struct kernel_param *kp);
-static int arb_intr_mma_get(char *buff, const struct kernel_param *kp);
-static void unthrottle_prints(struct work_struct *work);
-
-static struct arb_emem_intr_info arb_intr_info = {
-	.lock = __SPIN_LOCK_UNLOCKED(arb_intr_info.lock),
-};
-static int arb_intr_count;
-
-static struct kernel_param_ops arb_intr_mma_ops = {
-	.get = arb_intr_mma_get,
-	.set = arb_intr_mma_set,
-};
-
-module_param_cb(arb_intr_mma_in_ms, &arb_intr_mma_ops,
-		&arb_intr_info.arb_intr_mma, S_IRUGO | S_IWUSR);
-module_param(arb_intr_count, int, S_IRUGO | S_IWUSR);
-
-/*
- * Some platforms report SMMU errors via the SMMU driver.
- */
-static int report_smmu_errs;
-static const char *const smmu_page_attrib[] = {
-	"nr-nw-s",
-	"nr-nw-ns",
-	"nr-wr-s",
-	"nr-wr-ns",
-	"rd-nw-s",
-	"rd-nw-ns",
-	"rd-wr-s",
-	"rd-wr-ns"
-};
-
-/*
- * Table of known errors and their interrupt signatures.
- */
-static const struct mc_error mc_errors[] = {
-	MC_ERR(MC_INT_DECERR_EMEM,
-	       "EMEM address decode error",
-	       0, MC_ERR_STATUS, MC_ERR_ADR),
-	MC_ERR(MC_INT_DECERR_EMEM | MC_INT_SECURITY_VIOLATION,
-	       "EMEM address decode error",
-	       0, MC_ERR_STATUS, MC_ERR_ADR),
-	MC_ERR(MC_INT_DECERR_VPR,
-	       "MC request violates VPR requirements",
-	       E_VPR, MC_ERR_VPR_STATUS, MC_ERR_VPR_ADR),
-	MC_ERR(MC_INT_SECURITY_VIOLATION,
-	       "non secure access to secure region",
-	       0, MC_ERR_STATUS, MC_ERR_ADR),
-	MC_ERR(MC_INT_SECERR_SEC,
-	       "MC request violated SEC carveout requirements",
-	       0, MC_ERR_SEC_STATUS, MC_ERR_SEC_ADR),
-
-	/*
-	 * SMMU related faults.
-	 */
-	MC_ERR(MC_INT_INVALID_SMMU_PAGE,
-	       "SMMU address translation fault",
-	       E_SMMU, MC_ERR_STATUS, MC_ERR_ADR),
-	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_DECERR_EMEM,
-	       "EMEM decode error on PDE or PTE entry",
-	       E_SMMU, MC_ERR_STATUS, MC_ERR_ADR),
-	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_SECERR_SEC,
-	       "secure SMMU address translation fault",
-	       E_SMMU, MC_ERR_SEC_STATUS, MC_ERR_SEC_ADR),
-	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_DECERR_VPR,
-	       "VPR SMMU address translation fault",
-	       E_VPR | E_SMMU, MC_ERR_VPR_STATUS, MC_ERR_VPR_ADR),
-	MC_ERR(MC_INT_INVALID_SMMU_PAGE | MC_INT_DECERR_VPR |
-	       MC_INT_DECERR_EMEM,
-	       "EMEM decode error on PDE or PTE entry on VPR context",
-	       E_VPR | E_SMMU, MC_ERR_VPR_STATUS, MC_ERR_VPR_ADR),
-
-	/*
-	 * MTS access violation.
-	 */
-	MC_ERR(MC_INT_DECERR_MTS,
-	       "MTS carveout access violation",
-	       0, MC_ERR_MTS_STATUS, MC_ERR_MTS_ADR),
-
-	/*
-	 * Generalized carveouts.
-	 */
-	MC_ERR(MC_INT_DECERR_GENERALIZED_CARVEOUT,
-	       "GSC access violation", 0,
-	       MC_ERR_GENERALIZED_CARVEOUT_STATUS,
-	       MC_ERR_GENERALIZED_CARVEOUT_ADR),
-	MC_ERR(MC_INT_DECERR_GENERALIZED_CARVEOUT | MC_INT_DECERR_EMEM,
-	       "EMEM GSC access violation", 0,
-	       MC_ERR_GENERALIZED_CARVEOUT_STATUS,
-	       MC_ERR_GENERALIZED_CARVEOUT_ADR),
-
-	/*
-	 * Miscellaneous errors.
-	 */
-	MC_ERR(MC_INT_INVALID_APB_ASID_UPDATE,
-	       "invalid APB ASID update", 0,
-	       MC_ERR_STATUS, MC_ERR_ADR),
-
-	/* NULL terminate. */
-	MC_ERR(0, NULL, 0, 0, 0),
-};
-
 static atomic_t error_count;
 
+static void unthrottle_prints(struct work_struct *work);
 static DECLARE_DELAYED_WORK(unthrottle_prints_work, unthrottle_prints);
-
 static struct dentry *mcerr_debugfs_dir;
-
 u32 mc_int_mask;
 
 /*
  * Chip specific functions.
  */
 static struct mcerr_chip_specific chip_specific;
-static struct mcerr_chip_specific *cs_ops = &chip_specific;
-
-static int arb_intr_mma_set(const char *arg, const struct kernel_param *kp)
-{
-	int ret;
-	unsigned long flags;
-
-	spin_lock_irqsave(&arb_intr_info.lock, flags);
-	ret = param_set_int(arg, kp);
-	spin_unlock_irqrestore(&arb_intr_info.lock, flags);
-	return ret;
-}
-
-static int arb_intr_mma_get(char *buff, const struct kernel_param *kp)
-{
-	return param_get_int(buff, kp);
-}
-
-static void arb_intr(void)
-{
-	u64 time;
-	u32 time_diff_ms;
-	unsigned long flags;
-
-	spin_lock_irqsave(&arb_intr_info.lock, flags);
-	arb_intr_count++;
-	time = sched_clock();
-	time_diff_ms = (time - arb_intr_info.time) >> 20;
-	arb_intr_info.time = time;
-	arb_intr_info.arb_intr_mma =
-		((MMA_HISTORY_SAMPLES - 1) * time_diff_ms +
-		 arb_intr_info.arb_intr_mma) / MMA_HISTORY_SAMPLES;
-	spin_unlock_irqrestore(&arb_intr_info.lock, flags);
-}
+static struct mcerr_chip_specific *cs_ops;
 
 static void unthrottle_prints(struct work_struct *work)
 {
 	atomic_set(&error_count, 0);
-}
-
-static void mcerr_info_update(struct mc_client *c, u32 stat)
-{
-	int i;
-
-	for (i = 0; i < (sizeof(stat) * 8); i++) {
-		if (stat & (1 << i))
-			c->intr_counts[i]++;
-	}
-}
-
-static void log_mcerr_fault(unsigned int irq)
-{
-	struct mc_client *client;
-	const struct mc_error *fault;
-	const char *smmu_info;
-	phys_addr_t addr;
-	u32 status, write, secure, client_id;
-	int src_chan = MC_BROADCAST_CHANNEL;
-	u32 intstatus = mc_int_mask &
-			__mc_readl(src_chan, MC_INTSTATUS);
-
-
-	if (intstatus & MC_INT_ARBITRATION_EMEM) {
-		arb_intr();
-		if (intstatus == MC_INT_ARBITRATION_EMEM)
-			return;
-		intstatus &= ~MC_INT_ARBITRATION_EMEM;
-	}
-
-	fault = chip_specific.mcerr_info(intstatus & mc_int_mask);
-	if (WARN(!fault, "Unknown error! intr sig: 0x%08x\n",
-		 intstatus & mc_int_mask))
-		return;
-
-	if (fault->flags & E_NO_STATUS) {
-		mcerr_pr("MC fault - no status: %s\n", fault->msg);
-		return;
-	}
-
-	status = __mc_readl(src_chan, fault->stat_reg);
-	addr = __mc_readl(src_chan, fault->addr_reg);
-
-	if (fault->flags & E_TWO_STATUS) {
-		mcerr_pr("MC fault - %s\n", fault->msg);
-		mcerr_pr("status: 0x%08x status2: 0x%08llx\n",
-			status, addr);
-		return;
-	}
-
-	secure = !!(status & MC_ERR_STATUS_SECURE);
-	write = !!(status & MC_ERR_STATUS_WRITE);
-	client_id = status & 0xff;
-	client = &mc_clients[client_id <= mc_client_last
-			     ? client_id : mc_client_last];
-
-	mcerr_info_update(client, intstatus & mc_int_mask);
-
-	/*
-	 * LPAE: make sure we get the extra 2 physical address bits available
-	 * and pass them down to the printing function.
-	 */
-	addr |= (((phys_addr_t)(status & MC_ERR_STATUS_ADR_HI)) << 12);
-
-	if (fault->flags & E_SMMU)
-		smmu_info = smmu_page_attrib[MC_ERR_SMMU_BITS(status)];
-	else
-		smmu_info = NULL;
-
-	chip_specific.mcerr_print(fault, client, status, addr, secure, write,
-				  smmu_info);
 }
 
 static void disable_interrupt(unsigned int irq)
@@ -277,11 +67,6 @@ static void disable_interrupt(unsigned int irq)
 static void enable_interrupt(unsigned int irq)
 {
 	mc_writel(mc_int_mask, MC_INTMASK);
-}
-
-static void clear_interrupt(unsigned int irq)
-{
-	mc_writel(0x00033F40, MC_INTSTATUS);
 }
 
 static irqreturn_t tegra_mcerr_thread(int irq, void *data)
@@ -321,59 +106,6 @@ static irqreturn_t tegra_mcerr_hard_irq(int irq, void *data)
 	  */
 	cs_ops->disable_interrupt(irq);
 	return IRQ_WAKE_THREAD;
-}
-
-static const struct mc_error *mcerr_default_info(u32 intr)
-{
-	const struct mc_error *err;
-
-	for (err = mc_errors; err->sig && err->msg; err++) {
-		if (intr != err->sig)
-			continue;
-		return err;
-	}
-
-	return NULL;
-}
-
-void __weak smmu_dump_pagetable(int swgid, dma_addr_t addr)
-{
-}
-
-/*
- * This will print at least 8 hex digits for address. If the address is bigger
- * then more digits will be printed but the full 16 hex digits for a 64 bit
- * address will not get printed by the current code.
- */
-static void mcerr_default_print(const struct mc_error *err,
-				const struct mc_client *client,
-				u32 status, phys_addr_t addr,
-				int secure, int rw, const char *smmu_info)
-{
-	if (smmu_info)
-		smmu_dump_pagetable(client->swgid, addr);
-
-	if (err->flags & E_VPR)
-		mcerr_pr("vpr base=%x:%x, size=%x, ctrl=%x, override:(%x, %x, %x, %x)\n",
-			 mc_readl(MC_VIDEO_PROTECT_BOM_ADR_HI),
-			 mc_readl(MC_VIDEO_PROTECT_BOM),
-			 mc_readl(MC_VIDEO_PROTECT_SIZE_MB),
-			 mc_readl(MC_VIDEO_PROTECT_REG_CTRL),
-			 mc_readl(MC_VIDEO_PROTECT_VPR_OVERRIDE),
-			 mc_readl(MC_VIDEO_PROTECT_VPR_OVERRIDE1),
-			 mc_readl(MC_VIDEO_PROTECT_GPU_OVERRIDE_0),
-			 mc_readl(MC_VIDEO_PROTECT_GPU_OVERRIDE_1));
-
-	mcerr_pr("(%d) %s: %s\n", client->swgid, client->name, err->msg);
-	mcerr_pr("  status = 0x%08x; addr = 0x%08llx\n", status,
-		 (long long unsigned int)addr);
-	if (report_smmu_errs)
-		mcerr_pr("  secure: %s, access-type: %s, SMMU fault: %s\n",
-			secure ? "yes" : "no", rw ? "write" : "read",
-			smmu_info ? smmu_info : "none");
-	else
-		mcerr_pr("  secure: %s, access-type: %s\n",
-			secure ? "yes" : "no", rw ? "write" : "read");
 }
 
 /*
@@ -450,25 +182,6 @@ DEFINE_SIMPLE_ATTRIBUTE(mcerr_throttle_debugfs_fops, __get_throttle,
 			__set_throttle, "%llu\n");
 
 /*
- * This function will return true for the tegra chip versions
- * which support emc error handling and returns false for chips
- * which does not support emc error handling
- */
-static int tegra_soc_emc_err_handling(struct platform_device *pdev)
-{
-	bool emc_err_handling;
-	const void *prop;
-
-	prop = of_get_property(pdev->dev.of_node, "compatible", NULL);
-	if (prop && strcmp(prop, "nvidia,tegra-mc") == 0)
-		emc_err_handling = false;
-	else
-		emc_err_handling = true;
-
-	return emc_err_handling;
-}
-
-/*
  * This will always be successful. However, if something goes wrong in the
  * init a message will be printed to the kernel log. Since this is a
  * non-essential piece of the kernel no reason to fail the entire MC init
@@ -479,20 +192,9 @@ int tegra_mcerr_init(struct dentry *mc_parent, struct platform_device *pdev)
 	int irq;
 	const void *prop;
 
-	chip_specific.mcerr_info         = mcerr_default_info;
-	chip_specific.mcerr_print        = mcerr_default_print;
-	chip_specific.mcerr_debugfs_show = mcerr_default_debugfs_show;
 	chip_specific.enable_interrupt   = enable_interrupt;
 	chip_specific.disable_interrupt  = disable_interrupt;
-	chip_specific.clear_interrupt    = clear_interrupt;
-	chip_specific.log_mcerr_fault    = log_mcerr_fault;
-	chip_specific.nr_clients = 0;
-
-	prop = of_get_property(pdev->dev.of_node,"compatible", NULL);
-	if (prop && strcmp(prop, "nvidia,tegra-t18x-mc") == 0)
-		report_smmu_errs = 0;
-	else
-		report_smmu_errs = 1;
+	chip_specific.mcerr_debugfs_show = mcerr_default_debugfs_show;
 
 	/*
 	 * mcerr_chip_specific_setup() can override any of the default
@@ -527,8 +229,7 @@ int tegra_mcerr_init(struct dentry *mc_parent, struct platform_device *pdev)
 		goto done;
 	}
 
-	if (tegra_soc_emc_err_handling(pdev))
-		tegra_emcerr_init(mc_parent, pdev);
+	tegra_emcerr_init(mc_parent, pdev);
 
 	if (!mc_parent)
 		goto done;

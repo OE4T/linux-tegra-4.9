5f16b8cda1d5960defbacbbfde4056f30e95edce
Pritesh Raithatha <praithatha@nvidia.com>
2017-05-25 11:12:31 +0530

    i2c: i2c-tegra-hv: fix global-out-of-bounds in tegra_hv_i2c_devtype
    Adding empty bracket at end of array to fix following KASAN defect:
    
    BUG: KASAN: global-out-of-bounds in platform_match+0xbc/0x120
    at addr ffff20000b3305e0
    Address belongs to variable tegra_hv_i2c_devtype+0x20/0x6e0
    ...
    [<ffff2000089f76cc>] driver_register+0xb4/0x198
    [<ffff2000089f93e0>] __platform_driver_register+0x90/0xa0
    [<ffff20000a0ce7c8>] tegra_hv_i2c_init_driver+0x18/0x20
    
    Bug 200312168
    
    Change-Id: I0751a478cf3aed5e41ce2a67d3afd17bbd4a3a4a
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: http://git-master/r/1489429
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/i2c/busses/i2c-tegra-hv.c
+++ drivers/i2c/busses/i2c-tegra-hv.c
@@ -326,7 +326,7 @@ static struct platform_device_id tegra_hv_i2c_devtype[] = {
 		.name = "tegra12-hv-i2c",
 		.driver_data = 0,
 	},
-
+	{}
 };
 
 static struct platform_driver tegra_hv_i2c_driver = {

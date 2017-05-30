646dd95f0afc19493f0b76212bf4c375441b7fb9
Samuel Payne <spayne@nvidia.com>
2017-05-30 14:18:53 -0700

    iio: sensors: fix def to build without suspend
    iio sensors' suspend and resume handlers are not
    used if system suspend is not enabled. This patch
    makes definition of these functions conditional on
    the CONFIG_PM_SLEEP kernel config flag
    
    Change-Id: I1b351f5bf4e8b7eecf6e84f0d484e374f2baf7ec
    Signed-off-by: Samuel Payne <spayne@nvidia.com>
    Reviewed-on: http://git-master/r/1492419
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Akhilesh Khumbum <akhumbum@nvidia.com>
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Robert Collins <rcollins@nvidia.com>

--- drivers/iio/magnetometer/nvi_ak89xx.c
+++ drivers/iio/magnetometer/nvi_ak89xx.c
@@ -1060,6 +1060,7 @@ static struct nvs_fn_dev akm_fn_dev = {
 	.nvs_read			= akm_nvs_read,
 };
 
+#ifdef CONFIG_PM_SLEEP
 static int akm_suspend(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
@@ -1087,7 +1088,7 @@ static int akm_resume(struct device *dev)
 		dev_info(&client->dev, "%s\n", __func__);
 	return ret;
 }
-
+#endif /* CONFIG_PM_SLEEP */
 static SIMPLE_DEV_PM_OPS(akm_pm_ops, akm_suspend, akm_resume);
 
 static void akm_shutdown(struct i2c_client *client)

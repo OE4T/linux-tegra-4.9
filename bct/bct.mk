c60d12e0ddb0147ade6932dedc0d83a000a98e66
Laxman Dewangan <ldewangan@nvidia.com>
2017-06-05 12:52:03 +0530

    platform: t19x: bct: Add GPIO interrupt mapping CFG
    Add sample GPIO interrupt mapping CFG file to route
    all interrupt to INT0 line of GPIO controller.
    
    This file can act as reference and modification done
    in the copied version of this file based on platform
    specific requirements.
    
    Change-Id: I0ed96a4f8cd4465d9fa48d54766d580c7e4f3e4d
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/1495698
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    GVS: Gerrit_Virtual_Submit

---
+++ bct/bct.mk
@@ -0,0 +1,4 @@
+BCT_FILES_PATH := hardware/nvidia/platform/t19x/common/bct
+
+PRODUCT_COPY_FILES += \
+    $(BCT_FILES_PATH)/gpio-intmap/tegra194-mb1-bct-gpio-int-to-all-int0.cfg:tegra194-mb1-bct-gpio-int-to-all-int0.cfg

fe61d24cb10235bd54475dcb0e6a2ea957d1501c
Sri Krishna chowdary <schowdary@nvidia.com>
2017-06-08 18:51:35 +0530

    video: tegra: nvmap: register t18x cache operations
    Register t18x cache operations in the __nvmap_cache_of_table
    so that they can be selected based on compatible property.
    
    Bug 1939440
    
    Change-Id: I3fb16797f00ca31bb9ba3ea5a54af38a824f9d65
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1504798
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_cache_nvmap_t18x.c
+++ drivers/video/tegra/nvmap/nvmap_cache_nvmap_t18x.c
@@ -63,3 +63,13 @@ void nvmap_override_cache_ops(void)
 	inner_flush_cache_all();
 	inner_clean_cache_all();
 }
+
+void nvmap_setup_t18x_cache_ops(struct nvmap_chip_cache_op *op)
+{
+	op->inner_flush_cache_all = nvmap_roc_flush_cache;
+	op->inner_clean_cache_all = nvmap_roc_clean_cache;
+        pr_info("set roc flush ops to replace cache ops by set/ways\n");
+        inner_flush_cache_all();
+        inner_clean_cache_all();
+}
+NVMAP_CACHE_OF_DECLARE("nvidia,carveouts-t18x", nvmap_setup_t18x_cache_ops);

010ccdfd1527aa9b9dc4d3c0368d07919d367bf9
Ujwal Patel <ujwalp@nvidia.com>
2017-06-20 17:25:55 -0700

    nvdisp: t19x: add register offset header-file
    Add hw_nvdisp_t19x_nvdisp.h header-file to capture register
    definitions specific to t19x and remove temporary register offset
    hard-codings.
    
    TDS-2254
    
    Change-Id: Idb131a416c88becf41eb1f368939c28c766f35d6
    Signed-off-by: Ujwal Patel <ujwalp@nvidia.com>
    Reviewed-on: http://git-master/r/1506058
    Reviewed-by: Arun Swain <arswain@nvidia.com>
    Reviewed-by: Shu Zhong <shuz@nvidia.com>

---
+++ drivers/video/tegra/dc/nvdisp/hw_nvdisp_t19x_nvdisp.h
@@ -0,0 +1,201 @@
+/*
+ * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Function naming determines intended use:
+ *
+ *     <x>_r(void) : Returns the offset for register <x>.
+ *
+ *     <x>_o(void) : Returns the offset for element <x>.
+ *
+ *     <x>_w(void) : Returns the word offset for word (4 byte) element <x>.
+ *
+ *     <x>_<y>_s(void) : Returns size of field <y> of register <x> in bits.
+ *
+ *     <x>_<y>_f(u32 v) : Returns a value based on 'v' which has been shifted
+ *         and masked to place it at field <y> of register <x>.  This value
+ *         can be |'d with others to produce a full register value for
+ *         register <x>.
+ *
+ *     <x>_<y>_m(void) : Returns a mask for field <y> of register <x>.  This
+ *         value can be ~'d and then &'d to clear the value of field <y> for
+ *         register <x>.
+ *
+ *     <x>_<y>_<z>_f(void) : Returns the constant value <z> after being shifted
+ *         to place it at field <y> of register <x>.  This value can be |'d
+ *         with others to produce a full register value for <x>.
+ *
+ *     <x>_<y>_v(u32 r) : Returns the value of field <y> from a full register
+ *         <x> value 'r' after being shifted to place its LSB at bit 0.
+ *         This value is suitable for direct comparison with other unshifted
+ *         values appropriate for use in field <y> of register <x>.
+ *
+ *     <x>_<y>_<z>_v(void) : Returns the constant value for <z> defined for
+ *         field <y> of register <x>.  This value is suitable for direct
+ *         comparison with unshifted values appropriate for use in field <y>
+ *         of register <x>.
+ */
+#ifndef _hw_nvdisp_t19x_nvdisp_h_
+#define _hw_nvdisp_t19x_nvdisp_h_
+
+static inline u32 nvdisp_t19x_win_options_r(void)
+{
+	return 0x00000402;
+}
+static inline u32 nvdisp_t19x_win_options_cursor_is_enable_v(u32 r)
+{
+	return (r >> 16) & 0x1;
+}
+static inline u32 nvdisp_t19x_win_options_cursor_set_cursor_enable_f(void)
+{
+	return 0x10000;
+}
+static inline u32 nvdisp_t19x_win_options_sor_is_enable_v(u32 r)
+{
+	return (r >> 25) & 0x1;
+}
+static inline u32 nvdisp_t19x_win_options_sor_set_sor_enable_f(void)
+{
+	return 0x2000000;
+}
+static inline u32 nvdisp_t19x_win_options_sor1_is_enable_v(u32 r)
+{
+	return (r >> 26) & 0x1;
+}
+static inline u32 nvdisp_t19x_win_options_sor1_set_sor1_enable_f(void)
+{
+	return 0x4000000;
+}
+static inline u32 nvdisp_t19x_win_options_sor2_is_enable_v(u32 r)
+{
+	return (r >> 28) & 0x1;
+}
+static inline u32 nvdisp_t19x_win_options_sor2_set_sor2_enable_f(void)
+{
+	return 0x10000000;
+}
+static inline u32 nvdisp_t19x_win_options_sor3_is_enable_v(u32 r)
+{
+	return (r >> 29) & 0x1;
+}
+static inline u32 nvdisp_t19x_win_options_sor3_set_sor3_enable_f(void)
+{
+	return 0x20000000;
+}
+static inline u32 nvdisp_t19x_sor_control_r(void)
+{
+	return 0x00000403;
+}
+static inline u32 nvdisp_t19x_sor_control_protocol_tmdsa_f(void)
+{
+	return 0x100;
+}
+static inline u32 nvdisp_t19x_sor_control_protocol_tmdsb_f(void)
+{
+	return 0x200;
+}
+static inline u32 nvdisp_t19x_sor_control_protocol_dpa_f(void)
+{
+	return 0x600;
+}
+static inline u32 nvdisp_t19x_sor_control_protocol_dpb_f(void)
+{
+	return 0x700;
+}
+static inline u32 nvdisp_t19x_sor_control_protocol_custom_f(void)
+{
+	return 0xf00;
+}
+static inline u32 nvdisp_t19x_sor1_control_r(void)
+{
+	return 0x00000404;
+}
+static inline u32 nvdisp_t19x_sor1_control_protocol_tmdsa_f(void)
+{
+	return 0x100;
+}
+static inline u32 nvdisp_t19x_sor1_control_protocol_tmdsb_f(void)
+{
+	return 0x200;
+}
+static inline u32 nvdisp_t19x_sor1_control_protocol_dpa_f(void)
+{
+	return 0x600;
+}
+static inline u32 nvdisp_t19x_sor1_control_protocol_dpb_f(void)
+{
+	return 0x700;
+}
+static inline u32 nvdisp_t19x_sor1_control_protocol_custom_f(void)
+{
+	return 0xf00;
+}
+static inline u32 nvdisp_t19x_sor2_control_r(void)
+{
+	return 0x00000405;
+}
+static inline u32 nvdisp_t19x_sor2_control_protocol_tmdsa_f(void)
+{
+	return 0x100;
+}
+static inline u32 nvdisp_t19x_sor2_control_protocol_tmdsb_f(void)
+{
+	return 0x200;
+}
+static inline u32 nvdisp_t19x_sor2_control_protocol_dpa_f(void)
+{
+	return 0x600;
+}
+static inline u32 nvdisp_t19x_sor2_control_protocol_dpb_f(void)
+{
+	return 0x700;
+}
+static inline u32 nvdisp_t19x_sor2_control_protocol_custom_f(void)
+{
+	return 0xf00;
+}
+static inline u32 nvdisp_t19x_sor3_control_r(void)
+{
+	return 0x00000406;
+}
+static inline u32 nvdisp_t19x_sor3_control_protocol_tmdsa_f(void)
+{
+	return 0x100;
+}
+static inline u32 nvdisp_t19x_sor3_control_protocol_tmdsb_f(void)
+{
+	return 0x200;
+}
+static inline u32 nvdisp_t19x_sor3_control_protocol_dpa_f(void)
+{
+	return 0x600;
+}
+static inline u32 nvdisp_t19x_sor3_control_protocol_dpb_f(void)
+{
+	return 0x700;
+}
+static inline u32 nvdisp_t19x_sor3_control_protocol_custom_f(void)
+{
+	return 0xf00;
+}
+static inline u32 nvdisp_t19x_rg_vsync_ptimer0_r(void)
+{
+	return 0x0000035c;
+}
+static inline u32 nvdisp_t19x_rg_vsync_ptimer1_r(void)
+{
+	return 0x0000035d;
+}
+#endif
--- drivers/video/tegra/dc/nvdisp/nvdisp_t19x.c
+++ drivers/video/tegra/dc/nvdisp/nvdisp_t19x.c
@@ -16,7 +16,7 @@
 
 #include "dc_priv.h"
 #include "nvdisp.h"
-#include "hw_nvdisp_nvdisp.h"
+#include "hw_nvdisp_t19x_nvdisp.h"
 
 #define TEGRA_WINBUF_ADDR_FLAG_BLOCKLINEAR ((dma_addr_t)0x1 << 39)
 
@@ -112,14 +112,14 @@ int tegra_nvdisp_set_control_t19x(struct tegra_dc *dc)
 
 	if (dc->out->type == TEGRA_DC_OUT_HDMI) {
 
-		/* sor1 in the function name is irrelevant */
-		protocol = nvdisp_sor1_control_protocol_tmdsa_f();
+		/* sor in the function name is irrelevant */
+		protocol = nvdisp_t19x_sor_control_protocol_tmdsa_f();
 	} else if ((dc->out->type == TEGRA_DC_OUT_DP) ||
 		   (dc->out->type == TEGRA_DC_OUT_NVSR_DP) ||
 		   (dc->out->type == TEGRA_DC_OUT_FAKE_DP)) {
 
 		/* sor in the function name is irrelevant */
-		protocol = nvdisp_sor_control_protocol_dpa_f();
+		protocol = nvdisp_t19x_sor_control_protocol_dpa_f();
 	} else {
 		dev_err(&dc->ndev->dev, "%s: unsupported out_type=%d\n",
 				__func__, dc->out->type);
@@ -128,16 +128,16 @@ int tegra_nvdisp_set_control_t19x(struct tegra_dc *dc)
 
 	switch (dc->out_ops->get_connector_instance(dc)) {
 	case 0:
-		reg = nvdisp_sor_control_r();
+		reg = nvdisp_t19x_sor_control_r();
 		break;
 	case 1:
-		reg = nvdisp_sor1_control_r();
+		reg = nvdisp_t19x_sor1_control_r();
 		break;
 	case 2:
-		reg = 0x00000405; /* todo: add t19x hw_reg headerfile */
+		reg = nvdisp_t19x_sor2_control_r();
 		break;
 	case 3:
-		reg = 0x00000406; /* todo: add t19x hw_reg headerfile */
+		reg = nvdisp_t19x_sor3_control_r();
 		break;
 	default:
 		pr_err("%s: invalid sor_num:%d\n", __func__,
@@ -154,21 +154,20 @@ int tegra_nvdisp_set_control_t19x(struct tegra_dc *dc)
 void tegra_dc_enable_sor_t19x(struct tegra_dc *dc, int sor_num, bool enable)
 {
 	u32 enb;
-	u32 reg_val = tegra_dc_readl(dc, nvdisp_win_options_r());
+	u32 reg_val = tegra_dc_readl(dc, nvdisp_t19x_win_options_r());
 
-	/* todo: add t19x hw_reg headerfile */
 	switch (sor_num) {
 	case 0:
-		enb = nvdisp_win_options_sor_set_sor_enable_f();
+		enb = nvdisp_t19x_win_options_sor_set_sor_enable_f();
 		break;
 	case 1:
-		enb = nvdisp_win_options_sor1_set_sor1_enable_f();
+		enb = nvdisp_t19x_win_options_sor1_set_sor1_enable_f();
 		break;
 	case 2:
-		enb = BIT(28); /* todo: add t19x hw_reg headerfile */
+		enb = nvdisp_t19x_win_options_sor2_set_sor2_enable_f();
 		break;
 	case 3:
-		enb = BIT(29); /* todo: add t19x hw_reg headerfile */
+		enb = nvdisp_t19x_win_options_sor3_set_sor3_enable_f();
 		break;
 	default:
 		pr_err("%s: invalid sor_num:%d\n", __func__, sor_num);
@@ -176,7 +175,7 @@ void tegra_dc_enable_sor_t19x(struct tegra_dc *dc, int sor_num, bool enable)
 	}
 
 	reg_val = enable ? reg_val | enb : reg_val & ~enb;
-	tegra_dc_writel(dc, reg_val, nvdisp_win_options_r());
+	tegra_dc_writel(dc, reg_val, nvdisp_t19x_win_options_r());
 }
 
 void tegra_dc_populate_t19x_hw_data(struct tegra_dc_hw_data *hw_data)
@@ -214,19 +213,8 @@ uint64_t tegra_dc_get_vsync_timestamp_t19x(struct tegra_dc *dc)
 	u32 lsb = 0;
 	u64 msb = 0;
 
-#define DC_COM_RG_VSYNC_PTIMER0 0x35c
-#define DC_COM_RG_VSYNC_PTIMER1 0x35d
-
-	/*
-	 * TODO : Add t19x_hw_reg headerfile for the above
-	 * registers.
-	 */
-
-	lsb = tegra_dc_readl(dc, DC_COM_RG_VSYNC_PTIMER0);
-	msb = tegra_dc_readl(dc, DC_COM_RG_VSYNC_PTIMER1);
-
-#undef DC_COM_RG_VSYNC_PTIMER0
-#undef DC_COM_RG_VSYNC_PTIMER1
+	lsb = tegra_dc_readl(dc, nvdisp_t19x_rg_vsync_ptimer0_r());
+	msb = tegra_dc_readl(dc, nvdisp_t19x_rg_vsync_ptimer1_r());
 
 	return (((msb << 32) | lsb) << 5);
 }

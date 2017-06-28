c9b92595b2ac50dbfc2f6e2f9998d469b5ab4fbe
Sunny He <suhe@nvidia.com>
2017-06-28 16:00:52 -0700

    gpu: nvgpu: gv11b: Reorg misc HAL initialization
    Reorganize HAL initialization to remove inheritance and construct
    the gpu_ops struct at compile time. This patch covers the lone
    function pointers of the gpu_ops struct.
    
    Perform HAL function assignments in hal_gxxxx.c through the
    population of a chip-specific copy of gpu_ops.
    
    Jira NVGPU-74
    
    Change-Id: I098559103ef280faca4e82708bb47b9b37057cfd
    Signed-off-by: Sunny He <suhe@nvidia.com>
    Reviewed-on: https://git-master/r/1510390
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gv11b/hal_gv11b.c
+++ drivers/gpu/nvgpu/gv11b/hal_gv11b.c
@@ -52,75 +52,6 @@
 
 #include <nvgpu/hw/gv11b/hw_proj_gv11b.h>
 
-static const struct gpu_ops gv11b_ops = {
-	.ltc = {
-		.determine_L2_size_bytes = gp10b_determine_L2_size_bytes,
-		.set_zbc_s_entry = gv11b_ltc_set_zbc_stencil_entry,
-		.set_zbc_color_entry = gm20b_ltc_set_zbc_color_entry,
-		.set_zbc_depth_entry = gm20b_ltc_set_zbc_depth_entry,
-		.init_cbc = NULL,
-		.init_fs_state = gv11b_ltc_init_fs_state,
-		.init_comptags = gp10b_ltc_init_comptags,
-		.cbc_ctrl = gm20b_ltc_cbc_ctrl,
-		.isr = gv11b_ltc_isr,
-		.cbc_fix_config = gv11b_ltc_cbc_fix_config,
-		.flush = gm20b_flush_ltc,
-#ifdef CONFIG_DEBUG_FS
-		.sync_debugfs = gp10b_ltc_sync_debugfs,
-#endif
-	},
-	.clock_gating = {
-		.slcg_bus_load_gating_prod =
-			gv11b_slcg_bus_load_gating_prod,
-		.slcg_ce2_load_gating_prod =
-			gv11b_slcg_ce2_load_gating_prod,
-		.slcg_chiplet_load_gating_prod =
-			gv11b_slcg_chiplet_load_gating_prod,
-		.slcg_ctxsw_firmware_load_gating_prod =
-			gv11b_slcg_ctxsw_firmware_load_gating_prod,
-		.slcg_fb_load_gating_prod =
-			gv11b_slcg_fb_load_gating_prod,
-		.slcg_fifo_load_gating_prod =
-			gv11b_slcg_fifo_load_gating_prod,
-		.slcg_gr_load_gating_prod =
-			gr_gv11b_slcg_gr_load_gating_prod,
-		.slcg_ltc_load_gating_prod =
-			ltc_gv11b_slcg_ltc_load_gating_prod,
-		.slcg_perf_load_gating_prod =
-			gv11b_slcg_perf_load_gating_prod,
-		.slcg_priring_load_gating_prod =
-			gv11b_slcg_priring_load_gating_prod,
-		.slcg_pmu_load_gating_prod =
-			gv11b_slcg_pmu_load_gating_prod,
-		.slcg_therm_load_gating_prod =
-			gv11b_slcg_therm_load_gating_prod,
-		.slcg_xbar_load_gating_prod =
-			gv11b_slcg_xbar_load_gating_prod,
-		.blcg_bus_load_gating_prod =
-			gv11b_blcg_bus_load_gating_prod,
-		.blcg_ce_load_gating_prod =
-			gv11b_blcg_ce_load_gating_prod,
-		.blcg_ctxsw_firmware_load_gating_prod =
-			gv11b_blcg_ctxsw_firmware_load_gating_prod,
-		.blcg_fb_load_gating_prod =
-			gv11b_blcg_fb_load_gating_prod,
-		.blcg_fifo_load_gating_prod =
-			gv11b_blcg_fifo_load_gating_prod,
-		.blcg_gr_load_gating_prod =
-			gv11b_blcg_gr_load_gating_prod,
-		.blcg_ltc_load_gating_prod =
-			gv11b_blcg_ltc_load_gating_prod,
-		.blcg_pwr_csb_load_gating_prod =
-			gv11b_blcg_pwr_csb_load_gating_prod,
-		.blcg_pmu_load_gating_prod =
-			gv11b_blcg_pmu_load_gating_prod,
-		.blcg_xbar_load_gating_prod =
-			gv11b_blcg_xbar_load_gating_prod,
-		.pg_gr_load_gating_prod =
-			gr_gv11b_pg_gr_load_gating_prod,
-	}
-};
-
 static int gv11b_get_litter_value(struct gk20a *g, int value)
 {
 	int ret = EINVAL;
@@ -202,6 +133,77 @@ static int gv11b_get_litter_value(struct gk20a *g, int value)
 	return ret;
 }
 
+static const struct gpu_ops gv11b_ops = {
+	.ltc = {
+		.determine_L2_size_bytes = gp10b_determine_L2_size_bytes,
+		.set_zbc_s_entry = gv11b_ltc_set_zbc_stencil_entry,
+		.set_zbc_color_entry = gm20b_ltc_set_zbc_color_entry,
+		.set_zbc_depth_entry = gm20b_ltc_set_zbc_depth_entry,
+		.init_cbc = NULL,
+		.init_fs_state = gv11b_ltc_init_fs_state,
+		.init_comptags = gp10b_ltc_init_comptags,
+		.cbc_ctrl = gm20b_ltc_cbc_ctrl,
+		.isr = gv11b_ltc_isr,
+		.cbc_fix_config = gv11b_ltc_cbc_fix_config,
+		.flush = gm20b_flush_ltc,
+#ifdef CONFIG_DEBUG_FS
+		.sync_debugfs = gp10b_ltc_sync_debugfs,
+#endif
+	},
+	.clock_gating = {
+		.slcg_bus_load_gating_prod =
+			gv11b_slcg_bus_load_gating_prod,
+		.slcg_ce2_load_gating_prod =
+			gv11b_slcg_ce2_load_gating_prod,
+		.slcg_chiplet_load_gating_prod =
+			gv11b_slcg_chiplet_load_gating_prod,
+		.slcg_ctxsw_firmware_load_gating_prod =
+			gv11b_slcg_ctxsw_firmware_load_gating_prod,
+		.slcg_fb_load_gating_prod =
+			gv11b_slcg_fb_load_gating_prod,
+		.slcg_fifo_load_gating_prod =
+			gv11b_slcg_fifo_load_gating_prod,
+		.slcg_gr_load_gating_prod =
+			gr_gv11b_slcg_gr_load_gating_prod,
+		.slcg_ltc_load_gating_prod =
+			ltc_gv11b_slcg_ltc_load_gating_prod,
+		.slcg_perf_load_gating_prod =
+			gv11b_slcg_perf_load_gating_prod,
+		.slcg_priring_load_gating_prod =
+			gv11b_slcg_priring_load_gating_prod,
+		.slcg_pmu_load_gating_prod =
+			gv11b_slcg_pmu_load_gating_prod,
+		.slcg_therm_load_gating_prod =
+			gv11b_slcg_therm_load_gating_prod,
+		.slcg_xbar_load_gating_prod =
+			gv11b_slcg_xbar_load_gating_prod,
+		.blcg_bus_load_gating_prod =
+			gv11b_blcg_bus_load_gating_prod,
+		.blcg_ce_load_gating_prod =
+			gv11b_blcg_ce_load_gating_prod,
+		.blcg_ctxsw_firmware_load_gating_prod =
+			gv11b_blcg_ctxsw_firmware_load_gating_prod,
+		.blcg_fb_load_gating_prod =
+			gv11b_blcg_fb_load_gating_prod,
+		.blcg_fifo_load_gating_prod =
+			gv11b_blcg_fifo_load_gating_prod,
+		.blcg_gr_load_gating_prod =
+			gv11b_blcg_gr_load_gating_prod,
+		.blcg_ltc_load_gating_prod =
+			gv11b_blcg_ltc_load_gating_prod,
+		.blcg_pwr_csb_load_gating_prod =
+			gv11b_blcg_pwr_csb_load_gating_prod,
+		.blcg_pmu_load_gating_prod =
+			gv11b_blcg_pmu_load_gating_prod,
+		.blcg_xbar_load_gating_prod =
+			gv11b_blcg_xbar_load_gating_prod,
+		.pg_gr_load_gating_prod =
+			gr_gv11b_pg_gr_load_gating_prod,
+	},
+	.chip_init_gpu_characteristics = gv11b_init_gpu_characteristics,
+	.get_litter_value = gv11b_get_litter_value,
+};
+
 int gv11b_init_hal(struct gk20a *g)
 {
 	struct gpu_ops *gops = &g->ops;
@@ -209,6 +211,11 @@ int gv11b_init_hal(struct gk20a *g)
 
 	gops->ltc = gv11b_ops.ltc;
 	gops->clock_gating = gv11b_ops.clock_gating;
+
+	/* Lone functions */
+	gops->chip_init_gpu_characteristics =
+		gv11b_ops.chip_init_gpu_characteristics;
+	gops->get_litter_value = gv11b_ops.get_litter_value;
 
 	/* boot in non-secure modes for time beeing */
 	gops->privsecurity = 0;
@@ -235,8 +242,6 @@ int gv11b_init_hal(struct gk20a *g)
 	gk20a_init_css_ops(gops);
 #endif
 	g->name = "gv11b";
-	gops->chip_init_gpu_characteristics = gv11b_init_gpu_characteristics;
-	gops->get_litter_value = gv11b_get_litter_value;
 
 	c->twod_class = FERMI_TWOD_A;
 	c->threed_class = VOLTA_A;

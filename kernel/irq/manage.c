2e2845234da44a2b22f70e8c74e7e620606443c5
Thomas Gleixner <tglx@linutronix.de>
2017-06-29 23:33:36 +0200

    genirq: Move bus locking into __setup_irq()
    There is no point in having the irq_bus_lock() protection around all
    callers to __setup_irq().
    
    Move it into __setup_irq(). This is also a preparatory patch for addressing
    the issues with the irq resource callbacks.
    
    Bug 3699159
    
    Change-Id: If1f7382dd751c52cc7a73b9b3187c2a49fc704ba
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Heiko Stuebner <heiko@sntech.de>
    Cc: Julia Cartwright <julia@ni.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Brian Norris <briannorris@chromium.org>
    Cc: Doug Anderson <dianders@chromium.org>
    Cc: linux-rockchip@lists.infradead.org
    Cc: John Keeping <john@metanate.com>
    Cc: linux-gpio@vger.kernel.org
    Link: http://lkml.kernel.org/r/20170629214343.960949031@linutronix.de
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-4.9/+/2747219
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Tested-by: Ken Chang <kenc@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1220,6 +1220,8 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 	if (desc->irq_data.chip->flags & IRQCHIP_ONESHOT_SAFE)
 		new->flags &= ~IRQF_ONESHOT;
 
+	chip_bus_lock(desc);
+
 	/*
 	 * The following block of code has to be executed atomically
 	 */
@@ -1393,6 +1395,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 	}
 
 	raw_spin_unlock_irqrestore(&desc->lock, flags);
+	chip_bus_sync_unlock(desc);
 
 	/*
 	 * Strictly no need to wake it up, but hung_task complains
@@ -1422,6 +1425,8 @@ mismatch:
 
 out_mask:
 	raw_spin_unlock_irqrestore(&desc->lock, flags);
+
+	chip_bus_sync_unlock(desc);
 	free_cpumask_var(mask);
 
 out_thread:
@@ -1463,9 +1468,7 @@ int setup_irq(unsigned int irq, struct irqaction *act)
 	if (retval < 0)
 		return retval;
 
-	chip_bus_lock(desc);
 	retval = __setup_irq(irq, desc, act);
-	chip_bus_sync_unlock(desc);
 
 	if (retval)
 		irq_chip_pm_put(&desc->irq_data);
@@ -1712,9 +1715,7 @@ int request_threaded_irq(unsigned int irq, irq_handler_t handler,
 		return retval;
 	}
 
-	chip_bus_lock(desc);
 	retval = __setup_irq(irq, desc, action);
-	chip_bus_sync_unlock(desc);
 
 	if (retval) {
 		irq_chip_pm_put(&desc->irq_data);
@@ -1962,9 +1963,7 @@ int setup_percpu_irq(unsigned int irq, struct irqaction *act)
 	if (retval < 0)
 		return retval;
 
-	chip_bus_lock(desc);
 	retval = __setup_irq(irq, desc, act);
-	chip_bus_sync_unlock(desc);
 
 	if (retval)
 		irq_chip_pm_put(&desc->irq_data);
@@ -2018,9 +2017,7 @@ int request_percpu_irq(unsigned int irq, irq_handler_t handler,
 		return retval;
 	}
 
-	chip_bus_lock(desc);
 	retval = __setup_irq(irq, desc, action);
-	chip_bus_sync_unlock(desc);
 
 	if (retval) {
 		irq_chip_pm_put(&desc->irq_data);

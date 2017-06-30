69b42146e8460e5188e86e8ce85ee54dd44a4a52
Laxman Dewangan <ldewangan@nvidia.com>
2017-06-30 14:31:08 +0530

    watchdog: tegra: add legacy watchdog driver
    Add legacy (downstream) watchdog timer for Tegra210
    as platform is going to use this driver.
    
    The upstream version of Tegra watchdog timer does not have
    all feature needed in Tegra platform. Once all features are
    ported to upstream version of driver, the legacy driver will
    be deleted.
    
    bug 200319328
    
    Change-Id: Id7510ded6293f1d060b60ba85979fafd576aa7c5
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master/r/1511443
    (cherry picked from commit 70aae6836b30a20d7e65701bf90194f2ad5ed644)
    Reviewed-on: https://git-master.nvidia.com/r/1554621
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- include/linux/suspend.h
+++ include/linux/suspend.h
@@ -405,6 +405,7 @@ static inline bool hibernation_available(void) { return false; }
 #define PM_POST_SUSPEND		0x0004 /* Suspend finished */
 #define PM_RESTORE_PREPARE	0x0005 /* Going to restore a saved image */
 #define PM_POST_RESTORE		0x0006 /* Restore failed */
+#define PM_USERSPACE_FROZEN	0x0007 /* Userspace frozen */
 
 extern struct mutex pm_mutex;
 

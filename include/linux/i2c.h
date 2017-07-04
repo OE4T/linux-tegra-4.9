a49a269f0f308eaa288867ff398b54bb0eb9e752
Shardar Shariff Md <smohammed@nvidia.com>
2017-07-04 18:06:31 +0530

    i2c: slave: modify slave buffer event callback
    - Modify slave buffer event callback to send buffer instead
    of only single byte.
    - Add new slave event enum for buffer read/write states
    - Add buffer size member in client data structure to know
    client max buffer size.
    - Add i2c slave data structure
    
    JIRA TKIP-134
    
    Change-Id: I7834d37471e1af83bf71de5823fd4fc33d077ca2
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: https://git-master/r/1513196
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -47,7 +47,8 @@ struct i2c_driver;
 union i2c_smbus_data;
 struct i2c_board_info;
 enum i2c_slave_event;
-typedef int (*i2c_slave_cb_t)(struct i2c_client *, enum i2c_slave_event, u8 *);
+typedef int (*i2c_slave_cb_t)(struct i2c_client *, enum i2c_slave_event,
+			      void *);
 
 struct module;
 
@@ -241,6 +242,7 @@ struct i2c_client {
 	struct list_head detected;
 #if IS_ENABLED(CONFIG_I2C_SLAVE)
 	i2c_slave_cb_t slave_cb;	/* callback for slave mode	*/
+	u32 buffer_size;		/* slave buffer size */
 #endif
 };
 #define to_i2c_client(d) container_of(d, struct i2c_client, dev)
@@ -272,14 +274,24 @@ enum i2c_slave_event {
 	I2C_SLAVE_WRITE_REQUESTED,
 	I2C_SLAVE_READ_PROCESSED,
 	I2C_SLAVE_WRITE_RECEIVED,
+	I2C_SLAVE_READ_BUFFER_REQUESTED,
+	I2C_SLAVE_WRITE_BUFFER_REQUESTED,
+	I2C_SLAVE_READ_BUFFER_PROCESSED,
+	I2C_SLAVE_WRITE_BUFFER_RECEIVED,
+	I2C_SLAVE_READ_BUFFER_COUNT,
 	I2C_SLAVE_STOP,
+};
+
+struct i2c_slave_data {
+	u8 *buf;
+	u32 size;
 };
 
 extern int i2c_slave_register(struct i2c_client *client, i2c_slave_cb_t slave_cb);
 extern int i2c_slave_unregister(struct i2c_client *client);
 
 static inline int i2c_slave_event(struct i2c_client *client,
-				  enum i2c_slave_event event, u8 *val)
+				  enum i2c_slave_event event, void *val)
 {
 	return client->slave_cb(client, event, val);
 }

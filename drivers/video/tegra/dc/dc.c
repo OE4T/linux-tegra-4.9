962d3186ea8d663f2bedb0e959c9a4881a3abf03
Mitch Luban <mluban@nvidia.com>
2017-07-10 15:44:05 -0700

    tegra: dc: fix memset warning
    Array size needs to be multiplied with the size of
    the array elements
    
    Bug 1929635
    
    Change-Id: Iae235a5531b316342fd512467e296424c39aada8
    Signed-off-by: Mitch Luban <mluban@nvidia.com>
    Reviewed-on: https://git-master/r/1516433
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -6594,7 +6594,7 @@ static int __init parse_disp_params(char *options, struct tegra_dc_mode *mode)
 	int i, params[11];
 	char *p;
 
-	memset(params, 0, ARRAY_SIZE(params));
+	memset(params, 0, ARRAY_SIZE(params) * sizeof(int));
 	for (i = 0; i < ARRAY_SIZE(params); i++) {
 		if ((p = strsep(&options, ",")) != NULL) {
 			if (*p)

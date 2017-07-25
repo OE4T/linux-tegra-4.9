c64adb3217a5283f871480a53b21e49bb4cb02f8
Rakesh Chekuri <rchekuri@nvidia.com>
2017-07-25 16:35:38 +0530

    hid: hid-logitech-dj: Avoid unchecked return
    Fails to check the returned value from the function.
    
    Coverity ID 37841
    
    Change-Id: I81a2765facc3061b8ebc997269c8d446805a0e55
    Signed-off-by: Rakesh Chekuri <rchekuri@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1526113
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- drivers/hid/hid-logitech-dj.c
+++ drivers/hid/hid-logitech-dj.c
@@ -517,8 +517,13 @@ static void logi_dj_recv_queue_notification(struct dj_receiver_dev *djrcv_dev,
 					   struct dj_report *dj_report)
 {
 	/* We are called from atomic context (tasklet && djrcv->lock held) */
+	int err;
 
-	kfifo_in(&djrcv_dev->notif_fifo, dj_report, sizeof(struct dj_report));
+	err = kfifo_in(&djrcv_dev->notif_fifo, dj_report,
+					sizeof(struct dj_report));
+	if (!err)
+		dev_err(&djrcv_dev->hdev->dev,
+			"kfifo_in failed = %d\n", err);
 
 	if (schedule_work(&djrcv_dev->work) == 0) {
 		dbg_hid("%s: did not schedule the work item, was already "

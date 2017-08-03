d945a1438d4649b72a0d8bbbb9cefa56b6a65c53
Rohit Khanna <rokhanna@nvidia.com>
2017-08-03 16:01:02 -0700

    Revert "Revert "iommu: Split iommu_unmaps""
    This reverts commit 934e3c63915af8a27882118422499c1a764dea9b.
    This change caused failure in ap_camera tpg_test. Thus reverting 
    it.
    
    Bug 1968999
    Change-Id: I7db90a56117b6aff736c5e7ba5d72922c3206802
    Reviewed-on: https://git-master.nvidia.com/r/1532587
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1412,9 +1412,9 @@ size_t iommu_unmap(struct iommu_domain *domain, unsigned long iova, size_t size)
 	 * or we hit an area that isn't mapped.
 	 */
 	while (unmapped < size) {
-		size_t left = size - unmapped;
+		size_t pgsize = iommu_pgsize(domain, iova, size - unmapped);
 
-		unmapped_page = domain->ops->unmap(domain, iova, left);
+		unmapped_page = domain->ops->unmap(domain, iova, pgsize);
 		if (!unmapped_page)
 			break;
 

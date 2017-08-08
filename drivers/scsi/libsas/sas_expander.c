f58a3f56101727d054ab794e0184234b890f8ade
Srikar Bakka <sbakka@nvidia.com>
2017-08-08 14:18:17 +0530

    scsi: lib-sas: Checking the return value
    Print error message if return value of sas_ex_phy_discover has an error
    
    CID: 2476464
    
    BUG 200314096
    
    Change-Id: Ib776f8ec7a9f11ed00f86d5876fa82e70da126ef
    Signed-off-by: Srikar Bakka <sbakka@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1534870
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- drivers/scsi/libsas/sas_expander.c
+++ drivers/scsi/libsas/sas_expander.c
@@ -2029,9 +2029,11 @@ static int sas_rediscover_dev(struct domain_device *dev, int phy_id, bool last)
 		   dev_type_flutter(type, phy->attached_dev_type)) {
 		struct domain_device *ata_dev = sas_ex_to_ata(dev, phy_id);
 		char *action = "";
+		int err;
 
-		sas_ex_phy_discover(dev, phy_id);
-
+		err = sas_ex_phy_discover(dev, phy_id);
+		if(!err)
+			SAS_DPRINTK("ERROR,%d\n",err);
 		if (ata_dev && phy->attached_dev_type == SAS_SATA_PENDING)
 			action = ", needs recovery";
 		SAS_DPRINTK("ex %016llx phy 0x%x broadcast flutter%s\n",

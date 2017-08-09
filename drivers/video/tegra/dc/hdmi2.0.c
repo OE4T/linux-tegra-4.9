84f3c5140dc942bf31f91e90c842cbfd13146ca6
Navneet Kumar <navneetk@nvidia.com>
2017-08-09 13:29:34 -0700

    tegra: dc: hdmi: fix hdmi hotplug across suspend
    Hdmi caches 'plug_state' across supend/resume. This can fool the SW state if an
    unplug event happens while in suspended state. With this use case, hdmi doesn't
    come up once the cable is plugged in after resume.
    
    The root cause to this is the cached orig_state/plug_state of hdmi. While this
    cached state serves meaningful purpose it is incomplete by itself across
    suspend/resume.
    
    Make this cached state dependent on cached 'plug_state' AND 'enabled' state.
    
    Bug 200333169
    
    Change-Id: I91bd4b46ba6de44661d2b47b4d6bcfb95203a6f5
    Signed-off-by: Navneet Kumar <navneetk@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1536023
    (cherry picked from commit 9cf8c96a24cdd9502ae22022942d9ba680757f90)
    Reviewed-on: https://git-master.nvidia.com/r/1538365
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/dc/hdmi2.0.c
+++ drivers/video/tegra/dc/hdmi2.0.c
@@ -715,7 +715,7 @@ static void tegra_hdmi_hpd_worker(struct work_struct *work)
 	mutex_lock(&hdmi->hpd_lock);
 
 	connected = tegra_dc_hpd(hdmi->dc);
-	orig_state = hdmi->plug_state;
+	orig_state = hdmi->enabled ? hdmi->plug_state : hdmi->enabled;
 
 	if (connected) {
 		switch (orig_state) {

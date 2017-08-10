c8dfd0c5eec992065535129295bb9eaa7597e9fc
Robert Collins <rcollins@nvidia.com>
2017-08-10 09:05:32 -0700

    trace: sensors: Add ATRACE macros for sensor events
    Adding ATRACE macros will enable tracking motion sensor
    events for end-to-end latency.
    
    AV-388
    
    Change-Id: I774dff325d92b954aaecb660f81899f277566cf2
    Signed-off-by: Robert Collins <rcollins@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1536969
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Akhilesh Khumbum <akhumbum@nvidia.com>

--- include/trace/events/atrace.h
+++ include/trace/events/atrace.h
@@ -3,7 +3,7 @@
  *
  * atrace specific events
  *
- * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2017-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

5248d6b4b5f74a5dca492d7666e80a819734f8da
Sunny He <suhe@nvidia.com>
2017-08-11 14:44:06 -0700

    Revert "gpu: nvgpu: remove kind map inheritance"
    Conflicts with gv100 changes
    
    This reverts commit 83e49b39181a86d32f6350c65659b5892f8f7670.
    
    Change-Id: Ic5720fd3c214fa306645fb628a28ecf7d6d0c26d
    Signed-off-by: Sunny He <suhe@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1537673
    Reviewed-by: Shu Zhong <shuz@nvidia.com>
    Tested-by: Shu Zhong <shuz@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/kind_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/kind_gk20a.c
@@ -23,6 +23,22 @@
 
 /* TBD: generate these from kind_macros.h */
 
+/* TBD: not sure on the work creation for gk20a, doubtful */
+static inline bool gk20a_kind_work_creation_sked(u8 k)
+{
+	return false;
+}
+static inline bool gk20a_kind_work_creation_host(u8 k)
+{
+	return false;
+}
+
+static inline bool gk20a_kind_work_creation(u8 k)
+{
+	return gk20a_kind_work_creation_sked(k) ||
+		gk20a_kind_work_creation_host(k);
+}
+
 /*
  * Use noinline_for_stack for these huge functions, otherwise GCC 4.7 will
  * blow up when building a coverage-enabled kernel. (error: the frame size of
@@ -32,174 +48,177 @@
 /* note: taken from the !2cs_compression case */
 static noinline_for_stack bool gk20a_kind_supported(u8 k)
 {
-	return gk20a_kind_work_creation(k)
-		|| (k == gmmu_pte_kind_invalid_v())
-		|| (k == gmmu_pte_kind_pitch_v())
-		|| (k >= gmmu_pte_kind_z16_v() &&
-			k <= gmmu_pte_kind_z16_ms8_2c_v())
-		|| (k >= gmmu_pte_kind_z16_2z_v() &&
-			k <= gmmu_pte_kind_z16_ms8_2z_v())
-		|| (k == gmmu_pte_kind_s8z24_v())
-		|| (k >= gmmu_pte_kind_s8z24_2cz_v() &&
-			k <= gmmu_pte_kind_s8z24_ms8_2cz_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_2zv_v())
-		|| (k == gmmu_pte_kind_z24s8_v())
-		|| (k >= gmmu_pte_kind_z24s8_2cz_v() &&
-			k <= gmmu_pte_kind_z24s8_ms8_2cz_v())
-		|| (k == gmmu_pte_kind_zf32_v())
-		|| (k >= gmmu_pte_kind_zf32_2cz_v() &&
-			k <= gmmu_pte_kind_zf32_ms8_2cz_v())
-		|| (k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_v() &&
-			k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_v())
-		|| (k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_2cszv_v() &&
-			k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_v())
-		|| (k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_2cszv_v() &&
-			k <= gmmu_pte_kind_zf32_x24s8_v())
-		|| (k >= gmmu_pte_kind_zf32_x24s8_2cszv_v() &&
-			k <= gmmu_pte_kind_zf32_x24s8_ms8_2cszv_v())
-		|| (k == gmmu_pte_kind_generic_16bx2_v())
-		|| (k == gmmu_pte_kind_c32_2c_v())
-		|| (k == gmmu_pte_kind_c32_2cra_v())
-		|| (k == gmmu_pte_kind_c32_ms2_2c_v())
-		|| (k == gmmu_pte_kind_c32_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c32_ms4_2c_v() &&
-			k <= gmmu_pte_kind_c32_ms4_2cbr_v())
-		|| (k >= gmmu_pte_kind_c32_ms4_2cra_v() &&
-			k <= gmmu_pte_kind_c64_2c_v())
-		|| (k == gmmu_pte_kind_c64_2cra_v())
-		|| (k == gmmu_pte_kind_c64_ms2_2c_v())
-		|| (k == gmmu_pte_kind_c64_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c64_ms4_2c_v() &&
-			k <= gmmu_pte_kind_c64_ms4_2cbr_v())
-		|| (k >= gmmu_pte_kind_c64_ms4_2cra_v() &&
-			k <= gmmu_pte_kind_c128_ms8_ms16_2cr_v())
-		|| (k == gmmu_pte_kind_pitch_no_swizzle_v());
-}
+	return gk20a_kind_work_creation(k) ||
+		(k == gmmu_pte_kind_invalid_v()) ||
+		(k == gmmu_pte_kind_pitch_v()) ||
+		(k >= gmmu_pte_kind_z16_v() &&
+		 k <= gmmu_pte_kind_z16_ms8_2c_v()) ||
+		(k >= gmmu_pte_kind_z16_2z_v() &&
+		 k <= gmmu_pte_kind_z16_ms8_2z_v()) ||
+		(k == gmmu_pte_kind_s8z24_v()) ||
+		(k >= gmmu_pte_kind_s8z24_2cz_v() &&
+		 k <= gmmu_pte_kind_s8z24_ms8_2cz_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_2zv_v()) ||
+		(k == gmmu_pte_kind_z24s8_v()) ||
+		(k >= gmmu_pte_kind_z24s8_2cz_v() &&
+		 k <= gmmu_pte_kind_z24s8_ms8_2cz_v()) ||
+		(k == gmmu_pte_kind_zf32_v()) ||
+		(k >= gmmu_pte_kind_zf32_2cz_v() &&
+		 k <= gmmu_pte_kind_zf32_ms8_2cz_v()) ||
+		(k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_v() &&
+		 k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_v()) ||
+		(k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_2cszv_v() &&
+		 k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_v()) ||
+		(k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_2cszv_v() &&
+		 k <= gmmu_pte_kind_zf32_x24s8_v()) ||
+		(k >= gmmu_pte_kind_zf32_x24s8_2cszv_v() &&
+		 k <= gmmu_pte_kind_zf32_x24s8_ms8_2cszv_v()) ||
+		(k == gmmu_pte_kind_generic_16bx2_v()) ||
+		(k == gmmu_pte_kind_c32_2c_v()) ||
+		(k == gmmu_pte_kind_c32_2cra_v()) ||
+		(k == gmmu_pte_kind_c32_ms2_2c_v()) ||
+		(k == gmmu_pte_kind_c32_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c32_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_c32_ms4_2cbr_v()) ||
+		(k >= gmmu_pte_kind_c32_ms4_2cra_v() &&
+		 k <= gmmu_pte_kind_c64_2c_v()) ||
+		(k == gmmu_pte_kind_c64_2cra_v()) ||
+		(k == gmmu_pte_kind_c64_ms2_2c_v()) ||
+		(k == gmmu_pte_kind_c64_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c64_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_c64_ms4_2cbr_v()) ||
+		(k >= gmmu_pte_kind_c64_ms4_2cra_v() &&
+		 k <= gmmu_pte_kind_c128_ms8_ms16_2cr_v()) ||
+		(k == gmmu_pte_kind_pitch_no_swizzle_v());
+		}
 
 static noinline_for_stack bool gk20a_kind_z(u8 k)
 {
 	return (k >= gmmu_pte_kind_z16_v() &&
-		k <= gmmu_pte_kind_v8z24_ms8_vc24_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_2cs_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
-			k <= gmmu_pte_kind_z24v8_ms8_vc24_v())
-		|| (k >= gmmu_pte_kind_z24v8_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_z24v8_ms8_vc24_2cs_v())
-		|| (k >= gmmu_pte_kind_z24v8_ms4_vc12_2czv_v() &&
-			k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_1cs_v())
-		|| (k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_1cs_v())
-		|| (k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_zf32_x24s8_ms16_1cs_v());
+		k <= gmmu_pte_kind_v8z24_ms8_vc24_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_2cs_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
+		 k <= gmmu_pte_kind_z24v8_ms8_vc24_v()) ||
+		(k >= gmmu_pte_kind_z24v8_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_z24v8_ms8_vc24_2cs_v()) ||
+		(k >= gmmu_pte_kind_z24v8_ms4_vc12_2czv_v() &&
+		 k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_1cs_v()) ||
+		(k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_1cs_v()) ||
+		(k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_zf32_x24s8_ms16_1cs_v())
+		/* ||
+		(k >= gmmu_pte_kind_zv32_x24s8_2cszv_v() &&
+		k <= gmmu_pte_kind_xf32_x24s8_ms16_2cs_v())*/;
 }
 
 static noinline_for_stack bool gk20a_kind_c(u8 k)
 {
-	return gk20a_kind_work_creation(k)
-		|| (k == gmmu_pte_kind_pitch_v())
-		|| (k == gmmu_pte_kind_generic_16bx2_v())
-		|| (k >= gmmu_pte_kind_c32_2c_v() &&
-			k <= gmmu_pte_kind_c32_ms2_2cbr_v())
-		|| (k == gmmu_pte_kind_c32_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c32_ms4_2c_v() &&
-			k <= gmmu_pte_kind_c64_ms2_2cbr_v())
-		|| (k == gmmu_pte_kind_c64_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c64_ms4_2c_v() &&
-			k <= gmmu_pte_kind_pitch_no_swizzle_v());
+	return gk20a_kind_work_creation(k) ||
+		(k == gmmu_pte_kind_pitch_v()) ||
+		(k == gmmu_pte_kind_generic_16bx2_v()) ||
+		(k >= gmmu_pte_kind_c32_2c_v() &&
+		 k <= gmmu_pte_kind_c32_ms2_2cbr_v()) ||
+		(k == gmmu_pte_kind_c32_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c32_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_c64_ms2_2cbr_v()) ||
+		(k == gmmu_pte_kind_c64_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c64_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_pitch_no_swizzle_v());
 }
 
 static noinline_for_stack bool gk20a_kind_compressible(u8 k)
 {
 	return (k >= gmmu_pte_kind_z16_2c_v() &&
-			k <= gmmu_pte_kind_z16_ms16_4cz_v())
-		|| (k >= gmmu_pte_kind_s8z24_1z_v() &&
-			k <= gmmu_pte_kind_s8z24_ms16_4cszv_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_2cs_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_4cszv_v())
-		|| (k >= gmmu_pte_kind_z24s8_1z_v() &&
-			k <= gmmu_pte_kind_z24s8_ms16_4cszv_v())
-		|| (k >= gmmu_pte_kind_z24v8_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_z24v8_ms8_vc24_2cs_v())
-		|| (k >= gmmu_pte_kind_z24v8_ms4_vc12_2czv_v() &&
-			k <= gmmu_pte_kind_z24v8_ms8_vc24_4cszv_v())
-		|| (k >= gmmu_pte_kind_zf32_1z_v() &&
-			k <= gmmu_pte_kind_zf32_ms16_2cz_v())
-		|| (k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1cs_v() &&
-			k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_1cs_v())
-		|| (k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_2cszv_v())
-		|| (k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1cs_v() &&
-			k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_1cs_v())
-		|| (k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1zv_v() &&
-			k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_2cszv_v())
-		|| (k >= gmmu_pte_kind_zf32_x24s8_1cs_v() &&
-			k <= gmmu_pte_kind_zf32_x24s8_ms16_1cs_v())
-		|| (k >= gmmu_pte_kind_zf32_x24s8_2cszv_v() &&
-			k <= gmmu_pte_kind_c32_ms2_2cbr_v())
-		|| (k == gmmu_pte_kind_c32_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c32_ms4_2c_v() &&
-			k <= gmmu_pte_kind_c64_ms2_2cbr_v())
-		|| (k == gmmu_pte_kind_c64_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c64_ms4_2c_v() &&
-			k <= gmmu_pte_kind_c128_ms8_ms16_2cr_v());
+		k <= gmmu_pte_kind_z16_ms16_4cz_v()) ||
+		(k >= gmmu_pte_kind_s8z24_1z_v() &&
+		 k <= gmmu_pte_kind_s8z24_ms16_4cszv_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_2cs_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_4cszv_v()) ||
+		(k >= gmmu_pte_kind_z24s8_1z_v() &&
+		 k <= gmmu_pte_kind_z24s8_ms16_4cszv_v()) ||
+		(k >= gmmu_pte_kind_z24v8_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_z24v8_ms8_vc24_2cs_v()) ||
+		(k >= gmmu_pte_kind_z24v8_ms4_vc12_2czv_v() &&
+		 k <= gmmu_pte_kind_z24v8_ms8_vc24_4cszv_v()) ||
+		(k >= gmmu_pte_kind_zf32_1z_v() &&
+		 k <= gmmu_pte_kind_zf32_ms16_2cz_v()) ||
+		(k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1cs_v() &&
+		 k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_1cs_v()) ||
+		(k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_2cszv_v()) ||
+		(k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1cs_v() &&
+		 k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_1cs_v()) ||
+		(k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1zv_v() &&
+		 k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_2cszv_v()) ||
+		(k >= gmmu_pte_kind_zf32_x24s8_1cs_v() &&
+		 k <= gmmu_pte_kind_zf32_x24s8_ms16_1cs_v()) ||
+		(k >= gmmu_pte_kind_zf32_x24s8_2cszv_v() &&
+		 k <= gmmu_pte_kind_c32_ms2_2cbr_v()) ||
+		(k == gmmu_pte_kind_c32_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c32_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_c64_ms2_2cbr_v()) ||
+		(k == gmmu_pte_kind_c64_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c64_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_c128_ms8_ms16_2cr_v());
 }
 
 static noinline_for_stack bool gk20a_kind_zbc(u8 k)
 {
 	return (k >= gmmu_pte_kind_z16_2c_v() &&
-			k <= gmmu_pte_kind_z16_ms16_2c_v())
-		|| (k >= gmmu_pte_kind_z16_4cz_v() &&
-			k <= gmmu_pte_kind_z16_ms16_4cz_v())
-		|| (k >= gmmu_pte_kind_s8z24_2cz_v() &&
-			k <= gmmu_pte_kind_s8z24_ms16_4cszv_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_2cs_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_2cs_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_2czv_v())
-		|| (k >= gmmu_pte_kind_v8z24_ms4_vc12_4cszv_v() &&
-			k <= gmmu_pte_kind_v8z24_ms8_vc24_4cszv_v())
-		|| (k >= gmmu_pte_kind_z24s8_2cs_v() &&
-			k <= gmmu_pte_kind_z24s8_ms16_4cszv_v())
-		|| (k >= gmmu_pte_kind_z24v8_ms4_vc12_2cs_v() &&
-			k <= gmmu_pte_kind_z24v8_ms8_vc24_2cs_v())
-		|| (k >= gmmu_pte_kind_z24v8_ms4_vc12_2czv_v() &&
-			k <= gmmu_pte_kind_z24v8_ms8_vc24_2czv_v())
-		|| (k >= gmmu_pte_kind_z24v8_ms4_vc12_4cszv_v() &&
-			k <= gmmu_pte_kind_z24v8_ms8_vc24_4cszv_v())
-		|| (k >= gmmu_pte_kind_zf32_2cs_v() &&
-			k <= gmmu_pte_kind_zf32_ms16_2cz_v())
-		|| (k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1cs_v() &&
-			k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_1cs_v())
-		|| (k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1czv_v() &&
-			k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_2cszv_v())
-		|| (k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1cs_v() &&
-			k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_1cs_v())
-		|| (k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1czv_v() &&
-			k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_2cszv_v())
-		|| (k >= gmmu_pte_kind_zf32_x24s8_1cs_v() &&
-			k <= gmmu_pte_kind_zf32_x24s8_ms16_1cs_v())
-		|| (k >= gmmu_pte_kind_zf32_x24s8_2cszv_v() &&
-			k <= gmmu_pte_kind_c32_2cra_v())
-		|| (k >= gmmu_pte_kind_c32_ms2_2c_v() &&
-			k <= gmmu_pte_kind_c32_ms2_2cbr_v())
-		|| (k == gmmu_pte_kind_c32_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c32_ms4_2c_v() &&
-			k <= gmmu_pte_kind_c32_ms4_2cra_v())
-		|| (k >= gmmu_pte_kind_c32_ms8_ms16_2c_v() &&
-			k <= gmmu_pte_kind_c64_2cra_v())
-		|| (k >= gmmu_pte_kind_c64_ms2_2c_v() &&
-			k <= gmmu_pte_kind_c64_ms2_2cbr_v())
-		|| (k == gmmu_pte_kind_c64_ms2_2cra_v())
-		|| (k >= gmmu_pte_kind_c64_ms4_2c_v() &&
-			k <= gmmu_pte_kind_c64_ms4_2cra_v())
-		|| (k >= gmmu_pte_kind_c64_ms8_ms16_2c_v() &&
-			k <= gmmu_pte_kind_c128_ms8_ms16_2cr_v());
+		k <= gmmu_pte_kind_z16_ms16_2c_v()) ||
+		(k >= gmmu_pte_kind_z16_4cz_v() &&
+		 k <= gmmu_pte_kind_z16_ms16_4cz_v()) ||
+		(k >= gmmu_pte_kind_s8z24_2cz_v() &&
+		 k <= gmmu_pte_kind_s8z24_ms16_4cszv_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_2cs_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_2cs_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_2czv_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_2czv_v()) ||
+		(k >= gmmu_pte_kind_v8z24_ms4_vc12_4cszv_v() &&
+		 k <= gmmu_pte_kind_v8z24_ms8_vc24_4cszv_v()) ||
+		(k >= gmmu_pte_kind_z24s8_2cs_v() &&
+		 k <= gmmu_pte_kind_z24s8_ms16_4cszv_v()) ||
+		(k >= gmmu_pte_kind_z24v8_ms4_vc12_2cs_v() &&
+		 k <= gmmu_pte_kind_z24v8_ms8_vc24_2cs_v()) ||
+		(k >= gmmu_pte_kind_z24v8_ms4_vc12_2czv_v() &&
+		 k <= gmmu_pte_kind_z24v8_ms8_vc24_2czv_v()) ||
+		(k >= gmmu_pte_kind_z24v8_ms4_vc12_4cszv_v() &&
+		 k <= gmmu_pte_kind_z24v8_ms8_vc24_4cszv_v()) ||
+		(k >= gmmu_pte_kind_zf32_2cs_v() &&
+		 k <= gmmu_pte_kind_zf32_ms16_2cz_v()) ||
+		(k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1cs_v() &&
+		 k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_1cs_v()) ||
+		(k >= gmmu_pte_kind_x8z24_x16v8s8_ms4_vc12_1czv_v() &&
+		 k <= gmmu_pte_kind_x8z24_x16v8s8_ms8_vc24_2cszv_v()) ||
+		(k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1cs_v() &&
+		 k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_1cs_v()) ||
+		(k >= gmmu_pte_kind_zf32_x16v8s8_ms4_vc12_1czv_v() &&
+		 k <= gmmu_pte_kind_zf32_x16v8s8_ms8_vc24_2cszv_v()) ||
+		(k >= gmmu_pte_kind_zf32_x24s8_1cs_v() &&
+		 k <= gmmu_pte_kind_zf32_x24s8_ms16_1cs_v()) ||
+		(k >= gmmu_pte_kind_zf32_x24s8_2cszv_v() &&
+		 k <= gmmu_pte_kind_c32_2cra_v()) ||
+		(k >= gmmu_pte_kind_c32_ms2_2c_v() &&
+		 k <= gmmu_pte_kind_c32_ms2_2cbr_v())  ||
+		(k == gmmu_pte_kind_c32_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c32_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_c32_ms4_2cra_v()) ||
+		(k >= gmmu_pte_kind_c32_ms8_ms16_2c_v() &&
+		 k <= gmmu_pte_kind_c64_2cra_v()) ||
+		(k >= gmmu_pte_kind_c64_ms2_2c_v() &&
+		 k <= gmmu_pte_kind_c64_ms2_2cbr_v()) ||
+		(k == gmmu_pte_kind_c64_ms2_2cra_v()) ||
+		(k >= gmmu_pte_kind_c64_ms4_2c_v() &&
+		 k <= gmmu_pte_kind_c64_ms4_2cra_v()) ||
+		(k >= gmmu_pte_kind_c64_ms8_ms16_2c_v() &&
+		 k <= gmmu_pte_kind_c128_ms8_ms16_2cr_v());
 }
 
 u8 gk20a_uc_kind_map[256];

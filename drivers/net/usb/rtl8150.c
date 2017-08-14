c16f73943e1cccc13dd76c0209963d48940f2d54
Rakesh Chekuri <rchekuri@nvidia.com>
2017-08-14 10:42:39 +0530

    drivers: rtl8150: Avoid logically dead code
    The if statement can never be true. Hence removed it.
    
    Coverity ID 2476602
    
    Change-Id: I4387b97d37185ae2da354e9c3298a5ea419782d7
    Signed-off-by: Rakesh Chekuri <rchekuri@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1538000
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- drivers/net/usb/rtl8150.c
+++ drivers/net/usb/rtl8150.c
@@ -641,8 +641,7 @@ static int enable_net_traffic(rtl8150_t * dev)
 	rcr = 0x9e;
 	tcr = 0xd8;
 	cr = 0x0c;
-	if (!(rcr & 0x80))
-		set_bit(RTL8150_HW_CRC, &dev->flags);
+
 	set_registers(dev, RCR, 1, &rcr);
 	set_registers(dev, TCR, 1, &tcr);
 	set_registers(dev, CR, 1, &cr);

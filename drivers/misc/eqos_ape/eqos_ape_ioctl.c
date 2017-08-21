b2bf6c2d9bf79225179e887dbc47c185ff50fb22
Dipesh Gandhi <dipeshg@nvidia.com>
2017-08-21 11:27:08 +0530

    misc: move eqos_ape to nvidia repo
    Change updates Makefile and Kconfig
    for eqos_ape movement to nvidia repo.
    Also remove redundant include file.
    
    Bug 200325734
    
    Change-Id: I7ff7aa41a029c0d19582d0cd75efc0344c31cfd0
    Signed-off-by: Dipesh Gandhi <dipeshg@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1536289
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>

--- drivers/misc/eqos_ape/eqos_ape_ioctl.c
+++ drivers/misc/eqos_ape/eqos_ape_ioctl.c
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <asm/mach-types.h>
 #include <linux/clk.h>
 #include <linux/module.h>
 #include <linux/err.h>

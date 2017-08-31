e4e6a4a73469cc6f6fc38d4e643afe746976bcb2
Seema Khowala <seemaj@nvidia.com>
2017-08-31 13:31:38 -0700

    gpu: nvgpu: gv11b: remove double declaration
    gv11b_fb_fault_buf_configure_hw is declared twice. Fix
    the same by removing one of the declarations.
    
    Change-Id: I12a857b4225164067f0329530249ffc0dcdc1412
    Signed-off-by: Seema Khowala <seemaj@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1549661
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-by: Seshendra Gadagottu <sgadagottu@nvidia.com>

--- drivers/gpu/nvgpu/gv11b/fb_gv11b.h
+++ drivers/gpu/nvgpu/gv11b/fb_gv11b.h
@@ -58,7 +58,6 @@ void gv11b_fb_enable_hub_intr(struct gk20a *g,
 	 unsigned int index, unsigned int intr_type);
 void gv11b_fb_disable_hub_intr(struct gk20a *g,
 	 unsigned int index, unsigned int intr_type);
-void gv11b_fb_fault_buf_configure_hw(struct gk20a *g, unsigned int index);
 bool gv11b_fb_mmu_fault_pending(struct gk20a *g);
 
 noinline_for_stack void gv11b_init_uncompressed_kind_map(void);

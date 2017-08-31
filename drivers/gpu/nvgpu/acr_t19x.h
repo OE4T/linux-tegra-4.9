56d03664d00f77fa760757467e23d9dbc1a08cf8
Mahantesh Kumbar <mkumbar@nvidia.com>
2017-08-31 15:44:57 +0530

    gpu: nvgpu: GV100 fecs/gpccs sign file path
    - Added paths of GV100 fecs/gpccs sign file.
    
    JIRA NVGPUGV100-7
    
    Change-Id: I92d1fa51c1e66b3eb4009a0a51f6f3252cba41d9
    Signed-off-by: Mahantesh Kumbar <mkumbar@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1549424
    Reviewed-by: Vijayakumar Subbu <vsubbu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>

---
+++ drivers/gpu/nvgpu/acr_t19x.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef _NVGPU_ACR_T19X_H_
+#define _NVGPU_ACR_T19X_H_
+
+#define BIGGPU_FECS_UCODE_SIG "gv100/fecs_sig.bin"
+#define BIGGPU_GPCCS_UCODE_SIG "gv100/gpccs_sig.bin"
+
+#endif

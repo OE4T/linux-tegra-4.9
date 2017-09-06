3acc082b0b9f442c2a060556fd41393c6aa3bb22
Rohith Seelaboyina <rseelaboyina@nvidia.com>
2017-09-06 10:28:03 +0530

    t19x: bct: common: Add UPHY cfg files
    Add UPHY lane config CFG's for all possible combinations
    - SATA only
    - UFS x2
    - UFS x1 (lane 10)
    - UFS x1 (lane 11)
    - UFS x1 (lane 11) + SATA (lane 10)
    
    Bug 200316649
    
    Change-Id: I1ed4c4eafc552e7bc926f64bbc1ff74c83d0513d
    Reviewed-on: https://git-master.nvidia.com/r/1553073
    Reviewed-by: Karthik Gunda <kgunda@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Tested-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- bct/bct.mk
+++ bct/bct.mk
@@ -1,4 +1,9 @@
 BCT_FILES_PATH := hardware/nvidia/platform/t19x/common/bct
 
 PRODUCT_COPY_FILES += \
-    $(BCT_FILES_PATH)/gpio-intmap/tegra194-mb1-bct-gpio-int-to-all-int0.cfg:tegra194-mb1-bct-gpio-int-to-all-int0.cfg
+    $(BCT_FILES_PATH)/gpio-intmap/tegra194-mb1-bct-gpio-int-to-all-int0.cfg:tegra194-mb1-bct-gpio-int-to-all-int0.cfg	\
+    $(BCT_FILES_PATH)/uphy-lane/tegra194-mb1-bct-uphy-lane-ufs-x1-lane10.cfg:tegra194-mb1-bct-uphy-lane-ufs-x1-lane10.cfg    \
+    $(BCT_FILES_PATH)/uphy-lane/tegra194-mb1-bct-uphy-lane-ufs-x1-lane11.cfg:tegra194-mb1-bct-uphy-lane-ufs-x1-lane11.cfg    \
+    $(BCT_FILES_PATH)/uphy-lane/tegra194-mb1-bct-uphy-lane-ufs-x2.cfg:tegra194-mb1-bct-uphy-lane-ufs-x2.cfg  \
+    $(BCT_FILES_PATH)/uphy-lane/tegra194-mb1-bct-uphy-lane-sata.cfg:tegra194-mb1-bct-uphy-lane-sata.cfg      \
+    $(BCT_FILES_PATH)/uphy-lane/tegra194-mb1-bct-uphy-lane-ufs-x1-sata.cfg:tegra194-mb1-bct-uphy-lane-ufs-x1-sata.cfg

9a77a4a2ecb039956ce713ad8fcfedce86c8f488
Manikanta Maddireddy <mmaddireddy@nvidia.com>
2017-09-06 12:49:32 +0530

    pcie: host: tegra: assert AFI, PCI and PCIX resets in suspend
    AFI, PCI and PCIX resets should be asserted before entering SC7.
    
    Bug 200337916
    Bug 200343199
    
    Change-Id: I5fa7a10e8690b86830bf1b3a4f858ff669b34121
    Signed-off-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1553147
    (cherry picked from commit a68ebbe5bbe7b706840beb5e70ae86ebf3029697)
    Reviewed-on: https://git-master.nvidia.com/r/1560575
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/pci/host/pci-tegra.c
+++ drivers/pci/host/pci-tegra.c
@@ -1837,6 +1837,9 @@ static int tegra_pcie_save_device(struct device *dev)
 		if (err < 0)
 			dev_err(dev, "enabling pex-io-dpd failed: %d\n", err);
 	}
+	reset_control_assert(pcie->pciex_rst);
+	reset_control_assert(pcie->pcie_rst);
+	reset_control_assert(pcie->afi_rst);
 
 	err = tegra_pcie_module_power_off(pcie);
 	if (err < 0)

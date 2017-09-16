2ed3cc771632dcec7568f7dec5c966fbb4b4c0a1
Bhadram Varka <vbhadram@nvidia.com>
2017-09-16 09:56:45 +0530

    ethernet: eqos: remove duplicate code
    It removes redundant code for stopping the PHY state machine.
    This code added as part of cherry-picking the code changes to k4.9
    
    Bug 200348218
    
    Change-Id: I51ffa2b65c90d196d2a510041c37fa00ae955fcf
    Signed-off-by: Bhadram Varka <vbhadram@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1561363
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/net/ethernet/nvidia/eqos/drv.c
+++ drivers/net/ethernet/nvidia/eqos/drv.c
@@ -5828,10 +5828,6 @@ void eqos_stop_dev(struct eqos_prv_data *pdata)
 	if (pdata->phydev)
 		phy_stop_machine(pdata->phydev);
 
-	/* Stop the PHY state machine */
-	if (pdata->phydev)
-		phy_stop_machine(pdata->phydev);
-
 	/* turn off sources of data into dev */
 	netif_tx_disable(pdata->dev);
 

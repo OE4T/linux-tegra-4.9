f7676ca495fa1219e65e6593a3311b25ca8a3ef7
Laxman Dewangan <ldewangan@nvidia.com>
2017-10-16 17:38:12 +0530

    bct: stardust: Add GPIO INT map and reset BCT
    Add MB1 BCT for the
    - GPIO interrupt map to int0
    - bootrom reset I2C commands.
    
    Change-Id: I5d4754ac1f0d7fa335b13064da85e9252683f529
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1579705
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Shardar Mohammed <smohammed@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- bct.mk
+++ bct.mk
@@ -1,4 +1,6 @@
 BCT_FILES_PATH := hardware/nvidia/platform/t19x/stardust/bct
 
 PRODUCT_COPY_FILES += \
-    $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg
+    $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg \
+    $(BCT_FILES_PATH)/gpioint/tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg \
+    $(BCT_FILES_PATH)/reset/tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg

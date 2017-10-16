b16ec92fa3ce96597461417de999505e53458f34
Laxman Dewangan <ldewangan@nvidia.com>
2017-10-16 18:24:28 +0530

    bct: stardust: Add pinmux/prod/pad-voltage BCT
    Add MB1 BCT for the
    - Pinmux with V11
    - pad voltage with V11.
    - Empty prod setting as place holder.
    
    Change-Id: I52cfa7c2515220c357b69dcf5e29d3b9b4816bd4
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1579743
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Shardar Mohammed <smohammed@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- bct.mk
+++ bct.mk
@@ -3,4 +3,7 @@ BCT_FILES_PATH := hardware/nvidia/platform/t19x/stardust/bct
 PRODUCT_COPY_FILES += \
     $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg \
     $(BCT_FILES_PATH)/gpioint/tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg \
-    $(BCT_FILES_PATH)/reset/tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg
+    $(BCT_FILES_PATH)/reset/tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg \
+    $(BCT_FILES_PATH)/pinmux/tegra19x-mb1-pinmux-p2888-0000-p2822-0000.cfg:tegra19x-mb1-pinmux-p2888-0000-p2822-0000.cfg \
+    $(BCT_FILES_PATH)/padvoltage/tegra19x-mb1-padvoltage-p2888-0000-p2822-0000.cfg:tegra19x-mb1-padvoltage-p2888-0000-p2822-0000.cfg \
+    $(BCT_FILES_PATH)/prod/tegra19x-mb1-prod-p2888-0000-p2822-0000.cfg:tegra19x-mb1-prod-p2888-0000-p2822-0000.cfg

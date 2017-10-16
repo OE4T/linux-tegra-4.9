99820872dfbc6e244120323507e72256fde9e5ca
Laxman Dewangan <ldewangan@nvidia.com>
2017-10-16 15:00:13 +0530

    bct: stardust: Add PMIC BCT for system configuration
    Add PMIC BCT for system configurations as
    - PMIC FPS to turn SD4 (VDD_DDR_1V1) on in time slot 0
    - PMIC FPS to set GPIO2 (EN_DDR_VDDQ)  high in time slot 1
    - PMIC FPS programming to reassign SD1, SD2, LDO4 and LDO5 to
      FPS0 to leave those rails on in SC7
    
    Bug 2001833
    
    Change-Id: I0008bc364f23d4ed4e028544ac32a90ada5bc3c4
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1579636
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Shardar Mohammed <smohammed@nvidia.com>
    GVS: Gerrit_Virtual_Submit

---
+++ bct.mk
@@ -0,0 +1,4 @@
+BCT_FILES_PATH := hardware/nvidia/platform/t19x/stardust/bct
+
+PRODUCT_COPY_FILES += \
+    $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg

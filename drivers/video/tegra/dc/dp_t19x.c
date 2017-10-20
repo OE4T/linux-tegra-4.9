5c2a17a9302e2099c5c82ffe67a1cae399054933
Jay Bhukhanwala <jbhukhanwala@nvidia.com>
2017-10-19 17:02:08 -0700

    t19x: dp: Eliminate usages of CHECK_RET macro
    The CHECK_RET macro has been removed on account of being inconsistent
    with the Linux Coding Style.
    
    - Remove usages of the CHECK_RET macro
    
    Change-Id: I1b6c79d4690ea111e3ed768796a41568e8e5c097
    Signed-off-by: Jay Bhukhanwala <jbhukhanwala@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1582271
    Reviewed-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Santosh Galma <galmar@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ujwal Patel <ujwalp@nvidia.com>

--- drivers/video/tegra/dc/dp_t19x.c
+++ drivers/video/tegra/dc/dp_t19x.c
@@ -26,11 +26,14 @@ int tegra_dp_init_max_link_cfg_t19x(struct tegra_dc_dp_data *dp,
 	u8 dpcd_data;
 	int ret;
 
-	if (dp->sink_cap_valid)
+	if (dp->sink_cap_valid) {
 		dpcd_data = dp->sink_cap[NV_DPCD_MAX_LANE_COUNT];
-	else
-		CHECK_RET(tegra_dc_dp_dpcd_read(dp,
-			NV_DPCD_MAX_LANE_COUNT, &dpcd_data));
+	} else {
+		ret = tegra_dc_dp_dpcd_read(dp, NV_DPCD_MAX_LANE_COUNT,
+					    &dpcd_data);
+		if (ret)
+			return ret;
+	}
 
 	cfg->max_lane_count = dpcd_data & NV_DPCD_MAX_LANE_COUNT_MASK;
 
@@ -52,15 +55,18 @@ int tegra_dp_init_max_link_cfg_t19x(struct tegra_dc_dp_data *dp,
 	(dpcd_data & NV_DPCD_MAX_LANE_COUNT_ENHANCED_FRAMING_YES) ?
 	true : false;
 
-	if (dp->sink_cap_valid)
+	if (dp->sink_cap_valid) {
 		dpcd_data = dp->sink_cap[NV_DPCD_MAX_DOWNSPREAD];
-	else
-		CHECK_RET(tegra_dc_dp_dpcd_read(dp,
-				NV_DPCD_MAX_DOWNSPREAD, &dpcd_data));
+	} else {
+		ret = tegra_dc_dp_dpcd_read(dp, NV_DPCD_MAX_DOWNSPREAD,
+					    &dpcd_data);
+		if (ret)
+			return ret;
+	}
 
 	/*
-	 * The check for TPS4 should be after the check for TPS3, to prefer
-	 * TPS4 to TPS3
+	 * The check for TPS4 should be after the check for TPS3. That helps
+	 * assign a higher priority to TPS4
 	 */
 	if (dpcd_data & NV_DPCD_MAX_DOWNSPREAD_TPS4_SUPPORTED_YES)
 		cfg->tps = TEGRA_DC_DP_TRAINING_PATTERN_4;
@@ -72,17 +78,21 @@ int tegra_dp_init_max_link_cfg_t19x(struct tegra_dc_dp_data *dp,
 		NV_DPCD_MAX_DOWNSPREAD_NO_AUX_HANDSHAKE_LT_T) ?
 		true : false;
 
-	CHECK_RET(tegra_dc_dp_dpcd_read(dp,
-		NV_DPCD_TRAINING_AUX_RD_INTERVAL, &dpcd_data));
+	ret = tegra_dc_dp_dpcd_read(dp, NV_DPCD_TRAINING_AUX_RD_INTERVAL,
+				    &dpcd_data);
+	if (ret)
+		return ret;
+
 	cfg->aux_rd_interval = dpcd_data;
 
-	if (dp->sink_cap_valid)
-		cfg->max_link_bw =
-			dp->sink_cap[NV_DPCD_MAX_LINK_BANDWIDTH];
-	else
-		CHECK_RET(tegra_dc_dp_dpcd_read(dp,
-			NV_DPCD_MAX_LINK_BANDWIDTH,
-			&cfg->max_link_bw));
+	if (dp->sink_cap_valid) {
+		cfg->max_link_bw = dp->sink_cap[NV_DPCD_MAX_LINK_BANDWIDTH];
+	} else {
+		ret = tegra_dc_dp_dpcd_read(dp, NV_DPCD_MAX_LINK_BANDWIDTH,
+					    &cfg->max_link_bw);
+		if (ret)
+			return ret;
+	}
 
 	tegra_dp_set_max_link_bw(dp->sor, cfg);
 
@@ -90,8 +100,10 @@ int tegra_dp_init_max_link_cfg_t19x(struct tegra_dc_dp_data *dp,
 		dp->pdata->link_bw < cfg->max_link_bw)
 		cfg->max_link_bw = dp->pdata->link_bw;
 
-	CHECK_RET(tegra_dc_dp_dpcd_read(dp, NV_DPCD_EDP_CONFIG_CAP,
-		&dpcd_data));
+	ret = tegra_dc_dp_dpcd_read(dp, NV_DPCD_EDP_CONFIG_CAP, &dpcd_data);
+	if (ret)
+		return ret;
+
 	cfg->alt_scramber_reset_cap =
 		(dpcd_data & NV_DPCD_EDP_CONFIG_CAP_ASC_RESET_YES) ?
 		true : false;
@@ -102,8 +114,10 @@ int tegra_dp_init_max_link_cfg_t19x(struct tegra_dc_dp_data *dp,
 		NV_DPCD_EDP_CONFIG_CAP_DISPLAY_CONTROL_CAP_YES) ?
 		true : false;
 
-	CHECK_RET(tegra_dc_dp_dpcd_read(dp, NV_DPCD_FEATURE_ENUM_LIST,
-		&dpcd_data));
+	ret = tegra_dc_dp_dpcd_read(dp, NV_DPCD_FEATURE_ENUM_LIST, &dpcd_data);
+	if (ret)
+		return ret;
+
 	cfg->support_vsc_ext_colorimetry = (dpcd_data &
 		NV_DPCD_FEATURE_ENUM_LIST_VSC_EXT_COLORIMETRY) ?
 		true : false;

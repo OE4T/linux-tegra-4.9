449645925539e981dab03edc547cc406ea825c01
Darren Sun <darrens@nvidia.com>
2017-10-21 11:41:35 +0800

    bct: stardust: rename stardust to galen
    bct: stardust: rename stardust to galen
    Bug 200356597
    Change-Id: I19da542e0398113a1f3190c25e86427b5e6716a4
    Signed-off-by: Darren Sun <darrens@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1583024
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- bct.mk
+++ bct.mk
@@ -1,4 +1,4 @@
-BCT_FILES_PATH := hardware/nvidia/platform/t19x/stardust/bct
+BCT_FILES_PATH := hardware/nvidia/platform/t19x/galen/bct
 
 PRODUCT_COPY_FILES += \
     $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg \

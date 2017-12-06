a11f66d2fbc3e6440b510ce47140a6741f7756d3
Karthik Gunda <kgunda@nvidia.com>
2017-12-06 14:37:14 +0530

    platform: t19x: common: add device config files
    Added device config files for sdmmc, qspi flash, sata, ufs
    
    Bug 200363571
    
    Change-Id: I370b50c90315e55752180f8f817fd63317d404a7
    Reviewed-on: https://git-master.nvidia.com/r/1612318
    Reviewed-by: Konduri Praveen <kondurip@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Karthik Gunda <kgunda@nvidia.com>
    Tested-by: Karthik Gunda <kgunda@nvidia.com>

--- bct/bct.mk
+++ bct/bct.mk
@@ -1,6 +1,10 @@
 BCT_FILES_PATH := hardware/nvidia/platform/t19x/common/bct
 
 PRODUCT_COPY_FILES += \
+    $(BCT_FILES_PATH)/device/tegra19x-mb1-bct-device-sdmmc.cfg:tegra19x-mb1-bct-device-sdmmc.cfg \
+    $(BCT_FILES_PATH)/device/tegra19x-mb1-bct-device-qspi.cfg:tegra19x-mb1-bct-device-qspi.cfg \
+    $(BCT_FILES_PATH)/device/tegra19x-mb1-bct-device-ufs.cfg:tegra19x-mb1-bct-device-ufs.cfg \
+    $(BCT_FILES_PATH)/device/tegra19x-mb1-bct-device-sata.cfg:tegra19x-mb1-bct-device-sata.cfg \
     $(BCT_FILES_PATH)/gpio-intmap/tegra194-mb1-bct-gpio-int-to-all-int0.cfg:tegra194-mb1-bct-gpio-int-to-all-int0.cfg	\
     $(BCT_FILES_PATH)/uphy-lane/tegra194-mb1-bct-uphy-lane-ufs-x1-lane10.cfg:tegra194-mb1-bct-uphy-lane-ufs-x1-lane10.cfg    \
     $(BCT_FILES_PATH)/uphy-lane/tegra194-mb1-bct-uphy-lane-ufs-x1-lane11.cfg:tegra194-mb1-bct-uphy-lane-ufs-x1-lane11.cfg    \

0dc31ac38c7bc6e8a1e4f931b659e33099b3de16
Vidya Sagar <vidyas@nvidia.com>
2017-12-24 03:26:41 +0530

    PCI: designware: limit MSI addr to 32-bit
    limits MSI address to only 32-bit region to enable
    some of the PCIe end points where only 32-bit MSIs
    are supported. using GFP_DMA instead of GFP_KERNEL
    takes care of this requirement.
    
    Bug 200374807
    
    Change-Id: Id17126fd9900677ede6479e2b6704b8d61089b3e
    Signed-off-by: Vidya Sagar <vidyas@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1625404
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Tested-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/pci/host/pcie-designware.c
+++ drivers/pci/host/pcie-designware.c
@@ -286,7 +286,7 @@ void dw_pcie_msi_init(struct pcie_port *pp)
 {
 	u64 msi_target;
 
-	pp->msi_data = __get_free_pages(GFP_KERNEL, 0);
+	pp->msi_data = __get_free_pages(GFP_DMA, 0);
 	msi_target = virt_to_phys((void *)pp->msi_data);
 
 	/* program the msi_data */

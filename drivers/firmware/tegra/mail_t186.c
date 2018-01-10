b9f24cc0c4cd2d143527a920714f4e6dabc9d2a8
Timo Alho <talho@nvidia.com>
2018-01-10 15:45:33 +0200

    firmware: tegra: use soc/tegra/chip-id.h
    Use "soc/tegra/chip-id.h" in place "linux/tegra-soc.h" as the former
    is available on all current downstream kernel versions.
    
    Bug 200325739
    
    Change-Id: I7f8359c82bd6ed24fe4dc430522dddb701fad844
    Signed-off-by: Timo Alho <talho@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1635656
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/firmware/tegra/mail_t186.c
+++ drivers/firmware/tegra/mail_t186.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -22,8 +22,8 @@
 #include <linux/tegra-hsp.h>
 #include <linux/tegra-ivc-instance.h>
 #include <linux/tegra-ivc.h>
-#include <linux/tegra-soc.h>
 #include <soc/tegra/bpmp_abi.h>
+#include <soc/tegra/chip-id.h>
 #include "bpmp.h"
 
 #define HSP_SHRD_SEM_1_STA	0x1b0000

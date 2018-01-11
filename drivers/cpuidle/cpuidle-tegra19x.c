67d114950f5ec45745ab10fe00187ff52d77cbbf
Krishna Sitaraman <ksitaraman@nvidia.com>
2018-01-11 14:27:11 -0800

    t194: cpuidle: reset actlr_el1 after c-state exit
    Bug 200379019
    
    After exit from c6 or c7 cpu power state, the pm_state field of
    actlr_el1 register should be reset. It shoudl be handled in MTS.
    Until a proper fix is available making the change in cpuidle
    driver.
    
    This change should be reverted after proper fix is available in
    MTS.
    
    Change-Id: Ie30e62f1cf59da78554bbbfcb439b8cb1a865c20
    Signed-off-by: Krishna Sitaraman <ksitaraman@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1636768
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/cpuidle/cpuidle-tegra19x.c
+++ drivers/cpuidle/cpuidle-tegra19x.c
@@ -104,6 +104,7 @@ static int t19x_cpu_enter_state(
 {
 	u32 wake_time;
 	struct timespec t;
+	uint64_t value;
 
 	if (tegra_platform_is_vdk()) {
 		asm volatile("wfi\n");
@@ -132,6 +133,12 @@ static int t19x_cpu_enter_state(
 		t19x_cpu_enter_c6(wake_time);
 	else
 		asm volatile("wfi\n");
+
+	/* Temp fix. Remove after proper fix available on MTS */
+	/* Reset the pmstate field of actlr_el1 after exit from a cpu power state */
+	asm volatile("mrs %0, actlr_el1" : "=r" (value));
+	value = value & 0xfffffffffffffff0;
+	asm volatile("msr actlr_el1, %0\n" :: "r" (value));
 
 	return index;
 }

9a5b7c59b1df1b720243164fca9248afc6b03a17
Brad Love <brad@nextdimension.cc>
2018-01-17 16:32:36 -0500

    BACKPORT: media: em28xx: Enable inversion for Solo/Dual HD DVB models
    Hauppauge Solo/Dual HD DVB models use a si2157 tuner, which is set to
    produce inverted spectrum. This configures the si2168 DVB demod for
    inverted spectrum on both affected models.
    
    upstream commit 309d4c4cc989 ("media: em28xx: Enable inversion for Solo/Dual HD DVB models")
    
    Bug 2476424
    
    Change-Id: I9aee8a5d0d5ab19d3e17d072c96672c24a546a7f
    [mchehab@s-opensource.com: rebased on the top of upstream]
    Signed-off-by: Brad Love <brad@nextdimension.cc>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    [akaginalkar@nvidia.com: solved conflict since board-based helper
     functions don't exist downstream in em28xx-dvb.c]
    Signed-off-by: Abhiroop Kaginalkar <akaginalkar@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2096687
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/media/usb/em28xx/em28xx-dvb.c
+++ drivers/media/usb/em28xx/em28xx-dvb.c
@@ -1726,6 +1726,7 @@ static int em28xx_dvb_init(struct em28xx *dev)
 			si2168_config.i2c_adapter = &adapter;
 			si2168_config.fe = &dvb->fe[0];
 			si2168_config.ts_mode = SI2168_TS_PARALLEL;
+			si2168_config.spectral_inversion = true;
 			memset(&info, 0, sizeof(struct i2c_board_info));
 			strlcpy(info.type, "si2168", I2C_NAME_SIZE);
 			info.addr = 0x64;
@@ -1910,6 +1911,7 @@ static int em28xx_dvb_init(struct em28xx *dev)
 			si2168_config.i2c_adapter = &adapter;
 			si2168_config.fe = &dvb->fe[0];
 			si2168_config.ts_mode = SI2168_TS_SERIAL;
+			si2168_config.spectral_inversion = true;
 			memset(&info, 0, sizeof(struct i2c_board_info));
 			strlcpy(info.type, "si2168", I2C_NAME_SIZE);
 			if (dev->ts == PRIMARY_TS)

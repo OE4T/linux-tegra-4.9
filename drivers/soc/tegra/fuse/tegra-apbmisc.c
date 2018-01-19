d55f6392a9f3f762210231ff144edf2ad44c9550
Chetan Kumar <chetankumarn@nvidia.com>
2018-01-18 16:06:42 -0800

    soc/tegra: fuse: introduce entropy in sim platform
    The kernel boot hangs before init occasionally on simulator
    platforms because of empty entropy pool.
    
    This patch manually adds 1000 bits of entropy during kernel init
    on simulator platforms.
    
    Bug 1943392
    
    Change-Id: I2f9989c2a1941d9e4db182a469ed6c3cf7315ea4
    Signed-off-by: Chetan Kumar <chetankumarn@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1641727
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/soc/tegra/fuse/tegra-apbmisc.c
+++ drivers/soc/tegra/fuse/tegra-apbmisc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -19,6 +19,7 @@
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/io.h>
+#include <linux/hw_random.h>
 
 #include <soc/tegra/fuse.h>
 #include <soc/tegra/common.h>
@@ -186,6 +187,7 @@ void tegra_init_revision(void)
 			break;
 		case 3:
 			revision = TEGRA_REVISION_SIM;
+			add_hwgenerator_randomness(NULL, 0, 1000);
 			break;
 		}
 		goto exit;

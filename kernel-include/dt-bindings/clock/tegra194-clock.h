c4ac567086fe1477c794c083c40d08448d7fcb20
Sivaram Nair <sivaramn@nvidia.com>
2018-01-25 09:44:31 -0800

    bpmp: ABI headers update
    b8428b8 [t194] expose TEGRA194_CLK_PLLE_HPS
    
    Bug 2041352
    
    Change-Id: I56cd2fb8fe2230a8d3c2ef2f5928601500a0245b
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1646135
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Navneet Kumar <navneetk@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- kernel-include/dt-bindings/clock/tegra194-clock.h
+++ kernel-include/dt-bindings/clock/tegra194-clock.h
@@ -681,5 +681,7 @@
 #define TEGRA194_CLK_PEX1_CORE_5M		324
 /** @brief NVHS UPHY PLL0-based NVLINK TX clock output */
 #define TEGRA194_CLK_NVLINK_PLL_TXCLK		325
+/** @brief PLLE hardware power sequencer (overrides 'manual' programming of PLL) */
+#define TEGRA194_CLK_PLLE_HPS			326
 
 #endif

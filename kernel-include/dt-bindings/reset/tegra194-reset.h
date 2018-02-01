e973eaf99e9f981b0335497039fe5065a87b7d39
Sivaram Nair <sivaramn@nvidia.com>
2018-02-01 11:12:04 -0800

    bpmp: ABI headers update
    b76217b [t194][reset] remove DSI IDs
    
    Jira BPMP-1779
    
    Change-Id: I767f01cdb5223734b0062e86259f31fa2a27008f
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1650557
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Navneet Kumar <navneetk@nvidia.com>
    Reviewed-by: Matt Longnecker <mlongnecker@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- kernel-include/dt-bindings/reset/tegra194-reset.h
+++ kernel-include/dt-bindings/reset/tegra194-reset.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2018, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -28,11 +28,6 @@
 #define TEGRA194_RESET_DPAUX1			9
 #define TEGRA194_RESET_DPAUX2			10
 #define TEGRA194_RESET_DPAUX3			11
-#define TEGRA194_RESET_DSI			12
-#define TEGRA194_RESET_DSIB			13
-#define TEGRA194_RESET_DSIC			14
-#define TEGRA194_RESET_DSID			15
-#define TEGRA194_RESET_DSIPADCTL		16
 #define TEGRA194_RESET_EQOS			17
 #define TEGRA194_RESET_GPCDMA			18
 #define TEGRA194_RESET_GPU			19

c2bcd896965a7ce50db4ad3918917c6a023794f2
Lakshmanan M <lm@nvidia.com>
2018-02-15 15:04:56 +0530

    gpu: nvgpu: vgpu: add characteristic flag for syncpoint address support
    Add characteristic flag NVGPU_GPU_FLAGS_SUPPORT_SYNCPOINT_ADDRESS to indicate if
    platform supports semaphore GPU_VA address for a syncpoint
    
    Bug 200327559
    
    Change-Id: I20f532e22c29d1adaff0fbc4204e36cc8455e572
    Signed-off-by: Lakshmanan M <lm@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1657983
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Jitendra Pratap Singh Chauhan <jchauhan@nvidia.com>
    Reviewed-by: Vijayakumar Subbu <vsubbu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/common/linux/vgpu/gv11b/vgpu_gv11b.c
+++ drivers/gpu/nvgpu/common/linux/vgpu/gv11b/vgpu_gv11b.c
@@ -36,6 +36,7 @@ int vgpu_gv11b_init_gpu_characteristics(struct gk20a *g)
 	__nvgpu_set_enabled(g, NVGPU_SUPPORT_TSG_SUBCONTEXTS, true);
 	__nvgpu_set_enabled(g, NVGPU_SUPPORT_IO_COHERENCE, true);
 	__nvgpu_set_enabled(g, NVGPU_SUPPORT_SCG, true);
+	__nvgpu_set_enabled(g, NVGPU_SUPPORT_SYNCPOINT_ADDRESS, true);
 
 	return 0;
 }

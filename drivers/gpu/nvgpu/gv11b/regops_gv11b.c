b6ab47d39683f607de166fef1111b20d8eda097e
Alex Waterman <alexw@nvidia.com>
2018-02-14 16:45:20 -0800

    gpu: nvgpu: Delete more Linux includes
    Delete yet more Linux includes in gv11b code.
    
    Change-Id: I96f2c053cb903ac7cc51ad8cdb12f4a75ac95ae1
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1657742
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/gv11b/regops_gv11b.c
+++ drivers/gpu/nvgpu/gv11b/regops_gv11b.c
@@ -22,10 +22,6 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#include <linux/slab.h>
-#include <linux/err.h>
-#include <linux/bsearch.h>
-
 #include "gk20a/gk20a.h"
 #include "gk20a/dbg_gpu_gk20a.h"
 #include "gk20a/regops_gk20a.h"

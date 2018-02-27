a68650eee2ed7cee2ccde001db223b78d07eb657
Rohit Khanna <rokhanna@nvidia.com>
2018-02-27 11:05:26 -0800

    tegra: denver-knobs: prints mts version on boot
    This patch prints MTS version during denver_knobs_init
    if any of the CPUs have been implemented by NVIDIA.
    
    Bug 2067249
    
    Change-Id: I65a8d043891c6540cf207f3f62dfbabf7ae4a4f6
    Signed-off-by: Rohit Khanna <rokhanna@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1665230
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/platform/tegra/denver-knobs.c
+++ drivers/platform/tegra/denver-knobs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2017, NVIDIA Corporation. All rights reserved.
+ * Copyright (c) 2018, NVIDIA Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -813,6 +813,10 @@ static int __init denver_knobs_init(void)
 
 	/* Cancel the notifier as mts_version should be set now. */
 	unregister_cpu_notifier(&mts_version_cpu_nb);
+
+	/* mts_version will be non-zero, only if its an NVIDIA CPU */
+	if (mts_version)
+		pr_info("%s:MTS_VERSION:%d\n", __func__, mts_version);
 
 	if (mts_version && !proc_create("mts_version", 0, NULL, &mts_version_fops)) {
 		pr_err("Failed to create /proc/mts_version!\n");

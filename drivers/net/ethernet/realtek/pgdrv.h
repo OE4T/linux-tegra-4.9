4a0ca352cbbefb7e80b2a2ee8c5aaad35b3c311e
Vladislav Zhurba <vzhurba@nvidia.com>
2018-03-02 10:55:13 -0800

    drivers: net: realtek: Add missing copyright
    Following files were missing copyright headers:
      - rtltool.h
      - rtltool.c
      - pgdrv.h
    
    Change-Id: Ie0b542004995c53f3e73ac03662ff781a155a5b5
    Signed-off-by: Vladislav Zhurba <vzhurba@nvidia.com>
    Signed-off-by: Hayes Wang <hayeswang@realtek.com>

--- drivers/net/ethernet/realtek/pgdrv.h
+++ drivers/net/ethernet/realtek/pgdrv.h
@@ -1,3 +1,36 @@
+/*
+################################################################################
+#
+# r8168 is the Linux device driver released for Realtek Gigabit Ethernet
+# controllers with PCI-Express interface.
+#
+# Copyright(c) 2014-2018, Realtek Semiconductor Corp. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Author:
+# Realtek NIC software team <nicfae@realtek.com>
+# No. 2, Innovation Road II, Hsinchu Science Park, Hsinchu 300, Taiwan
+#
+################################################################################
+*/
+
+/************************************************************************************
+ *  This product is covered by one or more of the following patents:
+ *  US6,570,884, US6,115,776, and US6,327,625.
+ ***********************************************************************************/
+
 #ifndef _PG_DRV_H
 #define _PG_DRV_H
 

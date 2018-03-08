7253c7cd24c61f8445c0e21746c0fe7f059c64a0
Venkat Reddy Talla <vreddytalla@nvidia.com>
2018-03-08 09:30:59 +0530

    platform: galen: pinmux: add new pinmux cfg for SLVS_EC
    Adding new pinmux cfg file for SLVS_EC, to get SLVS-EC working
    there are some reworks need to be done on Galen, also once the
    rework is done, PCIe will no longer work, therefore creating
    new pinmux cfg for slvs-ec.
    
    Bug 2046341
    
    Change-Id: I2304b831ac30723189f55b34a1d31a3769a579d3
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1670928
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Frank Chen <frankc@nvidia.com>
    Tested-by: Frank Chen <frankc@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- bct.mk
+++ bct.mk
@@ -9,6 +9,7 @@ PRODUCT_COPY_FILES += \
     $(BCT_FILES_PATH)/reset/tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg \
     $(BCT_FILES_PATH)/pinmux/tegra19x-mb1-pinmux-p2888-0000-p2822-0000.cfg:tegra19x-mb1-pinmux-p2888-0000-p2822-0000.cfg \
     $(BCT_FILES_PATH)/pinmux/tegra19x-mb1-pinmux-p2888-0000-a00-p2822-0000-a00.cfg:tegra19x-mb1-pinmux-p2888-0000-a00-p2822-0000-a00.cfg \
+    $(BCT_FILES_PATH)/pinmux/tegra19x-mb1-pinmux-p2888-slvs-0000-a00-p2822-0000-a00.cfg:tegra19x-mb1-pinmux-p2888-slvs-0000-a00-p2822-0000-a00.cfg \
     $(BCT_FILES_PATH)/padvoltage/tegra19x-mb1-padvoltage-p2888-0000-p2822-0000.cfg:tegra19x-mb1-padvoltage-p2888-0000-p2822-0000.cfg \
     $(BCT_FILES_PATH)/padvoltage/tegra19x-mb1-padvoltage-p2888-0000-a00-p2822-0000-a00.cfg:tegra19x-mb1-padvoltage-p2888-0000-a00-p2822-0000-a00.cfg \
     $(BCT_FILES_PATH)/prod/tegra19x-mb1-prod-p2888-0000-p2822-0000.cfg:tegra19x-mb1-prod-p2888-0000-p2822-0000.cfg \

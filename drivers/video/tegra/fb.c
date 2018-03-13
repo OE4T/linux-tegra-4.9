c692a385ba73827622b9e4d631f015c947efe74d
Naveen Kumar S <nkumars@nvidia.com>
2018-03-13 20:39:23 +0530

    video: tegra: fb: clear modedb pointer after free
    Tegra framebuffer driver frees existing modedb before re-populating
    it with the new EDID mode data. kfree() is used to free modedb.
    kfree() adds the memory to free pool, but doesn't set the pointer
    variable to NULL. This causes issues if this pointer is accessed
    before assigning a valid memory again. To avoid such issues, set
    the modedb pointer to NULL after freeing.
    
    bug 200392123
    
    Change-Id: I2ed4e1d6fb00ab7e815ef6993994e4461dd18743
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1674351
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Santosh Galma <galmar@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/fb.c
+++ drivers/video/tegra/fb.c
@@ -826,6 +826,7 @@ void tegra_fb_update_monspecs(struct tegra_fb_info *fb_info,
 	console_lock();
 	b_locked_fb_info = lock_fb_info(fb_info->info);
 	fb_destroy_modedb(fb_info->info->monspecs.modedb);
+	fb_info->info->monspecs.modedb = NULL;
 	fb_destroy_modelist(&fb_info->info->modelist);
 	event.info = fb_info->info;
 	/* Notify layers above fb.c that the hardware is unavailable */

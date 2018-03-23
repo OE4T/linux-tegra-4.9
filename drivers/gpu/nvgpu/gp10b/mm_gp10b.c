a3a00f1d941f0bd180e0191a708a23579b1bede9
Alex Waterman <alexw@nvidia.com>
2018-03-23 13:35:34 -0700

    gpu: nvgpu: Check return code in gp10b_get_pde0_pgsz()
    nvgpu_mem_begin() needs to have it's return code checked to
    make sure that it did not fail.
    
    Coverity defect id: 2883904
    Bug 200291879
    
    Change-Id: Ic845a73439a905997a1f871708b1f847f5b93293
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1681249
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/gp10b/mm_gp10b.c
+++ drivers/gpu/nvgpu/gp10b/mm_gp10b.c
@@ -345,7 +345,9 @@ static enum gmmu_pgsz_gk20a gp10b_get_pde0_pgsz(struct gk20a *g,
 	if (!pd->mem)
 		return pgsz;
 
-	nvgpu_mem_begin(g, pd->mem);
+	if (nvgpu_mem_begin(g, pd->mem))
+		return pgsz;
+
 	for (i = 0; i < GP10B_PDE0_ENTRY_SIZE >> 2; i++)
 		pde_v[i] = nvgpu_mem_rd32(g, pd->mem, pde_offset + i);
 	nvgpu_mem_end(g, pd->mem);

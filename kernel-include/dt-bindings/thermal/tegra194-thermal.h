0b37f419b91712a4a1cede7722def5a6692c3f1a
Sivaram Nair <sivaramn@nvidia.com>
2018-03-27 18:28:14 -0700

    bpmp: ABI headers update
    b021a2b [abi][mach-t194] add tj_max thermal zone
    
    JIRA BPMP-937
    
    Change-Id: I5cbebe66d8e62958d78c5724ba4b0ab61cbc8611
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1683164
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Navneet Kumar <navneetk@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- kernel-include/dt-bindings/thermal/tegra194-thermal.h
+++ kernel-include/dt-bindings/thermal/tegra194-thermal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2017-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -24,7 +24,7 @@
 #define THERMAL_ZONE_AUX	4
 #define THERMAL_ZONE_PLLX	5
 #define THERMAL_ZONE_AO		6
-#define THERMAL_ZONE_RESERVED2	7
+#define THERMAL_ZONE_TJ_MAX	7
 #define THERMAL_ZONE_COUNT	8
 
 #endif

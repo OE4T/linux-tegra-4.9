d693952a0b52127f23093c28a2896a3fd395485b
Debarshi Dutta <ddutta@nvidia.com>
2018-03-28 16:42:11 +0530

    gpu: nvgpu: check null before accessing nvgpu_firmware.
    check for null value of nvgpu_firmware before accessing them in
    nvgpu_firmware_release().
    
    Coverity defect id: 2983427, 2983428 
    Bug 200291879
    
    Change-Id: I946cb448351441ee820aa3e5d8db649943d20d16
    Signed-off-by: Debarshi Dutta <ddutta@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1683505
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vijayakumar Subbu <vsubbu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/common/linux/firmware.c
+++ drivers/gpu/nvgpu/common/linux/firmware.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2017-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -109,6 +109,9 @@ err:
 
 void nvgpu_release_firmware(struct gk20a *g, struct nvgpu_firmware *fw)
 {
+	if(!fw)
+		return;
+
 	nvgpu_kfree(g, fw->data);
 	nvgpu_kfree(g, fw);
 }

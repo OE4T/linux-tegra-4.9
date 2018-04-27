2a14ace99e21296ae3190269d754754310a71dce
Aniruddha Tvs Rao <anrao@nvidia.com>
2018-04-27 03:05:48 -0700

    mmc: host: Avoid unncessary rtpm helper func calls
    Add return on success to avoid unncessary call disabling rtpm on SDMMC.
    
    Bug 200406528
    
    Change-Id: I107cb064b2874057b317f399bba0f7fd902d43bb
    Signed-off-by: Aniruddha Tvs Rao <anrao@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1703937
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: R Raj Kumar <rrajk@nvidia.com>
    Tested-by: Leo He <leoh@nvidia.com>
    Reviewed-by: Rajkumar Kasirajan <rkasirajan@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -1856,6 +1856,7 @@ static void sdhci_delayed_detect(struct work_struct *work)
 
 	/* Initialize debugfs */
 	sdhci_tegra_debugfs_init(host);
+	return;
 
 err_add_host:
 	if (!tegra_host->disable_rtpm)

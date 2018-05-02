81022ddbf62688c4f699da7c50eaf46e21ecf18f
David Gilhooley <dgilhooley@nvidia.com>
2018-05-02 10:41:30 -0700

    iommu: arm-smmu: Move mapping spinlocks to pgtable allocations
    The IOVA allocator protects us from having two devices
    trying to map or unmap from the same location at once.
    We only need to worry about two threads allocating the
    same page table at the same time.
    
    This should provide a large performance boost for
    devices making lots of multithreaded mappings.
    
    Bug 200376222
    
    Change-Id: Iab571939841e6dfe9f20bbdfea974da0d713de92
    Signed-off-by: David Gilhooley <dgilhooley@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1707029
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/iommu/arm-smmu-t19x.c
+++ drivers/iommu/arm-smmu-t19x.c
@@ -2190,8 +2190,11 @@ static void *arm_smmu_alloc_pgtable_page(struct arm_smmu_domain *domain, int lvl
 {
 	struct arm_smmu_device *smmu = domain->smmu;
 	void * ret = NULL;
+	unsigned long flags;
 
 	pr_debug("Alloc for address %lx level: %d\n", addr, lvl);
+
+	spin_lock_irqsave(&domain->lock, flags);
 
 	if (lvl == 0) {
 #ifndef __PAGETABLE_PUD_FOLDED
@@ -2262,6 +2265,7 @@ static void *arm_smmu_alloc_pgtable_page(struct arm_smmu_domain *domain, int lvl
 	}
 
 unlock_ret:
+	spin_unlock_irqrestore(&domain->lock, flags);
 	return ret;
 }
 
@@ -2436,7 +2440,6 @@ static int arm_smmu_handle_mapping(struct arm_smmu_domain *smmu_domain,
 	struct arm_smmu_device *smmu = smmu_domain->smmu;
 	struct arm_smmu_cfg *cfg = &smmu_domain->cfg;
 	pgd_t *pgd = cfg->pgd;
-	unsigned long flags;
 
 	u64 time_before = 0;
 
@@ -2477,7 +2480,6 @@ static int arm_smmu_handle_mapping(struct arm_smmu_domain *smmu_domain,
 		time_before = local_clock();
 #endif
 
-	spin_lock_irqsave(&smmu_domain->lock, flags);
 	pgd += pgd_index(iova);
 	end = iova + size;
 	do {
@@ -2501,7 +2503,6 @@ out_unlock:
 		else
 			arm_smmu_tlb_inv_context(smmu_domain);
 	}
-	spin_unlock_irqrestore(&smmu_domain->lock, flags);
 
 	if (time_before)
 		trace_arm_smmu_handle_mapping(time_before, cfg->cbndx,

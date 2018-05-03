dafd249f5659be6926815a1a526fb20bbf8ce8d7
Laxman Dewangan <ldewangan@nvidia.com>
2018-05-03 14:25:07 +0530

    platform: galen: Remove unused PMIC BCT
    Remove unused PMIC BCT.
    
    Change-Id: I422a848596da8a0618b76c71964200c0f5935d45
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1708168
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Tested-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- bct.mk
+++ bct.mk
@@ -1,8 +1,6 @@
 BCT_FILES_PATH := hardware/nvidia/platform/t19x/galen/bct
 
 PRODUCT_COPY_FILES += \
-    $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0000-p2822-0000.cfg \
-    $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0001-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0001-p2822-0000.cfg \
     $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0001-a01-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0001-a01-p2822-0000.cfg \
     $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0001-a02-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0001-a02-p2822-0000.cfg \
     $(BCT_FILES_PATH)/gpioint/tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg \

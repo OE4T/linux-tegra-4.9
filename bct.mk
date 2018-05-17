944083a86289d7327d0c7d792bb47690a2888f1c
Darren Sun <darrens@nvidia.com>
2018-05-17 12:46:39 +0800

    platform: galen: bct: Configure PWM Fan to stop in mb1
    On new Galen combination P2822 A03 and P2822 B00, the
    polarity of PWM fan is normal. So we don't need to config
    the fan PWM control. 0% duty means shutdown the fan.
    
    Bug 2084734
    
    Change-Id: I24bcb52f4191e2b5c4fd543206a32bfe1ca26d39
    Signed-off-by: Darren Sun <darrens@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1721416
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- bct.mk
+++ bct.mk
@@ -3,6 +3,7 @@ BCT_FILES_PATH := hardware/nvidia/platform/t19x/galen/bct
 PRODUCT_COPY_FILES += \
     $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0001-a01-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0001-a01-p2822-0000.cfg \
     $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0001-a02-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0001-a02-p2822-0000.cfg \
+    $(BCT_FILES_PATH)/pmic/tegra194-mb1-bct-pmic-p2888-0001-a03-p2822-0000.cfg:tegra194-mb1-bct-pmic-p2888-0001-a03-p2822-0000.cfg \
     $(BCT_FILES_PATH)/gpioint/tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg \
     $(BCT_FILES_PATH)/reset/tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg:tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg \
     $(BCT_FILES_PATH)/pinmux/tegra19x-mb1-pinmux-p2888-0000-p2822-0000.cfg:tegra19x-mb1-pinmux-p2888-0000-p2822-0000.cfg \

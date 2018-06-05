3ada968bb16f811c601d185b22ee4107d04f7761
Vignesh Radhakrishnan <vigneshr@nvidia.com>
2018-06-04 17:17:02 -0700

    block: do not allow updates through sysfs until registration completes
    When a new disk shows up, sysfs queue directory is created before elevator
    is registered. This allows a user to attempt a scheduler switch even though
    the initial registration hasn't completed yet.
    
    In one scenario, blk_register_queue() calls elv_register_queue() and
    right before cfq_registered_queue() is called, another process executes
    elevator_switch() and replaces q->elevator with deadline scheduler. When
    cfq_registered_queue() executes it interprets e->elevator_data as struct
    cfq_data even though it is actually struct deadline_data.
    
    Grab q->sysfs_lock in blk_register_queue() to synchronize with sysfs
    callers.
    
    Bug 2090405
    
    Signed-off-by: Tahsin Erdogan <tahsin@google.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry-picked from commit b410aff2bd9f101bc096069612391aecce204cd1 in dev-kernel-4.14)
    (vigneshr, resolved conflicts in blk-sysfs.c)
    Change-Id: Idfecc66b370b4db129056c415773a5b003872496
    Signed-off-by: Vignesh Radhakrishnan <vigneshr@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1740885
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -695,16 +695,18 @@ int blk_register_queue(struct gendisk *disk)
 	if (ret)
 		return ret;
 
+	if (q->mq_ops)
+		blk_mq_register_dev(dev, q);
+
+	mutex_lock(&q->sysfs_lock);
+
 	ret = kobject_add(&q->kobj, kobject_get(&dev->kobj), "%s", "queue");
 	if (ret < 0) {
 		blk_trace_remove_sysfs(dev);
-		return ret;
+		goto unlock;
 	}
 
 	kobject_uevent(&q->kobj, KOBJ_ADD);
-
-	if (q->mq_ops)
-		blk_mq_register_dev(dev, q);
 
 	if (!q->request_fn)
 		return 0;
@@ -715,10 +717,13 @@ int blk_register_queue(struct gendisk *disk)
 		kobject_del(&q->kobj);
 		blk_trace_remove_sysfs(dev);
 		kobject_put(&dev->kobj);
-		return ret;
+		goto unlock;
 	}
 
-	return 0;
+	ret = 0;
+unlock:
+	mutex_unlock(&q->sysfs_lock);
+	return ret;
 }
 
 void blk_unregister_queue(struct gendisk *disk)

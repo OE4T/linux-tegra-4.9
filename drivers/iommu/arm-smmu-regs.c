10688142612c5eaea64e5f41b100b9c0f453027f
David Gilhooley <dgilhooley@nvidia.com>
2018-06-19 10:58:06 -0700

    iommu: arm-smmu-regs: Add more downstream functionality
    Add the following functionality:
     - Use the downstream folder hierachy so scripts will work
     - Call the function from the smmu driver so we don't hardcode values
     - Add ability to add masters with context banks and stream ids
    
    Bug 2196211
    
    Change-Id: I13216bdaa52d1dffa30903d4bdf32fb0bb480ee3
    Signed-off-by: David Gilhooley <dgilhooley@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1764548
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/iommu/arm-smmu-regs.c
+++ drivers/iommu/arm-smmu-regs.c
@@ -14,110 +14,14 @@
 #include <linux/slab.h>
 #include <linux/of.h>
 #include <linux/io.h>
+#include <linux/device.h>
+#include <linux/iommu.h>
 
-struct arm_smmu_device {
-	void __iomem *base;
-	int size;
+#include "arm-smmu-regs.h"
+#include "arm-smmu-debug.h"
 
-	u32 num_context_banks;
-	unsigned long pgshift;
-
-	struct debugfs_regset32 *regset;
-	struct debugfs_regset32 *perf_regset;
-	struct dentry *debugfs_root;
-	struct dentry *cb_root;
-	struct dentry *masters_root;
-};
-
-static struct arm_smmu_device *smmu_handle;
-
-/* Identification registers */
-#define ARM_SMMU_GR0_sCR0		0x0
-#define ARM_SMMU_GR0_ID0		0x20
-#define ARM_SMMU_GR0_ID1		0x24
-#define ARM_SMMU_GR0_ID2		0x28
-#define ARM_SMMU_GR0_ID3		0x2c
-#define ARM_SMMU_GR0_ID4		0x30
-#define ARM_SMMU_GR0_ID5		0x34
-#define ARM_SMMU_GR0_ID6		0x38
-#define ARM_SMMU_GR0_ID7		0x3c
-#define ARM_SMMU_GR0_sGFSR		0x48
-#define ARM_SMMU_GR0_sGFSYNR0		0x50
-#define ARM_SMMU_GR0_sGFSYNR1		0x54
-#define ARM_SMMU_GR0_sGFSYNR2		0x58
-#define ARM_SMMU_GR0_nsCR0		0x400
-#define ARM_SMMU_GR0_nsGFSR		0x448
-#define ARM_SMMU_GR0_nsGFSYNR0		0x450
-#define ARM_SMMU_GR0_nsGFSYNR1		0x454
-#define ARM_SMMU_GR0_nsGFSYNR2		0x458
-#define ARM_SMMU_GR0_PIDR0		0xfe0
-#define ARM_SMMU_GR0_PIDR1		0xfe4
-#define ARM_SMMU_GR0_PIDR2		0xfe8
-
-#define ARM_SMMU_GR0_S2CR(n)		(0xc00 + ((n) << 2))
-
-/* Stream mapping registers */
-#define ARM_SMMU_GR0_SMR(n)		(0x800 + ((n) << 2))
-
-/* Context bank attribute registers */
-#define ARM_SMMU_GR1_CBAR(n)		(0x0 + ((n) << 2))
-#define ARM_SMMU_GR1_CBA2R(n)		(0x800 + ((n) << 2))
-
-/* Global TLB invalidation */
-#define ARM_SMMU_GR0_STLBIALL		0x60
-#define ARM_SMMU_GR0_TLBIVMID		0x64
-#define ARM_SMMU_GR0_TLBIALLNSNH	0x68
-#define ARM_SMMU_GR0_TLBIALLH		0x6c
-#define ARM_SMMU_GR0_sTLBGSYNC		0x70
-#define ARM_SMMU_GR0_sTLBGSTATUS	0x74
-#define ARM_SMMU_GR0_nsTLBGSYNC		0x470
-#define ARM_SMMU_GR0_nsTLBGSTATUS	0x474
-
-/* Perf Monitor registers */
-#define ARM_SMMU_GNSR0_PMCNTENSET_0	0xc00
-#define ARM_SMMU_GNSR0_PMCNTENCLR_0	0xc20
-#define ARM_SMMU_GNSR0_PMINTENSET_0	0xc40
-#define ARM_SMMU_GNSR0_PMINTENCLR_0	0xc60
-#define ARM_SMMU_GNSR0_PMOVSCLR_0	0xc80
-#define ARM_SMMU_GNSR0_PMOVSSET_0	0xcc0
-#define ARM_SMMU_GNSR0_PMCFGR_0		0xe00
-#define ARM_SMMU_GNSR0_PMCR_0		0xe04
-#define ARM_SMMU_GNSR0_PMCEID0_0	0xe20
-#define ARM_SMMU_GNSR0_PMAUTHSTATUS_0	0xfb8
-#define ARM_SMMU_GNSR0_PMDEVTYPE_0	0xfcc
-
-#define ARM_SMMU_GNSR0_PMEVTYPER(n)	(0x400 + ((n) << 2))
-#define ARM_SMMU_GNSR0_PMEVCNTR(n)	(0x0 + ((n) << 2))
-#define ARM_SMMU_GNSR0_PMCGCR(n)	(0x800 + ((n) << 2))
-#define ARM_SMMU_GNSR0_PMCGSMR(n)	(0xa00 + ((n) << 2))
-
-/* Translation context bank */
-#define ARM_SMMU_CB_BASE(smmu)		((smmu)->base + ((smmu)->size >> 1))
-#define ARM_SMMU_CB(smmu, n)		((n) * (1 << (smmu)->pgshift))
-
-#define ARM_SMMU_CB_SCTLR		0x0
-#define ARM_SMMU_CB_RESUME		0x8
-#define ARM_SMMU_CB_TTBCR2		0x10
-#define ARM_SMMU_CB_TTBR0_LO		0x20
-#define ARM_SMMU_CB_TTBR0_HI		0x24
-#define ARM_SMMU_CB_TTBCR		0x30
-#define ARM_SMMU_CB_S1_MAIR0		0x38
-#define ARM_SMMU_CB_FSR			0x58
-#define ARM_SMMU_CB_FAR_LO		0x60
-#define ARM_SMMU_CB_FAR_HI		0x64
-#define ARM_SMMU_CB_FSYNR0		0x68
-#define ARM_SMMU_CB_S1_TLBIASID		0x610
-#define ARM_SMMU_CB_S1_TLBIVA		0x600
-#define ARM_SMMU_CB_S1_TLBIVAL		0x620
-#define ARM_SMMU_CB_S2_TLBIIPAS2	0x630
-#define ARM_SMMU_CB_S2_TLBIIPAS2L	0x638
-#define ARM_SMMU_CB_TLBSYNC		0x7f0
-#define ARM_SMMU_CB_TLBSTATUS		0x7f4
-
-/* Counter group registers */
-#define PMCG_SIZE	32
-/* Event Counter registers */
-#define PMEV_SIZE	8
+/* TODO: Restructure code to remove handle global variable */
+static struct smmu_debugfs_info *smmu_handle;
 
 #define defreg(_name)				\
 	{					\
@@ -178,14 +82,78 @@ static const struct debugfs_reg32 arm_smmu_cb_regs[] = {
 	defreg_cb(FSYNR0),
 };
 
-static void debugfs_create_smmu_cb(struct arm_smmu_device *smmu, u8 cbndx)
+struct smmu_debugfs_master {
+	struct device 		*dev;
+	s16			*smendx;
+};
+
+static int smmu_master_show(struct seq_file *s, void *unused)
+{
+	int i;
+	struct smmu_debugfs_master *master = s->private;
+	struct iommu_fwspec *fwspec = master->dev->iommu_fwspec;
+
+	for (i = 0; i < fwspec->num_ids; i++) {
+		seq_printf(s, "streamids: % 3d ",
+				fwspec->ids[i] & smmu_handle->streamid_mask);
+	}
+	seq_printf(s, "\n");
+	for (i = 0; i < fwspec->num_ids; i++) {
+		seq_printf(s, "smrs: % 3d ", master->smendx[i]);
+	}
+	seq_printf(s, "\n");
+
+	return 0;
+}
+
+static int smmu_master_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, smmu_master_show, inode->i_private);
+}
+
+static const struct file_operations smmu_master_fops = {
+	.open           = smmu_master_open,
+	.read           = seq_read,
+	.llseek         = seq_lseek,
+	.release        = single_release,
+};
+
+void arm_smmu_debugfs_add_master(struct device *dev, u8 *cbndx, u16 smendx[])
+{
+	struct smmu_debugfs_master *master;
+	struct dentry *dent;
+	char name[] = "cb000";
+	char target[] = "../../cb000";
+
+	dent = debugfs_create_dir(dev_name(dev), smmu_handle->masters_root);
+	if (!dent)
+		return;
+
+	master = kmalloc(sizeof(*master), GFP_KERNEL);
+	master->dev = dev;
+	master->smendx = smendx;
+
+	// TODO create the streamids file
+	debugfs_create_file("streamids", 0444, dent, master,
+							&smmu_master_fops);
+	debugfs_create_u8("cbndx", 0444, dent, cbndx);
+	sprintf(name, "cb%03d", *cbndx);
+	sprintf(target, "../../cb%03d", *cbndx);
+	debugfs_create_symlink(name, dent, target);
+}
+
+/*
+ * TODO: Cbs downstream have the ability to print out iova-to-phys
+ * and a dump of their page tables
+ */
+static void debugfs_create_smmu_cb(struct smmu_debugfs_info *smmu, u8 cbndx)
 {
 	struct dentry *dent;
 	char name[] = "cb000";
 	struct debugfs_regset32	*cb;
 
 	sprintf(name, "cb%03d", cbndx);
-	dent = debugfs_create_dir(name, smmu->cb_root);
+	dent = debugfs_create_dir(name, smmu->debugfs_root);
 	if (!dent)
 		return;
 
@@ -236,7 +204,7 @@ DEFINE_SIMPLE_ATTRIBUTE(smmu_perf_regset_debugfs_fops,
 			smmu_perf_regset_debugfs_get,
 			smmu_perf_regset_debugfs_set, "%08llx\n");
 
-static void arm_smmu_debugfs_delete(struct arm_smmu_device *smmu)
+void arm_smmu_regs_debugfs_delete(struct smmu_debugfs_info *smmu)
 {
 	int i;
 
@@ -264,14 +232,15 @@ static void arm_smmu_debugfs_delete(struct arm_smmu_device *smmu)
 	debugfs_remove_recursive(smmu->debugfs_root);
 }
 
-static int arm_smmu_debugfs_create(struct arm_smmu_device *smmu)
+int arm_smmu_regs_debugfs_create(struct smmu_debugfs_info *smmu)
 {
 	int i;
 	struct debugfs_reg32 *regs;
 	size_t bytes;
 	struct dentry *dent_gr, *dent_gnsr;
 
-	smmu->debugfs_root = debugfs_create_dir("smmu_regs", NULL);
+	smmu_handle = smmu;
+
 	if (!smmu->debugfs_root)
 		return -1;
 
@@ -423,42 +392,6 @@ static int arm_smmu_debugfs_create(struct arm_smmu_device *smmu)
 	return 0;
 
 err_out:
-	arm_smmu_debugfs_delete(smmu);
+	arm_smmu_regs_debugfs_delete(smmu);
 	return -1;
 }
-
-static int __init smmu_regs_init(void)
-{
-	int ret = 0;
-
-	smmu_handle = kzalloc(sizeof(struct arm_smmu_device), GFP_KERNEL);
-	if (smmu_handle == NULL)
-		return -ENOMEM;
-
-	/* Get the smmu base register */
-	smmu_handle->base = ioremap(0x12000000, 0x800000);
-	smmu_handle->size = 0x800000;
-	smmu_handle->pgshift = PAGE_SHIFT;
-	smmu_handle->num_context_banks = 64;
-
-	if (IS_ERR(smmu_handle->base))
-		return PTR_ERR(smmu_handle->base);
-
-	ret = arm_smmu_debugfs_create(smmu_handle);
-	if (ret != 0)
-		pr_debug("arm_smmu_debugfs_create failed\n");
-
-	return 0;
-}
-
-static void __exit smmu_regs_exit(void)
-{
-	iounmap(smmu_handle->base);
-	arm_smmu_debugfs_delete(smmu_handle);
-	kfree(smmu_handle);
-}
-
-module_init(smmu_regs_init);
-module_exit(smmu_regs_exit);
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("arm-smmu regdump");

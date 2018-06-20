9558a94245b67686ae624de0873ea46097e65b94
James Huang <jamehuang@nvidia.com>
2018-06-20 10:37:25 +0800

    nvdumper: Update the copyright year of the driver
    bug 200416202
    
    Change-Id: I0f7536a390b3c12d7a3d4b8292c827e01b1a1ed5
    Signed-off-by: James Huang <jamehuang@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1755266
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Yifei Wan <ywan@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/platform/tegra/nvdumper/nvdumper_reg_tegra.c
+++ drivers/platform/tegra/nvdumper/nvdumper_reg_tegra.c
@@ -1,7 +1,7 @@
 /*
  * drivers/platform/tegra/nvdumper/nvdumper_reg_tegra.c
  *
- * Copyright (c) 2011-2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and

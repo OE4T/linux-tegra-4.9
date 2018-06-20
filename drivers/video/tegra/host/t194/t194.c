990badc03ac28dd939a923d65a5c6e0aa2b60051
Mikko Perttunen <mperttunen@nvidia.com>
2018-06-20 13:43:13 +0300

    video: tegra: host: Set idle EMC floors to zero
    The memory bandwidth request for VIC when idle was set to max, which
    is quite unnecessary, and 204MHz for other clients. It doesn't
    make sense to request a non-zero EMC clock while the engine is not in
    use. As such, change these to zero. It's mystery why they were
    something other than zero.
    
    Bug 200412455
    Jira HOSTX-351
    
    Change-Id: I4b82fd539a2de2f1e0ef89ca955b7c0a1daab91a
    Signed-off-by: Mikko Perttunen <mperttunen@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1755712
    (cherry picked from commit ff404dcb4ee4b9fe0428cd0076e5520148572ab1)
    Reviewed-on: https://git-master.nvidia.com/r/1788719
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Rajkumar Kasirajan <rkasirajan@nvidia.com>
    Reviewed-by: Leo He <leoh@nvidia.com>
    Tested-by: Rajkumar Kasirajan <rkasirajan@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/host/t194/t194.c
+++ drivers/video/tegra/host/t194/t194.c
@@ -67,9 +67,6 @@
 #include "streamid_regs.c"
 #include "cg_regs.c"
 
-#define HOST_EMC_FLOOR 204000000
-#define HOST_NVDEC_EMC_FLOOR 102000000
-
 /*
  * TODO: Move following functions to the corresponding files under
  * kernel-3.18 once kernel-t19x gets merged there. Until that
@@ -316,7 +313,7 @@ struct nvhost_device_data t19_msenc_info = {
 	.autosuspend_delay      = 500,
 	.clocks			= {
 		{"nvenc", UINT_MAX},
-		{"emc", HOST_EMC_FLOOR,
+		{"emc", 0,
 		 NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_SHARED_BW}
 	},
@@ -349,7 +346,7 @@ struct nvhost_device_data t19_nvenc1_info = {
 	.autosuspend_delay      = 500,
 	.clocks			= {
 		{"nvenc", UINT_MAX},
-		{"emc", HOST_EMC_FLOOR,
+		{"emc", 0,
 		 NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_SHARED_BW}
 	},
@@ -386,7 +383,7 @@ struct nvhost_device_data t19_nvdec_info = {
 		{"nvdec", UINT_MAX},
 		{"kfuse", 0, 0},
 		{"efuse", 0, 0},
-		{"emc", HOST_NVDEC_EMC_FLOOR,
+		{"emc", 0,
 		 NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_FLOOR}
 	},
@@ -421,7 +418,7 @@ struct nvhost_device_data t19_nvdec1_info = {
 		{"nvdec", UINT_MAX},
 		{"kfuse", 0, 0},
 		{"efuse", 0, 0},
-		{"emc", HOST_NVDEC_EMC_FLOOR,
+		{"emc", 0,
 		 NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_FLOOR}
 	},
@@ -456,7 +453,7 @@ struct nvhost_device_data t19_nvjpg_info = {
 	.autosuspend_delay      = 500,
 	.clocks			= {
 		{"nvjpg", UINT_MAX},
-		{"emc", HOST_EMC_FLOOR,
+		{"emc", 0,
 		 NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_SHARED_BW}
 	},
@@ -489,7 +486,7 @@ struct nvhost_device_data t19_tsec_info = {
 	.clocks			= {
 		{"tsec", 115200000},
 		{"efuse", 0, 0},
-		{"emc", HOST_EMC_FLOOR,
+		{"emc", 0,
 		 NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_FLOOR}
 	},
@@ -517,7 +514,7 @@ struct nvhost_device_data t19_tsecb_info = {
 	.clocks			= {
 		{"tsecb", UINT_MAX},
 		{"efuse", 0, 0},
-		{"emc", HOST_EMC_FLOOR,
+		{"emc", 0,
 		 NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_FLOOR}
 	},
@@ -543,7 +540,7 @@ struct nvhost_device_data t19_vic_info = {
 	.devfs_name		= "vic",
 	.clocks			= {
 		{"vic", UINT_MAX, 0},
-		{"emc", UINT_MAX, NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
+		{"emc", 0, NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER,
 		 0, TEGRA_BWMGR_SET_EMC_SHARED_BW},
 	},
 	.version		= NVHOST_ENCODE_FLCN_VER(4, 2),

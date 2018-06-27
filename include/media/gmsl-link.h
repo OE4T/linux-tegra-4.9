a126ba0addf99211530e1afdcec972c818685fcb
Sudhir Vyas <svyas@nvidia.com>
2018-06-27 23:10:54 +0530

    media: define gmsl header
    Bug 200407741
    
    Change-Id: I6c0de55073ecbec734aecb57392948787151510b
    Signed-off-by: Sudhir Vyas <svyas@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1763274
    Reviewed-by: Frank Chen <frankc@nvidia.com>
    Reviewed-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ include/media/gmsl-link.h
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2018, NVIDIA Corporation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GMSL_LINK_H__
+#define __GMSL_LINK_H__
+
+#define GMSL_CSI_1X4_MODE 0x1
+#define GMSL_CSI_2X4_MODE 0x2
+#define GMSL_CSI_2X2_MODE 0x3
+#define GMSL_CSI_4X2_MODE 0x4
+
+#define GMSL_CSI_PORT_A 0x0
+#define GMSL_CSI_PORT_B 0x1
+
+#define GMSL_ST_ID_UNUSED 0XFF
+
+#define GMSL_PIPE_X_ID 0x1
+#define GMSL_PIPE_Y_ID 0x2
+#define GMSL_PIPE_Z_ID 0x3
+#define GMSL_PIPE_U_ID 0x4
+
+/* Didn't find kernel defintions, for now adding here */
+#define GMSL_CSI_DT_RAW_12   0x2C
+#define GMSL_CSI_DT_UED_U1   0x30
+
+#define GMSL_DEV_MAX_NUM_DATA_STREAMS 4
+
+struct gmsl_stream {
+	__u32 st_id_sel;
+	__u32 st_data_type;
+	__u32 pipe_id;
+};
+
+struct gmsl_link_data {
+	__u32 st_vc;
+	__u32 dst_vc;
+	__u32 src_csi_port;
+	__u32 dst_csi_port;
+	__u32 num_streams;
+	__u32 num_csi_lanes;
+	__u32 csi_mode;
+	struct gmsl_stream streams[GMSL_DEV_MAX_NUM_DATA_STREAMS];
+	struct device *s_dev;
+};
+
+#endif  /* __GMSL_LINK_H__ */

e755d376737d2c93d5175bd62499fd041aff4882
Naveen Kumar S <nkumars@nvidia.com>
2018-07-26 19:07:36 +0530

    fbcon: powerdown DC before updating mode
    When X gets killed abruptly, it doesn't initiate the normal
    exit procedure. In such cases, fbconsole gets control without DC
    first getting powered down. Once fbconsole takes over, it does a
    modeset with console's mode. If DC is enabled when this modeset
    happens, it causes unexpected behavior. There is no issue if both
    X and fbconsole have the same mode, but that is not always the
    case. If the modes differ, display goes into a bad state. Mode
    update should always be done after powering down display controller.
    
    To avoid such scenarios, added instruction to explicitly power down
    DC before fbconsole does a modeset. This doesn't add any significant
    overhead during normal operations as display driver returns if DC
    is already powered down.
    
    bug 200434228
    
    Change-Id: I4c807b3d6ea5b3d85b5b0703139c8a5dbb123a02
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1786673
    (cherry picked from commit 5c6cf5641062890accccedb14a72d080a763a977)
    Reviewed-on: https://git-master.nvidia.com/r/1842401
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-by: Tow Wang <toww@nvidia.com>
    Reviewed-by: Ujwal Patel <ujwalp@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/console/fbcon.c
+++ drivers/video/console/fbcon.c
@@ -2354,6 +2354,7 @@ static int fbcon_blank(struct vc_data *vc, int blank, int mode_switch)
 		ops->graphics = 1;
 
 		if (!blank) {
+			fb_blank(info, FB_BLANK_POWERDOWN);
 			var.activate = FB_ACTIVATE_NOW | FB_ACTIVATE_FORCE;
 			fb_set_var(info, &var);
 			ops->graphics = 0;

59dc67904b161623be3326a5dbf716607f61007b
Sudhir Vyas <svyas@nvidia.com>
2018-08-24 17:30:47 +0530

    media: extend gmsl ctx
    Bug 200407741
    
    Change-Id: I6a65d84a171ed10ec2c3488e428963a9cdbff4bc
    Signed-off-by: Sudhir Vyas <svyas@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1806177
    Reviewed-by: Pekka Pessi <ppessi@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- include/media/gmsl-link.h
+++ include/media/gmsl-link.h
@@ -24,34 +24,40 @@
 
 #define GMSL_CSI_PORT_A 0x0
 #define GMSL_CSI_PORT_B 0x1
+#define GMSL_CSI_PORT_C 0x2
+#define GMSL_CSI_PORT_D 0x3
+#define GMSL_CSI_PORT_E 0x4
+#define GMSL_CSI_PORT_F 0x5
 
-#define GMSL_ST_ID_UNUSED 0XFF
-
-#define GMSL_PIPE_X_ID 0x1
-#define GMSL_PIPE_Y_ID 0x2
-#define GMSL_PIPE_Z_ID 0x3
-#define GMSL_PIPE_U_ID 0x4
+#define GMSL_SERDES_CSI_LINK_A 0x1
+#define GMSL_SERDES_CSI_LINK_B 0x2
 
 /* Didn't find kernel defintions, for now adding here */
-#define GMSL_CSI_DT_RAW_12   0x2C
-#define GMSL_CSI_DT_UED_U1   0x30
+#define GMSL_CSI_DT_RAW_12 0x2C
+#define GMSL_CSI_DT_UED_U1 0x30
+#define GMSL_CSI_DT_EMBED 0x12
+
+#define GMSL_ST_ID_UNUSED 0xFF
 
 #define GMSL_DEV_MAX_NUM_DATA_STREAMS 4
 
 struct gmsl_stream {
 	__u32 st_id_sel;
 	__u32 st_data_type;
-	__u32 pipe_id;
 };
 
-struct gmsl_link_data {
+struct gmsl_link_ctx {
 	__u32 st_vc;
 	__u32 dst_vc;
 	__u32 src_csi_port;
 	__u32 dst_csi_port;
+	__u32 serdes_csi_link;
 	__u32 num_streams;
 	__u32 num_csi_lanes;
 	__u32 csi_mode;
+	__u32 ser_reg;
+	__u32 sdev_reg;
+	__u32 sdev_def;
 	struct gmsl_stream streams[GMSL_DEV_MAX_NUM_DATA_STREAMS];
 	struct device *s_dev;
 };

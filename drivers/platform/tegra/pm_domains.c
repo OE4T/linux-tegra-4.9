b87965b85a84559e8bd3adb49e2fd0910e7ee0ff
Joseph Lo <josephl@nvidia.com>
2018-08-31 09:40:18 +0800

    platform: tegra: pm_domains: remove unused header
    Remove unused herader.
    
    Bug 200400829
    
    Change-Id: I5ebe990100311721705b32f965bb75965249c8dc
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1810355
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/platform/tegra/pm_domains.c
+++ drivers/platform/tegra/pm_domains.c
@@ -1,7 +1,7 @@
 /*
  * drivers/platform/tegra/pm_domains.c
  *
- * Copyright (c) 2012-2017, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2012-2018, NVIDIA CORPORATION. All rights reserved.
  *
  *
  * This software is licensed under the terms of the GNU General Public
@@ -21,11 +21,7 @@
 #include <linux/pm_domain.h>
 #include <linux/tegra_pm_domains.h>
 #include <linux/tegra-powergate.h>
-#ifdef CONFIG_TEGRA_APE_AGIC
-#include <linux/irqchip/tegra-agic.h>
-#endif
 #include <linux/slab.h>
-#include <linux/wakelock.h>
 
 typedef int (*of_tegra_pd_init_cb_t)(struct generic_pm_domain *);
 

fdf77eda18b59c305d4dd8436d8b09d42ec4718a
Tejal Kudav <tkudav@nvidia.com>
2018-09-05 17:08:54 +0530

    gpu: nvgpu: Fix entry_type value for GM20B Top
    In hw_top.ref, the entry_type value is 3 and not 2.
    
    JIRA NVGPU-1053
    
    Change-Id: I7de5788ae5d08a88f1734bff56acae6f2f03581a
    Signed-off-by: Tejal Kudav <tkudav@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1813267
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: svc-misra-checker <svc-misra-checker@nvidia.com>
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vijayakumar Subbu <vsubbu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/include/nvgpu/hw/gp10b/hw_top_gp10b.h
+++ drivers/gpu/nvgpu/include/nvgpu/hw/gp10b/hw_top_gp10b.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -190,7 +190,7 @@ static inline u32 top_device_info_entry_enum_v(void)
 }
 static inline u32 top_device_info_entry_engine_type_v(void)
 {
-	return 0x00000002U;
+	return 0x00000003U;
 }
 static inline u32 top_device_info_entry_data_v(void)
 {

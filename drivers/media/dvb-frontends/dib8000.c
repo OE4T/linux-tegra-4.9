7d6b5cde2d05be05af5bb657b450c80f69c263af
Ketan Patil <ketanp@nvidia.com>
2018-09-11 12:24:18 +0530

    media: dvb_frontend: fix coverity defect
    Remove the slave indexed at index_frontend -1 instead of
    index_frontend. Removing the slave at index index_frontend
    is not correct as it is pointing to NULL.
    
    Coverity ID 3231516
    
    Bug 200447703
    
    Change-Id: Icd210a6aac3f5d5470f93e5f741529e2b0910ae1
    Signed-off-by: Ketan Patil <ketanp@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1819139
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/media/dvb-frontends/dib8000.c
+++ drivers/media/dvb-frontends/dib8000.c
@@ -4257,7 +4257,7 @@ static int dib8000_remove_slave_frontend(struct dvb_frontend *fe)
 		index_frontend++;
 	if (index_frontend != 1) {
 		dprintk("remove slave fe %p (index %i)", state->fe[index_frontend-1], index_frontend-1);
-		state->fe[index_frontend] = NULL;
+		state->fe[index_frontend-1] = NULL;
 		return 0;
 	}
 

63518c0e69325710e911b89343ae3c6b962bb1a0
Jonathan Hunter <jonathanh@nvidia.com>
2018-12-17 13:25:46 +0000

    ASoC: tegra-alt: Fix failure path and removal of ADSP
    If the ADSP fail during the probe after the ADSP component is
    registered, the ADSP component will not be unregistered and this will
    cause the kernel to crash when attempting to the probe the component.
    Furthermore, if the removal of the ADSP device, the component and codec
    register by the ADSP driver are not removed. Fix these items by ensuring
    the the ADSP component and codec are removed as necessary during the
    probe failure path and driver removal.
    
    Finally, clean-up some of the prints in the driver probe to drop the
    unnecessary print when the netlink is created and use the dev_xxx
    functions instead of the pr_xxx functions.
    
    Bug 200458098
    
    Change-Id: I4b51949e1c10c86952008ebe5676832e381a68f4
    Signed-off-by: Jonathan Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1974520
    (cherry picked from commit e6e332abb6a9e815bccaf9cc33d09685b6342cce)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2534866
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: Uday Gupta <udayg@nvidia.com>
    Reviewed-by: Phoenix Jung <pjung@nvidia.com>
    Reviewed-by: Dipesh Gandhi <dipeshg@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Niranjan Dighe <ndighe@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- sound/soc/tegra-virt-alt/tegra210_adsp_virt_alt.c
+++ sound/soc/tegra-virt-alt/tegra210_adsp_virt_alt.c
@@ -2,7 +2,7 @@
  * tegra210_adsp_virt_alt.c - Tegra ADSP audio driver
  *
  * Author: Sumit Bhattacharya <sumitb@nvidia.com>
- * Copyright (c) 2014-2020, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2021, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -5791,7 +5791,7 @@ static int tegra210_adsp_audio_platform_probe(struct platform_device *pdev)
 				     ARRAY_SIZE(tegra210_adsp_codec_dai));
 	if (ret != 0) {
 		dev_err(&pdev->dev, "Could not register CODEC: %d\n", ret);
-		goto err_unregister_platform;
+		goto err_unregister_component;
 	}
 
 	spin_lock_init(&adsp->switch_lock);
@@ -5835,14 +5835,19 @@ static int tegra210_adsp_audio_platform_probe(struct platform_device *pdev)
 
 	adsp->nl_sk = netlink_kernel_create(&init_net, NETLINK_ADSP_EVENT, &cfg);
 	if (!adsp->nl_sk) {
-		pr_err("Error creating socket.\n");
-		return -1;
+		dev_err(&pdev->dev, "Error creating socket\n");
+		ret = -ENOMEM;
+		goto err_unregister_codec;
 	}
-	pr_info("Succssfully created NETLINK_ADSP_EVENT socket\n");
 
-	pr_info("tegra210_adsp_audio_platform_probe probe successfull.");
+	dev_info(&pdev->dev, "Tegra210 ADSP driver successfully registered\n");
+
 	return 0;
 
+err_unregister_codec:
+	snd_soc_unregister_codec(&pdev->dev);
+err_unregister_component:
+	snd_soc_unregister_component(&pdev->dev);
 err_unregister_platform:
 	snd_soc_unregister_platform(&pdev->dev);
 err_pm_disable:
@@ -5856,6 +5861,8 @@ static int tegra210_adsp_audio_platform_remove(struct platform_device *pdev)
 	struct tegra210_adsp *adsp = dev_get_drvdata(&pdev->dev);
 
 	netlink_kernel_release(adsp->nl_sk);
+	snd_soc_unregister_codec(&pdev->dev);
+	snd_soc_unregister_component(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 	tegra_pd_remove_device(&pdev->dev);
 	snd_soc_unregister_platform(&pdev->dev);

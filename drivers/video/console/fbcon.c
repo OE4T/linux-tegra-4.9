3f68b13a80ed25d5f90ab65e6c031db11743188f
Naveen Kumar S <nkumars@nvidia.com>
2019-01-03 14:57:56 +0530

    video: fbcon: update mode used for new consoles
    Currently, fb var_screeninfo is used when initializing a new conosle.
    This leads to console using the graphical client's mode if any such
    client (Xorg) is active when the console init happens. One such case
    is when system enters sc7 and X is active, the suspend console (created
    by power management framework) takes over the mode currently set by X.
    
    To avoid setting graphical client's mode for consoles, this change uses
    console's mode when initializing new consoles if fb_info has valid
    fbconsole data.
    
    bug 200471453
    
    Change-Id: I86ad5077ca52ba8864daa1618490b34021f1c3ba
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1986912
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/console/fbcon.c
+++ drivers/video/console/fbcon.c
@@ -1022,6 +1022,10 @@ static void fbcon_init(struct vc_data *vc, int init)
 {
 	struct fb_info *info = registered_fb[con2fb_map[vc->vc_num]];
 	struct fbcon_ops *ops;
+	struct fb_info *cur_fb_info = registered_fb[con2fb_map[fg_console]];
+	struct fbcon_ops *cur_ops = cur_fb_info->fbcon_par;
+	struct fb_var_screeninfo *var;
+
 	struct vc_data **default_mode = vc->vc_display_fg;
 	struct vc_data *svc = *default_mode;
 	struct display *t, *p = &fb_display[vc->vc_num];
@@ -1037,7 +1041,12 @@ static void fbcon_init(struct vc_data *vc, int init)
 	    (info->fix.type == FB_TYPE_TEXT))
 		logo = 0;
 
-	if (var_to_display(p, &info->var, info))
+	if (cur_ops && cur_ops->var.xres && cur_ops->var.yres)
+		var = &cur_ops->var;
+	else
+		var = &info->var;
+
+	if (var_to_display(p, var, info))
 		return;
 
 	if (!info->fbcon_par)

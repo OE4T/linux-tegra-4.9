05e46efaf16e51800065023949845394ebc64819
Aniruddha TVS Rao <anrao@nvidia.com>
2019-01-17 10:13:44 +0530

    mmc: host: Disable async probe type for SDMMC
    Disable asynchronous probe type for SDMMC driver to
    avoid any delay in SDMMC enumeration.
    
    Bug 200482013
    
    Change-Id: I1759b9113f0a52808bbca22c2de552870b3a85d8
    Signed-off-by: Aniruddha TVS Rao <anrao@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1997120
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -2697,7 +2697,6 @@ static struct platform_driver sdhci_tegra_driver = {
 		.name	= "sdhci-tegra",
 		.of_match_table	= sdhci_tegra_dt_match,
 		.pm	= &sdhci_pltfm_pmops,
-		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 	.probe		= sdhci_tegra_probe,
 	.remove		= sdhci_pltfm_unregister,

9bff0f0ab5753a6b5c009e9ff89d890450f5e426
sumitg <sumitg@nvidia.com>
2019-02-20 16:39:48 +0530

    devfreq: add dumpstack in debugfs creation failure
    Adding dump_stack() in error condition while creating
    debugfs to track possible double gpu gov addition path.
    	"podgov: can't create debugfs directory"
    
    Bug 200491990
    
    Change-Id: I6f7f3e1aa6ff0e86af9a399c7ce7a1bb75b4b7bb
    Signed-off-by: sumitg <sumitg@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2023741
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Tested-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/devfreq/governor_pod_scaling.c
+++ drivers/devfreq/governor_pod_scaling.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2019, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -366,6 +366,7 @@ static void nvhost_scale_emc_debug_init(struct devfreq *df)
 	podgov->debugdir = debugfs_create_dir(dirname, NULL);
 	if (!podgov->debugdir) {
 		pr_err("podgov: can\'t create debugfs directory\n");
+		dump_stack();
 		return;
 	}
 

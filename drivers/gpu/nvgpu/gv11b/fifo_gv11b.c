OE4T/linux-tegra-5.10

gpu: nvgpu: gv11b: add missing tsg_mark_error
nvgpu_tsg_mark_error is missing in teardown path for aborting tsg.
Without this, channels corresponding to tsg being aborted will not be
set to timedout (unserviceable) and also notifier_wq and semaphore_wq
will not be woken up.

Change-Id: Ie71c9a3b7a7fd1aa8cb9ec5d0dc30ccaeadfeae5
Signed-off-by: Seema Khowala <seemaj@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1999026
Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
Reviewed-by: svc-mobile-misra <svc-mobile-misra@nvidia.com>
Reviewed-by: svc-misra-checker <svc-misra-checker@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Debarshi Dutta <ddutta@nvidia.com>
Reviewed-by: Alex Waterman <alexw@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/gpu/nvgpu/gv11b/fifo_gv11b.c
@@ -1141,7 +1141,7 @@ void gv11b_fifo_teardown_ch_tsg(struct gk20a *g, u32 act_eng_bitmask,
 			if (rc_type == RC_TYPE_MMU_FAULT) {
 				nvgpu_tsg_set_ctx_mmu_error(g, tsg);
 			}
-
+			(void)nvgpu_tsg_mark_error(g, tsg);
 			gk20a_fifo_abort_tsg(g, tsg, false);
 		}
 	} else {

OE4T/linux-tegra-4.9

Bug 2092051
Bug 2429295
Bug 2484211
Signed-off-by: Debarshi Dutta <ddutta@nvidia.com>
(cherry picked from commit 7fed0c19379139081162536ead3682ab747775db
in dev-kernel)
Reviewed-on: https://git-master.nvidia.com/r/2086594
Reviewed-by: Bibek Basu <bbasu@nvidia.com>

drivers/gpu/nvgpu/gv11b/fifo_gv11b.c
@@ -1180,7 +1180,7 @@ void gv11b_fifo_teardown_ch_tsg(struct gk20a *g, u32 act_eng_bitmask,
 			if (rc_type == RC_TYPE_MMU_FAULT) {
 				gk20a_fifo_set_ctx_mmu_error_tsg(g, tsg);
 			}
-
+			(void)gk20a_fifo_error_tsg(g, tsg);
 			gk20a_fifo_abort_tsg(g, tsg, false);
 		}
 	} else {
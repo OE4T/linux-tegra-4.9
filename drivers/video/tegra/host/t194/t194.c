fdb2e947c854e710e6c24f87daea961eddf83eb2
Sameer Gumaste <sgumaste@nvidia.com>
2019-05-31 13:27:04 +0530

    kernel: nvidia: drivers: T194 Introduce mlock_timeout_factor for NVENC and NVDEC
    Introduce mlock_timeout_factor for NVENC and NVDEC for T194.
    Right now this is totally missing. Hence the engine mlock timeout
    happens within 16 msec itself for VM Server hypervisor case.
    
    Bug 2590792
    Bug 200523931
    
    Change-Id: I31b730c2841cfe153b91fd367fe7d347e721ff7b
    Signed-off-by: Sameer Gumaste <sgumaste@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2128399
    (cherry picked from commit c7b3a711e40feb3f002c91c8d16bd3d1dc644e84)
    Reviewed-on: https://git-master.nvidia.com/r/2144829
    Tested-by: Peter Daifuku <pdaifuku@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Satish Arora <satisha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/host/t194/t194.c
+++ drivers/video/tegra/host/t194/t194.c
@@ -1,7 +1,7 @@
 /*
  * Tegra Graphics Init for T194 Architecture Chips
  *
- * Copyright (c) 2016-2018, NVIDIA Corporation. All rights reserved.
+ * Copyright (c) 2016-2019, NVIDIA Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -343,6 +343,7 @@ struct nvhost_device_data t19_msenc_info = {
 	.can_powergate		= true,
 	.isolate_contexts	= true,
 	.enable_timestamps	= flcn_enable_timestamps,
+	.mlock_timeout_factor   = 4,
 };
 
 struct nvhost_device_data t19_nvenc1_info = {
@@ -376,6 +377,7 @@ struct nvhost_device_data t19_nvenc1_info = {
 	.can_powergate		= true,
 	.isolate_contexts	= true,
 	.enable_timestamps	= flcn_enable_timestamps,
+	.mlock_timeout_factor   = 4,
 };
 #endif
 
@@ -413,6 +415,7 @@ struct nvhost_device_data t19_nvdec_info = {
 	.engine_can_cg		= true,
 	.can_powergate		= true,
 	.isolate_contexts	= true,
+	.mlock_timeout_factor   = 4,
 };
 
 struct nvhost_device_data t19_nvdec1_info = {
@@ -448,6 +451,7 @@ struct nvhost_device_data t19_nvdec1_info = {
 	.engine_can_cg		= true,
 	.can_powergate		= true,
 	.isolate_contexts	= true,
+	.mlock_timeout_factor   = 4,
 };
 #endif
 

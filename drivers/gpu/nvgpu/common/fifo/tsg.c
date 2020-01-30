OE4T/linux-tegra-5.10

gpu: nvgpu: use refcnt for ch mmu_debug_mode
Replaced ch->mmu_debug_mode_enabled with ch->mmu_debug_mode_refcnt.
If channel is enabled multiple times by userspace, then ref count is
updated accordingly. There is an expectation that enable/disable
calls are balanced for setting channel's mmu debug mode.
When unbinding the channel, decrease refcnt for the channel until it
reaches 0.
Also, removed tsg parameter from nvgpu_tsg_set_mmu_debug_mode as it
can be retrieved from ch.

Bug 2515097

Change-Id: If334e374a55bd14ae219edbfd3b1fce5ff25c226
Signed-off-by: Thomas Fleury <tfleury@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/2184702
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/gpu/nvgpu/common/fifo/tsg.c
@@ -185,11 +185,12 @@ static int nvgpu_tsg_unbind_channel_common(struct nvgpu_tsg *tsg,
 	nvgpu_rwsem_up_write(&tsg->ch_list_lock);
 
 #ifdef CONFIG_NVGPU_DEBUGGER
-	if (ch->mmu_debug_mode_enabled) {
-		err = nvgpu_tsg_set_mmu_debug_mode(tsg, ch, false);
+	while (ch->mmu_debug_mode_refcnt > 0U) {
+		err = nvgpu_tsg_set_mmu_debug_mode(ch, false);
 		if (err != 0) {
 			nvgpu_err(g, "disable mmu debug mode failed ch:%u",
 				ch->chid);
+			break;
 		}
 	}
 #endif
@@ -887,13 +888,14 @@ void nvgpu_tsg_reset_faulted_eng_pbdma(struct gk20a *g, struct nvgpu_tsg *tsg,
 }
 
 #ifdef CONFIG_NVGPU_DEBUGGER
-int nvgpu_tsg_set_mmu_debug_mode(struct nvgpu_tsg *tsg,
-		struct nvgpu_channel *ch, bool enable)
+int nvgpu_tsg_set_mmu_debug_mode(struct nvgpu_channel *ch, bool enable)
 {
 	struct gk20a *g;
 	int err = 0;
+	u32 ch_refcnt;
 	u32 tsg_refcnt;
 	u32 fb_refcnt;
+	struct nvgpu_tsg *tsg = nvgpu_tsg_from_ch(ch);
 
 	if ((ch == NULL) || (tsg == NULL)) {
 		return -EINVAL;
@@ -906,17 +908,11 @@ int nvgpu_tsg_set_mmu_debug_mode(struct nvgpu_tsg *tsg,
 	}
 
 	if (enable) {
-		if (ch->mmu_debug_mode_enabled) {
-			/* already enabled for this channel */
-			return 0;
-		}
+		ch_refcnt = ch->mmu_debug_mode_refcnt + 1U;
 		tsg_refcnt = tsg->mmu_debug_mode_refcnt + 1U;
 		fb_refcnt = g->mmu_debug_mode_refcnt + 1U;
 	} else {
-		if (!ch->mmu_debug_mode_enabled) {
-			/* already disabled for this channel */
-			return 0;
-		}
+		ch_refcnt = ch->mmu_debug_mode_refcnt - 1U;
 		tsg_refcnt = tsg->mmu_debug_mode_refcnt - 1U;
 		fb_refcnt = g->mmu_debug_mode_refcnt - 1U;
 	}
@@ -941,7 +937,7 @@ int nvgpu_tsg_set_mmu_debug_mode(struct nvgpu_tsg *tsg,
 		g->ops.fb.set_mmu_debug_mode(g, fb_refcnt > 0U);
 	}
 
-	ch->mmu_debug_mode_enabled = enable;
+	ch->mmu_debug_mode_refcnt = ch_refcnt;
 	tsg->mmu_debug_mode_refcnt = tsg_refcnt;
 	g->mmu_debug_mode_refcnt = fb_refcnt;
 

drivers/gpu/nvgpu/include/nvgpu/tsg.h
@@ -622,7 +622,6 @@ void nvgpu_tsg_abort(struct gk20a *g, struct nvgpu_tsg *tsg, bool preempt);
 void nvgpu_tsg_reset_faulted_eng_pbdma(struct gk20a *g, struct nvgpu_tsg *tsg,
 		bool eng, bool pbdma);
 #ifdef CONFIG_NVGPU_DEBUGGER
-int nvgpu_tsg_set_mmu_debug_mode(struct nvgpu_tsg *tsg,
-		struct nvgpu_channel *ch, bool enable);
+int nvgpu_tsg_set_mmu_debug_mode(struct nvgpu_channel *ch, bool enable);
 #endif
 #endif /* NVGPU_TSG_H */

OE4T/linux-tegra-4.9

Bug 2713590
(cherry picked from commit f422aee39387a5aa337de69cc21a67f16697ae0e)
Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2208772
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Kajetan Dutka <kdutka@nvidia.com>
Reviewed-by: Alex Waterman <alexw@nvidia.com>
Reviewed-by: Winnie Hsu <whsu@nvidia.com>
Tested-by: Kajetan Dutka <kdutka@nvidia.com>
GVS: Gerrit_Virtual_Submit

drivers/gpu/nvgpu/common/fifo/tsg.c
@@ -446,13 +446,14 @@ void gk20a_tsg_update_sm_error_state_locked(struct tsg_gk20a *tsg,
 			sm_error_state->hww_warp_esr_report_mask;
 }
 
-int nvgpu_tsg_set_mmu_debug_mode(struct tsg_gk20a *tsg,
-		struct channel_gk20a *ch, bool enable)
+int nvgpu_tsg_set_mmu_debug_mode(struct channel_gk20a *ch, bool enable)
 {
 	struct gk20a *g;
 	int err = 0;
+	u32 ch_refcnt;
 	u32 tsg_refcnt;
 	u32 fb_refcnt;
+	struct tsg_gk20a *tsg = tsg_gk20a_from_ch(ch);
 
 	if ((ch == NULL) || (tsg == NULL)) {
 		return -EINVAL;
@@ -465,17 +466,11 @@ int nvgpu_tsg_set_mmu_debug_mode(struct tsg_gk20a *tsg,
 	}
 
 	if (enable) {
-		if (ch->mmu_debug_mode_enabled) {
-			/* already enabled for this channel */
-			return 0;
-		}
+		ch_refcnt = ch->mmu_debug_mode_refcnt + 1U;
 		tsg_refcnt = tsg->mmu_debug_mode_refcnt + 1U;
 		fb_refcnt = g->mmu_debug_mode_refcnt + 1U;
 	} else {
-		if (!ch->mmu_debug_mode_enabled) {
-			/* already disabled for this channel */
-			return 0;
-		}
+		ch_refcnt = ch->mmu_debug_mode_refcnt - 1U;
 		tsg_refcnt = tsg->mmu_debug_mode_refcnt - 1U;
 		fb_refcnt = g->mmu_debug_mode_refcnt - 1U;
 	}
@@ -500,7 +495,7 @@ int nvgpu_tsg_set_mmu_debug_mode(struct tsg_gk20a *tsg,
 		g->ops.fb.set_mmu_debug_mode(g, fb_refcnt > 0U);
 	}
 
-	ch->mmu_debug_mode_enabled = enable;
+	ch->mmu_debug_mode_refcnt = ch_refcnt;
 	tsg->mmu_debug_mode_refcnt = tsg_refcnt;
 	g->mmu_debug_mode_refcnt = fb_refcnt;
 

drivers/gpu/nvgpu/include/nvgpu/tsg.h
@@ -127,7 +127,6 @@ gk20a_event_id_data_from_event_id_node(struct nvgpu_list_node *node)
 		((uintptr_t)node - offsetof(struct gk20a_event_id_data, event_id_node));
 };
 
-int nvgpu_tsg_set_mmu_debug_mode(struct tsg_gk20a *tsg,
-		struct channel_gk20a *ch, bool enable);
+int nvgpu_tsg_set_mmu_debug_mode(struct channel_gk20a *ch, bool enable);
 
 #endif /* TSG_GK20A_H */
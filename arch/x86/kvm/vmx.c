310f19d098a7a0335f9305cbaae082548feed095
Paolo Bonzini <pbonzini@redhat.com>
2020-02-04 15:26:29 -0800

    KVM: nVMX: Don't emulate instructions in guest mode
    [ Upstream commit 07721feee46b4b248402133228235318199b05ec ]
    
    vmx_check_intercept is not yet fully implemented. To avoid emulating
    instructions disallowed by the L1 hypervisor, refuse to emulate
    instructions by default.
    
    Cc: stable@vger.kernel.org
    [Made commit, added commit msg - Oliver]
    Signed-off-by: Oliver Upton <oupton@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 86dc39e580d8e3ffa42c8157d3e28249fd9a12c5)

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -11331,7 +11331,7 @@ static int vmx_check_intercept(struct kvm_vcpu *vcpu,
 			       struct x86_instruction_info *info,
 			       enum x86_intercept_stage stage)
 {
-	return X86EMUL_CONTINUE;
+	return X86EMUL_UNHANDLEABLE;
 }
 
 #ifdef CONFIG_X86_64

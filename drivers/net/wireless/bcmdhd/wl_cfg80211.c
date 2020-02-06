0674f53622cb1a09450a08d17aad3a624e9fe461
Shobek Attupurath <sattupurath@nvidia.com>
2020-02-05 21:42:40 -0800

    bcmdhd: Update wifi power mode based on preference
    Issue: 1. Station does not need to be in connected state
              to apply preferred power mode
           2. On state change power save mode always gets set to
              power save enabled due to delayed worker that gets
              triggered on state change
    
    Solution: 1. Remove CONNECTED state check to allow setting of
                 preferred power save mode
              2. Store preferred power save mode in cfg struct and
                 check before scheduling delayed worker
    
    Bug 2781055
    
    Change-Id: Ib3c746b45eff2eb5bbd3703b3026dd54c9013e44
    Signed-off-by: Shobek Attupurath <sattupurath@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2290724
    Reviewed-by: Nagaraj Annaiah <nannaiah@nvidia.com>
    Reviewed-by: Srinivas Ramachandran <srinivasra@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -5,7 +5,7 @@
  * Copyright (C) 2019 NVIDIA Corporation. All rights reserved.
  *
  * Portions contributed by Nvidia
- * Copyright (C) 2015-2019 NVIDIA Corporation. All rights reserved.
+ * Copyright (C) 2015-2020 NVIDIA Corporation. All rights reserved.
  *
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
@@ -924,7 +924,7 @@ static void wl_add_remove_pm_enable_work(struct bcm_cfg80211 *cfg, bool add_remo
 	if (cfg == NULL)
 		return;
 
-	if (cfg->pm_enable_work_on) {
+	if (cfg->pm_enable_work_on && cfg->pwr_save) {
 		if (add_remove) {
 			schedule_delayed_work(&cfg->pm_enable_work,
 				msecs_to_jiffies(WL_PM_ENABLE_TIMEOUT));
@@ -5374,10 +5374,13 @@ wl_cfg80211_set_power_mgmt(struct wiphy *wiphy, struct net_device *dev,
 	struct bcm_cfg80211 *cfg = wiphy_priv(wiphy);
 	struct net_info *_net_info = wl_get_netinfo_by_netdev(cfg, dev);
 
+	/* Store preferred power mode to be checked when triggering
+	 * delayed worker for enabling power mode
+	 */
+	cfg->pwr_save = enabled;
 	RETURN_EIO_IF_NOT_UP(cfg);
 	WL_DBG(("Enter\n"));
-	if (cfg->p2p_net == dev || _net_info == NULL || cfg->vsdb_mode ||
-		!wl_get_drv_status(cfg, CONNECTED, dev)) {
+	if (cfg->p2p_net == dev || _net_info == NULL || cfg->vsdb_mode) {
 		return err;
 	}
 

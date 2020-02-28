bf63515ffe359abbcec7899599e42ec601b2df14
Jiri Slaby <jslaby@suse.cz>
2020-02-28 12:54:05 +0100

    vt: selection, push console lock down
    commit 4b70dd57a15d2f4685ac6e38056bad93e81e982f upstream.
    
    We need to nest the console lock in sel_lock, so we have to push it down
    a bit. Fortunately, the callers of set_selection_* just lock the console
    lock around the function call. So moving it down is easy.
    
    In the next patch, we switch the order.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Fixes: 07e6124a1a46 ("vt: selection, close sel_buffer race")
    Cc: stable <stable@vger.kernel.org>
    Link: https://lore.kernel.org/r/20200228115406.5735-1-jslaby@suse.cz
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit ccd35863147dd447110b726a0d4911ab686aade9)

--- drivers/tty/vt/selection.c
+++ drivers/tty/vt/selection.c
@@ -339,11 +339,9 @@ int set_selection(const struct tiocl_selection __user *v, struct tty_struct *tty
 {
 	int ret;
 
-	mutex_lock(&sel_lock);
 	console_lock();
 	ret = __set_selection(v, tty);
 	console_unlock();
-	mutex_unlock(&sel_lock);
 
 	return ret;
 }

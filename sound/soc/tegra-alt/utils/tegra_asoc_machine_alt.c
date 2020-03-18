5e85d71ce021a8af49eeaf733eaa432235cd861a
Achal Verma <achalv@nvidia.com>
2020-03-18 16:35:51 +0530

    tegra-alt: Fix 'use uninitialized pointer'
    Initialize pointer to NULL, and dont access
    pointer if not assigned legal value.
    
    This fixes coverity issue: 9848024, 9847888.
    
    Bug 2028892
    
    Change-Id: Ib92b21c52341726b2965c753b9f9c2992b922ece
    Signed-off-by: Achal Verma <achalv@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2314642
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Uday Gupta <udayg@nvidia.com>
    Reviewed-by: Dmitry Pervushin <dpervushin@nvidia.com>
    Reviewed-by: Phoenix Jung <pjung@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- sound/soc/tegra-alt/utils/tegra_asoc_machine_alt.c
+++ sound/soc/tegra-alt/utils/tegra_asoc_machine_alt.c
@@ -1,7 +1,7 @@
 /*
  * tegra_asoc_machine_alt.c - Tegra xbar dai link for machine drivers
  *
- * Copyright (c) 2014-2019 NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2020 NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -2898,17 +2898,19 @@ EXPORT_SYMBOL_GPL(tegra186_xbar_codec_conf);
 
 struct snd_soc_dai_link *tegra_machine_get_dai_link(void)
 {
-	struct snd_soc_dai_link *link;
+	struct snd_soc_dai_link *link = NULL;
 	unsigned int size = 0;
+
+	if (tegra_asoc_machine_links)
+		return tegra_asoc_machine_links;
 
 	if (of_machine_is_compatible("nvidia,tegra210")  ||
 		of_machine_is_compatible("nvidia,tegra210b01")) {
 		link = tegra210_xbar_dai_links;
 		size = TEGRA210_XBAR_DAI_LINKS;
+	} else {
+		return NULL;
 	}
-
-	if (tegra_asoc_machine_links)
-		return tegra_asoc_machine_links;
 
 	num_dai_links = size;
 
@@ -3009,17 +3011,19 @@ EXPORT_SYMBOL_GPL(tegra_machine_set_dai_fmt);
 
 struct snd_soc_codec_conf *tegra_machine_get_codec_conf(void)
 {
-	struct snd_soc_codec_conf *conf;
+	struct snd_soc_codec_conf *conf = NULL;
 	unsigned int size = 0;
+
+	if (tegra_asoc_codec_conf)
+		return tegra_asoc_codec_conf;
 
 	if (of_machine_is_compatible("nvidia,tegra210")  ||
 		of_machine_is_compatible("nvidia,tegra210b01")) {
 		conf = tegra210_xbar_codec_conf;
 		size = TEGRA210_XBAR_CODEC_CONF;
+	} else {
+		return NULL;
 	}
-
-	if (tegra_asoc_codec_conf)
-		return tegra_asoc_codec_conf;
 
 	tegra_asoc_codec_conf = kzalloc(size *
 		sizeof(struct snd_soc_codec_conf), GFP_KERNEL);

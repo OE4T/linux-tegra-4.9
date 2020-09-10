8b2c1e5be70abd78d07571ea121afbade573bc1d
snchen <snchen@nvidia.com>
2020-09-10 12:32:36 +0800

    drivers: media: sensor_dt_compliance cause reboot
    Remove the entry from the list if sensor doesn't connect.
    
    bug 3066654
    
    Change-Id: Ib3148215e72a516f16e9af1e06784fdefca20e41
    Signed-off-by: snchen <snchen@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2411330
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Ian Kaszubski <ikaszubski@nvidia.com>
    Reviewed-by: Frank Chen <frankc@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/media/i2c/imx185.c
+++ drivers/media/i2c/imx185.c
@@ -802,6 +802,7 @@ static int imx185_probe(struct i2c_client *client,
 
 	err = imx185_board_setup(priv);
 	if (err) {
+		tegracam_device_unregister(tc_dev);
 		dev_err(dev, "board setup failed\n");
 		return err;
 	}

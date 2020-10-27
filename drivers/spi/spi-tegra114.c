63e47566757c0af1342ca98e7450db98b955529d
Matt Madison <matt@madison.systems>
2020-10-27 06:39:08 -0700

    drivers: spi-tegra: set clocks during probe
    With the switch to using pm_runtime autosuspend,
    a runtime resume occurs before the clock source has been set up,
    causing a failure in the clk_prepare_enable call in
    tegra_spi_runtime_resume().
    
    Fix that by calling set_best_clk_source() in the probe
    function before enabling runtime power management.
    And since set_best_clk_source() needs the device
    tree node of the SPI master, move the setting of
    master->dev.of_node to before that call.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- drivers/spi/spi-tegra114.c
+++ drivers/spi/spi-tegra114.c
@@ -1920,6 +1920,7 @@ static int tegra_spi_probe(struct platform_device *pdev)
 		master->bus_num = bus_num;
 	master->auto_runtime_pm = true;
 	master->spi_cs_low  = tegra_spi_cs_low;
+	master->dev.of_node = pdev->dev.of_node;
 
 	tspi->master = master;
 	tspi->dev = &pdev->dev;
@@ -1967,6 +1968,8 @@ static int tegra_spi_probe(struct platform_device *pdev)
 
 	tspi->max_buf_size = SPI_FIFO_DEPTH << 2;
 	tspi->min_div = 0;
+
+	set_best_clk_source(tspi, master->max_speed_hz);
 
 	ret = tegra_spi_init_dma_param(tspi, true);
 	if (ret < 0)
@@ -2029,7 +2032,6 @@ static int tegra_spi_probe(struct platform_device *pdev)
 		goto exit_pm_disable;
 	}
 
-	master->dev.of_node = pdev->dev.of_node;
 	ret = devm_spi_register_master(&pdev->dev, master);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "can not register to master err %d\n", ret);

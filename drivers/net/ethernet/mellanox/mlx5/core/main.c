bc33885ffa5ecc4849d19d9018cac689c902ac14
Preetham Chandru Ramchandra <pchandru@nvidia.com>
2021-06-10 11:01:33 +0530

    mlx5: core: fix kernel crash
    deregister from thermal zone during shutdown.
    
    Bug 200660504
    
    Change-Id: Id2f5b6d63f5fae3d7bad0a317b225bd5063e9f84
    Signed-off-by: Preetham Chandru Ramchandra <pchandru@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-4.9/+/2542341
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: Rajkumar Kasirajan <rkasirajan@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1419,6 +1419,7 @@ static void shutdown(struct pci_dev *pdev)
 	struct mlx5_priv *priv = &dev->priv;
 
 	dev_info(&pdev->dev, "Shutdown was called\n");
+	mlx5_thermal_deinit(dev);
 	/* Notify mlx5 clients that the kernel is being shut down */
 	set_bit(MLX5_INTERFACE_STATE_SHUTDOWN, &dev->intf_state);
 	mlx5_unload_one(dev, priv, false);

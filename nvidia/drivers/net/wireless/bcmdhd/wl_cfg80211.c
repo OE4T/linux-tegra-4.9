1567cec44fca20d3ffd0d7682511ed0fb9bda592
Ilies CHERGUI <ilies.chergui@gmail.com>
2022-02-18 00:11:52 +0000

    net: wireless: bcmdhd: match wl_cfg80211_reg_notifier prototype/definition
    Signed-off-by: Ilies CHERGUI <ilies.chergui@gmail.com>

--- nvidia/drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ nvidia/drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -573,7 +573,11 @@ wl_cfg80211_add_iw_ie(struct bcm_cfg80211 *cfg, struct net_device *ndev, s32 bss
 static s32 wl_setup_wiphy(struct wireless_dev *wdev, struct device *dev, void *data);
 static void wl_free_wdev(struct bcm_cfg80211 *cfg);
 #ifdef CONFIG_CFG80211_INTERNAL_REGDB
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0))
 static int
+#else
+static void
+#endif /* kernel version < 3.10.0 */
 wl_cfg80211_reg_notifier(struct wiphy *wiphy, struct regulatory_request *request);
 #endif /* CONFIG_CFG80211_INTERNAL_REGDB */
 
@@ -8619,7 +8623,11 @@ s32 wl_mode_to_nl80211_iftype(s32 mode)
 }
 
 #ifdef CONFIG_CFG80211_INTERNAL_REGDB
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0))
 static int
+#else
+static void
+#endif /* kernel version < 3.10.0 */
 wl_cfg80211_reg_notifier(
 	struct wiphy *wiphy,
 	struct regulatory_request *request)
@@ -8629,7 +8637,11 @@ wl_cfg80211_reg_notifier(
 
 	if (!request || !cfg) {
 		WL_ERR(("Invalid arg\n"));
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0))
 		return -EINVAL;
+#else
+		return;
+#endif /* kernel version < 3.10.0 */
 	}
 
 	WL_DBG(("ccode: %c%c Initiator: %d\n",
@@ -8654,7 +8666,11 @@ wl_cfg80211_reg_notifier(
 		WL_ERR(("set country Failed :%d\n", ret));
 	}
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0))
 	return ret;
+#else
+	return;
+#endif /* kernel version < 3.10.0 */
 }
 #endif /* CONFIG_CFG80211_INTERNAL_REGDB */
 

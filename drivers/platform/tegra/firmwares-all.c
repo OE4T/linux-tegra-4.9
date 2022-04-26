c70230b75ce06c99203d99113afa488a7255541e
dmitry pervushin <dpervushin@nvidia.com>
2022-04-26 18:28:09 +0200

    firmwares: register mts info reader when cpu boots
    If nvidia CPU is not started at boot, reader won't be
    able to return anything.
    So register notification for cpu up/down and read AIDR
    (MTS version) when cpu is up
    
    Bug 3620579
    
    Change-Id: I1e6b99eab8847b2ce956209304d030db2947bb94
    Signed-off-by: dmitry pervushin <dpervushin@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2703133
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Phoenix Jung <pjung@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/platform/tegra/firmwares-all.c
+++ drivers/platform/tegra/firmwares-all.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2016-2022, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,36 +18,47 @@
 #include <linux/cpu.h>
 #include <asm/cpu.h>
 
-static ssize_t tegrafw_read_denver(struct device *dev,
-				char *version, size_t size)
+static enum cpuhp_state tegra_firmwares_cpu_state;
+static DEFINE_PER_CPU(struct device *, fwdev);
+
+static int tegra_cpu_online(unsigned int cpu)
 {
-	char *v = version;
-	int i;
-	size_t printed = 0;
+	char s_cpu[10];
+	char s_aidr[64];
+	struct cpuinfo_arm64 *cpuinfo = &per_cpu(cpu_data, cpu);
 
-	for_each_online_cpu(i) {
-		struct cpuinfo_arm64 *cpuinfo = &per_cpu(cpu_data, i);
-		u32 midr = cpuinfo->reg_midr;
-
-		if (MIDR_IMPLEMENTOR(midr) == ARM_CPU_IMP_NVIDIA) {
-			printed = snprintf(v, size, "CPU%d: %u(0x%x) ",
-				i, cpuinfo->reg_aidr, cpuinfo->reg_aidr);
-			size -= printed;
-			v += printed;
-		}
-	}
-
-	return v - version;
+	if (MIDR_IMPLEMENTOR(cpuinfo->reg_midr) != ARM_CPU_IMP_NVIDIA)
+		return 0;
+	snprintf(s_cpu, sizeof(s_cpu), "CPU%d", cpu);
+	snprintf(s_aidr, sizeof(s_aidr), "%u (0%x)",
+		cpuinfo->reg_aidr,
+		cpuinfo->reg_aidr);
+	per_cpu(fwdev, cpu) = tegrafw_register_string(s_cpu, s_aidr);
+	return 0;
 }
 
-#define FIRMWARES_SIZE  10
-static struct device *firmwares[FIRMWARES_SIZE];
-#define check_out_of_bounds(dev, err) \
-	if ((dev) - firmwares >= ARRAY_SIZE(firmwares)) { \
-		pr_err("Cannot register 'legacy' firmware info: increase " \
-			"firmwares array size"); \
-		return (err); \
-	}
+static int tegra_cpu_prepare_down(unsigned int cpu)
+{
+	if (per_cpu(fwdev, cpu))
+		tegrafw_unregister(per_cpu(fwdev, cpu));
+	per_cpu(fwdev, cpu) = NULL;
+	return 0;
+}
+
+static enum cpuhp_state tegra_cpu_fw_register(void)
+{
+	return cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,
+		"cpu/tegra-firmwares:online",
+		tegra_cpu_online,
+		tegra_cpu_prepare_down);
+}
+
+static void tegra_cpu_fw_unregister(enum cpuhp_state state)
+{
+	cpuhp_remove_state(state);
+}
+
+static struct device *firmwares[10];
 
 static int __init tegra_firmwares_init(void)
 {
@@ -55,14 +66,16 @@ static int __init tegra_firmwares_init(void)
 	char *versions[] = { "mb1", "mb2", "mb1-bct", "qb", "osl" };
 	int v;
 
-	check_out_of_bounds(dev, 0);
-	*dev++ = tegrafw_register("MTS", TFW_NORMAL, tegrafw_read_denver, NULL);
-
 	for (v = 0; v < ARRAY_SIZE(versions); v++) {
-		check_out_of_bounds(dev, 0);
+		if (dev - firmwares >= ARRAY_SIZE(firmwares)) {
+			pr_err("Increase firmwares array size");
+			return 0;
+		}
 		*dev++ = tegrafw_register_dt_string(versions[v],
 			"/tegra-firmwares", versions[v]);
 	}
+
+	tegra_firmwares_cpu_state = tegra_cpu_fw_register();
 	return 0;
 }
 
@@ -70,6 +83,7 @@ static void __exit tegra_firmwares_exit(void)
 {
 	struct device **dev = firmwares;
 
+	tegra_cpu_fw_unregister(tegra_firmwares_cpu_state);
 	while (dev - firmwares < ARRAY_SIZE(firmwares))
 		tegrafw_unregister(*dev++);
 }

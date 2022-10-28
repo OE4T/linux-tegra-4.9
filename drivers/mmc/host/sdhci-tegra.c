4bc9fea074f62b75fd632c35bce7ad292ba04796
Chenjian <jackchen@nvidia.com>
2022-10-28 16:29:11 +0800

    mmc: host: set pad calib true after resuming
    Suspend sequence resets the host controller settings.
    Hence auto-calibration is needed to be run in the resume
    path. Enable auto-calibration during resuming.
    
    Bug 3752857
    
    Change-Id: I8900e43e96ad3f0cc18261c057283335a64857b6
    Signed-off-by: Chenjian <jackchen@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-4.9/+/2799433
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Aniruddha Tvs Rao <anrao@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 Google, Inc.
- * Copyright (c) 2012-2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2022, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -2474,6 +2474,8 @@ static int tegra_sdhci_resume(struct sdhci_host *host)
 
 	if (host->mmc->rem_card_present == false)
 		tegra_host->tuning_status = TUNING_STATUS_RETUNE;
+
+	tegra_host->pad_calib_required = true;
 
 	/* Set min identificaion clock of 400 KHz */
 	tegra_sdhci_set_clock(host, 400000);
